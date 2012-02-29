33b5c8ce394045a04c13c363a371638b6f00a411
termac <terror.macbeth.I@gmail.com>
2012-02-29 21:12:48 +0100

    alter regular expression in ownedtv plugin to again find the channel id

--- src/livestreamer/plugins/ownedtv.py
+++ src/livestreamer/plugins/ownedtv.py
@@ -37,7 +37,7 @@ class OwnedTV(Plugin):
         data = fd.read()
         fd.close()
 
-        match = re.search(b"own3d.tv\/livestreamfb\/(\d+)", data)
+        match = re.search(b"document.location.hash='/live/(\d+)'", data)
         if match:
             return int(match.group(1))
 
