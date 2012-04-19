9663e667296278153ddb43c182d17be6eccc3e92
Christopher Rosell <chrippa@tanuki.se>
2012-04-19 21:45:06 +0200

    Only require argparse on Python <2.7 and <3.2.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+from sys import version_info
 
 version = "0.1"
+deps = []
+
+# require argparse on Python <2.7 and <3.2
+if (version_info[0] == 2 and version_info[1] < 7) or \
+   (version_info[0] == 3 and version_info[1] < 2):
+    deps.append("argparse")
 
 setup(name="livestreamer",
       version=version,
@@ -15,7 +22,5 @@ setup(name="livestreamer",
       entry_points={
           "console_scripts": ['livestreamer=livestreamer.cli:main']
       },
-      install_requires=[
-          'argparse',
-          ],
+      install_requires=deps
 )
