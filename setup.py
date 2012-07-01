6db1dc81c2bf5efab4329c17a09f9ee1985cdc5e
Christopher Rosell <chrippa@tanuki.se>
2012-07-01 02:48:52 +0200

    Release version 1.1.0.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 from setuptools import setup, find_packages
 from sys import version_info
 
-version = "1.0.0"
+version = "1.1.0"
 deps = ["pbs"]
 
 # require argparse on Python <2.7 and <3.2
@@ -13,7 +13,8 @@ if (version_info[0] == 2 and version_info[1] < 7) or \
 
 setup(name="livestreamer",
       version=version,
-      description="Util to play various livestreaming services in custom player",
+      description="CLI program that launches streams from various streaming services in a custom video player",
+      url="https://github.com/chrippa/livestreamer",
       author="Christopher Rosell",
       author_email="chrippa@tanuki.se",
       license="BSD",
@@ -22,5 +23,12 @@ setup(name="livestreamer",
       entry_points={
           "console_scripts": ['livestreamer=livestreamer.cli:main']
       },
-      install_requires=deps
+      install_requires=deps,
+      classifiers=["Operating System :: POSIX",
+                   "Operating System :: Microsoft :: Windows",
+                   "Environment :: Console",
+                   "Development Status :: 5 - Production/Stable",
+                   "Topic :: Internet :: WWW/HTTP",
+                   "Topic :: Multimedia :: Sound/Audio",
+                   "Topic :: Utilities"]
 )
