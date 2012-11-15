0a31cabb3aa488d328781b9aab7029eede92781a
Christopher Rosell <chrippa@tanuki.se>
2012-11-15 23:31:16 +0100

    plugins.dailymotion: Py3 fixes and a few tweaks.

--- src/livestreamer/plugins/dailymotion.py
+++ src/livestreamer/plugins/dailymotion.py
@@ -5,16 +5,14 @@ from livestreamer.utils import urlget, verifyjson
 
 import re
 import json
-import requests
 
 class DailyMotion(Plugin):
-
     QualityMap = {
-            'ld'    : '240p',
-            'sd'    : '360p',
-            'hq'    : '480p',
-            'hd720' : '720p',
-            'hd1080': '1080p'
+        "ld": "240p",
+        "sd": "360p",
+        "hq": "480p",
+        "hd720": "720p",
+        "hd1080": "1080p"
     }
 
     StreamInfoURL = "http://www.dailymotion.com/sequence/full/{0}"
@@ -23,48 +21,39 @@ class DailyMotion(Plugin):
     @classmethod
     def can_handle_url(self, url):
         # valid urls are of the form dailymotion.com/video/[a-z]{5}.*
-        # but we make 'video/' optionnal and allow for dai.ly as shortcut
+        # but we make "video/" optional and allow for dai.ly as shortcut
         return ("dailymotion.com" in url) or ("dai.ly" in url)
 
-    def _check_channel_live(self, id):
-        url = self.MetadataURL.format(self.channelname)
+    def _check_channel_live(self, channelname):
+        url = self.MetadataURL.format(channelname)
         res = urlget(url, params=dict(fields="mode"))
+
         if len(res.json) == 0:
             raise PluginError("Error retrieving stream live status")
-        return res.json['mode'] == 'live'
+
+        mode = verifyjson(res.json, "mode")
+
+        return mode == "live"
 
     def _get_channel_name(self, url):
         rpart = url.rstrip("/").rpartition("/")[2].lower()
-        name = re.sub('_.*', '', rpart)
+        name = re.sub("_.*", "", rpart)
+
         return name
 
     def _get_node_by_name(self, parent, name):
         res = None
         for node in parent:
-            if node['name'] == name:
+            if node["name"] == name:
                 res = node
                 break
+
         return res
 
-
-    def _get_rtmp_streams(self):
-
-        url = self.MetadataURL.format(self.channelname)
-
+    def _get_rtmp_streams(self, channelname):
         self.logger.debug("Fetching stream info")
-        res = urlget(url)
 
-        if not isinstance(res.json, dict):
-            raise PluginError("Stream info response is not JSON")
-
-        chan_id = verifyjson(res.json, 'id')
-
-        streams = {}
-
-        if not self._check_channel_live(chan_id):
-            return streams
-
-        url = self.StreamInfoURL.format(self.channelname)
+        url = self.StreamInfoURL.format(channelname)
 
         self.logger.debug("JSON data url: {0}", url)
 
@@ -79,83 +68,79 @@ class DailyMotion(Plugin):
         chan_info_json = res.json
 
         # This is ugly, not sure how to fix it.
-        back_json_node = chan_info_json['sequence'][0]['layerList'][0]
-        if back_json_node['name'] != 'background':
+        back_json_node = chan_info_json["sequence"][0]["layerList"][0]
+        if back_json_node["name"] != "background":
             raise PluginError("JSON data has unexpected structure")
 
-        rep_node = self._get_node_by_name(back_json_node['sequenceList'], 'reporting')['layerList']
-        main_node = self._get_node_by_name(back_json_node['sequenceList'], 'main')['layerList']
+        rep_node = self._get_node_by_name(back_json_node["sequenceList"], "reporting")["layerList"]
+        main_node = self._get_node_by_name(back_json_node["sequenceList"], "main")["layerList"]
 
-        if not(rep_node and main_node):
+        if not (rep_node and main_node):
             raise PluginError("Error parsing stream RTMP url")
 
-        swfurl = self._get_node_by_name(rep_node, 'reporting')['param']['extraParams']['videoSwfURL']
-        feeds_params = self._get_node_by_name(main_node, 'video')['param']
+        swfurl = self._get_node_by_name(rep_node, "reporting")["param"]["extraParams"]["videoSwfURL"]
+        feeds_params = self._get_node_by_name(main_node, "video")["param"]
 
-        if not(swfurl and feeds_params):
+        if not (swfurl and feeds_params):
             raise PluginError("Error parsing stream RTMP url")
 
+        streams = {}
 
-        # Different feed qualities are available are a dict under 'live'
+        # Different feed qualities are available are a dict under "live"
         # In some cases where there's only 1 quality available,
-        # it seems the 'live' is absent. We use the single stream available
-        # under the 'customURL' key.
+        # it seems the "live" is absent. We use the single stream available
+        # under the "customURL" key.
 
-        if feeds_params.has_key('live') and len(feeds_params['live']) > 0:
-            quals = feeds_params['live']
+        if "live" in feeds_params and len(feeds_params["live"]) > 0:
+            quals = feeds_params["live"]
+
+            for key, quality in quals.items():
+                info = {}
+
+                try:
+                    res = urlget(quality, exception=IOError)
+                except IOError:
+                    continue
+
+                rtmpurl = res.text
+                stream = RTMPStream(self.session, {
+                    "rtmp": rtmpurl,
+                    "swfVfy": swfurl,
+                    "live": True
+                })
+                self.logger.debug("Adding URL: {0}", rtmpurl)
+
+                if key in self.QualityMap:
+                    sname = self.QualityMap[key]
+                else:
+                    sname = key
+
+                streams[sname] = stream
         else:
-            res = urlget(feeds_params['customURL'])
-            rtmpurl = res.text
+            res = urlget(feeds_params["customURL"])
 
+            rtmpurl = res.text
             stream = RTMPStream(self.session, {
                 "rtmp": rtmpurl,
                 "swfVfy": swfurl,
                 "live": True
             })
-            self.logger.debug('Adding URL: '+feeds_params['customURL'])
-            streams['live'] = stream
-            return streams
 
-        for (k,q) in quals.iteritems():
-            info = {}
-
-            try:
-                res = urlget(q, exception=IOError)
-            except IOError:
-                continue
-            rtmpurl = res.text
-
-            stream = RTMPStream(self.session, {
-                "rtmp": rtmpurl,
-                "swfVfy": swfurl,
-                "live": True
-            })
-            self.logger.debug('Adding URL: '+rtmpurl)
-
-            if k in self.QualityMap:
-                sname = self.QualityMap[k]
-            else:
-                sname = k
-
-            streams[sname] = stream
+            self.logger.debug("Adding URL: {0}", feeds_params["customURL"])
+            streams["live"] = stream
 
         return streams
 
     def _get_streams(self):
-        self.channelname = self._get_channel_name(self.url)
+        channelname = self._get_channel_name(self.url)
 
-        if not self.channelname:
+        if not channelname:
             raise NoStreamsError(self.url)
 
-        streams = {}
+        if not self._check_channel_live(channelname):
+            raise NoStreamsError(self.url)
 
-        try:
-            rtmpstreams = self._get_rtmp_streams()
-            streams.update(rtmpstreams)
-        except PluginError as err:
-            self.logger.error("Error when fetching RTMP stream info: {0}", str(err))
-
-        return streams
+        return self._get_rtmp_streams(channelname)
 
 
 __plugin__ = DailyMotion
