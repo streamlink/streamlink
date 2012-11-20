3b6f5073fab2965a2e1945ad6eafd2f04aa771e9
Christopher Rosell <chrippa@tanuki.se>
2012-11-20 02:20:25 +0100

    Add plugin for the old livestream.com site.
    Only HLS streams, as getting the RTMP streams would involve
    talking RTMP to request stream info.

---
+++ src/livestreamer/plugins/oldlivestream.py
@@ -0,0 +1,24 @@
+from livestreamer.stream import HLSStream
+from livestreamer.plugins import Plugin, PluginError, NoStreamsError
+from livestreamer.utils import urlget
+
+class OldLivestream(Plugin):
+    PlaylistURL = "http://x{0}x.api.channel.livestream.com/3.0/playlist.m3u8"
+
+    @classmethod
+    def can_handle_url(self, url):
+        return "livestream.com" in url and not "new.livestream.com" in url
+
+    def _get_streams(self):
+        channelname = self.url.rstrip("/").rpartition("/")[2].lower()
+
+        try:
+            streams = HLSStream.parse_variant_playlist(self.session,
+                                                       self.PlaylistURL.format(channelname))
+        except IOError:
+            raise NoStreamsError(self.url)
+
+        return streams
+
+
+__plugin__ = OldLivestream
