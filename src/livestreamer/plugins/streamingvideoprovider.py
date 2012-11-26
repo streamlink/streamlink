53a4d165dda182328c2ea0fe13ce7524d2a3dc24
Christopher Rosell <chrippa@tanuki.se>
2012-11-26 02:50:43 +0100

    plugins.streamingvideoprovider: A few tweaks.

--- src/livestreamer/plugins/streamingvideoprovider.py
+++ src/livestreamer/plugins/streamingvideoprovider.py
@@ -6,7 +6,7 @@ from time import time
 import re
 
 class Streamingvideoprovider(Plugin):
-    HLSStreamURL = "http://player.webvideocore.net/index.php?l=info&a=ajax_video_info&file={0}&rid={1}"
+    HLSStreamURL = "http://player.webvideocore.net/index.php"
 
     @classmethod
     def can_handle_url(self, url):
@@ -14,13 +14,23 @@ class Streamingvideoprovider(Plugin):
 
     def _get_streams(self):
         channelname = self.url.rstrip("/").rpartition("/")[2].lower()
-        unixtime = str(int(time()))
+        streams = {}
+
         try:
-            res = urlget(self.HLSStreamURL.format(channelname, unixtime))
-            PlaylistURL = re.search("'(http://.+\.m3u8)'", res.text).group(1)
-            self.logger.info("PlaylistURL is {0}".format(PlaylistURL))
-            streams = {}
-            streams['live'] = HLSStream(self.session, PlaylistURL)
+            options = dict(l="info", a="ajax_video_info", file=channelname,
+                           rid=time())
+
+            res = urlget(self.HLSStreamURL, params=options, exception=IOError)
+            match = re.search("'(http://.+\.m3u8)'", res.text)
+
+            if not match:
+                raise PluginError("Failed to find HLS playlist in result")
+
+            playlisturl = match.group(1)
+
+            self.logger.debug("Playlist URL is {0}", playlisturl)
+
+            streams["live"] = HLSStream(self.session, playlisturl)
         except IOError:
             raise NoStreamsError(self.url)
 
