82e5ebe2e738ee2c2909152e9908f3e0c0861460
Christopher Rosell <chrippa@tanuki.se>
2012-11-27 03:59:18 +0100

    Add generic stream forwarding plugin.
    This plugin add support for akamaihd://, hls://,
    hlsvariant:// and rtmp*:// URLs to be forwarded to their
    respective *Stream classes.
    
    A argument list, with each argument separated by a space,
    can be added after the URL. These formats are possible:
    
    key=1
    key=1.0
    key=value
    key='value'
    key="value"

---
+++ src/livestreamer/plugins/stream.py
@@ -0,0 +1,79 @@
+from livestreamer.compat import urlparse
+from livestreamer.plugins import Plugin, PluginError, NoStreamsError
+from livestreamer.stream import *
+
+import re
+
+class StreamURL(Plugin):
+    ProtocolMap = {
+        "akamaihd": AkamaiHDStream,
+        "hls": HLSStream,
+        "hlsvariant": HLSStream.parse_variant_playlist,
+        "rtmp": RTMPStream,
+        "rtmpe": RTMPStream,
+        "rtmps": RTMPStream,
+        "rtmpt": RTMPStream,
+        "rtmpte": RTMPStream
+    }
+
+    @classmethod
+    def can_handle_url(self, url):
+        parsed = urlparse(url)
+
+        return parsed.scheme in self.ProtocolMap
+
+    def _parse_params(self, params):
+        rval = {}
+
+        matches = re.findall("(\w+)=(\d+|\d+.\d+|'(.+)'|\"(.+)\"|.+)", params)
+
+        for key, val, strval, ex in matches:
+            if len(strval) > 0:
+                rval[key] = strval
+            else:
+                try:
+                    val = float(val)
+                except ValueError:
+                    pass
+
+                try:
+                    val = int(val)
+                except ValueError:
+                    pass
+
+                rval[key] = val
+
+        return rval
+
+    def _get_streams(self):
+        parsed = urlparse(self.url)
+
+        if not parsed.scheme in self.ProtocolMap:
+            raise NoStreamsError(self.url)
+
+        cls = self.ProtocolMap[parsed.scheme]
+        split = self.url.split(" ")
+
+        url = split[0]
+        urlnoproto = re.match("^\w+://(.+)", url).group(1)
+
+        params = (" ").join(split[1:])
+        params = self._parse_params(params)
+
+        if cls == RTMPStream:
+            params["rtmp"] = url
+
+            for boolkey in ("live", "realtime", "quiet", "verbose", "debug"):
+                if boolkey in params:
+                    params[boolkey] = bool(params[boolkey])
+
+            stream = cls(self.session, params)
+        elif cls == HLSStream.parse_variant_playlist:
+            return cls(self.session, urlnoproto, **params)
+        else:
+            stream = cls(self.session, urlnoproto, **params)
+
+        return dict(live=stream)
+
+
+__plugin__ = StreamURL
---
+++ tests/test_plugin_stream.py
@@ -0,0 +1,65 @@
+import os
+import unittest
+
+from livestreamer import Livestreamer, PluginError, NoPluginError
+from livestreamer.plugins import Plugin
+from livestreamer.stream import *
+
+class TestPluginStream(unittest.TestCase):
+    def setUp(self):
+        self.session = Livestreamer()
+
+    def assertDictHas(self, a, b):
+        for key, value in a.items():
+            self.assertEqual(b[key], value)
+
+    def _test_akamaihd(self, surl, host, streamname):
+        channel = self.session.resolve_url(surl)
+        streams = channel.get_streams()
+
+        self.assertTrue("live" in streams)
+
+        stream = streams["live"]
+        self.assertTrue(isinstance(stream, AkamaiHDStream))
+        self.assertEqual(stream.host, host)
+        self.assertEqual(stream.streamname, streamname)
+
+    def _test_hls(self, surl, url):
+        channel = self.session.resolve_url(surl)
+        streams = channel.get_streams()
+
+        self.assertTrue("live" in streams)
+
+        stream = streams["live"]
+        self.assertTrue(isinstance(stream, HLSStream))
+        self.assertEqual(stream.url, url)
+
+    def _test_rtmp(self, surl, url, params):
+        channel = self.session.resolve_url(surl)
+        streams = channel.get_streams()
+
+        self.assertTrue("live" in streams)
+
+        stream = streams["live"]
+        self.assertTrue(isinstance(stream, RTMPStream))
+        self.assertEqual(stream.params["rtmp"], url)
+        self.assertDictHas(params, stream.params)
+
+    def test_plugin(self):
+        self._test_rtmp("rtmp://hostname.se/stream",
+                         "rtmp://hostname.se/stream", dict())
+
+        self._test_rtmp("rtmp://hostname.se/stream live=1 num=47",
+                        "rtmp://hostname.se/stream", dict(live=True, num=47))
+
+        self._test_rtmp("rtmp://hostname.se/stream live=1 qarg='a \'string' noq=test",
+                        "rtmp://hostname.se/stream", dict(live=True, qarg="a \'string", noq="test"))
+
+        self._test_hls("hls://http://hostname.se/playlist.m3u8",
+                       "http://hostname.se/playlist.m3u8")
+
+        self._test_akamaihd("akamaihd://http://hostname.se/stream",
+                            "http://hostname.se", "stream")
+
+if __name__ == "__main__":
+    unittest.main()
