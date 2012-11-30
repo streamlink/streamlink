1431f68af09754fb9ddcdf954a586e1f6f0ec5ab
Christopher Rosell <chrippa@tanuki.se>
2012-11-30 23:30:21 +0100

    stream.RTMPStream: Add redirect support.

--- src/livestreamer/stream/rtmpdump.py
+++ src/livestreamer/stream/rtmpdump.py
@@ -1,13 +1,17 @@
 from . import StreamProcess, StreamError
-from ..compat import str, sh
+from ..compat import str, sh, pbs_compat
+
+from time import sleep
 
 import re
 
 class RTMPStream(StreamProcess):
-    def __init__(self, session, params):
-        StreamProcess.__init__(self, session, params)
+    def __init__(self, session, params, redirect=False, timeout=30):
+        StreamProcess.__init__(self, session, params, timeout)
 
         self.cmd = self.session.options.get("rtmpdump")
+        self.redirect = redirect
+        self.logger = session.logger.new_module("stream.rtmp")
         self.params["flv"] = "-"
 
         if self.session.options.get("rtmpdump-proxy"):
@@ -20,7 +24,65 @@ class RTMPStream(StreamProcess):
         if "socks" in self.params and not self._supports_param("socks"):
             raise StreamError("Installed rtmpdump does not support --socks argument")
 
+        if self.redirect:
+            self._check_redirect()
+
         return StreamProcess.open(self)
+
+    def _check_redirect(self, timeout=5):
+        cmd = self._check_cmd()
+
+        params = self.params.copy()
+        params["verbose"] = True
+        params["_bg"] = True
+
+        self.logger.debug("Attempting to find tcURL redirect")
+
+        stream = cmd(**params)
+
+        elapsed = 0
+        process_alive = True
+
+        while elapsed < timeout and process_alive:
+            if pbs_compat:
+                process_alive = stream.process.returncode is None
+            else:
+                process_alive = stream.process.alive
+
+            sleep(0.25)
+            elapsed += 0.25
+
+        if process_alive:
+            try:
+                stream.process.kill()
+            except:
+                pass
+
+        try:
+            stderr = stream.stderr
+        except sh.ErrorReturnCode as err:
+            self._update_redirect(err.stderr)
+
+    def _update_redirect(self, stderr):
+        tcurl, redirect = None, None
+        stderr = str(stderr, "utf8")
+
+        m = re.search("DEBUG: tcUrl\s+: (.+)\n", stderr)
+        if m:
+            tcurl = m.group(1)
+
+        m = re.search("DEBUG: Property: <Name:\s+redirect,\s+STRING:\s+(\w+://.+?)>", stderr)
+        if m:
+            redirect = m.group(1)
+
+        if tcurl and redirect:
+            self.logger.debug("Found redirect tcUrl: {0}", redirect)
+
+            if "rtmp" in self.params:
+                self.params["rtmp"] = self.params["rtmp"].replace(tcurl, redirect)
+
+            if "tcUrl" in self.params:
+                self.params["tcUrl"] = redirect
 
     def _supports_param(self, param):
         cmd = self._check_cmd()
