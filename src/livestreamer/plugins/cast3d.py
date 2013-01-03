23ba56cd88147a9c1a70a7e8a8277d9a7315b797
Athanasios Oikonomou <athoik@gmail.com>
2013-01-03 09:36:11 +0200

    Add Plugin for cast3d.tv

---
+++ src/livestreamer/plugins/cast3d.py
@@ -0,0 +1,59 @@
+from livestreamer.compat import urlparse
+from livestreamer.stream import RTMPStream, HTTPStream
+from livestreamer.plugins import Plugin, PluginError, NoStreamsError
+from livestreamer.utils import urlget
+
+import re
+
+class Cast3d(Plugin):
+    SWFURL = "http://www.cast3d.biz/player.swf"
+    PlayerURL = "http://www.cast3d.tv/embed.php"
+
+    @classmethod
+    def can_handle_url(self, url):
+        return "cast3d.tv" in url
+
+    def _get_streams(self):
+        channelname = urlparse(self.url).path.rstrip("/").rpartition("/")[-1].lower()
+
+        self.logger.debug("Fetching stream info")
+
+        headers = {
+            "Referer": self.url,
+            "Accept-Encoding": "deflate"
+        }
+
+        options = dict(channel=channelname, vw="580", vh="390",
+                       domain="www.cast3d.tv")
+
+        res = urlget(self.PlayerURL, headers=headers, params=options)
+
+        match = re.search(".+?'streamer':'(.+?)'", res.text)
+        if not match:
+            raise NoStreamsError(self.url)
+
+        streams = {}
+        url = urlparse(match.group(1))
+        if url.scheme.startswith("rtmp"):
+            redirect = False
+            rtmp = match.group(1)
+            if "redirect" in rtmp:
+                redirect = True
+
+            streams["live"] = RTMPStream(self.session, {
+                "rtmp": rtmp,
+                "pageUrl": self.url,
+                "swfVfy": self.SWFURL,
+                "playpath": channelname,
+                "live": True
+            }, redirect=redirect)
+
+        elif url.scheme.startswith("http"):
+            streams["live"] = HTTPStream(self.session, match.group(1))
+        else:
+            raise PluginError(("Invalid stream type: {0}").format(url.scheme))
+
+        return streams
+
+
+__plugin__ = Cast3d
