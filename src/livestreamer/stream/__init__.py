640fdd6c3125850985876cd58206f7457c4ccf54
Christopher Rosell <chrippa@tanuki.se>
2013-01-25 19:58:16 +0100

    stream: Add StreamIOThreadWrapper class.
    This class makes it possible to gain control
    over read-timeout on a file-object that usually
    blocks forever.

--- src/livestreamer/stream/__init__.py
+++ src/livestreamer/stream/__init__.py
@@ -1,5 +1,8 @@
 from ..compat import bytes, str
 from ..packages import pbs as sh
+from ..utils import RingBuffer
+
+from threading import Thread
 
 import io
 import json
@@ -58,6 +61,70 @@ class StreamIOWrapper(io.IOBase):
         if hasattr(self.fd, "close"):
             self.fd.close()
 
+class StreamIOThreadWrapper(io.IOBase):
+    """
+        Wraps a file-like object in a thread.
+
+        Useful for getting control over read timeout where
+        timeout handling is missing or out of our control.
+    """
+    class Filler(Thread):
+        def __init__(self, fd, buffer):
+            Thread.__init__(self)
+
+            self.error = None
+            self.fd = fd
+            self.buffer = buffer
+            self.daemon = True
+            self.running = False
+
+        def run(self):
+            self.running = True
+
+            while self.running:
+                try:
+                    data = self.fd.read(8192)
+                except IOError as error:
+                    self.error = error
+                    break
+
+                if len(data) == 0:
+                    break
+
+                self.buffer.write(data)
+
+            self.stop()
+
+        def stop(self):
+            self.running = False
+            self.buffer.close()
+
+            if hasattr(self.fd, "close"):
+                try:
+                    self.fd.close()
+                except Exception:
+                    pass
+
+    def __init__(self, session, fd, timeout=30):
+        self.buffer = RingBuffer(session.get_option("ringbuffer-size"))
+        self.fd = fd
+        self.timeout = timeout
+
+        self.filler = StreamIOThreadWrapper.Filler(self.fd, self.buffer)
+        self.filler.start()
+
+    def read(self, size=-1):
+        if self.filler.error and self.buffer.length == 0:
+            raise self.filler.error
+
+        return self.buffer.read(size, block=self.filler.is_alive(),
+                                timeout=self.timeout)
+
+    def close(self):
+        self.filler.stop()
+
+        if self.filler.is_alive():
+            self.filler.join()
 
 class StreamProcess(Stream):
     def __init__(self, session, params={}, timeout=30):
@@ -92,7 +159,8 @@ class StreamProcess(Stream):
             else:
                 raise StreamError("Error while executing subprocess")
 
-        return stream.process.stdout
+        return StreamIOThreadWrapper(self.session, stream.process.stdout,
+                                     timeout=self.timeout)
 
     def _check_cmd(self):
         try:
