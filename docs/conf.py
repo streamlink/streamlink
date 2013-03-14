62b0a1c33c785a8fdf79bf8bd27fb7b185834b49
Christopher Rosell <chrippa@tanuki.se>
2013-03-14 03:07:38 +0100

    docs: Remove unused Sphinx extension.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ from livestreamer import __version__ as livestreamer_version
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.programoutput']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
