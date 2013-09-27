8284578a0a85b11f54f0d7d066f1988b8cfb783c
Christopher Rosell <chrippa@tanuki.se>
2013-09-28 00:37:27 +0200

    stream: Add a FLV concat stream that joins FLV segments.

--- src/livestreamer/stream/__init__.py
+++ src/livestreamer/stream/__init__.py
@@ -8,3 +8,6 @@ from .http import HTTPStream
 from .rtmpdump import RTMPStream
 from .streamprocess import StreamProcess
 from .wrappers import StreamIOWrapper, StreamIOThreadWrapper
+
+from .flvconcat import extract_flv_header_tags
+from .playlist import Playlist, FLVPlaylist
