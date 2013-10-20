e2371f546b2736003182cbcd737170ff5a3db892
Christopher Rosell <chrippa@tanuki.se>
2013-10-20 16:13:50 +0200

    plugins.livestream: Add support for HLS streams.

--- src/livestreamer/plugins/livestream.py
+++ src/livestreamer/plugins/livestream.py
@@ -1,14 +1,20 @@
+import re
+
+from collections import defaultdict
+
 from livestreamer.compat import urljoin
 from livestreamer.exceptions import PluginError, NoStreamsError
 from livestreamer.plugin import Plugin
-from livestreamer.stream import AkamaiHDStream
+from livestreamer.stream import AkamaiHDStream, HLSStream
 from livestreamer.utils import urlget, verifyjson, res_xml, parse_json
 
-import re
 
 SWF_URL = "http://cdn.livestream.com/swf/hdplayer-2.0.swf"
 
 class Livestream(Plugin):
+    @classmethod
+    def default_stream_types(cls, streams):
+        return ["akamaihd", "hls"]
 
     @classmethod
     def can_handle_url(self, url):
@@ -56,30 +62,33 @@ class Livestream(Plugin):
         event = verifyjson(info, "event")
         streaminfo = verifyjson(event, "stream_info")
 
-        if not streaminfo:
+        if not streaminfo or not streaminfo.get("is_live"):
             raise NoStreamsError(self.url)
 
-        qualities = verifyjson(streaminfo, "qualities")
-
-        if not streaminfo["is_live"]:
-            raise NoStreamsError(self.url)
-
-        streams = {}
-        if "play_url" in streaminfo:
+        streams = defaultdict(list)
+        play_url = streaminfo.get("play_url")
+        if play_url:
             swfurl = info.get("hdPlayerSwfUrl") or SWF_URL
             if not swfurl.startswith("http://"):
                 swfurl = "http://" + swfurl
 
+            qualities = streaminfo.get("qualities", [])
             smil = self._parse_smil(streaminfo["play_url"], swfurl)
-
             for bitrate, stream in smil.items():
-                sname = "{0}k".format(bitrate/1000)
-
+                name = "{0}k".format(bitrate/1000)
                 for quality in qualities:
                     if quality["bitrate"] == bitrate:
-                        sname = "{0}p".format(quality["height"])
+                        name = "{0}p".format(quality["height"])
 
-                streams[sname] = stream
+                streams[name].append(stream)
+
+        m3u8_url = streaminfo.get("m3u8_url")
+        if m3u8_url:
+            hls_streams = HLSStream.parse_variant_playlist(self.session,
+                                                           m3u8_url,
+                                                           namekey="pixels")
+            for name, stream in hls_streams.items():
+                streams[name].append(stream)
 
         return streams
 
