776607aab127909ad7d7549c935d2931236a9a59
Christopher Rosell <chrippa@tanuki.se>
2013-11-02 17:10:00 +0100

    plugins.filmon: Some minor tweaks.

--- src/livestreamer/plugins/filmon.py
+++ src/livestreamer/plugins/filmon.py
@@ -1,74 +1,66 @@
-from livestreamer.compat import urlparse, urljoin
-from livestreamer.exceptions import PluginError, NoStreamsError
-from livestreamer.plugin import Plugin
-from livestreamer.stream import RTMPStream
-from livestreamer.utils import urlget, urlopen, parse_json
-
 import re
 import requests
 
-class Filmon(Plugin):
-    SWFURL = "http://www.filmon.com/tv/modules/FilmOnTV/files/flashapp/filmon/FilmonPlayer.swf"
-    CHINFO = "http://www.filmon.com/ajax/getChannelInfo"
+from livestreamer.compat import urlparse
+from livestreamer.exceptions import PluginError, NoStreamsError
+from livestreamer.plugin import Plugin
+from livestreamer.stream import RTMPStream
+from livestreamer.utils import urlget, urlopen, res_json
 
+AJAX_HEADERS = {
+    "Referer": "http://www.filmon.com",
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0"
+}
+CHINFO_URL = "http://www.filmon.com/ajax/getChannelInfo"
+SWF_URL = "http://www.filmon.com/tv/modules/FilmOnTV/files/flashapp/filmon/FilmonPlayer.swf"
+
+
+class Filmon(Plugin):
     @classmethod
     def can_handle_url(self, url):
-        return "filmon.com" in url
+        return re.match("^http(s)?://(\w+\.)?filmon.com/tv/.+", url)
 
     def _get_streams(self):
-
         if not RTMPStream.is_usable(self.session):
             raise PluginError("rtmpdump is not usable and required by Filmon plugin")
 
         self.logger.debug("Fetching stream info")
-
         self.rsession = requests.session()
         res = urlget(self.url, session=self.rsession)
 
         match = re.search("/channels/(\d+)/extra_big_logo.png", res.text)
-        if match:
-            channel_id = match.group(1)
-        else:
-            raise NoStreamsError(self.url)
+        if not match:
+            return
 
+        channel_id = match.group(1)
         streams = {}
-        streams.update(self._get_stream(channel_id, "low"))
-        try:
-            streams.update(self._get_stream(channel_id, "high"))
-        except:
-            pass
+        for quality in ("low", "high"):
+            try:
+                streams[quality] = self._get_stream(channel_id, quality)
+            except NoStreamsError:
+                pass
 
         return streams
 
     def _get_stream(self, channel_id, quality):
-
-        headers = {
-            "Referer": "http://www.filmon.com",
-            "X-Requested-With": "XMLHttpRequest",
-            "User-Agent": "Mozilla/5.0"
-        }
-
         params = dict(channel_id=channel_id, quality=quality)
-
-        res = urlopen(self.CHINFO, data=params, headers=headers,
+        res = urlopen(CHINFO_URL, data=params, headers=AJAX_HEADERS,
                       session=self.rsession)
+        json = res_json(res)
 
-        if res:
-            json = parse_json(res.text)
-        else:
+        if not json:
             raise NoStreamsError(self.url)
-
-        if not isinstance(json, list):
+        elif not isinstance(json, list):
             raise PluginError("Invalid JSON response")
-        elif not len(json) > 0:
-            raise NoStreamsError(self.url)
-        elif not ("serverURL" in json[0] and "streamName" in json[0]):
+
+        info = json[0]
+        rtmp = info.get("serverURL")
+        playpath = info.get("streamName")
+        if not (rtmp and playpath):
             raise NoStreamsError(self.url)
 
-        rtmp = json[0]["serverURL"]
-        playpath = json[0]["streamName"]
         parsed = urlparse(rtmp)
-
         if not parsed.scheme.startswith("rtmp"):
             raise NoStreamsError(self.url)
 
@@ -77,17 +69,13 @@ class Filmon(Plugin):
         else:
             app = parsed.path[1:]
 
-        stream = {}
-        stream[quality] = RTMPStream(self.session, {
+        return RTMPStream(self.session, {
             "rtmp": rtmp,
             "pageUrl": self.url,
-            "swfUrl": self.SWFURL,
+            "swfUrl": SWF_URL,
             "playpath": playpath,
             "app": app,
             "live": True
         })
-
-        return stream
-
 
 __plugin__ = Filmon
