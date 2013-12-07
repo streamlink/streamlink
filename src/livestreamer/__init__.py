e818a76643af6cb3b2a418666c695b57f8b70089
Christopher Rosell <chrippa@tanuki.se>
2013-12-07 23:42:04 +0100

    Release version 1.7.1.

--- src/livestreamer/__init__.py
+++ src/livestreamer/__init__.py
@@ -11,7 +11,7 @@ Full documentation is available at http://livestreamer.tanuki.se/.
 
 
 __title__ = "livestreamer"
-__version__ = "1.7.0"
+__version__ = "1.7.1"
 __license__ = "Simplified BSD"
 __author__ = "Christopher Rosell"
 __copyright__ = "Copyright 2011-2013 Christopher Rosell"
