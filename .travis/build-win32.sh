6541f5a322b8cd6adab965b18a7174a02889f08c
Christopher Rosell <chrippa@tanuki.se>
2014-03-18 23:49:38 +0100

    travis: Attempt to fix weird issue with "git describe".

--- .travis/build-win32.sh
+++ .travis/build-win32.sh
@@ -4,6 +4,10 @@ if [ $TRAVIS_PYTHON_VERSION != "2.7" ]; then
 	exit 0
 fi
 
+# "git describe" seems to somtimes exit with "fatal: No names found, cannot
+# describe anything.", this makes sure we have tag information fetched.
+git fetch -t && sleep 1
+
 sh win32/build-with-bootstrap.sh
 cd dist/
 travis-artifacts upload --path *zip --target-path ""
