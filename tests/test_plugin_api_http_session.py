cd1bf031339d1055a659fec4f88499af6bafc8b7
Christopher Rosell <chrippa@tanuki.se>
2014-03-29 00:34:18 +0100

    tests: Python 2.6's unittest module is terrible.

--- tests/test_plugin_api_http_session.py
+++ tests/test_plugin_api_http_session.py
@@ -8,12 +8,13 @@ class TestPluginAPIHTTPSession(unittest.TestCase):
     def test_read_timeout(self):
         session = HTTPSession()
 
-        with self.assertRaises(PluginError):
+        def stream_data():
             res = session.get("http://httpbin.org/delay/6",
                               timeout=3, stream=True)
             next(res.iter_content(8192))
 
 
+        self.assertRaises(PluginError, stream_data)
 
 if __name__ == "__main__":
     unittest.main()
