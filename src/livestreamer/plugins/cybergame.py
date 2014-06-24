46f4c29dfafb7a8564e8b79e890a32dbe8833156
Christopher Rosell <chrippa@tanuki.se>
2014-06-24 11:30:50 +0200

    plugins.cybergame: Refactor to use the validation API.

--- src/livestreamer/plugins/cybergame.py
+++ src/livestreamer/plugins/cybergame.py
@@ -1,84 +1,80 @@
-from livestreamer.exceptions import PluginError, NoStreamsError
-from livestreamer.plugin import Plugin
-from livestreamer.plugin.api import http
-from livestreamer.stream import RTMPStream
-
 import re
 
+from livestreamer.compat import urlparse
+from livestreamer.plugin import Plugin
+from livestreamer.plugin.api import http, validate
+from livestreamer.stream import RTMPStream
+
 PLAYLIST_URL = "http://api.cybergame.tv/p/playlist.smil"
-CONFIG_URL = "http://api.cybergame.tv/p/config.php"
+
+_url_re = re.compile("""
+    http(s)?://(\w+\.)?cybergame.tv
+    (?:
+        /videos/(?P<video_id>\d+)
+    )?
+    (?:
+        /(?P<channel>[^/&?]+)
+    )?
+""", re.VERBOSE)
+
+_playlist_schema = validate.Schema(
+    validate.union({
+        "base": validate.all(
+            validate.xml_find("./head/meta"),
+            validate.get("base"),
+            validate.url(scheme="rtmp")
+        ),
+        "videos": validate.all(
+            validate.xml_findall(".//video"),
+            [
+                validate.union({
+                    "src": validate.all(
+                        validate.get("src"),
+                        validate.text
+                    ),
+                    "height": validate.all(
+                        validate.get("height"),
+                        validate.text,
+                        validate.transform(int)
+                    )
+                })
+            ]
+        )
+    })
+)
+
 
 class Cybergame(Plugin):
-
     @classmethod
     def can_handle_url(self, url):
-        return "cybergame.tv" in url
+        return _url_re.match(url)
 
-    def _get_rtmp_streams(self, params, alternative=""):
+    def _get_playlist(self, **params):
         res = http.get(PLAYLIST_URL, params=params)
-        root = http.xml(res)
-        rtmp = root.find("./head/meta").attrib.get("base")
-        if not rtmp:
-            raise NoStreamsError(self.url)
-
-        tmpsteams = {}
-
-        videos = root.findall("./body/switch/video") or root.findall("./body/video")
-        for video in videos:
-            src = video.attrib.get("src")
-            height = video.attrib.get("height")
-            quality = "{0}p{1}".format(height, alternative)
-            tmpsteams[quality] = RTMPStream(self.session, {
-                "rtmp": "{0}/{1}".format(rtmp,src),
+        playlist = http.xml(res, schema=_playlist_schema)
+        streams = {}
+        for video in playlist["videos"]:
+            name = "{0}p".format(video["height"])
+            stream = RTMPStream(self.session, {
+                "rtmp": "{0}/{1}".format(playlist["base"], video["src"]),
+                "app": urlparse(playlist["base"]).path[1:],
                 "pageUrl": self.url,
+                "rtmp": playlist["base"],
+                "playpath": video["src"],
                 "live": True
             })
-
-        return tmpsteams
-
-    def _get_vod_streams(self):
-        match = re.search("/videos/(\d+)", self.url)
-        if not match:
-            return
-        params = dict(vod=match.group(1))
-        return self._get_rtmp_streams(params)
-
-    def _get_streams(self):
-        if not RTMPStream.is_usable(self.session):
-            raise PluginError("rtmpdump is not usable and required by Cybergame plugin")
-
-        if "/videos/" in self.url:
-            return self._get_vod_streams()
-
-        self.logger.debug("Fetching live stream info")
-        res = http.get(self.url)
-
-        match = re.search("channel=([^\"]+)", res.text)
-        if not match:
-            raise NoStreamsError(self.url)
-
-        channelname = match.group(1)
-
-        res = http.get(CONFIG_URL, params=dict(c=channelname, ports="y"))
-        json = http.json(res)
-        servers = json.get("servers")
-        if not servers:
-            raise NoStreamsError(self.url)
-
-        alternative = ""
-        streams  = {}
-
-        for server in servers:
-            srv, port = server.split(":")
-            params = dict(channel=channelname, server=srv, port=port)
-            tmpstreams = self._get_rtmp_streams(params, alternative)
-            streams.update(tmpstreams)
-            if not alternative:
-                alternative = "_alt"
-            else:
-                break
+            streams[name] = stream
 
         return streams
 
+    def _get_streams(self):
+        match = _url_re.match(self.url)
+        video_id = match.group("video_id")
+        channel = match.group("channel")
+
+        if video_id:
+            return self._get_playlist(vod=video_id)
+        elif channel:
+            return self._get_playlist(channel=channel)
 
 __plugin__ = Cybergame
