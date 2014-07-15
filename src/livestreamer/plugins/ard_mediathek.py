2ed57e3dfce4ef97f765dcf433bf2a174ca9ee75
Christopher Rosell <chrippa@tanuki.se>
2014-07-15 22:50:49 +0200

    plugins.ard_mediathek: Refactor to use the validation API.

--- src/livestreamer/plugins/ard_mediathek.py
+++ src/livestreamer/plugins/ard_mediathek.py
@@ -1,14 +1,13 @@
 import re
 
-from livestreamer.plugin import Plugin
-from livestreamer.plugin.api import http
-from livestreamer.stream import HTTPStream, HDSStream, RTMPStream
-from livestreamer.utils import verifyjson
-from livestreamer.exceptions import NoStreamsError, PluginError, StreamError
+from collections import defaultdict
 
-PAGE_URL = "http://www.ardmediathek.de/tv"
+from livestreamer.plugin import Plugin
+from livestreamer.plugin.api import http, validate
+from livestreamer.stream import HTTPStream, HDSStream, RTMPStream
+
+MEDIA_URL = "http://www.ardmediathek.de/play/media/{0}"
 SWF_URL = "http://www.ardmediathek.de/ard/static/player/base/flash/PluginFlash.swf"
-CONFIG_URL = "http://www.ardmediathek.de/play/media/{0}"
 HDCORE_PARAMETER = "?hdcore=3.3.0"
 QUALITY_MAP = {
     "auto": "auto",
@@ -18,148 +17,127 @@ QUALITY_MAP = {
     0: "144p"
 }
 
+_url_re = re.compile("http(s)?://(\w+\.)?ardmediathek.de/tv")
+_media_id_re = re.compile("/play/config/(\d+)")
+_media_schema = validate.Schema({
+    "_mediaArray": [{
+        "_mediaStreamArray": [{
+            validate.optional("_server"): validate.text,
+            "_stream": validate.any(validate.text, [validate.text]),
+            "_quality": validate.any(int, validate.text)
+        }]
+    }]
+})
+_smil_schema = validate.Schema(
+    validate.union({
+        "base": validate.all(
+            validate.xml_find("head/meta"),
+            validate.get("base"),
+            validate.url(scheme="http")
+        ),
+        "videos": validate.all(
+            validate.xml_findall("body/seq/video"),
+            [validate.get("src")]
+        )
+    })
+)
+
+
 class ard_mediathek(Plugin):
     @classmethod
     def can_handle_url(cls, url):
-        return "ardmediathek.de/tv" in url.lower()
-        
-    def _get_mp4_streams(self, streamname, quality):
-        url = streamname
-        self.logger.debug("Stream URL: " + url)
-        qualityname = QUALITY_MAP[quality]
-        self.logger.debug("Quality: " + qualityname)
-        tmpstreams = {}
-        tmpstreams[qualityname] = HTTPStream(self.session, url)
-        return tmpstreams
-    
-    def _get_smil_streams(self, streamname):
-        res = http.get(streamname)
-        smil = http.xml(res, "SMIL config")
-        httpbase = smil.find("head/meta").attrib.get("base")
-        videos = smil.findall("body/seq/video")
-            
-        for video in videos:
-            src = video.attrib.get("src")
-            self.logger.debug(src)
-            url = httpbase + src + HDCORE_PARAMETER
-            self.logger.debug("Stream URL: " + url)
-            tmpstreams = {}
-            try:
-                tmpstreams = HDSStream.parse_manifest(self.session, url, pvswf=SWF_URL)
-            except IOError as err:
-                self.logger.warning("Failed to get HDS manifest: {0}", err)
-            return tmpstreams
-    
-    def _get_f4m_streams(self, streamname):
-        '''
-        needs to hdcore parameter set to work. 
-        '''
-        url = streamname + HDCORE_PARAMETER
-        self.logger.debug("Stream URL: " + url)
-        tmpstreams = {}
-        try:
-            tmpstreams = HDSStream.parse_manifest(self.session, url, pvswf=SWF_URL)
-        except IOError as err:
-            self.logger.warning("Failed to get HDS manifest: {0}", err)
-        return tmpstreams
-    
-    def _get_rtmp_streams(self, servername, streamname, quality):
-        '''
-        for some streams they used invalid urls with slashes missing. this is a 
-        fix for that problem. here is an example
-        http://www.ardmediathek.de/tv/live?kanal=1386988
-        http://www.ardmediathek.de/play/media/15806928?devicetype=pc&features=flash
-        '''        
-        if servername.endswith("/"):
-            if streamname.startswith("/"):
-                self.logger.debug("URL invalid")
-                url = servername[:-1] + streamname
+        return _url_re.match(url)
+
+    def _get_http_streams(self, info):
+        name = QUALITY_MAP.get(info["_quality"], "vod")
+        urls = info["_stream"]
+        if not isinstance(info["_stream"], list):
+            urls = [urls]
+
+        streams = {}
+        for url in urls:
+            stream = HTTPStream(self.session, url)
+            if streams:
+                streams[name + "_alt"] = stream
             else:
-                self.logger.debug("URL valid")
-                url = servername + streamname
-        else:
-            if streamname.startswith("/"):
-                self.logger.debug("URL valid")
-                url = servername + streamname
-            else:
-                self.logger.debug("URL invalid")
-                url = servername + "/" + streamname
-            
-        self.logger.debug("Stream URL: " + url)
-        qualityname = QUALITY_MAP[quality]
-        self.logger.debug("Quality: " + qualityname)
-        tmpstreams = {}
-        tmpstreams[qualityname] = RTMPStream(self.session, {
-            "rtmp": url,
-            "pageUrl": PAGE_URL,
+                streams[name] = stream
+
+        return streams
+
+    def _get_hds_streams(self, info):
+        # Needs the hdcore parameter added
+        url = info["_stream"] + HDCORE_PARAMETER
+
+        return HDSStream.parse_manifest(self.session, url, pvswf=SWF_URL)
+
+    def _get_rtmp_streams(self, info):
+        name = QUALITY_MAP.get(info["_quality"], "live")
+        params = {
+            "rtmp": info["_server"].strip(),
+            "playpath": info["_stream"],
+            "pageUrl": self.url,
             "swfVfy": SWF_URL,
             "live": True
-        })
-        return tmpstreams
-        
+        }
+        stream = RTMPStream(self.session, params)
+
+        return {name: stream}
+
+    def _get_smil_streams(self, info):
+        res = http.get(info["_stream"])
+        smil = http.xml(res, "SMIL config", schema=_smil_schema)
+
+        streams = {}
+        for video in smil["videos"]:
+            url = "{0}/{1}{2}".format(smil["base"], video, HDCORE_PARAMETER)
+            streams.update(
+                HDSStream.parse_manifest(self.session, url, pvswf=SWF_URL)
+            )
+
+        return streams
+
     def _get_streams(self):
-        self.logger.debug("Fetching stream info")
         res = http.get(self.url)
-        
-        match = re.search("/play/config/(\d+)", res.text)
+        match = _media_id_re.search(res.text)
         if match:
-            self.logger.debug("Stream ID: " + match.group(1))
-            streamid = match.group(1)
+            media_id = match.group(1)
         else:
             return
-        
-        self.logger.debug("Config URL: " + CONFIG_URL.format(streamid))
-        res = http.get(CONFIG_URL.format(streamid))
-        json = http.json(res)
-        mediaArray = verifyjson(json, "_mediaArray")
-        
-        streams = {}
-        for media in mediaArray:
-            mediaStreamArray = verifyjson(media, "_mediaStreamArray")
-            for mediaStream in mediaStreamArray:
+
+        res = http.get(MEDIA_URL.format(media_id))
+        media = http.json(res, schema=_media_schema)
+
+        streams = defaultdict(list)
+        for media in media["_mediaArray"]:
+            for stream in media["_mediaStreamArray"]:
+                server = stream.get("_server", "").strip()
+                stream_ = stream["_stream"]
+                if isinstance(stream_, list):
+                    if not stream_:
+                        continue
+                    stream_ = stream_[0]
+                stream_ = stream_.strip()
+
+                if server.startswith("rtmp://"):
+                    parser = self._get_rtmp_streams
+                    parser_name = "RTMP"
+                elif stream_.endswith(".f4m"):
+                    parser = self._get_hds_streams
+                    parser_name = "HDS"
+                elif stream_.endswith(".smil"):
+                    parser = self._get_smil_streams
+                    parser_name = "SMIL"
+                elif stream_.startswith("http"):
+                    parser = self._get_http_streams
+                    parser_name = "HTTP"
+
                 try:
-                    jsonvalid = True
-                    flash = verifyjson(mediaStream, "flashUrl")
-                    quality = verifyjson(mediaStream, "_quality")
-                    '''
-                    there is one channel where the servername is incorrect. it has
-                    spaces on the beginning. stripping whitespaces on both the server 
-                    and the stream name just for good measures. here is an example
-                    http://www.ardmediathek.de/tv/live?kanal=1386988
-                    http://www.ardmediathek.de/play/media/15806928?devicetype=pc&features=flash
-                    '''
-                    server = verifyjson(mediaStream, "_server").strip()
-                    stream = verifyjson(mediaStream, "_stream").strip()
-                except PluginError:
-                    '''
-                    there are some odd json respones where parts are missing and 
-                    instead of one video in _stream there is a list. this just igores
-                    them for now. here is an expample:
-                    http://www.ardmediathek.de/play/media/21897740
-                    http://www.ardmediathek.de/tv/FIFA-WM-2014/Bundestrainer-L%C3%B6w-lobt-M%C3%BCller/Das-Erste/Video?documentId=21897740&bcastId=21675666
-                    '''
-                    self.logger.debug("odd json format. ignoring this stream format")
-                    jsonvalid = False
-                    pass
-                if flash is True and server.startswith("rtmp://") and jsonvalid:
-                    self.logger.debug("RTMP stream")
-                    tmpstreams = self._get_rtmp_streams(server, stream, quality)
-                    streams.update(tmpstreams)
-                elif stream.endswith(".f4m") and jsonvalid:
-                    self.logger.debug("F4M manifest")
-                    tmpstreams = self._get_f4m_streams(stream)
-                    streams.update(tmpstreams)
-                elif stream.endswith(".smil") and jsonvalid:
-                    self.logger.debug("SMIL playlist")
-                    tmpstreams = self._get_smil_streams(stream)
-                    streams.update(tmpstreams)
-                elif stream.endswith(".mp4") and jsonvalid:
-                    self.logger.debug("MP4 video")
-                    tmpstreams = self._get_mp4_streams(stream, quality)
-                    streams.update(tmpstreams)
-                else:
-                    self.logger.debug("format not supported yet")
-                        
+                    for name, stream in parser(stream).items():
+                        streams[name].append(stream)
+                except IOError as err:
+                    self.logger.error("Failed to extract {0} streams: {1}",
+                                      parser_name, err)
+
         return streams
 
 __plugin__ = ard_mediathek
