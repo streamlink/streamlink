bb7a82d501a4753133e4065a3d4e110b9074dfd4
Christopher Rosell <chrippa@tanuki.se>
2014-08-03 23:23:19 +0200

    cli: Split the utils module into a package.

---
+++ src/livestreamer_cli/utils/__init__.py
@@ -0,0 +1,35 @@
+import json
+
+from contextlib import contextmanager
+
+from .http_server import HTTPServer
+from .named_pipe import NamedPipe
+from .player import find_default_player
+from .stream import stream_to_url
+
+
+__all__ = [
+    "NamedPipe", "HTTPServer", "JSONEncoder", "ignored",
+    "find_default_player", "stream_to_url"
+]
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if hasattr(obj, "__json__"):
+            return obj.__json__()
+        elif isinstance(obj, bytes):
+            return obj.decode("utf8", "ignore")
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
+@contextmanager
+def ignored(*exceptions):
+    try:
+        yield
+    except exceptions:
+        pass
+
+
+
