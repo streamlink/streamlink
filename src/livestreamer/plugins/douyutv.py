7fc6afb4663a7650681fbdda866d69ce66f72d11
Christopher Rosell <chrippa@tanuki.se>
2014-10-11 23:31:20 +0200

    plugins.douyutv: Add support for transcodes.

--- src/livestreamer/plugins/douyutv.py
+++ src/livestreamer/plugins/douyutv.py
@@ -5,6 +5,12 @@ from livestreamer.plugin.api import http, validate
 from livestreamer.stream import HTTPStream
 
 API_URL = "http://www.douyutv.com/api/client/room/{0}"
+SHOW_STATUS_ONLINE = 1
+SHOW_STATUS_OFFLINE = 2
+STREAM_WEIGHTS = {
+    "middle": 540,
+    "source": 1080
+}
 
 _url_re = re.compile("""
     http(s)?://(www\.)?douyutv.com
@@ -19,16 +25,30 @@ _room_schema = validate.Schema(
                 validate.transform(int)
             ),
             "rtmp_url": validate.text,
-            "rtmp_live": validate.text
+            "rtmp_live": validate.text,
+            "rtmp_multi_bitrate": validate.all(
+                validate.any([], {
+                    validate.text: validate.text
+                }),
+                validate.transform(dict)
+            )
         })
     },
     validate.get("data")
 )
 
+
 class Douyutv(Plugin):
     @classmethod
     def can_handle_url(self, url):
         return _url_re.match(url)
+
+    @classmethod
+    def stream_weight(cls, stream):
+        if stream in STREAM_WEIGHTS:
+            return STREAM_WEIGHTS[stream], "douyutv"
+
+        return Plugin.stream_weight(stream)
 
     def _get_streams(self):
         match = _url_re.match(self.url)
@@ -39,13 +59,16 @@ class Douyutv(Plugin):
         if not room:
             return
 
-        # 1 is live, 2 is offline
-        if room["show_status"] != 1:
+        if room["show_status"] != SHOW_STATUS_ONLINE:
             return
 
-        url = room["rtmp_url"] + "/" + room["rtmp_live"]
+        url = "{room[rtmp_url]}/{room[rtmp_live]}".format(room=room)
         stream = HTTPStream(self.session, url)
+        yield "source", stream
 
-        return dict(live=stream)
+        for name, url in room["rtmp_multi_bitrate"].items():
+            url = "{room[rtmp_url]}/{url}".format(room=room, url=url)
+            stream = HTTPStream(self.session, url)
+            yield name, stream
 
 __plugin__ = Douyutv
