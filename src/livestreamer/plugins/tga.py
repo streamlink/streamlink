faf60cd94bb522685c1132b1ae979cd1076a605c
wolftankk <wolftankk@gmail.com>
2015-01-16 13:38:10 +0800

    add source and mobile format

--- src/livestreamer/plugins/tga.py
+++ src/livestreamer/plugins/tga.py
@@ -5,11 +5,11 @@ import re
 from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http, validate
 from livestreamer.plugin.api.utils import parse_query
-from livestreamer.stream import HLSStream
+from livestreamer.stream import HLSStream, HTTPStream
 
 CHANNEL_INFO_URL = "http://api.plu.cn/tga/streams/%s"
 ROOM_INFO_URL   = "http://star.api.plu.cn/api/roomstatus?roomid=%d&pageType=live"
-STREAM_INFO_URL = "http://info.zb.qq.com/?cnlid=%d&host=plu.cn&cmd=2&qq=0&guid=fcd0fce8a00361c258937659febd5434&txvjsv=2.0&stream=2&debug=&ip=&system=1&sdtfrom=113"
+STREAM_INFO_URL = "http://info.zb.qq.com/?cnlid=%d&cmd=2&stream=%d&system=1&sdtfrom=113"
 
 _url_re = re.compile("http://star.(tga\.)?plu.cn/(m\/)?(?P<domain>[a-z0-9]+)");
 _stream_re = re.compile("<iframe style='(.*?)' frameborder='0' scrolling='no' src='http://v.qq.com/iframe/live_player.html\?cnlid=(?P<cnid>\d+)&width=\d+&height=\d+'></iframe>");
@@ -40,12 +40,24 @@ _room_schame = validate.Schema({
     validate.get("Broadcast")
 )
 
+STREAM_WEIGHTS = {
+    "middle": 540,
+    "source": 1080
+}
+
 class Tga(Plugin):
     @classmethod
     def can_handle_url(self, url):
         return _url_re.match(url)
 
-    def _get_channel_info(self, domain):
+    @classmethod
+    def stream_weight(cls, stream):
+        if stream in STREAM_WEIGHTS:
+            return STREAM_WEIGHTS[stream], "tga"
+
+        return Plugin.stream_weight(stream)
+
+    def _get_channel_info(self, domain, weight=1):
         channel_info = http.get(CHANNEL_INFO_URL % str(domain))
         info = http.json(channel_info, schema=_channel_schema)
         if info is None:
@@ -61,7 +73,7 @@ class Tga(Plugin):
             return False
         cnid = match.group('cnid');
         
-        qq_stream_url = http.get(STREAM_INFO_URL % int(cnid));
+        qq_stream_url = http.get(STREAM_INFO_URL % (int(cnid), int(weight)));
         qq_info = http.json(qq_stream_url, schema=_qq_schema)
 
         return qq_info;
@@ -70,12 +82,12 @@ class Tga(Plugin):
         match = _url_re.match(self.url);
         domain = match.group('domain')
         
-        view_url = self._get_channel_info(domain);
+        view_url = self._get_channel_info(domain, 1);
 
         if view_url == False:
             return;
 
-        yield "live", HLSStream(self.session, view_url)
-
+        yield "source", HTTPStream(self.session, view_url)
+        yield "middle", HLSStream(self.session, self._get_channel_info(domain, 2))
 
 __plugin__ = Tga
