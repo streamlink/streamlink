426db3af4a910e852b199477c5200b1996d990be
Christopher Rosell <chrippa@tanuki.se>
2015-01-25 19:27:52 +0100

    plugins.wattv: Minor tweaks and fixes.
    - Remove unnecessary headers.
    - Fix Python 3 compatiblity.

--- src/livestreamer/plugins/wattv.py
+++ src/livestreamer/plugins/wattv.py
@@ -5,11 +5,11 @@ from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http
 from livestreamer.stream import HDSStream
 
-_url_re = re.compile("http(s)?://(\w+\.)?wat.tv/")
-HTTP_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.9 Safari/537.36"
-}
+# Got the secret from the swf with rev number location
+# (tv/wat/player/media/Media.as)
+TOKEN_SECRET = '9b673b13fa4682ed14c3cfa5af5310274b514c4133e9b3a81e6e3aba009l2564'
 
+_url_re = re.compile("http(s)?://(\w+\.)?wat.tv/")
 _video_id_re = re.compile("href=\"http://m.wat.tv/video/([^\"]+)", re.IGNORECASE)
 
 
@@ -19,38 +19,49 @@ class WAT(Plugin):
         return _url_re.match(url)
 
     def _get_streams(self):
-        html = http.get(self.url, headers=HTTP_HEADERS).content
-        video_id = _video_id_re.search(html).group(1)
+        res = http.get(self.url)
+        match = video_id = _video_id_re.search(res.text)
+        if not match:
+            return
 
-        return self.generate_streams('web', video_id) + self.generate_streams('webhd', video_id)
+        video_id = match.group(1)
 
-    def generate_streams(self, type, video_id):
-        security_url = self.generate_security_url(type, video_id)
-        security_response = self.session.http.get(security_url, headers=HTTP_HEADERS)
+        # TODO: Replace with "yield from" when dropping Python 2.
+        for __ in self._create_streams('web', video_id).items():
+            yield __
 
-        url = security_response.content
-        cookies = security_response.cookies
+        for __ in self._create_streams('webhd', video_id).items():
+            yield __
 
-        return HDSStream.parse_manifest(self.session, url, headers=HTTP_HEADERS, cookies=cookies).items()
+    def _create_streams(self, type_, video_id):
+        url = self._generate_security_url(type_, video_id)
+        res = http.get(url)
 
-    def generate_security_url(self, type, video_id):
-        token = self.generate_security_token(type, video_id)
-        return "http://www.wat.tv/get/"+type+"/"+video_id+"?token="+token+"&domain=www.wat.tv&refererURL=wat.tv&revision=04.00.719%0A&synd=0&helios=1&context=playerWat&pub=1&country=FR&sitepage=WAT%2Ftv%2Ft%2Finedit%2Ftf1%2Fparamount_pictures_france&lieu=wat&playerContext=CONTEXT_WAT&getURL=1&version=LNX%2014,0,0,125"
+        return HDSStream.parse_manifest(self.session, res.text, cookies=res.cookies)
 
-    def generate_security_token(self, type, video_id):
-        # Got the secret from the swf with rev number location (tv/wat/player/media/Media.as)
-        # use www.showmycode.com or something to decompile the swf
-        secret = '9b673b13fa4682ed14c3cfa5af5310274b514c4133e9b3a81e6e3aba009l2564'
+    def _generate_security_url(self, type_, video_id):
+        token = self._generate_security_token(type_, video_id)
 
+        return ("http://www.wat.tv/get/{type_}/{video_id}?token={token}"
+                "&domain=www.wat.tv&refererURL=wat.tv&revision=04.00.719%0A&"
+                "synd=0&helios=1&context=playerWat&pub=1&country=FR"
+                "&sitepage=WAT%2Ftv%2Ft%2Finedit%2Ftf1%2Fparamount_pictures_"
+                "france&lieu=wat&playerContext=CONTEXT_WAT&getURL=1"
+                "&version=LNX%2014,0,0,125").format(**locals())
+
+    def _generate_security_token(self, type_, video_id):
         # Get timestamp
-        timestamp = int(http.get('http://www.wat.tv/servertime', headers=HTTP_HEADERS).content.split('|')[0])
+        res = http.get('http://www.wat.tv/servertime')
+        timestamp = int(res.text.split('|')[0])
         timestamp_hex = format(timestamp, 'x').rjust(8, '0')
 
-        # player id
-        player_prefix = "/"+type+"/"+video_id
+        # Player id
+        player_prefix = "/{0}/{1}".format(type_, video_id)
 
         # Create the token
-        token = hashlib.md5((secret + player_prefix) + timestamp_hex).hexdigest() + "/" + timestamp_hex
+        data = (TOKEN_SECRET + player_prefix + timestamp_hex).encode('utf8')
+        token = hashlib.md5(data)
+        token = "{0}/{1}".format(token.hexdigest(), timestamp_hex)
 
         return token
 
