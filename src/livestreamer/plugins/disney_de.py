0513bc622b518c1534c7b8db16aa1694f05e3ce5
Christopher Rosell <chrippa@tanuki.se>
2015-01-25 19:05:09 +0100

    plugins.disney_de: Minor tweaks and fixes.
    - Remove unnecessary headers.
    - Fix Python 3 compatiblity.

--- src/livestreamer/plugins/disney_de.py
+++ src/livestreamer/plugins/disney_de.py
@@ -7,16 +7,13 @@ Supports:
 """
 
 import re
-import json
 
 from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http
+from livestreamer.plugin.api.utils import parse_json
 from livestreamer.stream import HLSStream
 
 _url_re = re.compile("http(s)?://(\w+\.)?disney(channel)?.de/")
-HTTP_HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1944.9 Safari/537.36"
-}
 
 # stream urls are in `Grill.burger`->stack->data->externals->data
 _stream_hls_re = re.compile("\"hlsStreamUrl\":\s*(\"[^\"]+\")")
@@ -29,15 +26,14 @@ class DisneyDE(Plugin):
         return _url_re.match(url)
 
     def _get_streams(self):
-        html = http.get(self.url, headers=HTTP_HEADERS).content
+        res = http.get(self.url)
+        match = (_stream_hls_re.search(res.text) or
+                 _stream_data_re.search(res.text))
+        if not match:
+            return
 
-        url_match = _stream_hls_re.search(html)
-        if url_match is None:
-            url_match = _stream_data_re.search(html)
-
-        stream_url = json.loads(url_match.group(1))
+        stream_url = parse_json(match.group(1))
 
         return HLSStream.parse_variant_playlist(self.session, stream_url)
-
 
 __plugin__ = DisneyDE
