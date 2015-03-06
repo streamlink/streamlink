36ead780b7abec459550493f8405830e80197b61
blxd <blxd@users.noreply.github.com>
2015-03-06 10:06:54 +0000

    tidied up the plugin code for tvcatchup

--- src/livestreamer/plugins/tvcatchup.py
+++ src/livestreamer/plugins/tvcatchup.py
@@ -1,6 +1,5 @@
 import re
 
-from livestreamer import PluginError
 from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http
 from livestreamer.stream import HLSStream
@@ -12,24 +11,23 @@ _url_re = re.compile("http://(?:www\.)?tvcatchup.com/watch/(?P<channel_id>[0-9]+
 
 
 class TVCatchup(Plugin):
-    def __init__(self, url):
-        Plugin.__init__(self, url)
-        match = _url_re.match(url).groupdict()
-        self.channel_id = match["channel_id"]
-
     @classmethod
-    def can_handle_url(self, url):
+    def can_handle_url(cls, url):
         return _url_re.match(url)
 
     def _get_streams(self):
-        res = http.get(STREAM_URL_FORMAT.format(self.channel_id))
+        """
+        Finds the stream from tvcatchup, they only provide a single 480p stream per channel.
+        """
+        match = _url_re.match(self.url).groupdict()
+        channel_id = match["channel_id"]
 
-        stream_url = res.status_code in SUCCESS_HTTP_CODES and res.json().get('url')
+        res = http.get(STREAM_URL_FORMAT.format(channel_id))
 
-        if not stream_url:
-            raise PluginError("The program is not available, please try again later")
+        stream_url = http.json(res).get('url')
 
-        return {"480p": HLSStream(self.session, stream_url)}
+        if stream_url:
+            return {"480p": HLSStream(self.session, stream_url)}
 
 
 __plugin__ = TVCatchup
