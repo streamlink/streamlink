9723095fd2e8caea26d194bfbe971b3ebcf041e9
Christopher Rosell <chrippa@tanuki.se>
2015-03-29 18:11:58 +0200

    plugins.viasat: Fix stream mapping.
    Resolves #822.

--- src/livestreamer/plugins/viasat.py
+++ src/livestreamer/plugins/viasat.py
@@ -82,12 +82,12 @@ class Viasat(Plugin):
 
         mapper = StreamMapper(lambda pattern, video: re.search(pattern, video[1]))
         mapper.map(
-            r"/master\.m3u8",
+            r"/\w+\.m3u8",
             self._create_dynamic_streams,
             "HLS", HLSStream.parse_variant_playlist
         )
         mapper.map(
-            r"/manifest\.f4m",
+            r"/\w+\.f4m",
             self._create_dynamic_streams,
             "HDS", HDSStream.parse_manifest
         )
