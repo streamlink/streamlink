f77b977c86e0e2a1f29f5adb595dfb4b55e0e87d
Christopher Rosell <chrippa@tanuki.se>
2015-05-02 21:00:18 +0200

    Release version 1.12.2.

--- src/livestreamer/__init__.py
+++ src/livestreamer/__init__.py
@@ -12,7 +12,7 @@ Full documentation is available at http://docs.livestreamer.io/.
 
 
 __title__ = "livestreamer"
-__version__ = "1.12.1"
+__version__ = "1.12.2"
 __license__ = "Simplified BSD"
 __author__ = "Christopher Rosell"
 __copyright__ = "Copyright 2011-2015 Christopher Rosell"
