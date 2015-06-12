46302bac5397356827aa4b4a3557b73416b411a3
intact <intact.devel@gmail.com>
2015-06-12 21:54:40 +0200

    Updated douyutv.com plugin

--- src/livestreamer/plugins/douyutv.py
+++ src/livestreamer/plugins/douyutv.py
@@ -1,10 +1,13 @@
+import hashlib
 import re
+import time
 
 from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http, validate
 from livestreamer.stream import HTTPStream
 
-API_URL = "http://www.douyutv.com/api/client/room/{0}"
+API_URL = "http://www.douyutv.com/swf_api/room/{0}?cdn=&nofan=yes&_t={1}&sign={2}"
+API_SECRET = "bLFlashflowlad92"
 SHOW_STATUS_ONLINE = 1
 SHOW_STATUS_OFFLINE = 2
 STREAM_WEIGHTS = {
@@ -54,7 +57,10 @@ class Douyutv(Plugin):
         match = _url_re.match(self.url)
         channel = match.group("channel")
 
-        res = http.get(API_URL.format(channel))
+        ts = int(time.time() / 60)
+        sign = hashlib.md5(("{0}{1}{2}".format(channel, API_SECRET, ts)).encode("utf-8")).hexdigest()
+
+        res = http.get(API_URL.format(channel, ts, sign))
         room = http.json(res, schema=_room_schema)
         if not room:
             return
