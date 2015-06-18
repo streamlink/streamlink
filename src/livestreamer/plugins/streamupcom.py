a4d168aab143c91dc1ac3eedf8fed5603cc85ac2
intact <intact.devel@gmail.com>
2015-06-18 14:50:31 +0200

    plugins.streamupcom: Check live status

--- src/livestreamer/plugins/streamupcom.py
+++ src/livestreamer/plugins/streamupcom.py
@@ -6,6 +6,7 @@ from livestreamer.plugin.api import http, validate
 from livestreamer.stream import RTMPStream
 
 RTMP_URL = "rtmp://{0}/app/{1}"
+STATUS_REQUEST_URI = "https://lancer.streamup.com/api/channels/{0}"
 BALANCING_REQUEST_URI = "https://streamup-lancer.herokuapp.com/api/redirect/{0}"
 
 _url_re = re.compile("http(s)?://(\w+\.)?streamup.com/(?P<channel>[^/?]+)")
@@ -38,14 +39,21 @@ class StreamupCom(Plugin):
         res = http.get(self.url, schema=_schema)
         if not res:
             return
+        channel_name = res["vars"]["channel"]
+        swf_url = res["swf"]
 
-        stream_ip = http.get(BALANCING_REQUEST_URI.format(res["vars"]["channel"])).text
+        # Check if the stream is online
+        res = http.get(STATUS_REQUEST_URI.format(channel_name), raise_for_status=False)
+        if res.status_code == 404:
+            return
+
+        stream_ip = http.get(BALANCING_REQUEST_URI.format(channel_name)).text
 
         streams = {}
         streams["live"] = RTMPStream(self.session, {
-            "rtmp": RTMP_URL.format(stream_ip, res["vars"]["channel"]),
+            "rtmp": RTMP_URL.format(stream_ip, channel_name),
             "pageUrl": self.url,
-            "swfUrl": urljoin(self.url, res["swf"]),
+            "swfUrl": urljoin(self.url, swf_url),
             "live": True
         })
         return streams
