a260056a772d7cb74552f1e3f9278016ae83c9d6
Alan Love <alan@cattes.us>
2015-06-29 22:15:14 -0500

    added support for livecoding.tv

---
+++ src/livestreamer/plugins/livecodingtv.py
@@ -0,0 +1,36 @@
+import re
+
+from livestreamer.plugin import Plugin
+from livestreamer.stream import RTMPStream
+from livestreamer.plugin.api import http
+
+
+_rtmp_re = re.compile('rtmp://[^"]+/(?P<channel>\w+)+[^/"]+')
+_url_re = re.compile("http(s)?://(?:\w+.)?\livecoding\.tv")
+
+
+class LivecodingTV(Plugin):
+    @classmethod
+    def can_handle_url(cls, url):
+        return _url_re.match(url)
+
+    def _get_streams(self):
+        res = http.get(self.url)
+        match = _rtmp_re.search(res.text)
+        rtmp_url = match.group(0)
+
+        print("GOT MATCH: {0}".format(rtmp_url))
+
+        if not match:
+            return
+
+        stream = RTMPStream(self.session, {
+            "rtmp": rtmp_url,
+            "pageUrl": self.url,
+            "live": True,
+        })
+
+        return dict(live=stream)
+
+
+__plugin__ = LivecodingTV
