eb65222893fe0336c881b46afadd33e460b4d6fe
trocknet <trocknet@github>
2015-08-02 13:20:13 +0200

    plugins.afreeca: Fix HLS stream.
    Resolves #982.

--- src/livestreamer/plugins/afreeca.py
+++ src/livestreamer/plugins/afreeca.py
@@ -10,6 +10,7 @@ STREAM_INFO_URLS = {
     "rtmp": "http://sessionmanager01.afreeca.tv:6060/broad_stream_assign.html",
     "hls": "http://resourcemanager.afreeca.tv:9090/broad_stream_assign.html"
 }
+HLS_KEY_URL = "http://api.m.afreeca.com/broad/a/watch"
 
 CHANNEL_RESULT_ERROR = 0
 CHANNEL_RESULT_OK = 1
@@ -55,18 +56,37 @@ class AfreecaTV(Plugin):
 
         return http.json(res, schema=_channel_schema)
 
+    def _get_hls_key(self, broadcast, username):
+        headers = {
+            "Referer": self.url
+        }
+        data = {
+            "bj_id": username,
+            "broad_no": broadcast
+        }
+        res = http.post(HLS_KEY_URL, data=data, headers=headers)
+
+        return http.json(res)
+
     def _get_stream_info(self, broadcast, type):
         params = {
             "return_type": "gs_cdn",
+            "use_cors": "true",
+            "cors_origin_url": "m.afreeca.com",
+            "broad_no": "{broadcast}-mobile-hd-{type}".format(**locals()),
             "broad_key": "{broadcast}-flash-hd-{type}".format(**locals())
         }
         res = http.get(STREAM_INFO_URLS[type], params=params)
         return http.json(res, schema=_stream_schema)
 
-    def _get_hls_stream(self, broadcast):
+    def _get_hls_stream(self, broadcast, username):
+        keyjson = self._get_hls_key(broadcast, username)
+        if keyjson["result"] != CHANNEL_RESULT_OK:
+            return
+        key = keyjson["data"]["hls_authentication_key"]
         info = self._get_stream_info(broadcast, "hls")
         if "view_url" in info:
-            return HLSStream(self.session, info["view_url"])
+            return HLSStream(self.session, info["view_url"], params=dict(aid=key))
 
     def _get_rtmp_stream(self, broadcast):
         info = self._get_stream_info(broadcast, "rtmp")
@@ -90,7 +110,7 @@ class AfreecaTV(Plugin):
         if flash_stream:
             yield "live", flash_stream
 
-        mobile_stream = self._get_hls_stream(broadcast)
+        mobile_stream = self._get_hls_stream(broadcast, username)
         if mobile_stream:
             yield "live", mobile_stream
 
