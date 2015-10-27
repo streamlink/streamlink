b52c77a61397491318d0f3ff6f758a68eba3b0ef
steven7851 <steven7851@msn.com>
2015-10-27 09:05:54 +0800

    plugins.douyutv: Use new api.
    Use douyutv new api for android with HLS stream feature.

--- src/livestreamer/plugins/douyutv.py
+++ src/livestreamer/plugins/douyutv.py
@@ -4,10 +4,11 @@ import time
 
 from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http, validate
-from livestreamer.stream import HTTPStream
+from livestreamer.stream import (
+    HTTPStream, HLSStream
+)
 
-API_URL = "http://www.douyutv.com/swf_api/room/{0}?cdn=&nofan=yes&_t={1}&sign={2}"
-API_SECRET = "bLFlashflowlad92"
+API_URL = "http://www.douyutv.com/api/v1/room/{0}?aid=android&client_sys=android&time={1}&auth={2}"
 SHOW_STATUS_ONLINE = 1
 SHOW_STATUS_OFFLINE = 2
 STREAM_WEIGHTS = {
@@ -29,6 +30,7 @@ _room_schema = validate.Schema(
             ),
             "rtmp_url": validate.text,
             "rtmp_live": validate.text,
+            "hls_url": validate.text,
             "rtmp_multi_bitrate": validate.all(
                 validate.any([], {
                     validate.text: validate.text
@@ -57,8 +59,8 @@ class Douyutv(Plugin):
         match = _url_re.match(self.url)
         channel = match.group("channel")
 
-        ts = int(time.time() / 60)
-        sign = hashlib.md5(("{0}{1}{2}".format(channel, API_SECRET, ts)).encode("utf-8")).hexdigest()
+        ts = int(time.time())
+        sign = hashlib.md5(("room/{0}?aid=android&client_sys=android&time={1}".format(channel, ts) + "1231").encode("ascii")).hexdigest()
 
         res = http.get(API_URL.format(channel, ts, sign))
         room = http.json(res, schema=_room_schema)
@@ -67,6 +69,10 @@ class Douyutv(Plugin):
 
         if room["show_status"] != SHOW_STATUS_ONLINE:
             return
+
+        hls_url = "{room[hls_url]}?wsiphost=local".format(room=room)
+        hls_stream = HLSStream(self.session, hls_url)
+        yield "hls", hls_stream
 
         url = "{room[rtmp_url]}/{room[rtmp_live]}".format(room=room)
         stream = HTTPStream(self.session, url)
