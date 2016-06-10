741b8d250b0b09226a7287f3ac3e76c1a928fa38
kviktor <kviktor@cloud.bme.hu>
2016-06-10 16:48:23 +0200

    update mediaklikk plugin

--- src/livestreamer/plugins/mediaklikk.py
+++ src/livestreamer/plugins/mediaklikk.py
@@ -5,14 +5,11 @@ from livestreamer.stream import HLSStream
 from livestreamer.plugin.api import http
 
 
-_stream_url_re = re.compile(r"http(s)?://(www\.)?mediaklikk.hu/([A-Za-z0-9\-]+)/.*")
-_video_url_re = re.compile(r"http(s)?://(www\.)?mediaklikk.hu/video/([A-Za-z0-9\-]+)/.*")
+_stream_url_re = re.compile(r"http(s)?://(www\.)?mediaklikk.hu/([A-Za-z0-9\-]+)/?")
 _id_re = re.compile(r'.*data\-streamid="([a-z0-9]+)".*')
-_token_re = re.compile(r'.*data\-token="([A-Za-z0-9%]+)".*')
-_smil_re = re.compile(r'.*<MediaSource>(?P<smil_url>.*)</MediaSource>.*')
+_file_re = re.compile(r'.*{\'file\': ?\'([a-zA-Z0-9\./\?=:]+)\'}].*')
 
-_stream_player_url = "http://player.mediaklikk.hu/player/player-inside-full2.php?streamid={0}&noflash=yes&userid=mtva"
-_video_player_url = "http://player.mediaklikk.hu/player/player-external-vod-full.php?token={0}&noflash=yes"
+_stream_player_url = "http://player.mediaklikk.hu/player/player-inside-full3.php?userid=mtva&streamid={0}&flashmajor=21&flashminor=0"
 
 
 class Mediaklikk(Plugin):
@@ -20,61 +17,24 @@ class Mediaklikk(Plugin):
     def can_handle_url(cls, url):
         return _stream_url_re.match(url)
 
-    @classmethod
-    def stream_weight(cls, key):
-        try:
-            return int(key), "mediaklikk"
-        except:
-            return Plugin.stream_weight(key)
-
-    def _get_smil_url(self, regex, player_url):
-        # get the id/token
+    def _get_playlist_url(self):
+        # get the id
         content = http.get(self.url)
-        match = regex.match(content.text.replace("\n", ""))
+        match = _id_re.match(content.text.replace("\n", ""))
         if not match:
             return
 
-        # get the url Manifest
-        # note: there is load balancing here, we get a different url each
-        #       time (thats why we can't simply query by the stream id later)
-        content = http.get(player_url.format(match.group(1)))
-        match = _smil_re.match(content.text.replace("\n", ""))
-        if not match:
-            return
+        # get the m3u8 file url
+        player_url = _stream_player_url.format(match.group(1))
+        content = http.get(player_url)
 
-        return match.group("smil_url")
-
-    def _get_hls_streams(self):
-        smil_url = self._get_smil_url(_id_re, _stream_player_url)
-        if not smil_url:
-            return
-
-        res = http.get(smil_url)
-        xml = http.xml(res)
-        quality_levels = xml.find('StreamIndex[@Type="video"]').findall("QualityLevel")
-
-        stream_url = smil_url[:smil_url.find("Manifest")] + "/chunklist_b{0}.m3u8"
-        streams = {}
-        for level in quality_levels:
-            bitrate = level.get("Bitrate")
-            max_height = level.get("MaxHeight")
-            streams[max_height] = HLSStream(self.session, stream_url.format(bitrate))
-
-        return streams
-
-    def _get_variant_playlist_streams(self):
-        smil_url = self._get_smil_url(_token_re, _video_player_url)
-        if not smil_url:
-            return
-
-        stream_url = smil_url[:smil_url.find("Manifest")] + "/chunklist.m3u8"
-        return HLSStream.parse_variant_playlist(self.session, stream_url)
+        match = _file_re.match(content.text.replace("\n", ""))
+        if match:
+            return match.group(1)
 
     def _get_streams(self):
-        if _video_url_re.match(self.url):
-            return self._get_variant_playlist_streams()
-        else:
-            return self._get_hls_streams()
+        playlist = self._get_playlist_url()
+        return HLSStream.parse_variant_playlist(self.session, playlist)
 
 
 __plugin__ = Mediaklikk
