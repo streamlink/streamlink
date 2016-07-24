885ef2572ba4a1c7914b52722c7e0c23b81c83fc
steven7851 <steven7851@msn.com>
2016-07-25 00:05:46 +0800

    update douyu
    use new api

--- src/livestreamer/plugins/douyutv.py
+++ src/livestreamer/plugins/douyutv.py
@@ -1,23 +1,23 @@
 import hashlib
 import re
 import time
+import random
+import string
 
 from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http, validate
-from livestreamer.stream import (
-    HTTPStream, HLSStream
-)
+from livestreamer.stream import HTTPStream
 
-API_URL = "http://www.douyutv.com/api/v1/room/{0}?aid=android&client_sys=android&time={1}&auth={2}"
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36"
+SWFAPI_URL = "http://www.douyu.com/swf_api/room/{0}?cdn=&nofan=yes&_t={1}&sign={2}"
+LAPI_URL = "http://www.douyu.com/lapi/live/getPlay/{0}"
+SWFAPI_SECRET = "bLFlashflowlad92"
+LAPI_SECRET = "A12Svb&%1UUmf@hC"
 SHOW_STATUS_ONLINE = 1
 SHOW_STATUS_OFFLINE = 2
-STREAM_WEIGHTS = {
-    "middle": 540,
-    "source": 1080
-}
 
 _url_re = re.compile("""
-    http(s)?://(www\.)?douyutv.com
+    http(s)?://(www\.)?douyu.com
     /(?P<channel>[^/]+)
 """, re.VERBOSE)
 
@@ -27,16 +27,17 @@ _room_schema = validate.Schema(
             "show_status": validate.all(
                 validate.text,
                 validate.transform(int)
-            ),
-            "rtmp_url": validate.text,
-            "rtmp_live": validate.text,
-            "hls_url": validate.text,
-            "rtmp_multi_bitrate": validate.all(
-                validate.any([], {
-                    validate.text: validate.text
-                }),
-                validate.transform(dict)
             )
+        })
+    },
+    validate.get("data")
+)
+
+_lapi_schema = validate.Schema(
+    {
+        "data": validate.any(None, {
+            "rtmp_url": validate.text,
+            "rtmp_live": validate.text
         })
     },
     validate.get("data")
@@ -48,21 +49,17 @@ class Douyutv(Plugin):
     def can_handle_url(self, url):
         return _url_re.match(url)
 
-    @classmethod
-    def stream_weight(cls, stream):
-        if stream in STREAM_WEIGHTS:
-            return STREAM_WEIGHTS[stream], "douyutv"
-
-        return Plugin.stream_weight(stream)
-
     def _get_streams(self):
         match = _url_re.match(self.url)
         channel = match.group("channel")
 
-        ts = int(time.time())
-        sign = hashlib.md5(("room/{0}?aid=android&client_sys=android&time={1}".format(channel, ts) + "1231").encode("ascii")).hexdigest()
+        ts = int(time.time() / 60)
+        did = ''.join([random.choice(string.ascii_uppercase + string.digits) for n in xrange(32)])
+        swf_sign = hashlib.md5(("{0}{1}{2}".format(channel, SWFAPI_SECRET, ts)).encode("utf-8")).hexdigest()
+        l_sign = hashlib.md5(("{0}{1}{2}{3}".format(channel, did, LAPI_SECRET, ts)).encode("utf-8")).hexdigest()
 
-        res = http.get(API_URL.format(channel, ts, sign))
+        http.headers.update({"User-Agent": USER_AGENT})
+        res = http.get(SWFAPI_URL.format(channel, ts, swf_sign))
         room = http.json(res, schema=_room_schema)
         if not room:
             return
@@ -70,17 +67,19 @@ class Douyutv(Plugin):
         if room["show_status"] != SHOW_STATUS_ONLINE:
             return
 
-        hls_url = "{room[hls_url]}?wsiphost=local".format(room=room)
-        hls_stream = HLSStream(self.session, hls_url)
-        yield "hls", hls_stream
+        data = {
+            "cdn": "tct",
+            "rate": "0",
+            "tt": ts,
+            "did": did,
+            "sign": l_sign
+        }
+
+        res = http.post(LAPI_URL.format(channel), data=data)
+        room = http.json(res, schema=_lapi_schema)
 
         url = "{room[rtmp_url]}/{room[rtmp_live]}".format(room=room)
         stream = HTTPStream(self.session, url)
         yield "source", stream
-
-        for name, url in room["rtmp_multi_bitrate"].items():
-            url = "{room[rtmp_url]}/{url}".format(room=room, url=url)
-            stream = HTTPStream(self.session, url)
-            yield name, stream
 
 __plugin__ = Douyutv
