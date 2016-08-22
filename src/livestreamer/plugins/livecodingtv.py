b10babba649c788f16a5bb26ef1ab1a1c11f21c6
int3l <int3l@users.noreply.github.com>
2016-08-23 01:17:54 +0300

    Refactoring and update for the VOD support

--- src/livestreamer/plugins/livecodingtv.py
+++ src/livestreamer/plugins/livecodingtv.py
@@ -1,12 +1,12 @@
 import re
-
 from livestreamer.plugin import Plugin
-from livestreamer.stream import RTMPStream
+from livestreamer.stream import RTMPStream, HTTPStream
 from livestreamer.plugin.api import http
 
 
+_vod_re = re.compile('\"(http(s)?://.*\.mp4\?t=.*)\"')
 _rtmp_re = re.compile('rtmp://[^"]+/(?P<channel>\w+)+[^/"]+')
-_url_re = re.compile("http(s)?://(?:\w+.)?\livecoding\.tv")
+_url_re = re.compile('http(s)?://(?:\w+.)?\livecoding\.tv')
 
 
 class LivecodingTV(Plugin):
@@ -16,19 +16,18 @@ class LivecodingTV(Plugin):
 
     def _get_streams(self):
         res = http.get(self.url)
-        match = _rtmp_re.search(res.text)
-        rtmp_url = match.group(0)
-
-        if not match:
+        match = _rtmp_re.search(res.content)
+        if match:
+            params = {
+                "rtmp": match.group(0),
+                "pageUrl": self.url,
+                "live": True,
+            }
+            yield 'live', RTMPStream(self.session, params)
             return
 
-        stream = RTMPStream(self.session, {
-            "rtmp": rtmp_url,
-            "pageUrl": self.url,
-            "live": True,
-        })
-
-        return dict(live=stream)
-
+        match = _vod_re.search(res.content)
+        if match:
+            yield 'vod', HTTPStream(self.session, match.group(1))
 
 __plugin__ = LivecodingTV
