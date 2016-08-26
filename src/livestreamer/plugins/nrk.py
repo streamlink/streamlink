6edee2f0f634d4bede24ec14b4733cd16c954708
Jon Bergli Heier <snakebite@jvnv.net>
2016-08-26 20:44:54 +0200

    plugins.nrk: Fixed _id_re regex not matching series URLs.
    Also removed unused _program_id regex.

--- src/livestreamer/plugins/nrk.py
+++ src/livestreamer/plugins/nrk.py
@@ -11,10 +11,9 @@ COOKIE_PARAMS = (
     "preferred-player-live=hlslink"
 )
 
-_id_re = re.compile("/(?:program|direkte)/([^/]+)")
+_id_re = re.compile("/(?:program|direkte|serie/[^/]+)/([^/]+)")
 _url_re = re.compile("https?://(tv|radio).nrk.no/")
 _api_baseurl_re = re.compile('apiBaseUrl:\s*"(?P<baseurl>[^"]+)"')
-_program_id = re.compile('programId:\s*"(?P<programid>[^"]+)"')
 
 _schema = validate.Schema(
     validate.transform(_api_baseurl_re.search),
