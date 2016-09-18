65428583f066c887d99f885a4fc516f6a5f83f17
Kari Hänninen <lonefox@kapsi.fi>
2016-09-18 10:05:17 +0300

    Remove spurious print statement that made the plugin incompatible with python 3.

--- src/livestreamer/plugins/rtlxl.py
+++ src/livestreamer/plugins/rtlxl.py
@@ -10,14 +10,13 @@ class rtlxl(Plugin):
     @classmethod
     def can_handle_url(cls, url):
         return _url_re.match(url)
-			
+
     def _get_streams(self):
         match = _url_re.match(self.url)
         uuid = match.group("uuid")
         html = http.get('http://www.rtl.nl/system/s4m/vfd/version=2/uuid={}/d=pc/fmt=adaptive/'.format(uuid)).text
-        
+
         playlist_url = "http://manifest.us.rtl.nl" + re.compile('videopath":"(?P<playlist_url>.*?)",', re.IGNORECASE).search(html).group("playlist_url")
-        print playlist_url
         return HLSStream.parse_variant_playlist(self.session, playlist_url)
 
 __plugin__ = rtlxl
