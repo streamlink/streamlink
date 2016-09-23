be6a65c1355bb6a7154e0cdefdb377273842ec15
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2016-09-23 19:56:38 -0400

    prevent the build from hanging

--- script/pushdocs.sh
+++ script/pushdocs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" != "master" ] || [ "$BUILD_DOCS" != "yes" ] ; then
+if [ "$TRAVIS_BRANCH" != "master" ] || [ "$BUILD_DOCS" != "yes" ] || [ "$TRAVIS_SECURE_ENV_VARS" == "false" ] ; then
     exit 0
 fi
 
