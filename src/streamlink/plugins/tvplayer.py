27905cb194e53d98762bac79e0b51517a8971c1a
beardypig <beardypig@users.noreply.github.com>
2016-10-06 13:34:17 +0100

    TVPlayer plugin supports the latest version of the website

--- src/streamlink/plugins/tvplayer.py
+++ src/streamlink/plugins/tvplayer.py
@@ -5,39 +5,43 @@ from streamlink.plugin import Plugin
 from streamlink.plugin.api import http, validate
 from streamlink.stream import HLSStream
 
-USER_AGENT_STRING = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) "
-                     "AppleWebKit/537.36 (KHTML, like Gecko) "
-                     "Chrome/43.0.2357.65 Safari/537.36")
-STREAM_INFO_URL = "http://lapi.cdn.tvplayer.com/tvplayer/stream/live/id/{id}"
-_url_re = re.compile(r"http://(?:www.)?tvplayer.com/watch/(.+)")
-_channel_map_re = re.compile(r'href="/watch/([a-z]+?)".*?img.*?src=".*?/(\d+).png"', re.S)
-_channel_schema = validate.Schema({
-    "stream": validate.url(scheme=validate.any("http"))
-})
-
 
 class TVPlayer(Plugin):
+    _user_agent = (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/43.0.2357.65 Safari/537.36")
+    API_URL = "http://api.tvplayer.com/api/v2/stream/live"
+    _url_re = re.compile(r"http://(?:www.)?tvplayer.com/watch/(.+)")
+    _stream_attrs_ = re.compile(r'var\s+(validate|platform|resourceId)\s+=\s*(.*?);', re.S)
+    _stream_schema = validate.Schema({
+        "tvplayer": validate.Schema({
+            "status": u'200 OK',
+            "response": validate.Schema({
+                "stream": validate.url(scheme=validate.any("http"))
+            })
+        })
+    })
+
     @classmethod
     def can_handle_url(cls, url):
-        match = _url_re.match(url)
+        match = TVPlayer._url_re.match(url)
         return match
 
     def _get_streams(self):
-        url_match = _url_re.match(self.url)
-        if url_match:
-            # find the list of channels from the html in the page
-            res = http.get(self.url)
-            channel_map = dict(_channel_map_re.findall(res.text))
-            channel_id = channel_map.get(url_match.group(1))
+        # find the list of channels from the html in the page
+        res = http.get(self.url)
+        stream_attrs = dict((k, v.strip('"')) for k, v in TVPlayer._stream_attrs_.findall(res.text))
 
-            # get the stream urls
-            res = http.get(STREAM_INFO_URL.format(id=channel_id))
-            stream_data = http.json(res, schema=_channel_schema)
+        # get the stream urls
+        res = http.post(TVPlayer.API_URL, data=dict(id=stream_attrs["resourceId"],
+                                                    validate=stream_attrs["validate"],
+                                                    platform=stream_attrs["platform"]))
 
-            return HLSStream.parse_variant_playlist(self.session,
-                                                    stream_data['stream'],
-                                                    headers={'user-agent': USER_AGENT_STRING})
+        stream_data = http.json(res, schema=TVPlayer._stream_schema)
+
+        return HLSStream.parse_variant_playlist(self.session,
+                                                stream_data["tvplayer"]["response"]["stream"],
+                                                headers={'user-agent': TVPlayer._user_agent})
 
 
 __plugin__ = TVPlayer
-
