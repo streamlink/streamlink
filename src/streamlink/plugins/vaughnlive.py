25706a6cf939807439948ba418f25ac566217c5d
mmetak <mmetak@users.noreply.github.com>
2016-11-09 01:06:12 +0100

    fix vaughnlive.tv info_url (yet again...) (#143)
    * fix vaughnlive.tv info_url
    
    * fix fix vaughnlive.tv info_url  (again)

--- src/streamlink/plugins/vaughnlive.py
+++ src/streamlink/plugins/vaughnlive.py
@@ -6,7 +6,7 @@ from streamlink.plugin.api import http, validate
 from streamlink.stream import RTMPStream
 
 PLAYER_VERSION = "0.1.1.765"
-INFO_URL = "http://mvn.vaughnsoft.net/video/edge/mzn-{domain}_{channel}?{version}_{ms}-{ms}-{random}"
+INFO_URL = "http://mvn.vaughnsoft.net/video/edge/mnt-{domain}_{channel}?{version}_{ms}-{ms}-{random}"
 
 DOMAIN_MAP = {
     "breakers": "btv",
