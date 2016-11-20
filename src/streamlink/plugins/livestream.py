2b32349dc75a2c9a5ee7c206ef00f0bc3f555133
Brainzyy <Brainzyy@users.noreply.github.com>
2016-11-20 23:40:57 +0100

    fix livestream thanks to @intact

--- src/streamlink/plugins/livestream.py
+++ src/streamlink/plugins/livestream.py
@@ -22,7 +22,10 @@ _stream_config_schema = validate.Schema({
             ),
         }, None)
     },
-    validate.optional("playerUri"): validate.text
+    validate.optional("playerUri"): validate.text,
+    validate.optional("viewerPlusSwfUrl"): validate.url(scheme="http"),
+    validate.optional("lsPlayerSwfUrl"): validate.text,
+    validate.optional("hdPlayerSwfUrl"): validate.text
 })
 _smil_schema = validate.Schema(validate.union({
     "http_base": validate.all(
@@ -93,7 +96,7 @@ class Livestream(Plugin):
 
         play_url = stream_info.get("play_url")
         if play_url:
-            swf_url = info.get("playerUri")
+            swf_url = info.get("playerUri") or info.get("hdPlayerSwfUrl") or info.get("lsPlayerSwfUrl") or info.get("viewerPlusSwfUrl")
             if swf_url:
                 if not swf_url.startswith("http"):
                     swf_url = "http://" + swf_url
@@ -103,7 +106,7 @@ class Livestream(Plugin):
 
             qualities = stream_info["qualities"]
             for bitrate, stream in self._parse_smil(play_url, swf_url):
-                name = "{0:d}k".format(int(bitrate / 1000))
+                name = "{0}k".format(bitrate / 1000)
                 for quality in qualities:
                     if quality["bitrate"] == bitrate:
                         name = "{0}p".format(quality["height"])
