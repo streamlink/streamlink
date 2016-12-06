31e793ed25a7f07a7adb272f41b4310418c104f0
beardypig <beardypig@users.noreply.github.com>
2016-12-06 13:15:50 +0000

    add support for reading files from stdin

--- src/streamlink/packages/requests_file.py
+++ src/streamlink/packages/requests_file.py
@@ -14,6 +14,8 @@ Copyright 2015 Red Hat, Inc.
    limitations under the License.
 """
 from io import BytesIO
+
+import sys
 from requests.adapters import BaseAdapter
 from requests.compat import urlparse, unquote, urljoin
 from requests import Response, codes
@@ -41,7 +43,7 @@ class FileAdapter(BaseAdapter):
         url_parts = urlparse(request.url)
 
         # Reject URLs with a hostname component
-        if url_parts.netloc and not url_parts.netloc in ("localhost", ".", ".."):
+        if url_parts.netloc and not url_parts.netloc in ("localhost", ".", "..", "-"):
             raise ValueError("file: URLs with hostname components are not permitted")
 
         # If the path is relative update it to be absolute
@@ -56,50 +58,56 @@ class FileAdapter(BaseAdapter):
         # Use urllib's unquote to translate percent escapes into whatever
         # they actually need to be
         try:
-            # Split the path on / (the URL directory separator) and decode any
-            # % escapes in the parts
-            path_parts = [unquote(p) for p in url_parts.path.split('/')]
+            # If the netloc is - then read from stdin
+            if url_parts.netloc == "-":
+                resp.raw = sys.stdin
+                # make a fake response URL, the current directory
+                resp.url = "file://" + os.path.abspath(".").replace(os.sep, "/") + "/"
+            else:
+                # Split the path on / (the URL directory separator) and decode any
+                # % escapes in the parts
+                path_parts = [unquote(p) for p in url_parts.path.split('/')]
 
-            # Strip out the leading empty parts created from the leading /'s
-            while path_parts and not path_parts[0]:
-                path_parts.pop(0)
-
-            # If os.sep is in any of the parts, someone fed us some shenanigans.
-            # Treat is like a missing file.
-            if any(os.sep in p for p in path_parts):
-                raise IOError(errno.ENOENT, os.strerror(errno.ENOENT))
-
-            # Look for a drive component. If one is present, store it separately
-            # so that a directory separator can correctly be added to the real
-            # path, and remove any empty path parts between the drive and the path.
-            # Assume that a part ending with : or | (legacy) is a drive.
-            if path_parts and (path_parts[0].endswith('|') or
-                               path_parts[0].endswith(':')):
-                path_drive = path_parts.pop(0)
-                if path_drive.endswith('|'):
-                    path_drive = path_drive[:-1] + ':'
-
+                # Strip out the leading empty parts created from the leading /'s
                 while path_parts and not path_parts[0]:
                     path_parts.pop(0)
-            else:
-                path_drive = ''
 
-            # Try to put the path back together
-            # Join the drive back in, and stick os.sep in front of the path to
-            # make it absolute.
-            path = path_drive + os.sep + os.path.join(*path_parts)
+                # If os.sep is in any of the parts, someone fed us some shenanigans.
+                # Treat is like a missing file.
+                if any(os.sep in p for p in path_parts):
+                    raise IOError(errno.ENOENT, os.strerror(errno.ENOENT))
 
-            # Check if the drive assumptions above were correct. If path_drive
-            # is set, and os.path.splitdrive does not return a drive, it wasn't
-            # reall a drive. Put the path together again treating path_drive
-            # as a normal path component.
-            if path_drive and not os.path.splitdrive(path):
-                path = os.sep + os.path.join(path_drive, *path_parts)
+                # Look for a drive component. If one is present, store it separately
+                # so that a directory separator can correctly be added to the real
+                # path, and remove any empty path parts between the drive and the path.
+                # Assume that a part ending with : or | (legacy) is a drive.
+                if path_parts and (path_parts[0].endswith('|') or
+                                   path_parts[0].endswith(':')):
+                    path_drive = path_parts.pop(0)
+                    if path_drive.endswith('|'):
+                        path_drive = path_drive[:-1] + ':'
 
-            # Use io.open since we need to add a release_conn method, and
-            # methods can't be added to file objects in python 2.
-            resp.raw = io.open(path, "rb")
-            resp.raw.release_conn = resp.raw.close
+                    while path_parts and not path_parts[0]:
+                        path_parts.pop(0)
+                else:
+                    path_drive = ''
+
+                # Try to put the path back together
+                # Join the drive back in, and stick os.sep in front of the path to
+                # make it absolute.
+                path = path_drive + os.sep + os.path.join(*path_parts)
+
+                # Check if the drive assumptions above were correct. If path_drive
+                # is set, and os.path.splitdrive does not return a drive, it wasn't
+                # reall a drive. Put the path together again treating path_drive
+                # as a normal path component.
+                if path_drive and not os.path.splitdrive(path):
+                    path = os.sep + os.path.join(path_drive, *path_parts)
+
+                # Use io.open since we need to add a release_conn method, and
+                # methods can't be added to file objects in python 2.
+                resp.raw = io.open(path, "rb")
+                resp.raw.release_conn = resp.raw.close
         except IOError as e:
             if e.errno == errno.EACCES:
                 resp.status_code = codes.forbidden
