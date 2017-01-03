6af70d5a2de72ddcd0dc481b78f45a751d5b7c12
beardypig <beardypig@users.noreply.github.com>
2017-01-03 17:44:14 +0000

    plugins.kanal7: fixed page change in kanal7 live stream (#348)

--- src/streamlink/plugins/kanal7.py
+++ src/streamlink/plugins/kanal7.py
@@ -9,7 +9,8 @@ from streamlink.stream import HLSStream
 
 class Kanal7(Plugin):
     url_re = re.compile(r"https?://(?:www.)?kanal7.com/canli-izle")
-    data_re = re.compile(r'''videoPlayer.setup\(\{\s*file:\s*"(http[^"]*?)"''', re.DOTALL)
+    iframe_re = re.compile(r'iframe .*?src="(http://[^"]*?)"')
+    stream_re = re.compile(r'source .*?src=*"(http[^"]*?)"')
 
     @classmethod
     def can_handle_url(cls, url):
@@ -17,11 +18,19 @@ class Kanal7(Plugin):
 
     def _get_streams(self):
         res = http.get(self.url)
-        data_m = self.data_re.search(res.text)
-        if data_m:
-            stream_url = data_m.group(1)
-            self.logger.debug("Found stream: {}", stream_url)
-            yield "live", HLSStream(self.session, stream_url)
+        # find iframe url
+        # open iframe page and extract m3u8 url
+        iframe = self.iframe_re.search(res.text)
+        iframe_url = iframe and iframe.group(1)
+        if iframe_url:
+            self.logger.debug("Found iframe: {}", iframe_url)
+            ires = http.get(iframe_url)
+            stream_m = self.stream_re.search(ires.text)
+            stream_url = stream_m and stream_m.group(1)
+            if stream_url:
+                yield "live", HLSStream(self.session, stream_url)
+        else:
+            self.logger.error("Could not find iframe, has the page layout changed?")
 
 
 __plugin__ = Kanal7
