1bf2c82da55d3dcca8d1a9e238225ab35b7824c6
beardypig <beardypig@users.noreply.github.com>
2017-01-12 12:18:28 +0000

    plugins.adultswim: add support for VOD

--- tests/test_plugin_adultswim.py
+++ tests/test_plugin_adultswim.py
@@ -9,8 +9,8 @@ class TestPluginAdultSwim(unittest.TestCase):
         self.assertTrue(AdultSwim.can_handle_url("http://www.adultswim.com/videos/streams/toonami"))
         self.assertTrue(AdultSwim.can_handle_url("http://www.adultswim.com/videos/streams/"))
         self.assertTrue(AdultSwim.can_handle_url("http://www.adultswim.com/videos/streams/last-stream-on-the-left"))
+        self.assertTrue(AdultSwim.can_handle_url("http://www.adultswim.com/videos/specials/the-adult-swim-golf-classic-extended/"))
 
         # shouldn't match
-        self.assertFalse(AdultSwim.can_handle_url("http://www.adultswim.com/videos/specials/the-adult-swim-golf-classic-extended/"))
         self.assertFalse(AdultSwim.can_handle_url("http://www.tvcatchup.com/"))
         self.assertFalse(AdultSwim.can_handle_url("http://www.youtube.com/"))
