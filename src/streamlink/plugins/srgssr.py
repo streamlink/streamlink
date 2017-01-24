5fafd4fcfff2275ebfe24cf119699be1d73a88d4
beardypig <beardypig@users.noreply.github.com>
2017-01-24 12:34:01 +0000

    plugins.srgssr: fix playlist reload auth issue

--- src/streamlink/plugins/srgssr.py
+++ src/streamlink/plugins/srgssr.py
@@ -2,7 +2,8 @@ from __future__ import print_function
 
 import re
 
-from streamlink.compat import urlparse, parse_qsl
+from streamlink import PluginError
+from streamlink.compat import urlparse, parse_qsl, urlencode, urlunparse
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import http
 from streamlink.plugin.api import validate
@@ -66,13 +67,16 @@ class SRGSSR(Plugin):
 
         return site, video_id
 
-    def get_authparams(self, url):
+    def auth_url(self, url):
         parsed = urlparse(url)
         path, _ = parsed.path.rsplit("/", 1)
         token_res = http.get(self.token_url, params=dict(acl=path + "/*"))
         authparams = http.json(token_res, schema=self.token_schema)
-        self.logger.debug("Found authparams: {0}", authparams)
-        return dict(parse_qsl(authparams))
+
+        existing = dict(parse_qsl(parsed.query))
+        existing.update(dict(parse_qsl(authparams)))
+
+        return urlunparse(parsed._replace(query=urlencode(existing)))
 
     def _get_streams(self):
         site, video_id = self.get_video_id()
@@ -80,13 +84,15 @@ class SRGSSR(Plugin):
         if video_id and site:
             self.logger.debug("Found {0} video ID {1}", site, video_id)
 
-            res = http.get(self.api_url.format(site=site, id=video_id))
+            try:
+                res = http.get(self.api_url.format(site=site, id=video_id))
+            except PluginError:
+                return
 
             for stream_info in http.json(res, schema=self.api_schema):
                 for url in stream_info["url"]:
                     if stream_info["@protocol"] == "HTTP-HLS":
-                        params = self.get_authparams(url["text"])
-                        for s in HLSStream.parse_variant_playlist(self.session, url["text"], params=params).items():
+                        for s in HLSStream.parse_variant_playlist(self.session, self.auth_url(url["text"])).items():
                             yield s
 
 
