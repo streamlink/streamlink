4bd06ff11b403a35c73fdf2503a266f517d5c5e7
fozzy <fozzy@fozzy.co>
2017-01-28 04:52:32 +0800

    use PLU new API

--- src/streamlink/plugins/tga.py
+++ src/streamlink/plugins/tga.py
@@ -1,6 +1,7 @@
 #coding: utf-8
 
 import re
+import json
 
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import http, validate
@@ -9,7 +10,7 @@ from streamlink.stream import HLSStream, HTTPStream, RTMPStream
 
 CHANNEL_INFO_URL = "http://api.plu.cn/tga/streams/%s"
 QQ_STREAM_INFO_URL = "http://info.zb.qq.com/?cnlid=%d&cmd=2&stream=%d&system=1&sdtfrom=113"
-PLU_STREAM_INFO_URL = "http://star.api.plu.cn/live/GetLiveUrl?roomId=%d"
+PLU_STREAM_INFO_URL = "http://livestream.plu.cn/live/getlivePlayurl?roomId=%d"
 
 _quality_re = re.compile(r"\d+x(\d+)$")
 _url_re = re.compile(r"http://star\.longzhu\.(?:tv|com)/(m\/)?(?P<domain>[a-z0-9]+)")
@@ -24,16 +25,6 @@ _channel_schema = validate.Schema(
         })
     },
     validate.get("data")
-)
-
-_plu_schema = validate.Schema(
-    {
-        "urls": [{
-            "securityUrl": validate.url(scheme=validate.any("rtmp", "http")),
-            "resolution": validate.text,
-            "ext": validate.text
-        }]
-    }
 )
 
 _qq_schema = validate.Schema(
@@ -87,8 +78,8 @@ class Tga(Plugin):
 
     def _get_plu_streams(self, cid):
         res = http.get(PLU_STREAM_INFO_URL % cid)
-        info = http.json(res, schema=_plu_schema)
-        for source in info["urls"]:
+        info = json.loads(res.content)
+        for source in info["playLines"][0]["urls"]:
             quality = self._get_quality(source["resolution"])
             if source["ext"] == "m3u8":
                 yield quality, HLSStream(self.session, source["securityUrl"])
