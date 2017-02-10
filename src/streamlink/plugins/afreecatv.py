faab17b17ed7f77600eb75ab847d623fab9ddd4c
406NotAcceptable <406NotAcceptable@somewhere>
2017-02-11 01:59:44 +0800

    plugins.afreecatv: API changes

--- src/streamlink/plugins/afreecatv.py
+++ src/streamlink/plugins/afreecatv.py
@@ -6,27 +6,10 @@ from streamlink.plugin.api.utils import parse_query
 from streamlink.stream import RTMPStream
 
 
-VIEW_LIVE_API_URL = "http://api.afreeca.tv/live/view_live.php"
-VIEW_LIVE_API_URL_TW = "http://api.afreecatv.com.tw/live/view_live.php"
-VIEW_LIVE_API_URL_JP = "http://api.afreecatv.jp/live/view_live.php"
+VIEW_LIVE_API_URL = "http://api.{region}/live/view_live.php"
 
-_url_re = re.compile(r"http(s)?://(\w+\.)?(afreecatv.com.tw|afreeca.tv|afreecatv.jp)/(?P<channel>[\w\-_]+)")
-_url_re_tw = re.compile(r"http(s)?://(\w+\.)?(afreecatv.com.tw)/(?P<channel>[\w\-_]+)")
-_url_re_jp = re.compile(r"http(s)?://(\w+\.)?(afreecatv.jp)/(?P<channel>[\w\-_]+)")
-_flashvars_re = re.compile(r'<param name="flashvars" value="([^"]+)" />')
+_url_re = re.compile(r"https?://(\w+\.)?(?P<region>afreecatv\.com\.tw|afreeca\.tv|afreecatv\.jp)/(?P<channel>[\w\-_]+)")
 
-_flashvars_schema = validate.Schema(
-    validate.transform(_flashvars_re.findall),
-    validate.get(0),
-    validate.transform(parse_query),
-    validate.any(
-        {
-            "s": validate.text,
-            "id": validate.text
-        },
-        {}
-    )
-)
 _view_live_schema = validate.Schema(
     {
         "channel": {
@@ -47,26 +30,14 @@ class AfreecaTV(Plugin):
         return _url_re.match(url)
 
     def _get_streams(self):
-        flashvars = http.get(self.url, schema=_flashvars_schema)
-        if not flashvars:
-            return
+        match = _url_re.match(self.url)
 
         params = {
-            "rt": "json",
-            "lc": "en_US",
             "pt": "view",
-            "bpw": "",
-            "bid": flashvars["id"],
-            "adok": "",
-            "bno": ""
+            "bid": match.group("channel")
         }
 
-        if re.search(_url_re_tw, self.url):
-            res = http.get(VIEW_LIVE_API_URL_TW, params=params)
-        elif re.search(_url_re_jp, self.url):
-            res = http.get(VIEW_LIVE_API_URL_JP, params=params)
-        else:
-            res = http.get(VIEW_LIVE_API_URL, params=params)
+        res = http.get(VIEW_LIVE_API_URL.format(region=match.group("region")), params=params)
 
         streams = http.json(res, schema=_view_live_schema)
 
