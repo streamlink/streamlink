577eecfc4f46cde6572ffaac67c68d0ebcd00745
wlerin <wlerin@gmail.com>
2017-02-27 18:39:54 -0800

    Add tests for Showroom.stream_weight

--- tests/test_plugin_showroom.py
+++ tests/test_plugin_showroom.py
@@ -10,7 +10,6 @@ _should_match = (
     "https://www.showroom-live.com/157941217780",
     "https://www.showroom-live.com/madokacom"
 )
-
 _should_not_match = (
     "https://www.showroom-live.com/mypage",
     "https://www.showroom-live.com/ranking",
@@ -20,6 +19,11 @@ _should_not_match = (
     "http://www.dailymotion.com/video/x5cyk8f",
     "http://www.crunchyroll.com/gintama"
 )
+_stream_weights = {
+    'original': (720, "quality"),
+    'other': (360, "quality"),
+    'low': (160, "quality")
+}
 
 
 class TestPluginShowroom(unittest.TestCase):
@@ -30,3 +34,6 @@ class TestPluginShowroom(unittest.TestCase):
         for url in _should_not_match:
             self.assertFalse(Showroom.can_handle_url(url))
 
+    def test_stream_weight(self):
+        for name, weight in _stream_weights.items():
+            self.assertEqual(Showroom.stream_weight(name), weight)
