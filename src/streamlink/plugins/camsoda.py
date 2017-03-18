a6192e3000104165e8a494d5fdc67642fc5c2c5a
back-to <back-to@users.noreply.github.com>
2017-03-18 13:37:28 +0000

    plugins.camsoda - Added tests and small update for the plugin

--- src/streamlink/plugins/camsoda.py
+++ src/streamlink/plugins/camsoda.py
@@ -2,23 +2,16 @@ import random
 import re
 
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import http, validate
+from streamlink.plugin.api import http
+from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream
 
-API_USER_URL = "https://www.camsoda.com/api/v1/user/{0}"
-API_VIDEO_URL = "https://www.camsoda.com/api/v1/video/vtoken/{0}?username=guest_{1}"
-
-HLS_VIDEO_URL = "https://{server}/{app}/mp4:{stream_name}_mjpeg/playlist.m3u8?token={token}"
-
-_url_re = re.compile(r"http(s)?://(www\.)?camsoda\.com/(?P<username>.+)")
+_url_re = re.compile(r"http(s)?://(www\.)?camsoda\.com/(?P<username>[^\"\']+)")
 
 _api_user_schema = validate.Schema(
     {
         "status": validate.any(int, validate.text),
         validate.optional("user"): {
-            "settings": {
-                "cam_password": validate.any(int, validate.text),
-            },
             "online": validate.any(int, validate.text),
             "chatstatus": validate.text,
         }
@@ -38,39 +31,40 @@ _api_video_schema = validate.Schema(
 
 
 class Camsoda(Plugin):
+    API_URL_USER = "https://www.camsoda.com/api/v1/user/{0}"
+    API_URL_VIDEO = "https://www.camsoda.com/api/v1/video/vtoken/{0}?username=guest_{1}"
+    HLS_URL_VIDEO = "https://{server}/{app}/mp4:{stream_name}_mjpeg/playlist.m3u8?token={token}"
+
     @classmethod
     def can_handle_url(cls, url):
         return _url_re.match(url)
 
-    def _get_streams(self):
-        match = _url_re.match(self.url)
-        username = match.group("username")
-        username = username.replace("/", "")
-
-        res = http.get(API_USER_URL.format(username))
-        data_user = http.json(res, schema=_api_user_schema)
-
-        found_username = data_user["status"] is True
-        if found_username is False:
+    def _stream_status(self, data_user):
+        invalid_username = data_user["status"] is False
+        if invalid_username:
             self.logger.info("No validate username found for {0}".format(self.url))
             return
 
-        is_online = data_user["user"]["online"] is True
-        if is_online is False:
+        is_offline = data_user["user"]["online"] is False
+        if is_offline:
             self.logger.info("This stream is currently offline")
             return
 
-        is_protected = data_user["user"]["settings"]["cam_password"] is True
-        if is_protected:
-            self.logger.info("Stream is protected with a password")
-            return
+        return True
 
-        res = http.get(API_VIDEO_URL.format(username, str(random.randint(1000, 99999))))
+    def _get_api_user(self, username):
+        res = http.get(self.API_URL_USER.format(username))
+        data_user = http.json(res, schema=_api_user_schema)
+        return data_user
+
+    def _get_api_video(self, username):
+        res = http.get(self.API_URL_VIDEO.format(username, str(random.randint(1000, 99999))))
         data_video = http.json(res, schema=_api_video_schema)
+        return data_video
 
+    def _get_hls_url(self, data_user, data_video):
         is_edge = data_user["user"]["chatstatus"] == "online"
         is_priv = data_user["user"]["chatstatus"] == "private"
-
         if is_edge:
             server = data_video["edge_servers"][0]
         elif is_priv:
@@ -78,12 +72,25 @@ class Camsoda(Plugin):
         else:
             server = data_video["mjpeg_server"]
 
-        hls_url = HLS_VIDEO_URL.format(server=server, app=data_video["app"], stream_name=data_video["stream_name"], token=data_video["token"])
+        hls_url = self.HLS_URL_VIDEO.format(server=server, app=data_video["app"], stream_name=data_video["stream_name"], token=data_video["token"])
+        return hls_url
 
-        try:
-            for s in HLSStream.parse_variant_playlist(self.session, hls_url).items():
-                yield s
-        except IOError as err:
-            self.logger.warning("Error parsing stream: {0}", err)
+    def _get_streams(self):
+        match = _url_re.match(self.url)
+        username = match.group("username")
+        username = username.replace("/", "")
+
+        data_user = self._get_api_user(username)
+        stream_status = self._stream_status(data_user)
+
+        if stream_status:
+            data_video = self._get_api_video(username)
+            hls_url = self._get_hls_url(data_user, data_video)
+
+            try:
+                for s in HLSStream.parse_variant_playlist(self.session, hls_url).items():
+                    yield s
+            except IOError as err:
+                self.logger.error("Error parsing stream: {0}", err)
 
 __plugin__ = Camsoda
