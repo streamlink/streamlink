71cb1b591154b1461a3a3e088ff5502659aa5dd4
beardypig <beardypig@users.noreply.github.com>
2017-03-21 17:25:48 +0000

    plugins.googledocs: updated the url regex and added a status check

--- src/streamlink/plugins/googledocs.py
+++ src/streamlink/plugins/googledocs.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import re
 
 from streamlink.compat import parse_qsl
@@ -9,7 +7,7 @@ from streamlink.stream import HTTPStream
 
 
 class GoogleDocs(Plugin):
-    url_re = re.compile(r"https://docs.google.com/file/d/(.+?)/preview")
+    url_re = re.compile(r"https://docs.google.com/file/d/([^/]+)/?")
     api_url = "https://docs.google.com/get_video_info"
 
     @classmethod
@@ -18,13 +16,17 @@ class GoogleDocs(Plugin):
 
     def _get_streams(self):
         docid = self.url_re.match(self.url).group(1)
+        self.logger.debug("Google Docs ID: {0}", docid)
         res = http.get(self.api_url, params=dict(docid=docid))
         data = dict(parse_qsl(res.text))
-        fmts = dict([s.split('/')[:2] for s in data["fmt_list"].split(",")])
-        streams = [s.split('|') for s in data["fmt_stream_map"].split(",")]
-        for qcode, url in streams:
-            _, h = fmts[qcode].split("x")
-            yield "{0}p".format(h), HTTPStream(self.session, url)
 
+        if data["status"] == "ok":
+            fmts = dict([s.split('/')[:2] for s in data["fmt_list"].split(",")])
+            streams = [s.split('|') for s in data["fmt_stream_map"].split(",")]
+            for qcode, url in streams:
+                _, h = fmts[qcode].split("x")
+                yield "{0}p".format(h), HTTPStream(self.session, url)
+        else:
+            self.logger.error("{0} (ID: {1})", data["reason"], docid)
 
 __plugin__ = GoogleDocs
