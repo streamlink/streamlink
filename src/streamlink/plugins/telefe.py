69ca75d56d4da3549554495ccb6747673d9ce2bb
RosadinTV <rosadintv@outlook.com>
2017-04-18 12:08:45 +0200

    Applied @beardypig suggestions # 2

--- src/streamlink/plugins/telefe.py
+++ src/streamlink/plugins/telefe.py
@@ -2,7 +2,8 @@ import re
 
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import http, useragents, validate
-from streamlink.stream import HLSStream
+from streamlink.stream import HLSStream, HTTPStream
+from streamlink.utils import parse_json
 
 
 class Telefe(Plugin):
@@ -10,25 +11,40 @@ class Telefe(Plugin):
 
     @classmethod
     def can_handle_url(cls, url):
-        return Telefe._url_re.match(url)
+        return cls._url_re.match(url)
 
     def _get_streams(self):
-        TELEFE_UA = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36 OPR/44.0.2510.1218"
-        res = http.get(self.url, headers={'User-Agent': TELEFE_UA})
-        busqueda_videoID = res.content.decode()
-        busqueda_videoID = busqueda_videoID[busqueda_videoID.index('"PlayerContainer","model":{"id":') + 32 :]
-        busqueda_videoID = busqueda_videoID[: busqueda_videoID.index(',')]
-        #print('ID encontrado: ' + busqueda_videoID)
+        res = http.get(self.url, headers={'User-Agent': useragents.CHROME})
+        busqueda_video = res.text
+        busqueda_video = busqueda_video[busqueda_video.index('{"top":{"view":"PlayerContainer","model":{'):]
+        busqueda_video = busqueda_video[: busqueda_video.index('}]}}') +4] + "}"
+        
+        url_video_encontrado_hls  = ""
+        url_video_encontrado_http = ""
 
-        hls_streams = HLSStream.parse_variant_playlist(
-            self.session,
-            "http://telefe.com/Api/Videos/GetSourceUrl/" + busqueda_videoID + "/0/HLS",
-            headers={'Referer': self.url,
-            'User-Agent': TELEFE_UA,
+        json_busqueda_video = parse_json(busqueda_video)
+        json_busqueda_video_sources = json_busqueda_video["top"]["model"]["videos"][0]["sources"]
+        self.logger.debug('ID encontrado: {0}', json_busqueda_video["top"]["model"]["id"])
+        for video_source_actual in json_busqueda_video_sources:
+            if "HLS" in video_source_actual["type"]:
+                url_video_encontrado_hls = "http://telefe.com" + video_source_actual["url"]
+                self.logger.debug("Contenido HLS disponible")
+            if "HTTP" in video_source_actual["type"]:
+                url_video_encontrado_http = "http://telefe.com" + video_source_actual["url"]
+                self.logger.debug("Contenido HTTP disponible")
+        
+        http.headers = {'Referer': self.url,
+            'User-Agent': useragents.CHROME,
             'X-Requested-With': 'ShockwaveFlash/25.0.0.148'}
-        )
-        for s in hls_streams.items():
-            yield s       
+            
+        if url_video_encontrado_hls:
+            hls_streams = HLSStream.parse_variant_playlist(self.session, url_video_encontrado_hls)
+            for s in hls_streams.items():
+                yield s  
+
+        if url_video_encontrado_http:
+            yield "http", HTTPStream(self.session, url_video_encontrado_http)   
+               
 
 
 __plugin__ = Telefe
