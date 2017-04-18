2d2c922e7f9cfa627c6f809f499cdb1257d518c7
RosadinTV <rosadintv@outlook.com>
2017-04-18 07:26:30 +0200

    Added test_plugin_telefe.py

---
+++ tests/test_plugin_telefe.py
@@ -0,0 +1,15 @@
+import unittest
+
+from streamlink.plugins.telefe import Telefe
+
+
+class TestPluginTelefe(unittest.TestCase):
+    def test_can_handle_url(self):
+        # should match
+        self.assertTrue(Telefe.can_handle_url("http://telefe.com/pone-a-francella/temporada-1/programa-01/"))
+        self.assertTrue(Telefe.can_handle_url("http://telefe.com/los-simuladores/temporada-1/capitulo-01/"))
+        self.assertTrue(Telefe.can_handle_url("http://telefe.com/dulce-amor/capitulos/capitulo-01/"))
+
+        # shouldn't match
+        self.assertFalse(Telefe.can_handle_url("http://local.local/"))
+        self.assertFalse(Telefe.can_handle_url("http://localhost.localhost/"))
