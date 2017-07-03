8630d10e642b62eb78a298cd28069e4b04093dcf
supergonkas <supergonkas@gmail.com>
2017-07-03 20:08:04 +0100

    Update test_plugin_rtpplay.py

--- tests/test_plugin_rtpplay.py
+++ tests/test_plugin_rtpplay.py
@@ -1,16 +1,16 @@
 import unittest
 
-from streamlink.plugins.eurocom import Eurocom
+from streamlink.plugins.rtpplay import RTPPlay
 
 
 class TestPluginEurocom(unittest.TestCase):
     def test_can_handle_url(self):
         # should match
-        self.assertTrue(Eurocom.can_handle_url("http://www.rtp.pt/play/"))
-        self.assertTrue(Eurocom.can_handle_url("https://www.rtp.pt/play/"))
+        self.assertTrue(RTPPlay.can_handle_url("http://www.rtp.pt/play/"))
+        self.assertTrue(RTPPlay.can_handle_url("https://www.rtp.pt/play/"))
 
         # shouldn't match
-        self.assertFalse(Eurocom.can_handle_url("https://www.rtp.pt/programa/"))
-        self.assertFalse(Eurocom.can_handle_url("http://www.rtp.pt/programa/"))
-        self.assertFalse(Eurocom.can_handle_url("https://media.rtp.pt/"))
-        self.assertFalse(Eurocom.can_handle_url("http://media.rtp.pt/"))
+        self.assertFalse(RTPPlay.can_handle_url("https://www.rtp.pt/programa/"))
+        self.assertFalse(RTPPlay.can_handle_url("http://www.rtp.pt/programa/"))
+        self.assertFalse(RTPPlay.can_handle_url("https://media.rtp.pt/"))
+        self.assertFalse(RTPPlay.can_handle_url("http://media.rtp.pt/"))
