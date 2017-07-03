aee1dded13635c6badc1930d35a645e11e2e6405
supergonkas <supergonkas@gmail.com>
2017-07-03 20:08:45 +0100

    Update rtpplay.py

--- src/streamlink/plugins/rtpplay.py
+++ src/streamlink/plugins/rtpplay.py
@@ -5,13 +5,13 @@ from streamlink.plugin.api import http, useragents
 from streamlink.stream import HLSStream
 
 
-class RTPPLay(Plugin):
+class RTPPlay(Plugin):
     _url_re = re.compile(r"https?:\/\/www.rtp.pt\/play\/")
     _m3u8_re = re.compile(r"file:(?:\s+)?(?:\'|\")(?P<url>[^\"']+)(?:\'|\")")
 
     @classmethod
     def can_handle_url(cls, url):
-        return RTPPLay._url_re.match(url)
+        return RTPPlay._url_re.match(url)
 
     def _get_streams(self):
         
@@ -21,7 +21,7 @@ class RTPPLay(Plugin):
 
         res = http.get(self.url, headers=headers)
 
-        url_match = RTPPLay._m3u8_re.search(res.text)
+        url_match = RTPPlay._m3u8_re.search(res.text)
         
         if url_match:
             hls_url = url_match.group("url")
@@ -37,4 +37,4 @@ class RTPPLay(Plugin):
             except IOError as err:
                 self.logger.error("Failed to extract streams: {0}", err)
 
-__plugin__ = RTPPLay
+__plugin__ = RTPPlay
