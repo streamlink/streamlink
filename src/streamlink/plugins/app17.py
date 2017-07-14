25a5c8cba3520587f4b5d22bdc6d30fcd8ce1b1f
steven7851 <steven7851@msn.com>
2017-07-15 00:01:36 +0800

    plugins.app17: new layout

--- src/streamlink/plugins/app17.py
+++ src/streamlink/plugins/app17.py
@@ -1,26 +1,14 @@
 import re
 
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import http, validate, useragents
-from streamlink.plugin.api.utils import parse_json
+from streamlink.plugin.api import http, useragents
 from streamlink.stream import HLSStream, RTMPStream, HTTPStream
 
-API_URL = "https://api-dsa.17app.co/api/v1/liveStreams/isUserOnLiveStream"
-ROOM_URL = "http://17.media/share/live/{0}"
+API_URL = "https://api-dsa.17app.co/api/v1/liveStreams/getLiveStreamInfo"
 
-_url_re = re.compile(r"http://(17app.co|17.media)/share/(?P<page>[^/]+)/(?P<channel>[^/&?]+)")
-_userid_re = re.compile(r'"userID"\s*:\s*"(.+?)"')
-_status_re = re.compile(r'"userIsOnLive"\s*:\s*([A-z]+)')
-_rtmp_re = re.compile(r'"url"\s*:\s*"(.+?)"')
-
-_user_api_schema = validate.Schema(validate.all(
-    {"data": validate.transform(parse_json)},
-    validate.get("data"),
-    {
-        "liveStreamID": int,
-        "userIsOnLive": int
-    }
-))
+_url_re = re.compile(r"https://17.live/live/(?P<channel>[^/&?]+)")
+_status_re = re.compile(r'\\"closeBy\\":\\"\\"')
+_rtmp_re = re.compile(r'\\"url\\"\s*:\s*\\"(.+?)\\"')
 
 
 class App17(Plugin):
@@ -30,41 +18,14 @@ class App17(Plugin):
 
     def _get_streams(self):
         match = _url_re.match(self.url)
-        page = match.group("page")
         channel = match.group("channel")
 
         http.headers.update({'User-Agent': useragents.CHROME})
 
-        if page == 'user':
-            res = http.get(self.url)
-            userid = _userid_re.search(res.text).group(1)
-            api = http.post(API_URL, data={"targetUserID": userid})
-            data = http.json(api, schema=_user_api_schema)
-            rid = data["liveStreamID"]
-            if rid == 0:
-                self.logger.info("Stream currently unavailable.")
-                return
-
-            url = ROOM_URL.format(rid)
-            res = http.get(url)
-        elif page == 'profile':
-            api = http.post(API_URL, data={"targetUserID": channel})
-            data = http.json(api, schema=_user_api_schema)
-            rid = data["liveStreamID"]
-            if rid == 0:
-                self.logger.info("Stream currently unavailable.")
-                return
-
-            url = ROOM_URL.format(rid)
-            res = http.get(url)
-        else:
-            res = http.get(self.url)
-
+        payload = '{"liveStreamID": "%s"}' % (channel)
+        res = http.post(API_URL, data=payload)
         status = _status_re.search(res.text)
         if not status:
-            return
-
-        if status.group(1) != 'true':
             self.logger.info("Stream currently unavailable.")
             return
 
