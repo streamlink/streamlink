0b5b77661972812c00299a8c48f5066905fe1d4e
Journey <timtag1190@gmail.com>
2017-09-04 09:22:20 -0500

    fix arconai test plugin

--- tests/test_plugin_arconai.py
+++ tests/test_plugin_arconai.py
@@ -6,9 +6,9 @@ from streamlink.plugins.arconai import ArconaiTv
 class TestPluginArconaiTv(unittest.TestCase):
     def test_can_handle_url(self):
         # should match
-        self.assertTrue(ArconaiTv.can_handle_url('http://arconaitv.me/stream.php?id=1'))
-        self.assertTrue(ArconaiTv.can_handle_url('http://arconaitv.me/stream.php?id=23'))
-        self.assertTrue(ArconaiTv.can_handle_url('http://arconaitv.me/stream.php?id=440'))
+        self.assertTrue(ArconaiTv.can_handle_url('http://arconaitv.co/stream.php?id=6'))
+        self.assertTrue(ArconaiTv.can_handle_url('http://arconaitv.co/stream.php?id=24'))
+        self.assertTrue(ArconaiTv.can_handle_url('http://arconaitv.co/stream.php?id=115'))
 
         # shouldn't match
-        self.assertFalse(ArconaiTv.can_handle_url('http://arconaitv.me/'))
+        self.assertFalse(ArconaiTv.can_handle_url('http://arconaitv.co/'))
