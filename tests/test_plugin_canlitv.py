a8039267d124632e690d423b917afdca9a9048ba
Emrah Er <emraher@users.noreply.github.com>
2017-10-29 21:25:47 +0300

    plugins.canlitv: fix URLs (#1281)
    * plugins.canlitv: fix URLs

--- tests/test_plugin_canlitv.py
+++ tests/test_plugin_canlitv.py
@@ -20,13 +20,22 @@ class TestPluginCanlitv(unittest.TestCase):
 
     def test_can_handle_url(self):
         # should match
+        self.assertTrue(Canlitv.can_handle_url("http://www.canlitv.plus/channel"))
         self.assertTrue(Canlitv.can_handle_url("http://www.canlitv.com/channel"))
-        self.assertTrue(Canlitv.can_handle_url("http://www.canlitv.life/channel"))
         self.assertTrue(Canlitv.can_handle_url("http://www.canlitvlive.co/izle/channel.html"))
         self.assertTrue(Canlitv.can_handle_url("http://www.canlitvlive.live/izle/channel.html"))
+        self.assertTrue(Canlitv.can_handle_url("http://www.canlitvlive.io/izle/channel.html"))
+        self.assertTrue(Canlitv.can_handle_url("http://www.canlitvlive.site/izle/channel.html"))
         self.assertTrue(Canlitv.can_handle_url("http://www.ecanlitvizle.net/channel/"))
         self.assertTrue(Canlitv.can_handle_url("http://www.ecanlitvizle.net/onizleme.php?kanal=channel"))
         self.assertTrue(Canlitv.can_handle_url("http://www.ecanlitvizle.net/tv.php?kanal=channel"))
         # shouldn't match
         self.assertFalse(Canlitv.can_handle_url("http://www.canlitv.com"))
+        self.assertFalse(Canlitv.can_handle_url("http://www.canlitv.plus"))
         self.assertFalse(Canlitv.can_handle_url("http://www.ecanlitvizle.net"))
+        self.assertFalse(Canlitv.can_handle_url("http://www.canlitvlive.co"))
+        self.assertFalse(Canlitv.can_handle_url("http://www.canlitvlive.live"))
+        self.assertFalse(Canlitv.can_handle_url("http://www.canlitvlive.io"))
+        self.assertFalse(Canlitv.can_handle_url("http://www.canlitvlive.site"))
+        self.assertFalse(Canlitv.can_handle_url("http://www.ecanlitvizle.net"))
+
