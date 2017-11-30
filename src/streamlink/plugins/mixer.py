9ec504f7fc68431e570c052eb06a50296df9a43d
back-to <back-to@users.noreply.github.com>
2017-11-30 20:39:12 +0100

    [mixer] replaced beam.pro with mixer.com
    - New domain mixer.com
    - Use Mixer API 1.0
    - Removed RTMPStream
    - Removed HTTP mp4 VODs (they won't load for me)
    - Fixed HLS Livestreams
    - Added tests for mixer.com

--- src/streamlink/plugins/mixer.py
+++ src/streamlink/plugins/mixer.py
@@ -1,27 +1,30 @@
+"""
+    Mixer API 1.0 documentation
+    https://dev.mixer.com/rest.html
+"""
 import re
 
+from streamlink import NoStreamsError
 from streamlink.compat import urlparse, parse_qsl, urljoin
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import http, validate
 from streamlink.stream import HLSStream
-from streamlink.stream import HTTPStream
-from streamlink.stream import RTMPStream
 
-_url_re = re.compile(r"http(s)?://(\w+.)?beam.pro/(?P<channel>[^/?]+)")
+_url_re = re.compile(r"http(s)?://(\w+.)?mixer\.com/(?P<channel>[^/?]+)")
 
 
-class Beam(Plugin):
-    api_url = "https://beam.pro/api/v1/{type}/{id}"
-    channel_manifest = "https://beam.pro/api/v1/channels/{id}/manifest.{type}"
+class Mixer(Plugin):
+    api_url = "https://mixer.com/api/v1/{type}/{id}"
 
-    _vod_schema = validate.Schema({
-        "state": "AVAILABLE",
-        "vods": [{
-            "baseUrl": validate.url(),
-            "data": validate.any(None, {
-                "Height": int
-            }),
-            "format": validate.text
+    _vod_schema = validate.Schema(
+        {
+            "state": "AVAILABLE",
+            "vods": [{
+                "baseUrl": validate.url(),
+                "data": validate.any(None, {
+                    "Height": int
+                }),
+                "format": validate.text
             }]
         },
         validate.get("vods"),
@@ -31,65 +34,40 @@ class Beam(Plugin):
             "format": validate.get("format"),
             "height": validate.all(validate.get("data"), validate.get("Height"))
         })])
-    _assets_schema = validate.Schema(
-        validate.union({
-            "base": validate.all(
-                validate.xml_find("./head/meta"),
-                validate.get("base"),
-                validate.url(scheme="rtmp")
-            ),
-            "videos": validate.all(
-                validate.xml_findall(".//video"),
-                [
-                    validate.union({
-                        "src": validate.all(
-                            validate.get("src"),
-                            validate.text
-                        ),
-                        "height": validate.all(
-                            validate.get("height"),
-                            validate.text,
-                            validate.transform(int)
-                        )
-                    })
-                ]
-            )
-        })
-    )
 
     @classmethod
     def can_handle_url(cls, url):
         return _url_re.match(url)
 
+    def _get_api_res(self, api_type, api_id):
+        try:
+            res = http.get(self.api_url.format(type=api_type, id=api_id))
+            return res
+        except Exception as e:
+            if "404" in str(e):
+                self.logger.error("invalid {0} - {1}".format(api_type, api_id))
+            elif "429" in str(e):
+                self.logger.error("Too Many Requests, API rate limit exceeded.")
+            raise NoStreamsError(self.url)
+
     def _get_vod_stream(self, vod_id):
-        res = http.get(self.api_url.format(type="recordings", id=vod_id))
+        res = self._get_api_res("recordings", vod_id)
+
         for sdata in http.json(res, schema=self._vod_schema):
             if sdata["format"] == "hls":
                 hls_url = urljoin(sdata["url"], "manifest.m3u8")
                 yield "{0}p".format(sdata["height"]), HLSStream(self.session, hls_url)
-            elif sdata["format"] == "raw":
-                raw_url = urljoin(sdata["url"], "source.mp4")
-                yield "{0}p".format(sdata["height"]), HTTPStream(self.session, raw_url)
 
     def _get_live_stream(self, channel):
-        res = http.get(self.api_url.format(type="channels", id=channel))
-        channel_info = http.json(res)
+        res = self._get_api_res("channels", channel)
 
+        channel_info = http.json(res)
         if not channel_info["online"]:
             return
 
-        res = http.get(self.channel_manifest.format(id=channel_info["id"], type="smil"))
-        assets = http.xml(res, schema=self._assets_schema)
-
-        for video in assets["videos"]:
-            name = "{0}p".format(video["height"])
-            stream = RTMPStream(self.session, {
-                "rtmp": "{0}/{1}".format(assets["base"], video["src"])
-            })
-            yield name, stream
-
-        for s in HLSStream.parse_variant_playlist(self.session,
-                                                  self.channel_manifest.format(id=channel_info["id"], type="m3u8")).items():
+        user_id = channel_info["id"]
+        hls_url = self.api_url.format(type="channels", id="{0}/manifest.m3u8".format(user_id))
+        for s in HLSStream.parse_variant_playlist(self.session, hls_url).items():
             yield s
 
     def _get_streams(self):
@@ -106,4 +84,4 @@ class Beam(Plugin):
             return self._get_live_stream(channel)
 
 
-__plugin__ = Beam
+__plugin__ = Mixer
