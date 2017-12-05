525b48aafc9b05dff5abfd6b5e6bc60621f9ee06
Sean Greenslade <sean@seangreenslade.com>
2017-12-05 15:48:22 -0800

    Reworked picarto.tv plugin to deal with website changes.

--- src/streamlink/plugins/picarto.py
+++ src/streamlink/plugins/picarto.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import re
+import json
 
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import http
@@ -22,9 +23,11 @@ class Picarto(Plugin):
     _tech_switch_re = re.compile(r"""
     <div\s+class=".*?tech_switch.*?"(.*?)>
     """, re.VERBOSE)
-    # placeStream(channel, playerID, product, offlineImage, online, token, tech)
-    _place_stream_re = re.compile(r"""
-        <script>\s*placeStream\s*\((.*?)\);?\s*</script>
+    # Stream status regex - extracted from playersettings
+    # group(1) = token, group(2) = online status (string true/false)
+    _stream_status_re = re.compile(r"""
+        <script>\s[\s\S]*?token:\s?(.*?),[\s\S]*?online:\s(.*?),[\s\S]*?
+        </script>
     """, re.VERBOSE)
     # <source ...>
     _source_re = re.compile(r'''source src="(http[^"]+)"''')
@@ -35,24 +38,17 @@ class Picarto(Plugin):
 
     @classmethod
     def _stream_online(cls, page):
-        match = cls._place_stream_re.search(page.text)
+        match = cls._stream_status_re.search(page.text)
         if match:
-            return match.group(1).split(",")[4].strip() == "1"
+            return match.group(2).strip() == "true"
         return False
 
-    @classmethod
-    def _get_steam_list(cls, page):
-        for match in cls._tech_switch_re.findall(page.text):
-            args = {}
-            for attr in match.strip().split(" "):
-                key, value = attr.split("=")
-                _, key = key.split("-")
-                args[key] = value.strip('"')
-            yield args
-
-    def _create_hls_stream(self, server, args):
+    def _create_hls_stream(self, server, channel, token):
         streams = HLSStream.parse_variant_playlist(self.session,
-                                                   self.HLS_URL.format(server=server, **args),
+                                                   self.HLS_URL.format(
+                                                       server=server,
+                                                       channel=channel,
+                                                       token=token),
                                                    verify=False)
         if len(streams) > 1:
             self.logger.debug("Multiple HLS streams found")
@@ -64,10 +60,10 @@ class Picarto(Plugin):
             # one HLS streams, rename it to live
             return {"live": list(streams.values())[0]}
 
-    def _create_flash_stream(self, server, args):
+    def _create_flash_stream(self, server, channel, token):
         params = {
             "rtmp": self.RTMP_URL.format(server=server),
-            "playpath": self.RTMP_PLAYPATH.format(**args)
+            "playpath": self.RTMP_PLAYPATH.format(token=token, channel=channel)
         }
         return RTMPStream(self.session, params=params)
 
@@ -97,34 +93,27 @@ class Picarto(Plugin):
             return
 
         server = None
-        streams = list(self._get_steam_list(page))
-        multi = False
+        token = "public"
 
-        for args in streams:
-            channel, tech, token = args["channel"], args["tech"], args["token"]
-            if channel.lower() != page_channel.lower():
-                if not multi:
-                    self.logger.info("Skipping multi-channel stream for: {0}".format(channel))
-                    multi = True
-                continue
+        # Extract preferred edge server and available techs from channel API
+        channel_server_res = http.post(self.API_CHANNEL_INFO, data={"loadbalancinginfo": page_channel})
+        info_json = json.loads(channel_server_res.text)
+        pref = info_json["preferedEdge"]
+        for i in info_json["edges"]:
+            if i["id"] == pref:
+                server = i["ep"]
+                break
+        self.logger.debug("Using load balancing server {0} : {1} for channel {2}",
+                          pref,
+                          server,
+                          page_channel)
 
-            self.logger.debug("Found stream for {channel}; tech=\"{tech}\", token=\"{token}\"", **args)
-
-            # cache the load balancing info
-            if not server:
-                channel_server_res = http.post(self.API_CHANNEL_INFO, data={"loadbalancinginfo": channel})
-                server = channel_server_res.text
-                self.logger.debug("Using load balancing server {0} for channel {1}",
-                                  server,
-                                  channel)
-
-            # generate all the streams, for multi-channel streams also append the channel name
-            if tech == "hls":
-                for s in self._create_hls_stream(server, args).items():
+        for i in info_json["techs"]:
+            if i["label"] == "HLS":
+                for s in self._create_hls_stream(server, page_channel, token).items():
                     yield s
-
-            elif tech == "flash":
-                stream = self._create_flash_stream(server, args)
+            elif i["label"] == "RTMP Flash":
+                stream = self._create_flash_stream(server, page_channel, token)
                 yield "live", stream
 
 
