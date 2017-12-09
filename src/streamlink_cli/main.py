22aa7075e1e8b3377751175f09293748392ec69c
Allan Nordhøy <epost@anotheragency.no>
2017-12-09 22:50:17 +0100

    Streamlink, specified stream, …, no !

--- src/streamlink_cli/main.py
+++ src/streamlink_cli/main.py
@@ -349,7 +349,7 @@ def handle_stream(plugin, streams, stream_name):
 
             console.msg("{0}", cmdline)
         else:
-            console.exit("The stream specified cannot be translated to a command")
+            console.exit("The specified stream cannot be translated to a command")
 
     # Print JSON representation of the stream
     elif console.json:
@@ -359,7 +359,7 @@ def handle_stream(plugin, streams, stream_name):
         try:
             console.msg("{0}", stream.to_url())
         except TypeError:
-            console.exit("The stream specified cannot be translated to a URL")
+            console.exit("The specified stream cannot be translated to a URL")
 
     # Output the stream
     else:
@@ -981,7 +981,7 @@ def setup_plugin_options():
 def check_root():
     if hasattr(os, "getuid"):
         if os.geteuid() == 0:
-            console.logger.info("streamlink is running as root! Be careful!")
+            console.logger.info("Streamlink is running as root! Be careful!")
 
 
 def check_version(force=False):
@@ -1006,7 +1006,7 @@ def check_version(force=False):
                             "available!".format(latest_version))
         cache.set("version_info_printed", True, (60 * 60 * 6))
     elif force:
-        console.logger.info("Your Streamlink version ({0}) is up to date!",
+        console.logger.info("Your Streamlink version ({0}) is up to date.",
                             installed_version)
 
     if force:
@@ -1049,12 +1049,12 @@ def main():
             # Close output
             if output:
                 output.close()
-            console.msg("Interrupted! Exiting...")
+            console.msg("Interrupted! Exiting…")
             error_code = 130
         finally:
             if stream_fd:
                 try:
-                    console.logger.info("Closing currently open stream...")
+                    console.logger.info("Closing currently open stream…")
                     stream_fd.close()
                 except KeyboardInterrupt:
                     error_code = 130
