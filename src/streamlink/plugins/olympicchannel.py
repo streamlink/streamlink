e06525c467d5f47b03256efc40627c9adff51f71
xela722 <alex0722@comcast.net>
2017-12-08 19:51:00 -0500

    Add test plugin, fix compatability

--- src/streamlink/plugins/olympicchannel.py
+++ src/streamlink/plugins/olympicchannel.py
@@ -8,7 +8,7 @@ from streamlink.stream import HLSStream, HTTPStream
 
 class OlympicChannel(Plugin):
     _url_re = re.compile(r"http(?:s)?://(\w+)\.?olympicchannel.com/../(?P<type>tv|playback)/(livestream-.\d|.*)/")
-    _live_api_url = "https://www.olympicchannel.com{0}api/v2/metadata/tv/{1}"
+    _live_api_url = "https://www.olympicchannel.com{0}api/v2/metadata/{1}"
     _stream_get_url = "https://www.olympicchannel.com/en/proxy/viewings/"
     _stream_api_schema = validate.Schema({
         u'status': u'ok',
@@ -37,7 +37,10 @@ class OlympicChannel(Plugin):
         res = http.get(self._live_api_url.format(lang, path))
         live_res = http.json(res)['default']['uid']
         post_data = '{"channel_url":"/api/channels/%s/"}' % live_res
-        stream_data = http.json(http.post(self._stream_get_url, data=post_data))['stream_url']
+        try:
+            stream_data = http.json(http.post(self._stream_get_url, data=post_data))['stream_url']
+        except:
+            stream_data = http.json(http.post(self._stream_get_url, data=post_data))['channel_url']
         return HLSStream.parse_variant_playlist(self.session, stream_data)
 
     def _get_streams(self):
@@ -50,7 +53,8 @@ class OlympicChannel(Plugin):
         lang = re.search(r"/../", self.url).group(0)
         
         if type_of_stream == 'tv':
-            path = re.search(r"/livestream-\d/$", self.url).group(0)
+            path = re.search(r"tv/.*-\d/$", self.url).group(0)
+            
             return self._get_live_streams(lang, path)
         elif type_of_stream == 'playback':
             path = re.search(r"/playback/.*/$", self.url).group(0)
