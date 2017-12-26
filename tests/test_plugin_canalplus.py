6cd332809a4e3be96093873a3d47a4b964ae57fa
BZHDeveloper <inizan.yannick@gmail.com>
2017-12-26 21:02:34 +0100

    [canalplus] Update plugin according to website changes (#1378)
    * [canalplus] Update plugin according to website changes
    	C+ has centralized its replay services in mycanal.fr, and remove the old ones (c8.fr, cstar.fr).
    	Due to changes, this plugin cannot handle live url (for the moment).
    
    * remove unavailable test (web page doesn't exist anymore).
    
    * [canalplus] update plugin according to advices.
    	- allow http scheme for cnews.
    
    * [canalplus] add 'cnews.fr' domain to plugin_matrix documentation.

--- tests/test_plugin_canalplus.py
+++ tests/test_plugin_canalplus.py
@@ -6,21 +6,13 @@ from streamlink.plugins.canalplus import CanalPlus
 class TestPluginCanalPlus(unittest.TestCase):
     def test_can_handle_url(self):
         # should match
-        self.assertTrue(CanalPlus.can_handle_url("http://www.canalplus.fr/pid3580-live-tv-clair.html"))
-        self.assertTrue(CanalPlus.can_handle_url("http://www.canalplus.fr/emissions/pid8596-the-tonight-show.html"))
-        self.assertTrue(CanalPlus.can_handle_url("http://www.canalplus.fr/c-divertissement/pid1787-c-groland.html?vid=1430239"))
-        self.assertTrue(CanalPlus.can_handle_url("http://www.c8.fr/pid5323-c8-live.html"))
-        self.assertTrue(CanalPlus.can_handle_url("http://www.c8.fr/c8-divertissement/pid8758-c8-la-folle-histoire-de-sophie-marceau.html"))
-        self.assertTrue(CanalPlus.can_handle_url("http://www.c8.fr/c8-sport/pid5224-c8-direct-auto.html?vid=1430292"))
-        self.assertTrue(CanalPlus.can_handle_url("http://replay.c8.fr/video/1431076"))
-        self.assertTrue(CanalPlus.can_handle_url("http://www.cstar.fr/pid5322-cstar-live.html"))
-        self.assertTrue(CanalPlus.can_handle_url("http://www.cstar.fr/emissions/pid8754-wild-transport.html"))
-        self.assertTrue(CanalPlus.can_handle_url("http://www.cstar.fr/musique/pid6282-les-tops.html?vid=1430143"))
-        self.assertTrue(CanalPlus.can_handle_url("http://replay.cstar.fr/video/1430245"))
+        self.assertTrue(CanalPlus.can_handle_url("https://www.mycanal.fr/docus-infos/l-info-du-vrai-du-13-12-politique-les-affaires-reprennent/p/1473830"))
+        self.assertTrue(CanalPlus.can_handle_url("https://www.mycanal.fr/sport/infosport-laurey-et-claudia/p/1473752"))
+        self.assertTrue(CanalPlus.can_handle_url("https://www.mycanal.fr/docus-infos/ses-debuts-a-madrid-extrait-le-k-benzema/p/1469050"))
+        self.assertTrue(CanalPlus.can_handle_url("https://www.mycanal.fr/d8-docs-mags/au-revoir-johnny-hallyday-le-doc/p/1473054"))
         self.assertTrue(CanalPlus.can_handle_url("http://www.cnews.fr/direct"))
         self.assertTrue(CanalPlus.can_handle_url("http://www.cnews.fr/politique/video/des-electeurs-toujours-autant-indecis-174769"))
         self.assertTrue(CanalPlus.can_handle_url("http://www.cnews.fr/magazines/plus-de-recul/de-recul-du-14042017-174594"))
-
         # shouldn't match
         self.assertFalse(CanalPlus.can_handle_url("http://www.canalplus.fr/"))
         self.assertFalse(CanalPlus.can_handle_url("http://www.c8.fr/"))
