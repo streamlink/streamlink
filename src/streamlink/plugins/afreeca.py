40a0ce2f48792f7c7eff806e261f3a38a4f76ac7
back-to <backto@protonmail.ch>
2018-02-07 11:10:46 +0100

    [afreeca] Plugin update. - Login for +19 streams   --afreeca-username   --afreeca-password - Removed 15 sec countdown - Added some error messages - Removed old Global AfreecaTV plugin - Added url tests

--- src/streamlink/plugins/afreeca.py
+++ src/streamlink/plugins/afreeca.py
@@ -1,8 +1,10 @@
 import re
 
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import http, validate
-from streamlink.stream import RTMPStream, HLSStream
+from streamlink.plugin import PluginOptions
+from streamlink.plugin.api import http
+from streamlink.plugin.api import validate
+from streamlink.stream import HLSStream
 
 CHANNEL_API_URL = "http://live.afreecatv.com:8057/afreeca/player_live_api.php"
 STREAM_INFO_URLS = "{rmd}/broad_stream_assign.html"
@@ -10,7 +12,7 @@ STREAM_INFO_URLS = "{rmd}/broad_stream_assign.html"
 CHANNEL_RESULT_ERROR = 0
 CHANNEL_RESULT_OK = 1
 
-QUALITYS=["original", "hd", "sd"]
+QUALITYS = ["original", "hd", "sd"]
 
 QUALITY_WEIGHTS = {
     "original": 1080,
@@ -18,12 +20,13 @@ QUALITY_WEIGHTS = {
     "sd": 480
 }
 
-_url_re = re.compile(r"http(s)?://(?P<cdn>\w+\.)?afreeca(tv)?.com/(?P<username>\w+)(/\d+)?")
+_url_re = re.compile(r"http(s)?://(?P<cdn>\w+\.)?afreeca(tv)?\.com/(?P<username>\w+)(/\d+)?")
 
 _channel_schema = validate.Schema(
     {
         "CHANNEL": {
             "RESULT": validate.transform(int),
+            validate.optional("BPWD"): validate.text,
             validate.optional("BNO"): validate.text,
             validate.optional("RMD"): validate.text,
             validate.optional("AID"): validate.text,
@@ -42,7 +45,16 @@ _stream_schema = validate.Schema(
     }
 )
 
+
 class AfreecaTV(Plugin):
+
+    login_url = "https://member.afreecatv.com:8111/login/LoginAction.php"
+
+    options = PluginOptions({
+        "username": None,
+        "password": None
+    })
+
     @classmethod
     def can_handle_url(self, url):
         return _url_re.match(url)
@@ -81,7 +93,7 @@ class AfreecaTV(Plugin):
         return http.json(res, schema=_channel_schema)
 
     def _get_stream_info(self, broadcast, quality, cdn, rmd):
-        params={
+        params = {
             "return_type": cdn,
             "broad_key": "{broadcast}-flash-{quality}-hls".format(**locals())
         }
@@ -100,13 +112,49 @@ class AfreecaTV(Plugin):
         if "view_url" in info:
             return HLSStream(self.session, info["view_url"], params=dict(aid=key))
 
+    def _login(self, username, password):
+        data = {
+            "szWork": "login",
+            "szType": "json",
+            "szUid": username,
+            "szPassword": password,
+            "isSaveId": "true",
+            "isSavePw": "false",
+            "isSaveJoin": "false"
+        }
+
+        res = http.post(self.login_url, data=data)
+        res = http.json(res)
+        if res["RESULT"] == 1:
+            return True
+        else:
+            return False
 
     def _get_streams(self):
+        if not self.session.get_option("hls-segment-ignore-names"):
+            ignore_segment = ["_0", "_1", "_2"]
+            self.session.set_option("hls-segment-ignore-names", ignore_segment)
+
+        login_username = self.get_option("username")
+        login_password = self.get_option("password")
+        if login_username and login_password:
+            self.logger.debug("Attempting login as {0}".format(login_username))
+            if self._login(login_username, login_password):
+                self.logger.info("Successfully logged in as {0}".format(login_username))
+            else:
+                self.logger.info("Failed to login as {0}".format(login_username))
+
         match = _url_re.match(self.url)
         username = match.group("username")
 
         channel = self._get_channel_info(username)
-        if channel["RESULT"] != CHANNEL_RESULT_OK:
+        if channel.get("BPWD") == "Y":
+            self.logger.error("Stream is Password-Protected")
+            return
+        elif channel.get("RESULT") == -6:
+            self.logger.error("Login required")
+            return
+        elif channel.get("RESULT") != CHANNEL_RESULT_OK:
             return
 
         (broadcast, rmd, cdn) = (channel["BNO"], channel["RMD"], channel["CDN"])
@@ -117,5 +165,6 @@ class AfreecaTV(Plugin):
             hls_stream = self._get_hls_stream(broadcast, username, qkey, cdn, rmd)
             if hls_stream:
                 yield qkey, hls_stream
+
 
 __plugin__ = AfreecaTV
