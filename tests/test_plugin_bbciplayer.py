f5726144a3f2143a801242ddf623c08f47170e2e
Drew J. Sonne <drewsonne@users.noreply.github.com>
2018-02-09 17:21:28 +0000

    [bbciplayer] Fix authentication failures (#1411)
    * Fix authentication failures with bbciplayer plugin
    
    #1283 outlines issues with authenticating against the BBC iPlayer endpoints.
    
     - Change the auth endpoints to use the new ‘session.bbc.com/session' endpoint.
     - Remove schema validators as session and auth details are coming from the headers and cookie
     - Use requests.Session object through the plugin to reduce the amount of variables to be passed around
    
    * Fix python2-3 compatbility for urllib
    
    * Use built-in py23 compatibility library
    
    * Separate testable code into their own functions
    
    * Remove unused parameters
    
    * Run pep8 formatter
    
    * Added method doc blocks
    
    * Run pep8 formatter
    
    * Add docblock for BBCiPlayer._validate_login
    
    * Redefine test cases for BBC ID login
    
    Made the login validation less succeptable to differences in URLs which may be provided by the user:
    
     - Ignore http vs https
     - Ignore missing www.
    
    * Simplify bbciplayer log-in validation
    
    Removed redundant code carried over from previous implementation
    
    * Remove tests for code which no logner exists
    
    * Remove extra step of variable assignment
    
    * Removed unused regex

--- tests/test_plugin_bbciplayer.py
+++ tests/test_plugin_bbciplayer.py
@@ -1,12 +1,18 @@
+import json
+import logging
 import unittest
 
+from requests import Response, Request
+
+from streamlink.compat import urlencode
 from streamlink.plugins.bbciplayer import BBCiPlayer
 
 
 class TestPluginBBCiPlayer(unittest.TestCase):
     def test_can_handle_url(self):
         # should match
-        self.assertTrue(BBCiPlayer.can_handle_url("http://www.bbc.co.uk/iplayer/episode/b00ymh67/madagascar-1-island-of-marvels"))
+        self.assertTrue(
+            BBCiPlayer.can_handle_url("http://www.bbc.co.uk/iplayer/episode/b00ymh67/madagascar-1-island-of-marvels"))
         self.assertTrue(BBCiPlayer.can_handle_url("http://www.bbc.co.uk/iplayer/live/bbcone"))
 
         # shouldn't match
@@ -19,3 +25,24 @@ class TestPluginBBCiPlayer(unittest.TestCase):
             "71c345435589c6ddeea70d6f252e2a52281ecbf3",
             BBCiPlayer._hash_vpid("1234567890")
         )
+
+    def test_extract_nonce(self):
+        mock_nonce = "mock-nonce-nse"
+
+        last_response = Response()
+        last_response.request = Request('GET', "http://example.com/?" + urlencode(dict(
+            goto="http://example.com/?" + urlencode(dict(
+                state=json.dumps(dict(nonce=mock_nonce))
+            ))
+        )))
+
+        mock_response = Response()
+        mock_response.history = [
+            Response(),  # Add some extra dummy responses in to make sure we always get the last
+            Response(),
+            last_response
+        ]
+
+        self.assertEqual(BBCiPlayer._extract_nonce(mock_response), mock_nonce)
+
+
