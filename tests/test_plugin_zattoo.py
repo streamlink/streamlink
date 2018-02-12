fc413df10d0833921d1dd6881c19e2fbdf27cb05
back-to <backto@protonmail.ch>
2018-02-12 14:46:26 +0100

    [zattoo] Added support for zattoo recordings

--- tests/test_plugin_zattoo.py
+++ tests/test_plugin_zattoo.py
@@ -17,6 +17,9 @@ class TestPluginZattoo(unittest.TestCase):
         # zattoo vod
         self.assertTrue(Zattoo.can_handle_url('https://zattoo.com/ondemand/watch/ibR2fpisWFZGvmPBRaKnFnuT-alarm-am-airport'))
         self.assertTrue(Zattoo.can_handle_url('https://zattoo.com/ondemand/watch/G8S7JxcewY2jEwAgMzvFWK8c-berliner-schnauzen'))
+        # zattoo recording
+        self.assertTrue(Zattoo.can_handle_url('https://zattoo.com/ondemand/watch/srf_zwei/110223896-die-schweizermacher/52845783/1455130800000/1455137700000/6900000'))
+        self.assertTrue(Zattoo.can_handle_url('https://zattoo.com/watch/tve/130920738-viaje-al-centro-de-la-tele/96847859/1508777100000/1508779800000/0'))
 
         # shouldn't match
         self.assertFalse(Zattoo.can_handle_url('https://ewe.de'))
