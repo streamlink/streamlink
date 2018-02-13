81148668631feef066dbcd4f92f4ec0d6c9d7222
BZHDeveloper <inizan.yannick@gmail.com>
2018-02-13 18:10:35 +0100

    [TF1] Fix plugin (#1457)
    * [TF1] Fix plugin
    TF1 group renamed some of her channels, NT1 & HD1, as "TFX" & "TF1 Séries Films". So I've updated plugin according to last TF1 changes.
    
    * [TF1] allow "tf1-series-films" in url.
    
    * [TF1] add tests.
    NOTE : TF1 redirect old channel names to new ones (for now).

--- tests/test_plugin_tf1.py
+++ tests/test_plugin_tf1.py
@@ -7,12 +7,16 @@ class TestPluginTF1(unittest.TestCase):
     def test_can_handle_url(self):
         # should match
         self.assertTrue(TF1.can_handle_url("http://tf1.fr/tf1/direct/"))
+        self.assertTrue(TF1.can_handle_url("http://tf1.fr/tfx/direct/"))
+        self.assertTrue(TF1.can_handle_url("http://tf1.fr/tf1-series-films/direct/"))
         self.assertTrue(TF1.can_handle_url("http://lci.fr/direct"))
         self.assertTrue(TF1.can_handle_url("http://www.lci.fr/direct"))
         self.assertTrue(TF1.can_handle_url("http://tf1.fr/tmc/direct"))
 
         # shouldn't match
         self.assertFalse(TF1.can_handle_url("http://tf1.fr/direct"))
+#       self.assertFalse(TF1.can_handle_url("http://tf1.fr/nt1/direct")) NOTE : TF1 redirect old channel names to new ones (for now).
+#       self.assertFalse(TF1.can_handle_url("http://tf1.fr/hd1/direct"))
         self.assertFalse(TF1.can_handle_url("http://www.tf1.fr/direct"))
         self.assertFalse(TF1.can_handle_url("http://www.tvcatchup.com/"))
         self.assertFalse(TF1.can_handle_url("http://www.youtube.com/"))
