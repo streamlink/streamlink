31e1b6f4fa66b3e8c4d243dd93a93275fa074ea8
BZHDeveloper <inizan.yannick@gmail.com>
2018-02-13 18:10:35 +0100

    [TF1] Fix plugin (#1457)
    * [TF1] Fix plugin
    TF1 group renamed some of her channels, NT1 & HD1, as "TFX" & "TF1 Séries Films". So I've updated plugin according to last TF1 changes.
    
    * [TF1] allow "tf1-series-films" in url.
    
    * [TF1] add tests.
    NOTE : TF1 redirect old channel names to new ones (for now).

--- src/streamlink/plugins/tf1.py
+++ src/streamlink/plugins/tf1.py
@@ -8,13 +8,13 @@ from streamlink.stream import HLSStream
 
 
 class TF1(Plugin):
-    url_re = re.compile(r"https?://(?:www\.)?(?:tf1\.fr/(\w+)/direct|(lci).fr/direct)/?")
+    url_re = re.compile(r"https?://(?:www\.)?(?:tf1\.fr/(tf1|tmc|tfx|tf1-series-films)/direct|(lci).fr/direct)/?")
     embed_url = "http://www.wat.tv/embedframe/live{0}"
     embed_re = re.compile(r"urlLive.*?:.*?\"(http.*?)\"", re.MULTILINE)
     api_url = "http://www.wat.tv/get/{0}/591997"
     swf_url = "http://www.wat.tv/images/v70/PlayerLite.swf"
-    hds_channel_remap = {"tf1": "androidliveconnect", "lci": "androidlivelci"}
-    hls_channel_remap = {"lci": "LCI", "tf1": "V4"}
+    hds_channel_remap = {"tf1": "androidliveconnect", "lci": "androidlivelci", "tfx" : "nt1live", "tf1-series-films" : "hd1live" }
+    hls_channel_remap = {"lci": "LCI", "tf1": "V4", "tfx" : "nt1", "tf1-series-films" : "hd1" }
 
     @classmethod
     def can_handle_url(cls, url):
