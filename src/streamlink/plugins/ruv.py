2562f9101472ae61e144c936b5c6daed22a29a09
Hannes Petur Eggertsson <hannespetur@gmail.com>
2018-02-17 01:50:50 +0000

    Ruv plugin updated. Fixes #643.

--- src/streamlink/plugins/ruv.py
+++ src/streamlink/plugins/ruv.py
@@ -3,40 +3,38 @@
 import re
 
 from streamlink.plugin import Plugin
-from streamlink.stream import RTMPStream, HLSStream
+from streamlink.stream import HLSStream
 
 from streamlink.plugin.api import http
 
-RTMP_LIVE_URL = "rtmp://ruv{0}livefs.fplive.net/ruv{0}live-live/stream{1}"
-RTMP_SARPURINN_URL = "rtmp://sipvodfs.fplive.net/sipvod/{0}/{1}{2}.{3}"
+HLS_RUV_LIVE_URL = """http://ruvruv-live.hls.adaptive.level3.net/ruv/{0}/\
+index/stream{1}.m3u8"""
 
-HLS_RUV_LIVE_URL = "http://ruvruv-live.hls.adaptive.level3.net/ruv/ruv/index/stream{0}.m3u8"
-HLS_RADIO_LIVE_URL = "http://sip-live.hds.adaptive.level3.net/hls-live/ruv-{0}/_definst_/live/stream1.m3u8"
-HLS_SARPURINN_URL = "http://sip-ruv-vod.dcp.adaptive.level3.net/{0}/{1}{2}.{3}.m3u8"
+HLS_RADIO_LIVE_URL = """http://sip-live.hds.adaptive.level3.net/hls-live/\
+ruv-{0}/_definst_/live/stream1.m3u8"""
+
+HLS_SARPURINN_URL = """http://sip-ruv-vod.dcp.adaptive.level3.net/\
+{0}/{1}{2}"""
 
 
 _live_url_re = re.compile(r"""^(?:https?://)?(?:www\.)?ruv\.is/
-                                (?P<channel_path>
-                                    ruv|
-                                    ras1|
-                                    ras-1|
-                                    ras2|
-                                    ras-2|
-                                    rondo
+                                (?P<stream_id>
+                                    ruv/?$|
+                                    ruv2/?$|
+                                    ruv-2/?$|
+                                    ras1/?$|
+                                    ras2/?$|
+                                    rondo/?$
                                 )
                                 /?
                                 """, re.VERBOSE)
 
-_sarpurinn_url_re = re.compile(r"""^(?:https?://)?(?:www\.)?ruv\.is/sarpurinn/
-                                    (?:
+_sarpurinn_url_re = re.compile(r"""^(?:https?://)?(?:www\.)?ruv\.is/spila/
+                                    (?P<stream_id>
                                         ruv|
                                         ruv2|
                                         ruv-2|
                                         ruv-aukaras|
-                                        ras1|
-                                        ras-1|
-                                        ras2|
-                                        ras-2
                                     )
                                     /
                                     [a-zA-Z0-9_-]+
@@ -45,35 +43,31 @@ _sarpurinn_url_re = re.compile(r"""^(?:https?://)?(?:www\.)?ruv\.is/sarpurinn/
                                     /?
                                     """, re.VERBOSE)
 
-_rtmp_url_re = re.compile(r"""rtmp://sipvodfs\.fplive.net/sipvod/
-                                (?P<status>
-                                    lokad|
-                                    opid
-                                )
-                                /
-                                (?P<date>[0-9]+/[0-9][0-9]/[0-9][0-9]/)?
-                                (?P<id>[A-Z0-9\$_]+)
-                                \.
-                                (?P<ext>
-                                    mp4|
-                                    mp3
-                                )""", re.VERBOSE)
+_single_re = re.compile(r"""(?:http://)?sip-ruv-vod.dcp.adaptive.level3.net/
+                            (?P<status>
+                                lokad|
+                                opid
+                            )
+                            /
+                            (?P<date>[0-9]+/[0-9][0-9]/[0-9][0-9]/)?
+                            (?P<id>[A-Z0-9\$_]+)
+                            \.mp4\.m3u8
+                         """, re.VERBOSE)
 
-_id_map = {
-    "ruv": "ruv",
-    "ras1": "ras1",
-    "ras-1": "ras1",
-    "ras2": "ras2",
-    "ras-2": "ras2",
-    "rondo": "ras3"
-}
-
+_multi_re = re.compile(r"""(?:http://)?sip-ruv-vod.dcp.adaptive.level3.net/
+                            (?P<status>
+                                lokad|
+                                opid
+                            )
+                            /manifest.m3u8\?tlm=hls&streams=
+                            (?P<streams>[0-9a-zA-Z\/\.\,:]+)
+                         """, re.VERBOSE)
 
 class Ruv(Plugin):
     @classmethod
     def can_handle_url(cls, url):
         if _live_url_re.match(url):
-            return _live_url_re.match(url)
+            return True
         else:
             return _sarpurinn_url_re.match(url)
 
@@ -83,75 +77,83 @@ class Ruv(Plugin):
 
         if live_match:
             self.live = True
-            self.channel_path = live_match.group("channel_path")
+            self.stream_id = live_match.group("stream_id")
+
+            # Remove slashes
+            self.stream_id.replace("/", "")
+
+            # Remove dashes
+            self.stream_id.replace("-", "")
+
+            if self.stream_id == "rondo":
+                self.stream_id = "ras3"
         else:
             self.live = False
 
     def _get_live_streams(self):
-        stream_id = _id_map[self.channel_path]
-
-        if stream_id == "ruv":
-            qualities_rtmp = ["720p", "480p", "360p", "240p"]
-
-            for i, quality in enumerate(qualities_rtmp):
-                yield quality, RTMPStream(
-                    self.session,
-                    {
-                        "rtmp": RTMP_LIVE_URL.format(stream_id, i + 1),
-                        "pageUrl": self.url,
-                        "live": True
-                    }
-                )
-
+        if self.stream_id == "ruv" or self.stream_id == "ruv2":
             qualities_hls = ["240p", "360p", "480p", "720p"]
             for i, quality_hls in enumerate(qualities_hls):
                 yield quality_hls, HLSStream(
                     self.session,
-                    HLS_RUV_LIVE_URL.format(i + 1)
+                    HLS_RUV_LIVE_URL.format(self.stream_id, i+1)
                 )
-
         else:
-            yield "audio", RTMPStream(self.session, {
-                "rtmp": RTMP_LIVE_URL.format(stream_id, 1),
-                "pageUrl": self.url,
-                "live": True
-            })
-
             yield "audio", HLSStream(
                 self.session,
-                HLS_RADIO_LIVE_URL.format(stream_id)
+                HLS_RADIO_LIVE_URL.format(self.stream_id)
             )
 
     def _get_sarpurinn_streams(self):
         res = http.get(self.url)
-        match = _rtmp_url_re.search(res.text)
 
-        if not match:
-            yield
+        multi_stream_match = _multi_re.search(res.text)
 
-        token = match.group("id")
-        status = match.group("status")
-        extension = match.group("ext")
-        date = match.group("date")
-        if not date:
-            date = ""
+        if multi_stream_match and multi_stream_match.group("streams") is not None:
+            status = multi_stream_match.group("status")
+            streams = multi_stream_match.group("streams").split(",")
 
-        if extension == "mp3":
-            key = "audio"
+            for stream in streams:
+                if stream.count(":") != 1:
+                    continue
+
+                [url, quality] = stream.split(":")
+                quality = int(quality)
+                key = ""
+
+                if quality <= 500:
+                  key = "240p"
+                elif quality <= 800:
+                  key = "360p"
+                elif quality <= 1200:
+                  key = "480p"
+                elif quality <= 2400:
+                  key = "720p"
+                else:
+                  key = "1080p"
+
+                yield key, HLSStream(
+                    self.session,
+                    HLS_SARPURINN_URL.format(status, "", url)
+                )
+
         else:
-            key = "576p"
+            single_stream_match = _single_re.search(res.text)
 
-            # HLS on Sarpurinn is currently only available on videos
-            yield key, HLSStream(
-                self.session,
-                HLS_SARPURINN_URL.format(status, date, token, extension)
-            )
+            if single_stream_match:
+                status = single_stream_match.group("status")
+                date = single_stream_match.group("date")
+                token = single_stream_match.group("id")
+                key = "576p"
 
-        yield key, RTMPStream(self.session, {
-            "rtmp": RTMP_SARPURINN_URL.format(status, date, token, extension),
-            "pageUrl": self.url,
-            "live": True
-        })
+                # Set date as an empty string instread of None
+                if date is None:
+                    date = ""
+
+                yield key, HLSStream(
+                    self.session,
+                    HLS_SARPURINN_URL.format(status, date, token + ".mp4.m3u8")
+                )
 
     def _get_streams(self):
         if self.live:
