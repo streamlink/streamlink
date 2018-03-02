b8162489cb21165125791948a06864d3964144b2
back-to <backto@protonmail.ch>
2018-03-02 20:53:33 +0100

    [TVRPlus] Fix for hls_re and use headers for HLSStream
    best and worst won't work, the source is 404
    
    streamlink "http://www.tvrplus.ro/live-tvr-international" 1200k
    
    360k (worst) - broken 404
    1200k - works
    2500k (best) - broken 404

---
+++ tests/test_plugin_tvrplus.py
@@ -0,0 +1,21 @@
+import unittest
+
+from streamlink.plugins.tvrplus import TVRPlus
+
+
+class TestPluginTVRPlus(unittest.TestCase):
+    def test_can_handle_url(self):
+        should_match = [
+            "http://tvrplus.ro/live-tvr-1",
+            "http://www.tvrplus.ro/live-tvr-1",
+            "http://www.tvrplus.ro/live-tvr-3",
+            "http://www.tvrplus.ro/live-tvr-international",
+        ]
+        for url in should_match:
+            self.assertTrue(TVRPlus.can_handle_url(url))
+
+        should_not_match = [
+            "http://www.tvrplus.ro/",
+        ]
+        for url in should_not_match:
+            self.assertFalse(TVRPlus.can_handle_url(url))
