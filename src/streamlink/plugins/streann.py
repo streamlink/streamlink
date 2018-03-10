7e9b3d0f58099e816eee70bfd5356913754fc21d
back-to <backto@protonmail.ch>
2018-03-10 12:07:51 +0100

    [streann] Fixed broken plugin
    - more debug msg
    - set headers on every get request
    - use new url for stream_url the old one still works but the website
    uses this url

--- src/streamlink/plugins/streann.py
+++ src/streamlink/plugins/streann.py
@@ -20,12 +20,13 @@ class Streann(Plugin):
     base_url = "https://ott.streann.com"
     get_time_url = base_url + "/web/services/public/get-server-time"
     token_url = base_url + "/loadbalancer/services/web-players/{playerId}/token/{type}/{dataId}/{deviceId}"
-    stream_url = base_url + "/loadbalancer/services/web-players/{type}s-reseller/{dataId}/{playerId}/{token}/{resellerId}/playlist.m3u8?date={time}&device-type=web&device-name=web&device-os=web&device-id={deviceId}"
+    stream_url = base_url + "/loadbalancer/services/web-players/{type}s-reseller-secure/{dataId}/{playerId}/{token}/{resellerId}/playlist.m3u8?date={time}&device-type=web&device-name=web&device-os=web&device-id={deviceId}"
     passphrase_re = re.compile(r'''CryptoJS\.AES\.decrypt\(.*?,\s*(['"])(?P<passphrase>(?:(?!\1).)*)\1\s*?\);''')
 
     def __init__(self, url):
         super(Streann, self).__init__(url)
         self._device_id = None
+        self._headers = {"User-Agent": useragents.FIREFOX}
 
     @classmethod
     def can_handle_url(cls, url):
@@ -44,16 +45,18 @@ class Streann(Plugin):
 
     @property
     def time(self):
-        res = http.get(self.get_time_url)
+        res = http.get(self.get_time_url, headers=self._headers)
         data = http.json(res)
         return str(data.get("serverTime", int(time.time() * 1000)))
 
     def passphrase(self):
-        res = http.get(self.url)
+        self.logger.debug("passphrase ...")
+        res = http.get(self.url, headers=self._headers)
         passphrase_m = self.passphrase_re.search(res.text)
         return passphrase_m and passphrase_m.group("passphrase").encode("utf8")
 
     def get_token(self, **config):
+        self.logger.debug("get_token ...")
         pdata = dict(arg1=base64.b64encode("www.ellobo106.com".encode("utf8")),
                      arg2=base64.b64encode(self.time.encode("utf8")))
 
@@ -76,14 +79,15 @@ class Streann(Plugin):
         # and decrypt it
         passphrase = self.passphrase()
         if passphrase:
+            self.logger.debug("Found passphrase")
             params = decrypt_openssl(data, passphrase)
             config = parse_qsd(params.decode("utf8"))
-
-            return HLSStream.parse_variant_playlist(self.session,
-                                                    self.stream_url.format(time=self.time,
-                                                                           deviceId=self.device_id,
-                                                                           token=self.get_token(**config),
-                                                                           **config))
+            hls_url = self.stream_url.format(time=self.time,
+                                             deviceId=self.device_id,
+                                             token=self.get_token(**config),
+                                             **config)
+            self.logger.debug("URL={0}".format(hls_url))
+            return HLSStream.parse_variant_playlist(self.session, hls_url, headers=self._headers)
 
 
 __plugin__ = Streann
