33447c8267204e02353a4011436d4536776f3894
back-to <backto@protonmail.ch>
2018-03-10 12:07:51 +0100

    [streann] Fixed broken plugin
    - more debug msg
    - set headers on every get request
    - use new url for stream_url the old one still works but the website
    uses this url

---
+++ tests/test_plugin_streann.py
@@ -0,0 +1,18 @@
+import unittest
+
+from streamlink.plugins.streann import Streann
+
+
+class TestPluginStreann(unittest.TestCase):
+    def test_can_handle_url(self):
+        should_match = [
+            'https://ott.streann.com/streaming/player.html?U2FsdGVkX1/BPZsbal3VWfGa7sWzTIlFtDEU+hyCEHP9MavCxtCfL1G8nXtK5LHSsgFYhG3Nw9UilT1fx3H+lf5R54SLHAU6BKP/n7kjJBDF0HBFblSUHmApiEf5BrU3coECX9RQcRaOY+uLtkeyEktvlrQ5nhg2QRw5x1IjkxFD0Rk5/ylTYxMqnu8snxRe2p09iJLi9E6cG8eXSHZby2We+pyV0obzGDbI2Jpyh4dFL646strLxFraeAJq5+mfpTqK10XZQi3sXA1/ULDH5lz+I9bZW4q/wZtlEdyA0CuB9LpDvCM11imLIhthGnRz',
+        ]
+        for url in should_match:
+            self.assertTrue(Streann.can_handle_url(url))
+
+        should_not_match = [
+            'https://ott.streann.com',
+        ]
+        for url in should_not_match:
+            self.assertFalse(Streann.can_handle_url(url))
