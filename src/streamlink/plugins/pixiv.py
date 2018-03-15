8d7a5c6afd832e26af3cb162991c181c71318443
back-to <backto@protonmail.ch>
2018-03-15 11:29:39 +0100

    [pixiv] Added login option

--- src/streamlink/plugins/pixiv.py
+++ src/streamlink/plugins/pixiv.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import re
 
+from streamlink import PluginError
 from streamlink.compat import urljoin
 from streamlink.exceptions import NoStreamsError
 from streamlink.plugin import Plugin
+from streamlink.plugin import PluginOptions
 from streamlink.plugin.api import http
 from streamlink.plugin.api import useragents
 from streamlink.plugin.api import validate
@@ -18,6 +20,7 @@ class Pixiv(Plugin):
 
     _videopage_re = re.compile(r"""["']live-button["']><a\shref=["'](?P<path>[^"']+)["']""")
     _data_re = re.compile(r"""<script\sid=["']state["']>[^><{]+(?P<data>{[^><]+})</script>""")
+    _post_key_re = re.compile(r"""name=["']post_key["']\svalue=["'](?P<data>[^"']+)["']""")
 
     _data_schema = validate.Schema(
         validate.all(
@@ -35,11 +38,13 @@ class Pixiv(Plugin):
         )
     )
 
-    def __init__(self, url):
-        super(Pixiv, self).__init__(url)
-        self.headers = {
-            "User-Agent": useragents.FIREFOX
-        }
+    login_url_get = "https://accounts.pixiv.net/login"
+    login_url_post = "https://accounts.pixiv.net/api/login"
+
+    options = PluginOptions({
+        "username": None,
+        "password": None
+    })
 
     @classmethod
     def can_handle_url(cls, url):
@@ -47,7 +52,7 @@ class Pixiv(Plugin):
 
     def find_videopage(self):
         self.logger.debug("Not a videopage")
-        res = http.get(self.url, headers=self.headers)
+        res = http.get(self.url)
 
         m = self._videopage_re.search(res.text)
         if not m:
@@ -58,12 +63,46 @@ class Pixiv(Plugin):
         self.logger.debug("Found new path: {0}".format(path))
         return urljoin(self.url, path)
 
+    def _login(self, username, password):
+        res = http.get(self.login_url_get)
+        m = self._post_key_re.search(res.text)
+        if not m:
+            raise PluginError("Missing post_key, no login posible.")
+
+        post_key = m.group("data")
+        data = {
+            "lang": "en",
+            "source": "sketch",
+            "post_key": post_key,
+            "pixiv_id": username,
+            "password": password,
+        }
+
+        res = http.post(self.login_url_post, data=data)
+        res = http.json(res)
+
+        if res["body"].get("success"):
+            return True
+        else:
+            return False
+
     def _get_streams(self):
+        http.headers = {"User-Agent": useragents.FIREFOX}
+
+        login_username = self.get_option("username")
+        login_password = self.get_option("password")
+        if login_username and login_password:
+            self.logger.debug("Attempting login as {0}".format(login_username))
+            if self._login(login_username, login_password):
+                self.logger.info("Successfully logged in as {0}".format(login_username))
+            else:
+                self.logger.info("Failed to login as {0}".format(login_username))
+
         videopage = self._url_re.match(self.url).group("videopage")
         if not videopage:
             self.url = self.find_videopage()
 
-        data = http.get(self.url, headers=self.headers, schema=self._data_schema)
+        data = http.get(self.url, schema=self._data_schema)
 
         if not data.get("LiveStore"):
             self.logger.debug("No video url found, stream might be offline.")
