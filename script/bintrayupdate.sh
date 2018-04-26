a9e012facf569e86112e9fe92bd09ec105506a61
beardypig <beardypig@users.noreply.github.com>
2018-04-26 15:01:22 +0200

    deploy: use jq instead of grep for json (thanks @bastimeyer)

--- script/bintrayupdate.sh
+++ script/bintrayupdate.sh
@@ -12,7 +12,7 @@ versions_api_url="${api_base}/versions"
 curl -u "${BINTRAY_USER}:${BINTRAY_KEY}" -H "Content-Type: application/json" -d @"${build_dir}/bintray-changelog.json" "${current_api_url}"
 
 echo "Deleting versions older than ${old_date}..."
-versions=$(curl -s -u "${BINTRAY_USER}:${BINTRAY_KEY}" "${files_api_url}" | grep -Po '(?<="version":")[^"]+(?=")')
+versions=$(curl -s -u "${BINTRAY_USER}:${BINTRAY_KEY}" "${files_api_url}" | jq -r '.[].version')
 
 for version in $versions; do
     if [[ "$version" < "$old_date" ]]; then
