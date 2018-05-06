3bd24ac4d3039d1a934c2cf0321c3cebe2f26ca9
Igor Piddubnyi <igor.piddubnyi@gmail.com>
2018-05-06 17:44:11 +0200

    Fix review coments

--- src/streamlink/plugins/live_russia_tv.py
+++ src/streamlink/plugins/live_russia_tv.py
@@ -5,8 +5,8 @@ from streamlink.stream import HLSStream
 
 class LiveRussia(Plugin):
     url_re = re.compile(r"https?://(?:www.)?live.russia.tv/index/index/channel_id/")
-    iframe_re = re.compile(r"<iframe[^>]*src=[\"|']([^'\"]+)[\"|'][^>]*>")
-    stream_re = re.compile(r"window.pl.data(.*m3u8\":\"(.*)\"}.*)};")
+    iframe_re = re.compile(r"""<iframe[^>]*src=["']([^'"]+)["'][^>]*>""")
+    stream_re = re.compile(r"""window.pl.data.*m3u8":"(.*)"}.*};""")
 
     @classmethod
     def can_handle_url(cls, url):
@@ -14,9 +14,16 @@ class LiveRussia(Plugin):
 
     def _get_streams(self):
         res = http.get(self.url)
-        iframe_url = re.search(self.iframe_re, res.text).group(1)
+        iframe_url = self.check_re(self.iframe_re, res.text)
         res = http.get(iframe_url)
-        stream_url = re.search(self.stream_re, res.text).group(2)
+        stream_url = self.check_re(self.stream_re, res.text)
         return HLSStream.parse_variant_playlist(self.session, stream_url)
+
+    def check_re(self, search, text):
+        re_result = re.search(search, text)
+        if re_result:
+            return re_result.group(1)
+        self.logger.error("The requested content is unavailable.")
+
 
 __plugin__ = LiveRussia
