5b25abfbd991056ad9f76d8161d3a9823bae6136
beardypig <beardypig@users.noreply.github.com>
2018-05-16 21:14:38 +0200

    plugins.tf1: add lci url to the main tf1 domain (#1660)
    * plugins.tf1: add lci url to the main tf1 domain
    
    * plugins.tf1: removed hard-coded list of channels
    
    Removed the hardcoded list of channel name and added the maps for the
    old channels. Better error messages are produced if new channels are added
    or others renamed. For example when LCI was added, without the hardcoded
    list would have worked.

--- tests/test_plugin_tf1.py
+++ tests/test_plugin_tf1.py
@@ -12,11 +12,11 @@ class TestPluginTF1(unittest.TestCase):
         self.assertTrue(TF1.can_handle_url("http://lci.fr/direct"))
         self.assertTrue(TF1.can_handle_url("http://www.lci.fr/direct"))
         self.assertTrue(TF1.can_handle_url("http://tf1.fr/tmc/direct"))
+        self.assertTrue(TF1.can_handle_url("http://tf1.fr/lci/direct"))
 
+    def test_can_handle_url_negative(self):
         # shouldn't match
         self.assertFalse(TF1.can_handle_url("http://tf1.fr/direct"))
-#       self.assertFalse(TF1.can_handle_url("http://tf1.fr/nt1/direct")) NOTE : TF1 redirect old channel names to new ones (for now).
-#       self.assertFalse(TF1.can_handle_url("http://tf1.fr/hd1/direct"))
         self.assertFalse(TF1.can_handle_url("http://www.tf1.fr/direct"))
         self.assertFalse(TF1.can_handle_url("http://www.tvcatchup.com/"))
         self.assertFalse(TF1.can_handle_url("http://www.youtube.com/"))
