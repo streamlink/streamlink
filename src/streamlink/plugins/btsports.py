95ffb4e388124e84c3ad1020b96abdb076664832
beardypig <beardypig@users.noreply.github.com>
2018-05-17 18:28:36 +0200

    plugin.btsports: update to use PluginArguments

--- src/streamlink/plugins/btsports.py
+++ src/streamlink/plugins/btsports.py
@@ -3,7 +3,7 @@ import time
 from uuid import uuid4
 
 from streamlink.compat import quote
-from streamlink.plugin import Plugin, PluginOptions
+from streamlink.plugin import Plugin, PluginArguments, PluginArgument
 from streamlink.plugin.api import http, useragents
 from streamlink.stream import HLSStream
 from streamlink.utils import url_equal
@@ -11,10 +11,22 @@ from streamlink.utils import url_equal
 
 class BTSports(Plugin):
     url_re = re.compile(r"https?://sport.bt.com")
-    options = PluginOptions({
-        "username": None,
-        "password": None
-    })
+
+    arguments = PluginArguments(
+        PluginArgument(
+            "email",
+            requires=["password"],
+            metavar="EMAIL",
+            required=True,
+            help="The email associated with your BT Sport account, required to access any BT Sport stream."
+        ),
+        PluginArgument(
+            "password",
+            sensitive=True,
+            metavar="PASSWORD",
+            help="Your BT Sport account password."
+        )
+    )
 
     content_re = re.compile(r"CONTENT_(\w+)\s*=\s*'(\w+)'")
     saml_re = re.compile(r'''name="SAMLResponse" value="(.*?)"''', re.M | re.DOTALL)
