905e73d113da193e8126646329b1611540a6ef45
beardypig <beardypig@protonmail.com>
2018-06-04 00:35:46 +0200

    plugins.facebook: sd_src|hd_src can contain non-dash streams
    Uses the playlist regex as a fallback if the other method fails.

--- src/streamlink/plugins/facebook.py
+++ src/streamlink/plugins/facebook.py
@@ -8,7 +8,7 @@ from streamlink.utils import parse_json
 
 class Facebook(Plugin):
     _url_re = re.compile(r"https?://(?:www\.)?facebook\.com/[^/]+/videos")
-    _mpd_re = re.compile(r'''(sd|hd)_src["']?\s*:\s*(?P<quote>["'])(?P<url>.+?)(?P=quote)''')
+    _src_re = re.compile(r'''(sd|hd)_src["']?\s*:\s*(?P<quote>["'])(?P<url>.+?)(?P=quote)''')
     _playlist_re = re.compile(r'''video:\[({url:".+?}\])''')
     _plurl_re = re.compile(r'''url:"(.*?)"''')
 
@@ -19,19 +19,35 @@ class Facebook(Plugin):
     def _get_streams(self):
         res = http.get(self.url, headers={"User-Agent": useragents.CHROME})
 
-        for match in self._mpd_re.finditer(res.text):
-            manifest_url = match.group("url")
-            if "\\/" in manifest_url:
+        streams = {}
+        vod_urls = set([])
+
+        for match in self._src_re.finditer(res.text):
+            stream_url = match.group("url")
+            if "\\/" in stream_url:
                 # if the URL is json encoded, decode it
-                manifest_url = parse_json("\"{}\"".format(manifest_url))
-            for s in DASHStream.parse_manifest(self.session, manifest_url).items():
-                yield s
-        else:
-            match = self._playlist_re.search(res.text)
-            playlist = match and match.group(1)
-            if playlist:
-                for url in {url.group(1) for url in self._plurl_re.finditer(playlist)}:
-                    yield "live", HTTPStream(self.session, url)
+                stream_url = parse_json("\"{}\"".format(stream_url))
+            if ".mpd" in stream_url:
+                streams.update(DASHStream.parse_manifest(self.session, stream_url))
+            elif ".mp4" in stream_url:
+                streams[match.group(1)] = HTTPStream(self.session, stream_url)
+                vod_urls.add(stream_url)
+            else:
+                self.logger.debug("Non-dash/mp4 stream: {0}".format(stream_url))
+
+        if streams:
+            return streams
+
+        # fallback on to playlist
+        self.logger.debug("Falling back to playlist regex")
+        match = self._playlist_re.search(res.text)
+        playlist = match and match.group(1)
+        if playlist:
+            for url in {url.group(1) for url in self._plurl_re.finditer(playlist)}:
+                if url not in vod_urls:
+                    streams["sd"] = HTTPStream(self.session, url)
+
+        return streams
 
 
 __plugin__ = Facebook
