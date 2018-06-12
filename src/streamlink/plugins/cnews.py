98af5f3b9bea29addd2f2442664b28d606b15f64
BZHDeveloper <inizan.yannick@gmail.com>
2018-06-12 18:33:54 +0200

    plugins.cnews : fix regular expressions.

--- src/streamlink/plugins/cnews.py
+++ src/streamlink/plugins/cnews.py
@@ -7,8 +7,8 @@ from streamlink.stream import HDSStream, HLSStream, HTTPStream
 
 class CNEWS(Plugin):
     _url_re = re.compile(r'(https|http)://www.cnews.fr/(.*)')
-    _embed_video_url_re = re.compile(r'class="dm-video-embed_video" src="(?P<dm_url>)"')
-    _embed_live_url_re = re.compile(r'class="wrapper-live-player main-live-player"><iframe src="(?P<dm_url>)"')
+    _embed_video_url_re = re.compile(r'class="dm-video-embed_video" src="(?P<dm_url>.*)"')
+    _embed_live_url_re = re.compile(r'class="wrapper-live-player main-live-player"><iframe src="(?P<dm_url>.*)"')
 
     @classmethod
     def can_handle_url(cls, url):
