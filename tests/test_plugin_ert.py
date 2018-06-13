44192d79eac78d138cb7c4464c90868e49eab4d4
Twilight0 <twilight@freemail.gr>
2018-06-13 15:53:41 +0300

    Correct urls for unittests

--- tests/test_plugin_ert.py
+++ tests/test_plugin_ert.py
@@ -6,9 +6,9 @@ from streamlink.plugins.ert import Ert
 class TestPluginErt(unittest.TestCase):
     def test_can_handle_url(self):
         should_match = [
-            'http://webtv.ert.gr/ert1/',
-            'http://webtv.ert.gr/ert2/',
-            'http://webtv.ert.gr/ert3/',
+            'http://webtv.ert.gr/ert1-live/',
+            'http://webtv.ert.gr/ert2-live/',
+            'http://webtv.ert.gr/ert3-live/',
             'http://webtv.ert.gr/ertworld-live/',
             'http://webtv.ert.gr/ert-play-live/',
             'http://webtv.ert.gr/ert-play-2-live/',
