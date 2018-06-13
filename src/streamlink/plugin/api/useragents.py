7a501ea562e19abdf70ed383e4822c3e38fdb2fd
Twilight0 <twilight@freemail.gr>
2018-06-13 16:50:38 +0300

    Addition of random user agent string function
    Can be utilized to masquerade streamlink and python installation to multiple browser possibilities.

--- src/streamlink/plugin/api/useragents.py
+++ src/streamlink/plugin/api/useragents.py
@@ -22,3 +22,41 @@ SAFARI_7 = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) "
 FIREFOX = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:52.0) Gecko/20100101 Firefox/52.0"
 FIREFOX_MAC = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0"
 OPERA = "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"
+
+
+def random_agent():
+
+    import random
+    from streamlink.compat import range
+
+    BR_VERS = [
+        ['%s.0' % i for i in range(18, 50)],
+        ['37.0.2062.103', '37.0.2062.120', '37.0.2062.124', '38.0.2125.101', '38.0.2125.104', '38.0.2125.111',
+         '39.0.2171.71', '39.0.2171.95', '39.0.2171.99', '40.0.2214.93', '40.0.2214.111', '40.0.2214.115',
+         '42.0.2311.90', '42.0.2311.135', '42.0.2311.152', '43.0.2357.81', '43.0.2357.124', '44.0.2403.155',
+         '44.0.2403.157', '45.0.2454.101', '45.0.2454.85', '46.0.2490.71', '46.0.2490.80', '46.0.2490.86',
+         '47.0.2526.73', '47.0.2526.80', '48.0.2564.116', '49.0.2623.112', '50.0.2661.86', '51.0.2704.103',
+         '52.0.2743.116', '53.0.2785.143', '54.0.2840.71', '61.0.3163.100'],
+        ['11.0'],
+        ['8.0', '9.0', '10.0', '10.6']
+    ]
+
+    WIN_VERS = [
+        'Windows NT 10.0', 'Windows NT 7.0', 'Windows NT 6.3', 'Windows NT 6.2', 'Windows NT 6.1', 'Windows NT 6.0',
+        'Windows NT 5.1', 'Windows NT 5.0'
+    ]
+
+    FEATURES = ['; WOW64', '; Win64; IA64', '; Win64; x64', '']
+
+    RAND_UAS = ['Mozilla/5.0 ({win_ver}{feature}; rv:{br_ver}) Gecko/20100101 Firefox/{br_ver}',
+                'Mozilla/5.0 ({win_ver}{feature}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{br_ver} Safari/537.36',
+                'Mozilla/5.0 ({win_ver}{feature}; Trident/7.0; rv:{br_ver}) like Gecko',
+                'Mozilla/5.0 (compatible; MSIE {br_ver}; {win_ver}{feature}; Trident/6.0)']
+
+    index = random.randrange(len(RAND_UAS))
+
+    return RAND_UAS[
+        index
+    ].format(
+        win_ver=random.choice(WIN_VERS), feature=random.choice(FEATURES), br_ver=random.choice(BR_VERS[index])
+    )
