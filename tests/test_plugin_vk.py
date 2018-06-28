d979c10cd0cca207c0f725a930172e1903f83ff3
back-to <backto@protonmail.ch>
2018-06-28 19:32:27 +0000

    plugins.vk: _url_re update, allow embedded content, plugin cleanup (#1874)
    * plugins.vk: _url_re update, allow embedded content, plugin cleanup
    
    * tests.utils: Removed itertags tests in favor of another PR.

--- tests/test_plugin_vk.py
+++ tests/test_plugin_vk.py
@@ -14,6 +14,10 @@ class TestPluginVK(unittest.TestCase):
             "https://vk.com/videos-24136539?z=video-24136539_456241181%2Fpl_-24136539_-2"),
             "https://vk.com/video-24136539_456241181"
         )
+        self.assertEqual(VK.follow_vk_redirect(
+            "https://vk.com/videos132886594?z=video132886594_167211693"),
+            "https://vk.com/video132886594_167211693"
+        )
 
         # shouldn't redirect
         self.assertEqual(VK.follow_vk_redirect("http://vk.com/"), "http://vk.com/")
@@ -26,6 +30,11 @@ class TestPluginVK(unittest.TestCase):
         self.assertTrue(VK.can_handle_url("http://vk.com/video-24136539_456239830"))
         self.assertTrue(VK.can_handle_url("https://www.vk.com/video-34453259_456240574"))
         self.assertTrue(VK.can_handle_url("https://vk.com/videos-24136539?z=video-24136539_456241155%2Fpl_-24136539_-2"))
+        self.assertTrue(VK.can_handle_url("https://vk.com/video?z=video-15755094_456245149%2Fpl_cat_lives"))
+        self.assertTrue(VK.can_handle_url("https://vk.com/video?z=video132886594_167211693%2Fpl_cat_8"))
+        self.assertTrue(VK.can_handle_url("https://vk.com/video132886594_167211693"))
+        self.assertTrue(VK.can_handle_url("https://vk.com/videos132886594?z=video132886594_167211693"))
+        self.assertTrue(VK.can_handle_url("https://vk.com/video-73154028_456239128"))
 
         # shouldn't match
         self.assertFalse(VK.can_handle_url("https://vk.com/"))
