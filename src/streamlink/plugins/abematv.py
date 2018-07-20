3cb19a12b9eada54678e103f82042a2cfcb412ca
jackyzy823 <jackyzy823@gmail.com>
2018-07-20 10:14:15 +0800

    plugin.abematv: refactor some code according to the advice.

--- src/streamlink/plugins/abematv.py
+++ src/streamlink/plugins/abematv.py
@@ -22,6 +22,11 @@ class AbemaTVLicenseAdapter(BaseAdapter):
     '''
     Handling abematv-license:// protocol to get real video key_data.
     '''
+    SECRETKEY = b"v+Gjs=25Aw5erR!J8ZuvRrCx*rGswhB&qdHd_SYerEWdU&a?3DzN9BRbp5KwY4hEmcj5#fykMjJ=AuWz5GSMY-d@H7DMEh3M@9n2G552Us$$k9cD=3TxwWe86!x#Zyhe"
+    
+    STRTABLE = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+    
+    HMACKEY = b"3AF0298C219469522A313570E8583005A642E73EDD58E3EA2FB7339D3DF1597E"
 
     def __init__(self, session):
         self._plugin_session = session
@@ -33,29 +38,27 @@ class AbemaTVLicenseAdapter(BaseAdapter):
         ts_1hour = (int(time.time()) + 60*60) // 3600 * 3600 # for python3 : floor division
         time_struct = time.gmtime(ts_1hour)
 
-        secretkey = b"v+Gjs=25Aw5erR!J8ZuvRrCx*rGswhB&qdHd_SYerEWdU&a?3DzN9BRbp5KwY4hEmcj5#fykMjJ=AuWz5GSMY-d@H7DMEh3M@9n2G552Us$$k9cD=3TxwWe86!x#Zyhe"
-
-        h = hmac.new(secretkey, digestmod=hashlib.sha256)
-        h.update(secretkey)
+        h = hmac.new(SECRETKEY, digestmod=hashlib.sha256)
+        h.update(SECRETKEY)
         tmp = h.digest()
         for i in range(time_struct.tm_mon):
-            h = hmac.new(secretkey, digestmod=hashlib.sha256)
+            h = hmac.new(SECRETKEY, digestmod=hashlib.sha256)
             h.update(tmp)
             tmp = h.digest()
-        h = hmac.new(secretkey, digestmod=hashlib.sha256)
+        h = hmac.new(SECRETKEY, digestmod=hashlib.sha256)
         h.update(urlsafe_b64encode(tmp).rstrip(b"=")+deviceId)
         tmp = h.digest()
         for i in range(time_struct.tm_mday % 5):
-            h = hmac.new(secretkey, digestmod=hashlib.sha256)
+            h = hmac.new(SECRETKEY, digestmod=hashlib.sha256)
             h.update(tmp)
             tmp = h.digest()
 
-        h = hmac.new(secretkey, digestmod=hashlib.sha256)
+        h = hmac.new(SECRETKEY, digestmod=hashlib.sha256)
         h.update(urlsafe_b64encode(tmp).rstrip(b"=") + str(ts_1hour).encode("utf-8"))
         tmp = h.digest()
 
         for i in range(time_struct.tm_hour % 5):  # utc hour
-            h = hmac.new(secretkey, digestmod=hashlib.sha256)
+            h = hmac.new(SECRETKEY, digestmod=hashlib.sha256)
             h.update(tmp)
             tmp = h.digest()
 
@@ -75,12 +78,11 @@ class AbemaTVLicenseAdapter(BaseAdapter):
         cid = res.json()['cid']
         key = res.json()['k']
 
-        str_table = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
-        res = sum([str_table.find(key[i]) * (58 ** (len(key) - 1 - i))  for i in range(len(key))])
+        res = sum([STRTABLE.find(key[i]) * (58 ** (len(key) - 1 - i))  for i in range(len(key))])
         encVideoKey = struct.pack('>QQ', res >> 64, res & 0xffffffffffffffff)
 
         # Crypto.Cipher.ARC4.new('DB98A8E7CECA3424D975280F90BD03EE'.decode('hex')).decrypt('D4B718BBBA9CFB7D0192A58F9E2D146AFC5DB29E4352DE05FC4CF2C1005804BB'.decode('hex')).encode('hex')
-        h = hmac.new(unhexlify(b"3AF0298C219469522A313570E8583005A642E73EDD58E3EA2FB7339D3DF1597E"), (cid+deviceId).encode("utf-8"), digestmod=hashlib.sha256)
+        h = hmac.new(unhexlify(HMACKEY), (cid+deviceId).encode("utf-8"), digestmod=hashlib.sha256)
         enckey = h.digest()
 
         aes = AES.new(enckey, AES.MODE_ECB)
@@ -96,9 +98,8 @@ class AbemaTVLicenseAdapter(BaseAdapter):
         return resp
 
     def close(self):
-        pass
+        return
 
-    pass
 
 
 class AbemaTV(Plugin):
@@ -107,8 +108,7 @@ class AbemaTV(Plugin):
     Note: Streams are geo-restricted to Japan
 
     '''
-
-    _url_re = re.compile(r"https://abema\.tv/now-on-air/(?P<onair>.+)|https://abema\.tv/video/episode/(?P<episode>.+)|https://abema\.tv/channels/.+?/slots/(?P<slot>.+)")
+    _url_re = re.compile(r"https://abema\.tv/(now-on-air/(?P<onair>.+)|video/episode/(?P<episode>.+)|channels/.+?/slots/(?P<slot>.+))")
 
     @classmethod
     def can_handle_url(cls, url):
