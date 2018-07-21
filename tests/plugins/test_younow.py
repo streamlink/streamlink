a33ccf881f6f48fa701b6918985a220eff732738
back-to <backto@protonmail.ch>
2018-07-21 15:11:02 +0200

    plugins.younow: Fix for session error and plugin cleanup

--- tests/plugins/test_younow.py
+++ tests/plugins/test_younow.py
@@ -1,19 +1,19 @@
 import unittest
 
-from streamlink.plugins.younow import younow
+from streamlink.plugins.younow import YouNow
 
 
-class TestPluginyounow(unittest.TestCase):
+class TestPluginYouNow(unittest.TestCase):
     def test_can_handle_url(self):
         should_match = [
             'https://www.younow.com/example',
         ]
         for url in should_match:
-            self.assertTrue(younow.can_handle_url(url))
+            self.assertTrue(YouNow.can_handle_url(url))
 
     def test_can_handle_url_negative(self):
         should_not_match = [
             'https://example.com/index.html',
         ]
         for url in should_not_match:
-            self.assertFalse(younow.can_handle_url(url))
+            self.assertFalse(YouNow.can_handle_url(url))
