822d53aabafdefcd17db4d52b7e90efffe77a69a
beardypig <beardypig@protonmail.com>
2018-07-31 16:25:21 +0200

    plugins.facebook: support for videos in posts

--- tests/plugins/test_facebook.py
+++ tests/plugins/test_facebook.py
@@ -9,6 +9,7 @@ class TestPluginFacebook(unittest.TestCase):
         self.assertTrue(Facebook.can_handle_url("https://www.facebook.com/nos/videos/1725546430794241/"))
         self.assertTrue(Facebook.can_handle_url("https://www.facebook.com/nytfood/videos/1485091228202006/"))
         self.assertTrue(Facebook.can_handle_url("https://www.facebook.com/SporTurkTR/videos/798553173631138/"))
+        self.assertTrue(Facebook.can_handle_url("https://www.facebook.com/119555411802156/posts/500665313691162/"))
 
         # shouldn't match
         self.assertFalse(Facebook.can_handle_url("https://www.facebook.com"))
