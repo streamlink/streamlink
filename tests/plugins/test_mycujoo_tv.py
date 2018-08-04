2bf12a20bfee93d4d26863d021b5cf8dd849333d
mp107 <mp107@o2.pl>
2018-08-04 14:20:13 +0200

    plugins.mycujootv: Fix URL check regex and test for videos with "vid" identifier

--- tests/plugins/test_mycujoo_tv.py
+++ tests/plugins/test_mycujoo_tv.py
@@ -7,10 +7,16 @@ class TestPluginMycujooTv(unittest.TestCase):
     def test_can_handle_url(self):
         self.assertTrue(MycujooTv.can_handle_url("https://mycujoo.tv/video/example?id=123"))
         self.assertTrue(MycujooTv.can_handle_url("http://mycujoo.tv/video/example?id=123"))
+        self.assertTrue(MycujooTv.can_handle_url("https://mycujoo.tv/video/example?vid=123"))
+        self.assertTrue(MycujooTv.can_handle_url("http://mycujoo.tv/video/example?vid=123"))
         self.assertTrue(MycujooTv.can_handle_url("https://mycujoo.tv/video/example?abc=cde&id=123"))
         self.assertTrue(MycujooTv.can_handle_url("http://mycujoo.tv/video/example?abc=cde&id=123"))
+        self.assertTrue(MycujooTv.can_handle_url("https://mycujoo.tv/video/example?abc=cde&vid=123"))
+        self.assertTrue(MycujooTv.can_handle_url("http://mycujoo.tv/video/example?abc=cde&vid=123"))
         self.assertTrue(MycujooTv.can_handle_url("https://mycujoo.tv/video/example?abc=cde&id=123&fgh=ijk"))
         self.assertTrue(MycujooTv.can_handle_url("http://mycujoo.tv/video/example?abc=cde&id=123&fgh=ijk"))
+        self.assertTrue(MycujooTv.can_handle_url("https://mycujoo.tv/video/example?abc=cde&vid=123&fgh=ijk"))
+        self.assertTrue(MycujooTv.can_handle_url("http://mycujoo.tv/video/example?abc=cde&vid=123&fgh=ijk"))
 
     def test_can_handle_url_negative(self):
         self.assertFalse(MycujooTv.can_handle_url("https://mycujoo.tv/"))
@@ -21,5 +27,9 @@ class TestPluginMycujooTv(unittest.TestCase):
         self.assertFalse(MycujooTv.can_handle_url("http://mycujoo.tv/video/"))
         self.assertFalse(MycujooTv.can_handle_url("https://mycujoo.tv/video/id=123"))
         self.assertFalse(MycujooTv.can_handle_url("http://mycujoo.tv/video/id=123"))
+        self.assertFalse(MycujooTv.can_handle_url("https://mycujoo.tv/video/vid=123"))
+        self.assertFalse(MycujooTv.can_handle_url("http://mycujoo.tv/video/vid=123"))
         self.assertFalse(MycujooTv.can_handle_url("https://mycujoo.tv/video/?id=123"))
         self.assertFalse(MycujooTv.can_handle_url("http://mycujoo.tv/video/?id=123"))
+        self.assertFalse(MycujooTv.can_handle_url("https://mycujoo.tv/video/?vid=123"))
+        self.assertFalse(MycujooTv.can_handle_url("http://mycujoo.tv/video/?vid=123"))
