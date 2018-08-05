65e184559d01efef202d960ccc7c5f897357808b
mp107 <mp107@o2.pl>
2018-08-05 13:03:52 +0200

    plugins.mycujootv: Code refactoring

--- src/streamlink/plugins/mycujoo_tv.py
+++ src/streamlink/plugins/mycujoo_tv.py
@@ -1,39 +1,38 @@
+import logging
 import re
 
+from streamlink.compat import urlparse
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import useragents
 from streamlink.stream import HLSStream
 
+log = logging.getLogger(__name__)
 
 class MycujooTv(Plugin):
-    '''
+    """
     Support for live and archived transmission of the matches and channels on mycujoo.tv
-    '''
+    """
     url_re = re.compile(r'https?://mycujoo\.tv/video/')
-    streams_re = re.compile(r'meta .*?"twitter:player:stream" .*?content="((?:http(s)?:)?//[^"]*?)"') 
+    streams_re = re.compile(r'"filename"\:.*?"([^"]*?)"')
 
     @classmethod
     def can_handle_url(cls, url):
         return cls.url_re.match(url) is not None
 
     def _get_streams(self):
-        # get URL content
+        stream_url = None
+        url = None
         res = self.session.http.get(self.url)
-        # find stream url
         streams = self.streams_re.search(res.text)
-        streams_url = streams and streams.group(1)
-        if streams_url:
-            self.logger.debug("Found streams URL: {}", streams_url)
-            streams = HLSStream.parse_variant_playlist(self.session, streams_url)
-            if not streams:
-                self.logger.debug("Play whole m3u8 file")
-                yield 'live', HLSStream(self.session, video_url)
-            else:
-                self.logger.debug("Play single resolution stream")
-                for s in streams.items():
-                    yield s
-        else:
-            self.logger.error("Could not find the stream URL")
+
+        if streams:
+            stream_url = streams.group(1).replace("\/", "/")
+            url = urlparse(stream_url)
+
+        if url and ".m3u8" in url.path:
+            log.debug("Found stream URL: {0}".format(url.geturl()))
+            for s in HLSStream.parse_variant_playlist(self.session, stream_url).items():
+                yield s
+        log.error("Could not find the stream URL")
 
 
 __plugin__ = MycujooTv
