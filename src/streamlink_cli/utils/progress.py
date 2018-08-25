45aa5e864cfc4b5b1013209aedc1f3cbcca01e38
jackyzy823 <jackyzy823@gmail.com>
2018-08-25 16:31:17 +0800

    [cli.util] use more accurate character width data from unicode documents and add tests.

--- src/streamlink_cli/utils/progress.py
+++ src/streamlink_cli/utils/progress.py
@@ -14,22 +14,38 @@ PROGRESS_FORMATS = (
     "[download] {written}"
 )
 
+# widths generated from
+# http://www.unicode.org/Public/4.0-Update/EastAsianWidth-4.0.0.txt
+
+
+widths = [
+    (13, 1),    (15, 0),    (126, 1),   (159, 0),   (687, 1),   (710, 0),
+    (711, 1),   (727, 0),   (733, 1),   (879, 0),   (1154, 1),  (1161, 0),
+    (4347, 1),  (4447, 2),  (7467, 1),  (7521, 0),  (8369, 1),  (8426, 0),
+    (9000, 1),  (9002, 2),  (11021, 1), (12350, 2), (12351, 1), (12438, 2),
+    (12442, 0), (19893, 2), (19967, 1), (55203, 2), (63743, 1), (64106, 2),
+    (65039, 1), (65059, 0), (65131, 2), (65279, 1), (65376, 2), (65500, 1),
+    (65510, 2), (120831, 1), (262141, 2), (1114109, 1)
+]
+
+
+def get_width(o):
+    """Returns the screen column width for unicode ordinal."""
+    for num, wid in widths:
+        if o <= num:
+            return wid
+    return 1
+
 
 def terminal_width(value):
-    """Returns the length of the string it would be when displayed.
-
-    East Asian Characters take 2 cells in terminal.
-    Other Characters take 1 cell in terminal.
-    """
+    """Returns the width of the string it would be when displayed."""
     if isinstance(value, bytes):
         value = value.decode("utf8", "ignore")
-    # F: FullWidth W: Wide A: Ambiguous
-    return sum([2 if east_asian_width(i) in ('F', 'W', 'A')
-                else 1 for i in value])
+    return sum(map(get_width, map(ord, value)))
 
 
 def get_cut_prefix(value, max_len):
-    """Drop Characters by unicode not by bytes."""
+    """Drops Characters by unicode not by bytes."""
     should_convert = isinstance(value, bytes)
     if should_convert:
         value = value.decode("utf8", "ignore")
