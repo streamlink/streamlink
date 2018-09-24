79d25290dc9d128a54c64922fe5a8586f1c07142
Twilight0 <twilight@freemail.gr>
2018-09-24 20:17:57 +0300

    plugins.ssh101: Fixed plugin (#1916)
    - new playlist regex
    - open the playlist URL within the plugin,
      the channel is offline if the returned data is less than 10
    
    closes https://github.com/streamlink/streamlink/issues/1176

--- tests/plugins/test_ssh101.py
+++ tests/plugins/test_ssh101.py
@@ -7,6 +7,8 @@ class TestPluginSSH101(unittest.TestCase):
     def test_can_handle_url(self):
         should_match = [
             'http://ssh101.com/live/sarggg',
+            'https://www.ssh101.com/securelive/index.php?id=aigaiotvlive',
+            'https://www.ssh101.com/live/aigaiotvlive',
         ]
         for url in should_match:
             self.assertTrue(SSH101.can_handle_url(url))
@@ -14,6 +16,7 @@ class TestPluginSSH101(unittest.TestCase):
     def test_can_handle_url_negative(self):
         should_not_match = [
             'https://example.com/index.html',
+            'https://ssh101.com/m3u8/dyn/aigaiotvlive/index.m3u8',
         ]
         for url in should_not_match:
             self.assertFalse(SSH101.can_handle_url(url))
