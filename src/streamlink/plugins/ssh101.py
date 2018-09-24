3364b7cb81fafdaf012171d29e8eb78f149b1a6f
Twilight0 <twilight@freemail.gr>
2018-09-24 20:17:57 +0300

    plugins.ssh101: Fixed plugin (#1916)
    - new playlist regex
    - open the playlist URL within the plugin,
      the channel is offline if the returned data is less than 10
    
    closes https://github.com/streamlink/streamlink/issues/1176

--- src/streamlink/plugins/ssh101.py
+++ src/streamlink/plugins/ssh101.py
@@ -1,20 +1,22 @@
+import logging
 import re
 
 from streamlink.plugin import Plugin
 from streamlink.stream import HLSStream
 from streamlink.compat import urljoin
 
+log = logging.getLogger(__name__)
+
 
 class SSH101(Plugin):
-    url_re = re.compile(r"https?://(?:\w+\.)?ssh101\.com/(.+)(/vod)?")
-    src_re = re.compile(r'''source.*?src="(?P<url>.*?)"''')
-    iframe_re = re.compile(r'''iframe.*?src="(?P<url>.*?)"''')
+    url_re = re.compile(r'https?://(?:www\.)?ssh101\.com/(?:secure)?live/')
+    src_re = re.compile(r'sources.*?src:\s"(?P<url>.*?)"')
+    iframe_re = re.compile(r'iframe.*?src="(?P<url>.*?)"')
 
     @classmethod
     def can_handle_url(cls, url):
         return cls.url_re.match(url)
 
-    @Plugin.broken(1176)
     def _get_streams(self):
         res = self.session.http.get(self.url)
 
@@ -28,7 +30,17 @@ class SSH101(Plugin):
         stream_src = video and video.group("url")
 
         if stream_src and stream_src.endswith("m3u8"):
-            return HLSStream.parse_variant_playlist(self.session, stream_src)
+            # do not open empty m3u8 files
+            if len(self.session.http.get(stream_src).text) <= 10:
+                log.error("This stream is currently offline")
+                return
+
+            log.debug("URL={0}".format(stream_src))
+            streams = HLSStream.parse_variant_playlist(self.session, stream_src)
+            if not streams:
+                return {"live": HLSStream(self.session, stream_src)}
+            else:
+                return streams
 
 
 __plugin__ = SSH101
