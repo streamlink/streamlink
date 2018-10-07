a3dffc4a5fe19f3811ae98638908792244c2e450
Petar Kukolj <petarkukolj3@yahoo.com>
2018-10-07 02:38:19 +0200

    plugins.ok_live: Changed URL regex to support VoDs

--- tests/plugins/test_ok_live.py
+++ tests/plugins/test_ok_live.py
@@ -9,6 +9,7 @@ class TestPluginOK_live(unittest.TestCase):
         self.assertTrue(OK_live.can_handle_url("https://ok.ru/live/12345"))
         self.assertTrue(OK_live.can_handle_url("http://ok.ru/live/12345"))
         self.assertTrue(OK_live.can_handle_url("http://www.ok.ru/live/12345"))
+        self.assertTrue(OK_live.can_handle_url("https://ok.ru/video/266205792931"))
 
         # shouldn't match
         self.assertFalse(OK_live.can_handle_url("http://www.tvcatchup.com/"))
