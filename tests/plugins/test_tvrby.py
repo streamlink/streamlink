d368176c9f2cb1225035169ecc71b4be4e287cc0
catvlad <catvlad98@ya.ru>
2018-12-24 23:38:11 +0300

    Update test_tvrby.py

--- tests/plugins/test_tvrby.py
+++ tests/plugins/test_tvrby.py
@@ -6,26 +6,14 @@ from streamlink.plugins.tvrby import TVRBy
 class TestPluginTVRBy(unittest.TestCase):
     def test_can_handle_url(self):
         # should match
-        self.assertTrue(TVRBy.can_handle_url("http://www.tvr.by/televidenie/belarus-1/"))
-        self.assertTrue(TVRBy.can_handle_url("http://www.tvr.by/televidenie/belarus-1"))
-        self.assertTrue(TVRBy.can_handle_url("http://www.tvr.by/televidenie/belarus-24/"))
-        self.assertTrue(TVRBy.can_handle_url("http://www.tvr.by/televidenie/belarus-24"))
+        self.assertTrue(TVRBy.can_handle_url("https://www.tvr.by/plugines/online-tv-main.php?channel=tv&chan_id=bt1"))
+        self.assertTrue(TVRBy.can_handle_url("https://www.tvr.by/plugines/online-tv-main.php?channel=tv&chan_id=bt2"))
+        self.assertTrue(TVRBy.can_handle_url("https://www.tvr.by/plugines/online-tv-main.php?channel=tv&chan_id=bt3"))
+        self.assertTrue(TVRBy.can_handle_url("https://www.tvr.by/plugines/online-tv-main.php?channel=tv&chan_id=bt5"))
 
         # shouldn't match
         self.assertFalse(TVRBy.can_handle_url("http://www.tv8.cat/algo/"))
         self.assertFalse(TVRBy.can_handle_url("http://www.tvcatchup.com/"))
         self.assertFalse(TVRBy.can_handle_url("http://www.youtube.com/"))
 
-    def test_url_fix(self):
-        self.assertTrue(
-            "http://www.tvr.by/televidenie/belarus-1/",
-            TVRBy("http://www.tvr.by/televidenie/belarus-1/").url)
-        self.assertTrue(
-            "http://www.tvr.by/televidenie/belarus-1/",
-            TVRBy("http://www.tvr.by/televidenie/belarus-1").url)
-        self.assertTrue(
-            "http://www.tvr.by/televidenie/belarus-24/",
-            TVRBy("http://www.tvr.by/televidenie/belarus-24/").url)
-        self.assertTrue(
-            "http://www.tvr.by/televidenie/belarus-24/",
-            TVRBy("http://www.tvr.by/televidenie/belarus-24").url)
+
