44dc01ff1affa9c4d5593e9b659a47c95bc864aa
Roman <w.romankornev@gmail.com>
2019-01-01 19:57:27 +0300

    [FIX] Debug log arguments string

--- src/streamlink_cli/output.py
+++ src/streamlink_cli/output.py
@@ -207,7 +207,7 @@ class PlayerOutput(Output):
 
     def _open_call(self):
         args = self._create_arguments()
-        log.debug(u"Calling: {0}".format(subprocess.list2cmdline(args)))
+        log.debug(u"Calling: {0}".format(args))
         subprocess.call(args,
                         stdout=self.stdout,
                         stderr=self.stderr)
@@ -216,7 +216,7 @@ class PlayerOutput(Output):
         # Force bufsize=0 on all Python versions to avoid writing the
         # unflushed buffer when closing a broken input pipe
         args = self._create_arguments()
-        log.debug(u"Opening subprocess: {0}".format(subprocess.list2cmdline(args)))
+        log.debug(u"Opening subprocess: {0}".format(args))
         self.player = subprocess.Popen(args,
                                        stdin=self.stdin, bufsize=0,
                                        stdout=self.stdout,
