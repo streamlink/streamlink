0250aa45a9f27b8578e34e92dac8de899a81120a
beardypig <beardypig@protonmail.com>
2019-01-03 13:53:16 +0100

    plugin.powerapp: support for "tvs" URLs

--- tests/plugins/test_powerapp.py
+++ tests/plugins/test_powerapp.py
@@ -9,9 +9,10 @@ class TestPluginPowerApp(unittest.TestCase):
             'http://powerapp.com.tr/tv/powertv4k',
             'http://powerapp.com.tr/tv/powerturktv4k',
             'http://powerapp.com.tr/tv/powerEarthTV',
+            'http://www.powerapp.com.tr/tvs/powertv'
         ]
         for url in should_match:
-            self.assertTrue(PowerApp.can_handle_url(url))
+            self.assertTrue(PowerApp.can_handle_url(url), url)
 
     def test_can_handle_url_negative(self):
         should_not_match = [
