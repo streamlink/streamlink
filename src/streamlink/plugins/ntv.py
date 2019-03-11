1a6812057007dc07673202098b52a768c02c8ff0
Vladimir Stavrinov <vstavrinov@gmail.com>
2019-03-11 18:48:01 +0300

    plugins.ntv: major refactoring.
        * Change source url.
        * Rid of insecure exec().
        * Add more channels.

--- src/streamlink/plugins/ntv.py
+++ src/streamlink/plugins/ntv.py
@@ -4,18 +4,20 @@ from streamlink.stream import HLSStream
 
 
 class NTV(Plugin):
-    url_re = re.compile(r"https?://(?:www\.)?ntv.ru/air/$")
+    url_re = re.compile(r'https://www.ntv.ru/air/.*')
 
     @classmethod
     def can_handle_url(cls, url):
         return cls.url_re.match(url) is not None
 
     def _get_streams(self):
-        tns = 'http://www.ntv.ru/services/live/tns/'
-        body = self.session.http.get(tns).text
-        d = {}
-        exec(body, d)
-        mrl = d['hls']
-        return HLSStream.parse_variant_playlist(self.session, mrl)
+        body = self.session.http.get(self.url).text
+        mrl_re = re.compile(r'var camHlsURL = \'(.*)\'')
+        if mrl_re.search(body):
+            MRL = 'http:' + mrl_re.search(body).group(1)
+        else:
+            mrl_re = re.compile(r'var hlsURL = \'(.*)\'')
+            MRL = mrl_re.search(body).group(1)
+        return HLSStream.parse_variant_playlist(self.session, MRL)
 
 __plugin__ = NTV
