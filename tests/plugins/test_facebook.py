ebc18c5318b788632abac9e3cce35c2b821fbab6
Peter Rowlands (변기호) <peter@pmrowla.com>
2019-03-25 02:04:39 +0900

    plugins.facebook: Support manifest strings and tahoe player urls (#2286)
    Immediate playback for DASH streams with separate audio and video
    streams will not be supported until proper SegmentBase handling is
    implemented.
    
    closes https://github.com/streamlink/streamlink/issues/2133
    closes https://github.com/streamlink/streamlink/issues/2164
    closes https://github.com/streamlink/streamlink/pull/2324
    
    WIP for https://github.com/streamlink/streamlink/issues/2168

--- tests/plugins/test_facebook.py
+++ tests/plugins/test_facebook.py
@@ -10,6 +10,7 @@ class TestPluginFacebook(unittest.TestCase):
         self.assertTrue(Facebook.can_handle_url("https://www.facebook.com/nytfood/videos/1485091228202006/"))
         self.assertTrue(Facebook.can_handle_url("https://www.facebook.com/SporTurkTR/videos/798553173631138/"))
         self.assertTrue(Facebook.can_handle_url("https://www.facebook.com/119555411802156/posts/500665313691162/"))
+        self.assertTrue(Facebook.can_handle_url("https://www.facebookcorewwwi.onion/SporTurkTR/videos/798553173631138/"))
 
         # shouldn't match
         self.assertFalse(Facebook.can_handle_url("https://www.facebook.com"))
