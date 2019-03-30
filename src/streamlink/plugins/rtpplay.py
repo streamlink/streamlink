2f1c8f7549409de7de79dac42d184d2c0e7fd498
back-to <backto@protonmail.ch>
2019-03-30 10:53:01 +0100

    plugins.rtpplay: fix _m3u8_re and plugin cleanup
    closes https://github.com/streamlink/streamlink/issues/2379

--- src/streamlink/plugins/rtpplay.py
+++ src/streamlink/plugins/rtpplay.py
@@ -1,41 +1,37 @@
+import logging
 import re
 
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import useragents
 from streamlink.stream import HLSStream
 
+log = logging.getLogger(__name__)
+
 
 class RTPPlay(Plugin):
-    _url_re = re.compile(r"https?:\/\/www.rtp.pt\/play\/")
-    _m3u8_re = re.compile(r"file:(?:\s+)?(?:\'|\")(?P<url>[^\"']+)(?:\'|\")")
+    _url_re = re.compile(r"https?://www\.rtp\.pt/play/")
+    _m3u8_re = re.compile(r"hls:(?:\s+)?(?:\'|\")(?P<url>[^\"']+)(?:\'|\")")
 
     @classmethod
     def can_handle_url(cls, url):
-        return RTPPlay._url_re.match(url)
+        return cls._url_re.match(url) is not None
 
     def _get_streams(self):
+        self.session.http.headers.update({"User-Agent": useragents.CHROME,
+                                          "Referer": self.url})
+        res = self.session.http.get(self.url)
+        m = self._m3u8_re.search(res.text)
+        if not m:
+            log.error("Could not find _m3u8_re")
+            return
 
-        headers = {
-            "User-Agent": useragents.CHROME
-        }
-
-        res = self.session.http.get(self.url, headers=headers)
-
-        url_match = RTPPlay._m3u8_re.search(res.text)
-
-        if url_match:
-            hls_url = url_match.group("url")
-
-            self.logger.debug("Found URL: {0}".format(hls_url))
-
-            try:
-                s = []
-                for s in HLSStream.parse_variant_playlist(self.session, hls_url).items():
-                    yield s
-                if not s:
-                    yield "live", HLSStream(self.session, hls_url)
-            except IOError as err:
-                self.logger.error("Failed to extract streams: {0}", err)
+        hls_url = m.group("url")
+        log.debug("Found URL: {0}".format(hls_url))
+        streams = HLSStream.parse_variant_playlist(self.session, hls_url)
+        if not streams:
+            return {"live": HLSStream(self.session, hls_url)}
+        else:
+            return streams
 
 
 __plugin__ = RTPPlay
