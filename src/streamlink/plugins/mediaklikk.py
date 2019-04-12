1c04d4dc05a7df09b6638edeb25167321c953955
back-to <backto@protonmail.ch>
2019-04-12 21:28:07 +0200

    plugins.Mediaklikk: cleanup

--- src/streamlink/plugins/mediaklikk.py
+++ src/streamlink/plugins/mediaklikk.py
@@ -1,44 +1,45 @@
+import logging
 import re
 
+from streamlink.exceptions import PluginError
 from streamlink.plugin import Plugin
 from streamlink.stream import HLSStream
+from streamlink.utils.url import update_scheme
 
-
-_stream_url_re = re.compile(r"http(s)?://(www\.)?mediaklikk.hu/[\w\-]+\-elo/?")
-_id_re = re.compile(r'.*"streamId":"(\w+)".*')
-_file_re = re.compile(r'.*"file": "([\w\./\\=:\-\?]+)".*')
-_stream_player_url = "https://player.mediaklikk.hu/playernew/player.php?video={stream_id}&noflash=yes"
+log = logging.getLogger(__name__)
 
 
 class Mediaklikk(Plugin):
+    PLAYER_URL = "https://player.mediaklikk.hu/playernew/player.php"
+
+    _url_re = re.compile(r"https?://(?:www\.)?mediaklikk\.hu/[\w\-]+\-elo/?")
+    _id_re = re.compile(r'"streamId":"(\w+)"')
+    _file_re = re.compile(r'"file":\s*"([\w\./\\=:\-\?]+)"')
+
     @classmethod
     def can_handle_url(cls, url):
-        return _stream_url_re.match(url)
-
-    def _get_playlist_url(self):
-        # get the stream id
-        response = self.session.http.get(self.url)
-        match = _id_re.search(response.text)
-        if not match:
-            raise ValueError("Stream ID couldn't be extracted from page, probably time to update the regex.")
-
-        # get the m3u8 file url
-        player_url = _stream_player_url.format(stream_id=match.group(1))
-        response = self.session.http.get(player_url)
-
-        match = _file_re.search(response.text)
-        if match:
-            url = match.group(1).replace("\\/", "/")
-            if url.startswith("//"):
-                url = "https:{}".format(url)
-
-            return url
-
-        raise ValueError("Couldn't extract m3u8 file URL, probably time to update the regex.")
+        return cls._url_re.match(url) is not None
 
     def _get_streams(self):
-        playlist = self._get_playlist_url()
-        return HLSStream.parse_variant_playlist(self.session, playlist)
+        # get the stream id
+        res = self.session.http.get(self.url)
+        m = self._id_re.search(res.text)
+        if not m:
+            raise PluginError("Stream ID could not be extracted.")
+
+        # get the m3u8 file url
+        params = {
+            "video": m.group(1),
+            "noflash": "yes",
+        }
+        res = self.session.http.get(self.PLAYER_URL, params=params)
+        m = self._file_re.search(res.text)
+        if m:
+            url = update_scheme("https://",
+                                m.group(1).replace("\\/", "/"))
+
+            log.debug("URL={0}".format(url))
+            return HLSStream.parse_variant_playlist(self.session, url)
 
 
 __plugin__ = Mediaklikk
