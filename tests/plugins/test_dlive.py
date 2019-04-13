e25f20fed00887b360861ed227e76f8b944ac65d
Pascal Romahn <pascal.romahn@mailbox.org>
2019-04-13 15:38:05 +0200

    Fix name in tests

--- tests/plugins/test_dlive.py
+++ tests/plugins/test_dlive.py
@@ -6,7 +6,7 @@ from streamlink.plugins.dlive import DLive
 class TestPluginDLive(unittest.TestCase):
     def test_can_handle_url(self):
         # should match
-        self.assertTrue(dlive.can_handle_url("https://dlive.tv/pewdiepie"))
+        self.assertTrue(DLive.can_handle_url("https://dlive.tv/pewdiepie"))
 
         # shouldn't match
-        self.assertTrue(dlive.can_handle_url("https://twitch.tv/lirik"))
+        self.assertTrue(DLive.can_handle_url("https://twitch.tv/lirik"))
