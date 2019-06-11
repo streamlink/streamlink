d69b9024e4d095b651dd98c49870dea04d85cbd0
Vladimir Stavrinov <vstavrinov@gmail.com>
2019-06-11 18:53:27 +0300

    plugins.ovvatv: Adopt site changes. - Use html and javascript parsers instead of regex.

--- src/streamlink/plugins/ovvatv.py
+++ src/streamlink/plugins/ovvatv.py
@@ -1,9 +1,5 @@
-from __future__ import print_function
 import re
-import string
 from base64 import b64decode
-from pprint import pprint
-
 from streamlink import PluginError
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import useragents
@@ -11,15 +7,38 @@ from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream
 from streamlink.compat import urlparse
 from streamlink.utils import parse_json
+from html.parser import HTMLParser
+from slimit.parser import Parser
+from slimit.visitors import nodevisitor
+
+
+class Online_Parser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if tag == 'iframe':
+            attrs = dict(attrs)
+            if 'name' in attrs and \
+            attrs['name'] == 'twttrHubFrameSecure':
+                self.iframe_url = attrs['src']
+
+
+class Iframe_Parser(HTMLParser):
+    js = False
+    def handle_starttag(self, tag, attrs):
+        if tag == 'script':
+            attrs = dict(attrs)
+            if 'type' in attrs and attrs['type'] == 'text/javascript':
+                self.js = True
+
+    def handle_data(self, data):
+        if self.js and data.startswith('window.onload'):
+            self.data = data
 
 
 class ovvaTV(Plugin):
-    url_re = re.compile(r"https?://(?:www\.)?ovva.tv/(?:ua/)?tvguide/.*?/online")
-    iframe_re = re.compile(r"iframe .*?src=\"((?:https?:)?//(?:\w+\.)?ovva.tv/[^\"]+)\"", re.DOTALL)
-    data_re = re.compile(r"ovva\(\'(.*?)\'\);")
+    url_re = re.compile(r'https://1plus1.video/tvguide/.*/online')
     ovva_data_schema = validate.Schema({
-        "url": validate.url()
-    }, validate.get("url"))
+        "balancer": validate.url()
+    }, validate.get("balancer"))
     ovva_redirect_schema = validate.Schema(validate.all(
         validate.transform(lambda x: x.split("=")),
         ['302', validate.url()],
@@ -31,31 +50,54 @@ class ovvaTV(Plugin):
         return cls.url_re.match(url) is not None
 
     def find_iframe(self, res):
-        for url in self.iframe_re.findall(res.text):
-            if url.startswith("//"):
-                p = urlparse(self.url)
-                return "{0}:{1}".format(p.scheme, url)
-            else:
-                return url
+        parser = Online_Parser()
+        parser.feed(res.text)
+        url = parser.iframe_url
+        if url.startswith("//"):
+            p = urlparse(self.url)
+            return "{0}:{1}".format(p.scheme, url)
+        else:
+            return url
 
-    @Plugin.broken()
+    def get_data(self, res):
+        parser = Iframe_Parser()
+        parser.feed(res.text)
+        data = parser.data
+        parser = Parser()
+        tree = parser.parse(data)
+        for node in nodevisitor.visit(tree):
+            if isinstance(node, list) and node[0].value == '"ovva-player"':
+                return node[1].value.strip('"')
+
     def _get_streams(self):
         self.session.http.headers = {"User-Agent": useragents.ANDROID}
         res = self.session.http.get(self.url)
         iframe_url = self.find_iframe(res)
-
         if iframe_url:
             self.logger.debug("Found iframe: {0}", iframe_url)
-            res = self.session.http.get(iframe_url, headers={"Referer": self.url})
-            data = self.data_re.search(res.text)
+            res = self.session.http.get(
+                iframe_url, 
+                headers={"Referer": self.url})
+            data = self.get_data(res)
             if data:
                 try:
-                    ovva_url = parse_json(b64decode(data.group(1)).decode("utf8"), schema=self.ovva_data_schema)
-                    stream_url = self.session.http.get(ovva_url, schema=self.ovva_redirect_schema)
+                    ovva_url = parse_json(
+                        b64decode(data).decode(), 
+                        schema=self.ovva_data_schema)
+                    self.logger.debug("Found ovva: {0}", ovva_url)
+
+                    stream_url = self.session.http.get(
+                        ovva_url, 
+                        schema=self.ovva_redirect_schema, 
+                        headers={"Referer": iframe_url})
+                    self.logger.debug("Found stream: {0}", stream_url)
+
                 except PluginError as e:
                     self.logger.error("Could not find stream URL: {0}", e)
                 else:
-                    return HLSStream.parse_variant_playlist(self.session, stream_url)
+                    return HLSStream.parse_variant_playlist(
+                        self.session, 
+                        stream_url)
             else:
                 self.logger.error("Could not find player data.")
 
