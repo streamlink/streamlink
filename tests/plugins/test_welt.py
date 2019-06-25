a94c3a501a1fd18d2ec5372e15d84b75e5a0d3bc
bastimeyer <mail@bastimeyer.de>
2019-06-25 02:17:20 +0200

    plugins.welt: fix plugin

--- tests/plugins/test_welt.py
+++ tests/plugins/test_welt.py
@@ -25,45 +25,22 @@ class TestPluginWelt(unittest.TestCase):
 
     def test_validate_live(self):
         hls_url = Welt._schema.validate("""
-            foo
-            <script>
-            var funkotron = { config: {
-                "page": {
-                    "content": {
-                        "media": [
-                            {
-                                "sources": [
-                                    {
-                                        "file": "https://foo.bar/baz.mp4?qux"
-                                    },
-                                    {
-                                        "file": "https://foo.bar/baz.m3u8?qux"
-                                    }
-                                ]
-                            },
-                            {
-                                "sources": [
-                                    {
-                                        "file": "https://qux.baz/bar.mp4?foo"
-                                    },
-                                    {
-                                        "file": "https://qux.baz/bar.m3u8?foo"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
+            <!DOCTYPE html><html><body>
+            <script type="application/json" data-content="VideoPlayer.Config">
+                {
+                    "title": "foo",
+                    "sources": [
+                        {
+                            "src": "https://foo.bar/baz.mp4?qux",
+                            "extension": "mp4"
+                        },
+                        {
+                            "src": "https://foo.bar/baz.m3u8?qux",
+                            "extension": "m3u8"
+                        }
+                    ]
                 }
-            }}
             </script>
-            bar
+            </body></html>
         """)
         self.assertEqual(hls_url, "https://foo.bar/baz.m3u8?qux", "Finds the correct HLS live URL")
-
-    def test_validate_vod(self):
-        hls_url = Welt._schema_vod.validate("""
-            {
-                "urlWithToken": "https://foo.bar/baz.m3u8?qux"
-            }
-        """)
-        self.assertEqual(hls_url, "https://foo.bar/baz.m3u8?qux", "Finds the correct HLS VOD URL")
