a6de25a7c2e5c6867c3f0a108bb757f8c294e0b1
beardypig <beardypig@protonmail.com>
2019-07-08 13:49:14 +0200

    plugins.ustreamtv: add support for proxying WebSocket connections

--- src/streamlink_cli/argparser.py
+++ src/streamlink_cli/argparser.py
@@ -1070,8 +1070,8 @@ def build_parser():
         "--http-proxy",
         metavar="HTTP_PROXY",
         help="""
-        A HTTP proxy to use for all HTTP requests, by default this proxy
-        will be used for all HTTPS requests too.
+        A HTTP proxy to use for all HTTP requests, including WebSocket connections. 
+        By default this proxy will be used for all HTTPS requests too.
 
         Example: "http://hostname:port/"
         """
