809b8bece0c4cfa1b5ff6f914ffac4865f24a52d
Vladimir Stavrinov <vstavrinov@gmail.com>
2019-07-12 16:00:35 +0300

    plugins.live_russia_tv: improve error handling and debug info

--- src/streamlink/plugins/live_russia_tv.py
+++ src/streamlink/plugins/live_russia_tv.py
@@ -25,8 +25,8 @@ class LiveRussia(Plugin):
     def _get_iframe_url(self, url):
         live = self.url_re.match(self.url)
         channel = live.group(2)
+        log.debug("Channel: {0}".format(channel))
         if channel: 
-            log.debug("channel={0}".format(channel))
             self.channel = channel
             template = "https://live.russia.tv/api/now/channel/{0}"
             return template.format(channel)
@@ -73,40 +73,40 @@ class LiveRussia(Plugin):
         iframe_url = self._get_iframe_url(self.url)
 
         if iframe_url:
-            log.debug("Found iframe URL={0}".format(iframe_url))
+            log.debug("Found iframe URL: {0}".format(iframe_url))
             info_url = self._get_stream_info_url(iframe_url)
+            if info_url:
+                log.debug('Getting info from URL: {0}'.format(info_url))
+                res = self.session.http.get(
+                    info_url, headers={"Referer": iframe_url})
+                data = self.session.http.json(res)
+                if data['status'] == 200:
+                    for media in data['data']['playlist']['medialist']:
+                        if media['errors']:
+                            log.error(media['errors'].replace(
+                                '\n', '').replace('\r', ''))
 
-        if info_url:
-            log.debug('Getting info from URL: {0}'.format(info_url))
-            res = self.session.http.get(
-                info_url, headers={"Referer": iframe_url})
-            data = self.session.http.json(res)
+                        for media_type in media.get('sources', []):
+                            if media_type == "m3u8":
+                                hls_url = media['sources'][media_type]['auto']
+                                log.debug("hls_url={0}".format(hls_url))
+                                for s in HLSStream.parse_variant_playlist(
+                                    self.session, hls_url).items():
+                                    yield s
 
-            if data['status'] == 200:
-                for media in data['data']['playlist']['medialist']:
-                    if media['errors']:
-                        log.error(media['errors'].replace(
-                            '\n', '').replace('\r', ''))
-
-                    for media_type in media.get('sources', []):
-                        if media_type == "m3u8":
-                            hls_url = media['sources'][media_type]['auto']
-                            log.debug("hls_url={0}".format(hls_url))
-                            for s in HLSStream.parse_variant_playlist(
-                                self.session, hls_url).items():
-                                yield s
-
-                        if media_type == "http":
-                            for pix, http_url in media['sources'][
-                                media_type].items():
-                                log.debug("http_url={0}".format(http_url))
-                                yield "{0}p".format(pix), HTTPStream(
-                                    self.session, http_url)
+                            if media_type == "http":
+                                for pix, http_url in media['sources'][
+                                    media_type].items():
+                                    log.debug("http_url={0}".format(http_url))
+                                    yield "{0}p".format(pix), HTTPStream(
+                                        self.session, http_url)
+                else:
+                    log.error("An error occurred: {0}".format(
+                        data['errors'].replace('\n', '').replace('\r', '')))
             else:
-                log.error("An error occurred: {0}".format(
-                    data['errors'].replace('\n', '').replace('\r', '')))
-        else:
-            log.error("Unable to get stream info URL")
+                log.error("Unable to get stream info URL")
 
+        else:
+            log.error("Unable to get iframe URL")
 
 __plugin__ = LiveRussia
