d3acf4982b5a2e6a624def4590846757e7500bd5
Mohamed El Morabity <melmorabity@fedoraproject.org>
2019-07-23 11:11:39 +0200

    plugins.gulli: enable HTTPS support

--- tests/plugins/test_gulli.py
+++ tests/plugins/test_gulli.py
@@ -8,8 +8,8 @@ class TestPluginGulli(unittest.TestCase):
         # should match
         self.assertTrue(Gulli.can_handle_url("http://replay.gulli.fr/Direct"))
         self.assertTrue(Gulli.can_handle_url("http://replay.gulli.fr/dessins-animes/My-Little-Pony-les-amies-c-est-magique/VOD68328764799000"))
-        self.assertTrue(Gulli.can_handle_url("http://replay.gulli.fr/emissions/In-Ze-Boite2/VOD68639028668000"))
-        self.assertTrue(Gulli.can_handle_url("http://replay.gulli.fr/series/Power-Rangers-Dino-Super-Charge/VOD68612908435000"))
+        self.assertTrue(Gulli.can_handle_url("https://replay.gulli.fr/emissions/In-Ze-Boite2/VOD68639028668000"))
+        self.assertTrue(Gulli.can_handle_url("https://replay.gulli.fr/series/Power-Rangers-Dino-Super-Charge/VOD68612908435000"))
 
         # shouldn't match
         self.assertFalse(Gulli.can_handle_url("http://replay.gulli.fr/"))
