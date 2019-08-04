df5541a9234092066518e68e386a23a055d7a885
ssaqua <ssaqua@users.noreply.github.com>
2019-08-05 03:30:11 +1200

    plugins.linelive: new plugin for LINE LIVE (live.line.me) (#2574)
    closes #2573

---
+++ src/streamlink/plugins/linelive.py
@@ -0,0 +1,48 @@
+import re
+
+from streamlink.plugin import Plugin
+from streamlink.plugin.api import validate
+from streamlink.stream import HLSStream
+
+
+class LineLive(Plugin):
+    _url_re = re.compile(r"""
+        https?://live\.line\.me
+        /channels/(?P<channel>\d+)
+        /broadcast/(?P<broadcast>\d+)
+    """, re.VERBOSE)
+
+    _api_url = "https://live-api.line-apps.com/app/v3.2/channel/{0}/broadcast/{1}/player_status"
+
+    _player_status_schema = validate.Schema(
+        {
+            "liveStatus": validate.text,
+            "liveHLSURLs": validate.any(None, {
+                "720": validate.any(None, validate.url(scheme="http", path=validate.endswith(".m3u8"))),
+                "480": validate.any(None, validate.url(scheme="http", path=validate.endswith(".m3u8"))),
+                "360": validate.any(None, validate.url(scheme="http", path=validate.endswith(".m3u8"))),
+                "240": validate.any(None, validate.url(scheme="http", path=validate.endswith(".m3u8"))),
+                "144": validate.any(None, validate.url(scheme="http", path=validate.endswith(".m3u8"))),
+            })
+        }
+     )
+
+    @classmethod
+    def can_handle_url(cls, url):
+        return cls._url_re.match(url) is not None
+
+    def _get_streams(self):
+        match = self._url_re.match(self.url)
+        channel = match.group("channel")
+        broadcast = match.group("broadcast")
+        res = self.session.http.get(self._api_url.format(channel, broadcast))
+        json = self.session.http.json(res, schema=self._player_status_schema)
+        if json["liveStatus"] != "LIVE":
+            return
+        for stream in json["liveHLSURLs"]:
+            url = json["liveHLSURLs"][stream]
+            if url != None:
+                yield "{0}p".format(stream), HLSStream(self.session, url)
+
+
+__plugin__ = LineLive
