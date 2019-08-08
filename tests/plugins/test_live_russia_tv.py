72da57aab6d4692d0007689554c1b400d896016b
Vladimir Stavrinov <vstavrinov@gmail.com>
2019-08-08 17:44:01 +0300

    plugins.live_russia_tv: adjust to site changes (#2523)
    closes #2507

--- tests/plugins/test_live_russia_tv.py
+++ tests/plugins/test_live_russia_tv.py
@@ -6,11 +6,10 @@ from streamlink.plugins.live_russia_tv import LiveRussia
 class TestPluginLiveRussiaTv(unittest.TestCase):
     def test_can_handle_url(self):
         # should match
-        self.assertTrue(LiveRussia.can_handle_url('https://live.russia.tv/index/index/channel_id/1'))
-        self.assertTrue(LiveRussia.can_handle_url('https://live.russia.tv/index/index/channel_id/199'))
+        self.assertTrue(LiveRussia.can_handle_url('https://live.russia.tv/channel/1'))
+        self.assertTrue(LiveRussia.can_handle_url('https://live.russia.tv/channel/199'))
         self.assertTrue(LiveRussia.can_handle_url('https://live.russia.tv/'))
-        self.assertTrue(LiveRussia.can_handle_url('http://live.russia.tv/somethingelse'))
-        self.assertTrue(LiveRussia.can_handle_url('https://russia.tv/fifaworldcup/games/1832668/video/1800840'))
+        self.assertTrue(LiveRussia.can_handle_url('https://live.russia.tv/video/show/brand_id/60473/episode_id/2187772/video_id/2204594'))
 
     def test_can_handle_url_negative(self):
         self.assertFalse(LiveRussia.can_handle_url('http://live.france.tv/somethingelse'))
