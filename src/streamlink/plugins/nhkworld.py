bf1a951218fffada02947fa188a359ca552fa495
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2019-12-10 16:55:03 +0300

    plugins.nhkworld: the site migrates from xml to json stream data

--- src/streamlink/plugins/nhkworld.py
+++ src/streamlink/plugins/nhkworld.py
@@ -3,15 +3,11 @@
 import re
 
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream
 
-API_URL = "http://{}.nhk.or.jp/nhkworld/app/tv/hlslive_web.xml"
+API_URL = "http://{}.nhk.or.jp/nhkworld/app/tv/hlslive_web.json"
 
 _url_re = re.compile(r"http(?:s)?://(?:(\w+)\.)?nhk.or.jp/nhkworld")
-_schema = validate.Schema(
-    validate.xml_findtext("./main_url/wstrm")
-)
 
 
 class NHKWorld(Plugin):
@@ -20,11 +16,11 @@ class NHKWorld(Plugin):
         return _url_re.match(url) is not None
 
     def _get_streams(self):
-        # get the HLS xml from the same sub domain as the main url, defaulting to www
+        # get the HLS json from the same sub domain as the main url, defaulting to www
         sdomain = _url_re.match(self.url).group(1) or "www"
         res = self.session.http.get(API_URL.format(sdomain))
 
-        stream_url = self.session.http.xml(res, schema=_schema)
+        stream_url = self.session.http.json(res)['main']['wstrm']
         return HLSStream.parse_variant_playlist(self.session, stream_url)
 
 
