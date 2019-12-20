bd618ac040aec522b3c61bdb1e8c2e536d24dc13
BuildTools <unconfigured@null.spigotmc.org>
2019-12-20 13:06:46 -0500

    added vod, logging, json validation, refactors

--- src/streamlink/plugins/pluto.py
+++ src/streamlink/plugins/pluto.py
@@ -1,35 +1,109 @@
 import re
-
+import logging
 from uuid import uuid4
 
 from streamlink.plugin import Plugin
 from streamlink.stream import HLSStream
+from streamlink.plugin.api import validate
 
-import logging
 log = logging.getLogger(__name__)
 
 
 class Pluto(Plugin):
-    url_re = re.compile(r'https?://(?:www\.)?pluto\.tv/live-tv/(?P<slug>[a-zA-Z0-9-]+)')
+    url_re_live = re.compile(r'^https?://(?:www\.)?pluto\.tv/live-tv/(?P<slug>[a-zA-Z0-9()\.-]+)$')
+    url_re_tv = re.compile(r'^https?://(?:www\.)?pluto\.tv/on-demand/series/(?P<series_slug>[a-zA-Z0-9()\.-]+)/season/\d+/episode/(?P<episode_slug>[a-zA-Z0-9()\.-]+)$')
+    url_re_movie = re.compile(r'^https?://(?:www\.)?pluto\.tv/on-demand/movies/(?P<slug>[a-zA-Z0-9()\.-]+)$')
 
-    api_url = 'https://api.pluto.tv/v2/channels'
+    api_url_live_channels = 'https://api.pluto.tv/v2/channels'
+    api_url_tv_series_template = 'http://api.pluto.tv/v3/vod/slugs/%s'
+    api_url_movies = 'https://api.pluto.tv/v3/vod/categories?includeItems=true&deviceType=web'
+
+    media_schema = {
+        'slug': validate.text,
+        validate.optional('stitched'): {
+            'urls': [{
+                'url': validate.url()
+            }]
+        }
+    }
+
+    live_channels_schema = validate.Schema([
+        media_schema
+    ])
+
+    tv_schema = validate.Schema({
+        'seasons': [{
+            'episodes': [
+                media_schema
+            ]
+        }]
+    })
+
+    movies_schema = validate.Schema({
+        'categories': [{
+            'items': [
+                media_schema
+            ]
+        }]
+    })
 
     @classmethod
     def can_handle_url(cls, url):
-        return cls.url_re.match(url) is not None
+        return (
+            re.match(cls.url_re_live, url)
+            or re.match(cls.url_re_tv, url)
+            or re.match(cls.url_re_movie, url)
+        )
 
     def _get_streams(self):
-        slug = self.url_re.match(self.url).groups('slug').lower()
+        match = None
 
-        channels_res = self.session.http.get(self.api_url)
-        channels_data = channels_res.json()
-        channel_match = next(filter(lambda x: x['slug'] == slug, channels_data), None)
+        # live TV
+        if re.match(self.url_re_live, self.url):
+            slug = re.match(self.url_re_live, self.url).group('slug').lower()
 
-        if not channel_match:
-            log.error('Channel %s not found. It may have been removed.' % slug)
+            channels_res = self.session.http.get(self.api_url_live_channels)
+            channels_data = self.session.http.json(channels_res, schema=self.live_channels_schema)
+
+            match = next(filter(lambda channel: channel['slug'] == slug, channels_data), None)
+
+        # On-demand TV
+        elif re.match(self.url_re_tv, self.url):
+            re_match = re.match(self.url_re_tv, self.url)
+            series_slug = re_match.group('series_slug')
+            episode_slug = re_match.group('episode_slug')
+
+            tv_series_res = self.session.http.get(self.api_url_tv_series_template % series_slug)
+            tv_series_data = self.session.http.json(tv_series_res, schema=self.tv_schema)
+
+            for season in tv_series_data['seasons']:
+                for episode in season['episodes']:
+                    if episode['slug'] == episode_slug:
+                        match = episode
+
+        # On-demand movie
+        elif re.match(self.url_re_movie, self.url):
+            slug = re.match(self.url_re_movie, self.url).group('slug').lower()
+
+            movies_res = self.session.http.get(self.api_url_movies)
+            movies_data = self.session.http.json(movies_res, schema=self.movies_schema)
+
+            for category in movies_data['categories']:
+                for movie in category['items']:
+                    if movie['slug'] == slug:
+                        match = movie
+
+        return self.handle_media_match(match)
+
+    def handle_media_match(self, match):
+        if not match:
             return
 
-        stream_link_no_sid = channel_match['stitched']['urls'][0]['url']
+        if 'stitched' not in match:
+            log.error('No streams found. This video may use an unsupported format.')
+            return
+
+        stream_link_no_sid = match['stitched']['urls'][0]['url']
         sid = str(uuid4())
         stream_link = stream_link_no_sid.replace('&sid=', '&sid=' + sid)
 
