26fc271a6680d821b1fe97208d794596db6f1765
back-to <backto@protonmail.ch>
2020-03-21 15:05:17 +0100

    plugins.kanal7: Removed Plugin they use static URLs
    closes https://github.com/streamlink/streamlink/issues/2658
    
    Examples:
    
    ```
    streamlink --http-header "Referer=https://www.kanal7avrupa.com/canli-izle" "https://live.kanal7.com/live/kanal7AvrupaLive/index.m3u8" best
    streamlink --http-header "Referer=https://www.kanal7.com/canli-izle" "https://live.kanal7.com/live/kanal7LiveDesktop/index.m3u8" best
    streamlink --http-header "Referer=https://www.ulketv.com.tr/canli-yayin" "https://live.kanal7.com/live/ulkeTvLiveDesktop/index.m3u8" best
    ```

--- script/makeinstaller.sh
+++ script/makeinstaller.sh
@@ -40,7 +40,7 @@ old_files=(
   "aftonbladet" "aliez" "antenna" "arconai" "bongacams" "brittv" "cam4"
   "camsoda" "chaturbate" "expressen" "mips" "seetv" "speedrunslive" "streamboat"
   "vgtv" "weeb" "oldlivestream" "ok_live" "rte" "tvcatchup" "npo" "ovvatv"
-  "mlgtv"
+  "mlgtv" "kanal7"
 )
 for i in "${old_files[@]}"
 do
