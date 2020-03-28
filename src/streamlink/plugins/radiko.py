d58611bfcf2d7f39534f0b7414c1c9ad2f32852b
Lucario <16816842+iucario@users.noreply.github.com>
2020-03-28 21:02:37 +0800

    plugins.radiko: fix url

--- src/streamlink/plugins/radiko.py
+++ src/streamlink/plugins/radiko.py
@@ -1,9 +1,10 @@
-import re
-import random
-import hashlib
 import base64
 import datetime
+import hashlib
+import random
+import re
 import xml.etree.ElementTree as ET
+
 from streamlink.compat import urlencode
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import useragents
@@ -11,10 +12,10 @@ from streamlink.stream import HLSStream
 
 
 class Radiko(Plugin):
-    _url_re = re.compile(r'http://radiko\.jp/(#!/)?(?P<state>live|ts)/(?P<station_id>\w+)/?(?P<start_at>\d+)?')
-    _api_auth_1 = "https://radiko.jp/v2/api/auth1"
-    _api_auth_2 = "https://radiko.jp/v2/api/auth2"
-    _auth_key = "bcd151073c03b352e1ef2fd66c32209da9ca0afa"
+    _url_re = re.compile(r'http://radiko\.jp/(#!/)?(?P<state>live|ts)/(?P<station_id>[a-zA-Z0-9-]+)/?(?P<start_at>\d+)?')
+    _api_auth_1 = 'https://radiko.jp/v2/api/auth1'
+    _api_auth_2 = 'https://radiko.jp/v2/api/auth2'
+    _auth_key = 'bcd151073c03b352e1ef2fd66c32209da9ca0afa'
 
     @classmethod
     def can_handle_url(cls, url):
@@ -23,7 +24,7 @@ class Radiko(Plugin):
     def _get_streams(self):
         match = self._url_re.match(self.url)
         state = match.group('state')
-        station_id = match.group('station_id')
+        station_id = match.group('station_id').upper()
         if state == 'live':
             url, token = self._live(station_id)
         else:
@@ -34,16 +35,15 @@ class Radiko(Plugin):
             'X-Radiko-AuthToken': token
         }
         self.session.http.headers = headers
-        r = self.session.http.get(url)
-        url = r.text.split('\n')[-2]
-        yield 'source', HLSStream(self.session, url)
+        for s in HLSStream.parse_variant_playlist(self.session, url).items():
+            yield s
 
     def _live(self, station_id):
-        live_url = 'https://rpaa.smartstream.ne.jp/so/playlist.m3u8'
+        live_url = f'http://f-radiko.smartstream.ne.jp/{station_id}/_definst_/simul-stream.stream/playlist.m3u8'
         token, area_id = self._authorize()
         lsid = hashlib.md5(str(random.random()).encode('utf-8')).hexdigest()
         live_params = {
-            'station_id': station_id.upper(),
+            'station_id': station_id,
             'l': 15,
             'lsid': lsid,
             'type': 'b'
@@ -52,7 +52,7 @@ class Radiko(Plugin):
         return url, token
 
     def _timefree(self, station_id, start_at):
-        m3u8_url = 'https://tf-rpaa.smartstream.ne.jp/tf/playlist.m3u8'
+        m3u8_url = 'https://radiko.jp/v2/api/ts/playlist.m3u8'
         token, area_id = self._authorize()
         lsid = hashlib.md5(str(random.random()).encode('utf-8')).hexdigest()
         end_at = self._get_xml(start_at, station_id)
@@ -79,9 +79,9 @@ class Radiko(Plugin):
         }
         self.session.http.headers.update(headers)
         r = self.session.http.get(self._api_auth_1)
-        token = r.headers.get("x-radiko-authtoken")
-        offset = int(r.headers.get("x-radiko-keyoffset"))
-        length = int(r.headers.get("x-radiko-keylength"))
+        token = r.headers.get('x-radiko-authtoken')
+        offset = int(r.headers.get('x-radiko-keyoffset'))
+        length = int(r.headers.get('x-radiko-keylength'))
         partial_key = base64.b64encode(self._auth_key[offset:offset + length].encode('ascii')).decode('utf-8')
         headers = {
             'x-radiko-authtoken': token,
@@ -101,8 +101,7 @@ class Radiko(Plugin):
             date = yesterday.strftime('%Y%m%d')
         else:
             date = today.strftime('%Y%m%d')
-        api = 'http://radiko.jp/v3/program/station/date/{}/{}.xml'.format(
-            date, station_id)
+        api = f'http://radiko.jp/v3/program/station/date/{date}/{station_id}.xml'
         r = self.session.http.get(api)
         tree = ET.XML(r.content)
         for x in tree[2][0][1].findall('prog'):
