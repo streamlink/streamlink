4ce33b3f85cf86cbb49f2e816a5a0486d2cd15b0
Riolu <16816842+iucario@users.noreply.github.com>
2020-03-29 17:11:06 +0800

    plugins.radiko: Add support for radiko.jp (#2826)

---
+++ tests/plugins/test_radiko.py
@@ -0,0 +1,22 @@
+import unittest
+
+from streamlink.plugins.radiko import Radiko
+
+
+class TestPluginRadiko(unittest.TestCase):
+    def test_can_handle_url(self):
+        should_match = [
+            'http://radiko.jp/#!/live/QRR',
+            'http://radiko.jp/live/QRR',
+            'http://radiko.jp/#!/ts/QRR/20200308180000',
+            'http://radiko.jp/ts/QRR/20200308180000'
+        ]
+        for url in should_match:
+            self.assertTrue(Radiko.can_handle_url(url))
+
+    def test_can_handle_url_negative(self):
+        should_not_match = [
+            'https://example.com/index.html',
+        ]
+        for url in should_not_match:
+            self.assertFalse(Radiko.can_handle_url(url))
