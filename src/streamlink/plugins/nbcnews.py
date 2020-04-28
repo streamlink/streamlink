c00340ff0545d4cb58723ac3d4195f9108db581a
back-to <backto@protonmail.ch>
2020-04-28 18:26:03 +0200

    Update nbcnews.py

--- src/streamlink/plugins/nbcnews.py
+++ src/streamlink/plugins/nbcnews.py
@@ -10,34 +10,37 @@ log = logging.getLogger(__name__)
 
 
 class NBCNews(Plugin):
-    url_re = re.compile(r'https?://(www.)?nbcnews.com/now')
-    js_re = re.compile(r'https://ndassets.s-nbcnews.com/main-[0-9a-f]{20}.js')
+    url_re = re.compile(r'https?://(?:www\.)?nbcnews\.com/now')
+    js_re = re.compile(r'https://ndassets\.s-nbcnews\.com/main-[0-9a-f]{20}\.js')
     api_re = re.compile(r'NEWS_NOW_PID="([0-9]+)"')
     api_url = 'https://stream.nbcnews.com/data/live_sources_{0}.json'
     api_schema = validate.Schema(validate.transform(parse_json), {
-        "videoSources": [{
-            "sourceUrl": validate.url(),
-            "type": validate.text
+        'videoSources': [{
+            'sourceUrl': validate.url(),
+            'type': validate.text
         }]
-    }, validate.get("videoSources"), validate.get(0))
+    }, validate.get('videoSources'), validate.get(0))
 
     @classmethod
     def can_handle_url(cls, url):
         return cls.url_re.match(url) is not None
+
+    def get_title(self):
+        return 'NBC News Now'
 
     def _get_streams(self):
         html = self.session.http.get(self.url).text
         match = self.js_re.search(html)
         js = self.session.http.get(match.group(0)).text
         match = self.api_re.search(js)
-        log.debug("API ID: {0}".format(match.group(1)))
+        log.debug('API ID: {0}'.format(match.group(1)))
         api_url = self.api_url.format(match.group(1))
         stream = self.session.http.get(api_url, schema=self.api_schema)
+        log.trace('{0!r}'.format(stream))
         if stream['type'].lower() != 'live':
-            log.error("Stream type is {0}, but we need the live only".format(stream['type']))
+            log.error('invalid stream type "{0}"'.format(stream['type']))
             return
-        stream_url = stream['sourceUrl']
-        return HLSStream.parse_variant_playlist(self.session, stream_url)
+        return HLSStream.parse_variant_playlist(self.session, stream['sourceUrl'])
 
 
 __plugin__ = NBCNews
