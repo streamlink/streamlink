3bf2d71071f9d18ae2a6d8e2b77ff635ea6e71ba
steven7851 <steven7851@msn.com>
2020-05-16 08:44:47 +0800

    plugins.app17: add back HLSStream and RTMPStream

--- src/streamlink/plugins/app17.py
+++ src/streamlink/plugins/app17.py
@@ -2,7 +2,7 @@ import re
 
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import useragents
-from streamlink.stream import HTTPStream
+from streamlink.stream import HLSStream, RTMPStream, HTTPStream
 
 API_URL = "https://api-dsa.17app.co/api/v1/lives/{0}/viewers/alive"
 
@@ -31,6 +31,26 @@ class App17(Plugin):
 
         https_url = http_url.replace("http:", "https:")
         yield "live", HTTPStream(self.session, https_url)
+
+        if 'pull-rtmp' in http_url:
+            rtmp_url = http_url.replace("http:", "rtmp:").replace(".flv", "")
+            stream = RTMPStream(self.session, {
+                "rtmp": rtmp_url,
+                "live": True,
+                "pageUrl": self.url,
+            })
+            yield "live", stream
+
+        if 'wansu-' in http_url:
+            hls_url = http_url.replace(".flv", "/playlist.m3u8")
+        else:
+            hls_url = http_url.replace("live-hdl", "live-hls").replace(".flv", ".m3u8")
+
+        s = []
+        for s in HLSStream.parse_variant_playlist(self.session, hls_url).items():
+            yield s
+        if not s:
+            yield "live", HLSStream(self.session, hls_url)
 
 
 __plugin__ = App17
