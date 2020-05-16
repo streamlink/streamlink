ca65b554ef9a087ca55799ea7e846efeb9afc98f
back-to <backto@protonmail.ch>
2020-05-16 10:10:08 +0200

    plugins.app17: added debug message, use logging and use schema

--- src/streamlink/plugins/app17.py
+++ src/streamlink/plugins/app17.py
@@ -1,21 +1,33 @@
+import logging
 import re
 
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import useragents
+from streamlink.plugin.api import useragents, validate
 from streamlink.stream import HLSStream, RTMPStream, HTTPStream
 
-API_URL = "https://api-dsa.17app.co/api/v1/lives/{0}/viewers/alive"
-
-_url_re = re.compile(r"https://17.live/live/(?P<channel>[^/&?]+)")
+log = logging.getLogger(__name__)
 
 
 class App17(Plugin):
+    _url_re = re.compile(r"https://17.live/live/(?P<channel>[^/&?]+)")
+    API_URL = "https://api-dsa.17app.co/api/v1/lives/{0}/viewers/alive"
+
+    _api_schema = validate.Schema(
+        {
+            "rtmpUrls": [{
+                validate.optional("provider"): validate.any(int, None),
+                "url": validate.url(),
+            }],
+        },
+        validate.get("rtmpUrls"),
+    )
+
     @classmethod
     def can_handle_url(cls, url):
-        return _url_re.match(url)
+        return cls._url_re.match(url) is not None
 
     def _get_streams(self):
-        match = _url_re.match(self.url)
+        match = self._url_re.match(self.url)
         channel = match.group("channel")
 
         self.session.http.headers.update({'User-Agent': useragents.CHROME, 'Referer': self.url})
@@ -23,10 +35,13 @@ class App17(Plugin):
         data = '{"liveStreamID":"%s"}' % (channel)
 
         try:
-            res = self.session.http.post(API_URL.format(channel), data=data).json()
-            http_url = res.get("rtmpUrls")[0].get("url")
-        except Exception:
-            self.logger.info("Stream currently unavailable.")
+            res = self.session.http.post(self.API_URL.format(channel), data=data)
+            res_json = self.session.http.json(res, schema=self._api_schema)
+            log.trace("{0!r}".format(res_json))
+            http_url = res_json[0]["url"]
+        except Exception as e:
+            log.info("Stream currently unavailable.")
+            log.debug(str(e))
             return
 
         https_url = http_url.replace("http:", "https:")
@@ -46,11 +61,16 @@ class App17(Plugin):
         else:
             hls_url = http_url.replace("live-hdl", "live-hls").replace(".flv", ".m3u8")
 
-        s = []
-        for s in HLSStream.parse_variant_playlist(self.session, hls_url).items():
-            yield s
+        s = HLSStream.parse_variant_playlist(self.session, hls_url)
         if not s:
             yield "live", HLSStream(self.session, hls_url)
+        else:
+            if len(s) == 1:
+                for _n, _s in s.items():
+                    yield "live", _s
+            else:
+                for _s in s.items():
+                    yield _s
 
 
 __plugin__ = App17
