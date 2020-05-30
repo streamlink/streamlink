b024d6f521706badb1154d9fa9850bccc2996e57
Billy2011 <kschmidt2007@googlemail.com>
2020-05-30 21:04:47 +0200

    plugins.olympicchannel: fix/rewrite (#2981)

--- tests/plugins/test_olympicchannel.py
+++ tests/plugins/test_olympicchannel.py
@@ -5,10 +5,17 @@ from streamlink.plugins.olympicchannel import OlympicChannel
 
 class TestPluginOlympicChannel(unittest.TestCase):
     def test_can_handle_url(self):
-        # should match
-        self.assertTrue(OlympicChannel.can_handle_url("https://www.olympicchannel.com/en/playback/listen-up-olympic-dreamers/"))
-        self.assertTrue(OlympicChannel.can_handle_url("https://www.olympicchannel.com/en/tv/overflow-6/"))
-        self.assertTrue(OlympicChannel.can_handle_url("https://www.olympicchannel.com/en/tv/livestream-5/"))
+        urls = (
+            "https://www.olympicchannel.com/en/video/detail/stefanidi-husband-coach-krier-relationship/",
+            "https://www.olympicchannel.com/en/live/",
+            "https://www.olympicchannel.com/en/live/video/detail/olympic-ceremonies-channel/",
+            "https://www.olympicchannel.com/de/video/detail/stefanidi-husband-coach-krier-relationship/",
+            "https://www.olympicchannel.com/de/original-series/detail/body/body-season-season-1/episodes/"
+            "treffen-sie-aaron-wheelz-fotheringham-den-paten-des-rollstuhl-extremsports/",
+        )
+        for url in urls:
+            # should match
+            self.assertTrue(OlympicChannel.can_handle_url(url))
 
         # shouldn't match
         self.assertFalse(OlympicChannel.can_handle_url("https://www.olympicchannel.com/en/"))
