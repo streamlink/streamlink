775907e77575d04da18dbaaad9815590481df55d
wiresp33d <wiresp33dz@gmail.com>
2020-06-11 21:30:19 +0800

    plugins.bigo: fix bigo #3015

--- src/streamlink/plugins/bigo.py
+++ src/streamlink/plugins/bigo.py
@@ -2,7 +2,7 @@ import logging
 import re
 
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import useragents
+from streamlink.plugin.api import useragents, validate
 from streamlink.stream import HLSStream
 
 log = logging.getLogger(__name__)
@@ -13,20 +13,31 @@ class Bigo(Plugin):
     _video_re = re.compile(
         r"""videoSrc:\s?["'](?P<url>[^"']+)["']""",
         re.M)
-    api_url = "http://www.bigo.tv/OInterface/getVideoParam?bigoId="
+    _id_re = re.compile(r"[^/]+(?=/$|$)")
+    _video_info_schema = validate.Schema({
+        "code": 0,
+        "msg": "success",
+        "data": {
+            "videoSrc": validate.any(None, validate.url()),
+            "wsUrl": validate.any(None, validate.url())
+        }
+    })
+    api_url = "https://www.bigo.tv/OInterface/getVideoParam?bigoId="
 
     @classmethod
     def can_handle_url(cls, url):
         return cls._url_re.match(url) is not None
 
     def _get_streams(self):
-        extract_id = self.url.split('/')[-1]
-        page = self.session.http.get(
-            self.api_url + extract_id,
+        extract_id = self._id_re.search(self.url)
+        url = "%s%s" %(self.api_url, extract_id.group(0))
+        res = self.session.http.get(
+            url,
             allow_redirects=True,
             headers={"User-Agent": useragents.IPHONE_6}
         )
-        videomatch = page.json()['data']['videoSrc']
+        data = self.session.http.json(res, schema=self._video_info_schema)
+        videomatch = data['data']['videoSrc']
         if not videomatch:
             log.error("No playlist found.")
             return
