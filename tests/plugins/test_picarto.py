b4863daf1f2ee176d63c1b0cc753b0c1f2e090ab
back-to <backto@protonmail.ch>
2020-06-27 09:07:10 +0200

    plugins.picarto: fixed vod url detection

--- tests/plugins/test_picarto.py
+++ tests/plugins/test_picarto.py
@@ -7,13 +7,14 @@ class TestPluginPicarto(unittest.TestCase):
     def test_can_handle_url(self):
         should_match = [
             'https://picarto.tv/example',
+            'https://picarto.tv/videopopout/example_2020.00.00.00.00.00_nsfw.mkv',
         ]
         for url in should_match:
-            self.assertTrue(Picarto.can_handle_url(url))
+            self.assertTrue(Picarto.can_handle_url(url), url)
 
     def test_can_handle_url_negative(self):
         should_not_match = [
             'https://example.com/index.html',
         ]
         for url in should_not_match:
-            self.assertFalse(Picarto.can_handle_url(url))
+            self.assertFalse(Picarto.can_handle_url(url), url)
