b0c3fbe2dcb9fa438d06e7aaa394e6903b92295f
Billy2011 <kschmidt2007@googlemail.com>
2020-07-01 16:13:55 +0200

    plugins.ard_live: fix / rewrite

--- src/streamlink/plugins/ard_live.py
+++ src/streamlink/plugins/ard_live.py
@@ -1,56 +1,50 @@
 import re
+import json
+import logging
 
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import validate
-from streamlink.stream import HLSStream, HDSStream
-from streamlink.compat import urljoin
-from streamlink.stream import HTTPStream
+from streamlink.stream import HLSStream
+from streamlink.utils import update_scheme
+
+log = logging.getLogger(__name__)
 
 
-class ard_live(Plugin):
-    swf_url = "http://live.daserste.de/lib/br-player/swf/main.swf"
-    _url_re = re.compile(r"https?://(www.)?daserste.de/", re.I)
-    _player_re = re.compile(r'''dataURL\s*:\s*(?P<q>['"])(?P<url>.*?)(?P=q)''')
-    _player_url_schema = validate.Schema(
-        validate.transform(_player_re.search),
-        validate.any(
-            None,
-            validate.all(validate.get("url"), validate.text)
-        )
+class ARD_Live(Plugin):
+    _url_re = re.compile(r"https?://(www\.)?ardmediathek\.de/", re.I)
+    _context_data_re = re.compile(r"window\.__FETCHED_CONTEXT__\s*=\s*(\{.+\});")
+    _stream_infos_schema = validate.Schema(
+        validate.transform(_context_data_re.search),
+        validate.transform(lambda v: json.loads(v.group(1))),
+        validate.transform(lambda vl: [d for d in vl.values() if "widgets" in d]),
+        validate.transform(lambda v: v[0]), {
+            "widgets": [{
+                "mediaCollection": {
+                    "embedded": {
+                        "_isLive": bool,
+                        "_mediaArray": [{
+                            "_mediaStreamArray": [{
+                                "_stream": validate.all(validate.endswith(".m3u8"), validate.url(),
+                                                        validate.transform(lambda v: update_scheme("https://", v)))
+                            }]
+                        }],
+                    },
+                },
+            }],
+        }
     )
-    _livestream_schema = validate.Schema(
-        validate.xml_findall(".//assets"),
-        validate.filter(lambda x: x.attrib.get("type") != "subtitles"),
-        validate.get(0),
-        validate.xml_findall(".//asset"),
-        [validate.union({
-            "url": validate.xml_findtext("./fileName"),
-            "bitrate": validate.xml_findtext("./bitrateVideo")
-        })])
 
     @classmethod
     def can_handle_url(cls, url):
         return cls._url_re.match(url) is not None
 
     def _get_streams(self):
-        data_url = self.session.http.get(self.url, schema=self._player_url_schema)
-        if data_url:
-            res = self.session.http.get(urljoin(self.url, data_url))
-            stream_info = self.session.http.xml(res, schema=self._livestream_schema)
-
-            for stream in stream_info:
-                url = stream["url"]
-                try:
-                    if ".m3u8" in url:
-                        for s in HLSStream.parse_variant_playlist(self.session, url, name_key="bitrate").items():
-                            yield s
-                    elif ".f4m" in url:
-                        for s in HDSStream.parse_manifest(self.session, url, pvswf=self.swf_url, is_akamai=True).items():
-                            yield s
-                    elif ".mp4" in url:
-                        yield "{0}k".format(stream["bitrate"]), HTTPStream(self.session, url)
-                except IOError as err:
-                    self.logger.warning("Error parsing stream: {0}", err)
+        stream_infos = self.session.http.get(self.url, schema=self._stream_infos_schema)
+        log.trace("{0!r}".format(stream_infos))
+        for stream in stream_infos["widgets"][0]["mediaCollection"]["embedded"]["_mediaArray"][0]["_mediaStreamArray"]:
+            url = stream["_stream"]
+            for s in HLSStream.parse_variant_playlist(self.session, url).items():
+                yield s
 
 
-__plugin__ = ard_live
+__plugin__ = ARD_Live
