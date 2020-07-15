2160b4caa6bacb4b52be7512a87022719ea379bf
Topologist <listingod@gmail.com>
2020-07-16 01:09:47 +0200

    Add trovo plugin

---
+++ src/streamlink/plugins/trovo.py
@@ -0,0 +1,20 @@
+import re
+
+from streamlink.stream import HTTPStream
+from streamlink.plugin import Plugin
+
+class Trovo(Plugin):
+    url_re = re.compile(r"https?://(?:www\.)?trovo\.live/")
+    streams_re = re.compile(r'playUrl:"([^"]+)",desc:"([^"]+)"', re.DOTALL)
+
+    @classmethod
+    def can_handle_url(cls, url):
+        return cls.url_re.match(url) is not None
+
+    def _get_streams(self):
+        res = self.session.http.get(self.url)
+        for m in self.streams_re.finditer(res.text):
+            yield m.group(2), HTTPStream(self.session, m.group(1).replace('\\u002F','/'))
+
+
+__plugin__ = Trovo
