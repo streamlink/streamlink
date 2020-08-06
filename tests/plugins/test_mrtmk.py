9fe696e2b3e13f9c3c91448e2fd0a01fab63631a
Tr4sK <github@tr4sk.me>
2020-08-06 11:42:47 +0200

    Fix lint issue, indentation

--- tests/plugins/test_mrtmk.py
+++ tests/plugins/test_mrtmk.py
@@ -5,13 +5,13 @@ from streamlink.plugins.mrtmk import MRTmk
 
 class TestPluginBigo(unittest.TestCase):
     def test_can_handle_url(self):
-      # should match
-      self.assertTrue(MRTmk.can_handle_url("http://play.mrt.com.mk/live/658323455489957"))
-      self.assertTrue(MRTmk.can_handle_url("http://play.mrt.com.mk/live/47"))
-      self.assertTrue(MRTmk.can_handle_url("http://play.mrt.com.mk/play/1581"))
+        # should match
+        self.assertTrue(MRTmk.can_handle_url("http://play.mrt.com.mk/live/658323455489957"))
+        self.assertTrue(MRTmk.can_handle_url("http://play.mrt.com.mk/live/47"))
+        self.assertTrue(MRTmk.can_handle_url("http://play.mrt.com.mk/play/1581"))
 
-      #shouldn't match
-      self.assertFalse(MRTmk.can_handle_url("http://play.mrt.com.mk/"))
-      self.assertFalse(MRTmk.can_handle_url("http://play.mrt.com.mk/c/2"))
-      self.assertFalse(MRTmk.can_handle_url("http://www.tvcatchup.com/"))
-      self.assertFalse(MRTmk.can_handle_url("http://www.youtube.com/"))
+        # shouldn't match
+        self.assertFalse(MRTmk.can_handle_url("http://play.mrt.com.mk/"))
+        self.assertFalse(MRTmk.can_handle_url("http://play.mrt.com.mk/c/2"))
+        self.assertFalse(MRTmk.can_handle_url("http://www.tvcatchup.com/"))
+        self.assertFalse(MRTmk.can_handle_url("http://www.youtube.com/"))
