c597a30bf1ad7df988ce143ac74a61732a1b0677
Ian Cameron <1661072+mkbloke@users.noreply.github.com>
2020-08-18 02:03:04 +0100

    plugins.drdk: fix/rewrite

--- tests/plugins/test_drdk.py
+++ tests/plugins/test_drdk.py
@@ -6,9 +6,9 @@ from streamlink.plugins.drdk import DRDK
 class TestPluginDRDK(unittest.TestCase):
     def test_can_handle_url(self):
         should_match = [
-            'https://www.dr.dk/tv/live/dr1',
-            'https://www.dr.dk/tv/live/dr2',
-            'https://www.dr.dk/tv/se/matador/matador-saeson-3/matador-15-24',
+            'https://www.dr.dk/drtv/kanal/dr1_20875',
+            'https://www.dr.dk/drtv/kanal/dr2_20876',
+            'https://www.dr.dk/drtv/kanal/dr-ramasjang_20892',
         ]
         for url in should_match:
             self.assertTrue(DRDK.can_handle_url(url))
