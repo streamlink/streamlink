e9b50beb9bcb52007389fa6a671720617aad53dd
back-to <backto@protonmail.ch>
2020-09-09 10:31:09 +0200

    plugins.pluto: cleanup

--- src/streamlink/plugins/pluto.py
+++ src/streamlink/plugins/pluto.py
@@ -1,21 +1,23 @@
-import re
 import logging
+import re
+
 from uuid import uuid4
 
 from streamlink.plugin import Plugin
-from streamlink.stream import HLSStream
 from streamlink.plugin.api import validate
+from streamlink.stream import HLSStream
+from streamlink.utils.url import update_qsd
 
 log = logging.getLogger(__name__)
 
 
 class Pluto(Plugin):
-    url_re_live = re.compile(r'^https?://(?:www\.)?pluto\.tv/live-tv/(?P<slug>[^/]+)$')
+    url_re_live = re.compile(r'https?://(?:www\.)?pluto\.tv/live-tv/(?P<slug>[^/?]+)/?$')
     url_re_tv = re.compile(r'^https?://(?:www\.)?pluto\.tv/on-demand/series/(?P<series_slug>[^/]+)/season/\d+/episode/(?P<episode_slug>[^/]+)$')
     url_re_movie = re.compile(r'^https?://(?:www\.)?pluto\.tv/on-demand/movies/(?P<slug>[^/]+)$')
 
     api_url_live_channels = 'https://api.pluto.tv/v2/channels'
-    api_url_tv_series_template = 'http://api.pluto.tv/v3/vod/slugs/%s'
+    api_url_tv_series_template = 'http://api.pluto.tv/v3/vod/slugs/{0}'
     api_url_movies = 'https://api.pluto.tv/v3/vod/categories?includeItems=true&deviceType=web'
 
     media_schema = {
@@ -26,26 +28,9 @@ class Pluto(Plugin):
             }]
         }
     }
-
-    live_channels_schema = validate.Schema([
-        media_schema
-    ])
-
-    tv_schema = validate.Schema({
-        'seasons': [{
-            'episodes': [
-                media_schema
-            ]
-        }]
-    })
-
-    movies_schema = validate.Schema({
-        'categories': [{
-            'items': [
-                media_schema
-            ]
-        }]
-    })
+    live_channels_schema = validate.Schema([media_schema])
+    tv_schema = validate.Schema({'seasons': [{'episodes': [media_schema]}]})
+    movies_schema = validate.Schema({'categories': [{'items': [media_schema]}]})
 
     @classmethod
     def can_handle_url(cls, url):
@@ -65,7 +50,15 @@ class Pluto(Plugin):
             channels_res = self.session.http.get(self.api_url_live_channels)
             channels_data = self.session.http.json(channels_res, schema=self.live_channels_schema)
 
-            match = next(filter(lambda channel: channel['slug'] == slug, channels_data), None)
+            c_list = set()
+            for _d in channels_data:
+                c_list.add(_d['slug'])
+            log.trace('Available channels: {0}'.format(', '.join(sorted(c_list))))
+
+            for channel in channels_data:
+                if channel['slug'] == slug:
+                    match = channel
+                    break
 
         # On-demand TV
         elif self.url_re_tv.match(self.url):
@@ -73,13 +66,14 @@ class Pluto(Plugin):
             series_slug = re_match.group('series_slug')
             episode_slug = re_match.group('episode_slug')
 
-            tv_series_res = self.session.http.get(self.api_url_tv_series_template % series_slug)
+            tv_series_res = self.session.http.get(self.api_url_tv_series_template.format(series_slug))
             tv_series_data = self.session.http.json(tv_series_res, schema=self.tv_schema)
 
             for season in tv_series_data['seasons']:
                 for episode in season['episodes']:
                     if episode['slug'] == episode_slug:
                         match = episode
+                        break
 
         # On-demand movie
         elif self.url_re_movie.match(self.url):
@@ -92,6 +86,7 @@ class Pluto(Plugin):
                 for movie in category['items']:
                     if movie['slug'] == slug:
                         match = movie
+                        break
 
         if not match:
             return
@@ -101,11 +96,17 @@ class Pluto(Plugin):
             return
 
         stream_link_no_sid = match['stitched']['urls'][0]['url']
-        sid = str(uuid4())
-        stream_link = stream_link_no_sid.replace('&sid=', '&sid=' + sid)
 
-        return HLSStream.parse_variant_playlist(self.session, stream_link)
-
+        device_id = str(uuid4())
+        stream_url = update_qsd(stream_link_no_sid, {
+            'deviceId': device_id,
+            'sid': device_id,
+            'deviceType': 'web',
+            'deviceMake': 'Firefox',
+            'deviceModel': 'Firefox',
+            'appName': 'web',
+        })
+        return HLSStream.parse_variant_playlist(self.session, stream_url)
 
 
 __plugin__ = Pluto
--- tests/plugins/test_pluto.py
+++ tests/plugins/test_pluto.py
@@ -5,29 +5,33 @@ from streamlink.plugins.pluto import Pluto
 
 class TestPluginPluto(unittest.TestCase):
     def test_can_handle_url(self):
-        # should match
-        self.assertTrue(Pluto.can_handle_url("http://www.pluto.tv/live-tv/channel-lineup"))
-        self.assertTrue(Pluto.can_handle_url("http://pluto.tv/live-tv/nfl-channel"))
-        self.assertTrue(Pluto.can_handle_url("https://pluto.tv/live-tv/red-bull-tv-2"))
-        self.assertTrue(Pluto.can_handle_url("https://pluto.tv/live-tv/4k-tv"))
-        self.assertTrue(Pluto.can_handle_url("http://www.pluto.tv/on-demand/series/leverage/season/1/episode/the-nigerian-job-2009-1-1"))
-        self.assertTrue(Pluto.can_handle_url("http://pluto.tv/on-demand/series/fear-factor-usa-(lf)/season/5/episode/underwater-safe-savetongue-bob-and-transfertruck-car-ramp-2004-5-3"))
-        self.assertTrue(Pluto.can_handle_url("https://www.pluto.tv/on-demand/movies/dr.-no-1963-1-1"))
-        self.assertTrue(Pluto.can_handle_url("http://pluto.tv/on-demand/movies/the-last-dragon-(1985)-1-1"))
+        should_match = [
+            'http://www.pluto.tv/live-tv/channel-lineup',
+            'http://pluto.tv/live-tv/channel',
+            'http://pluto.tv/live-tv/channel/',
+            'https://pluto.tv/live-tv/red-bull-tv-2',
+            'https://pluto.tv/live-tv/4k-tv',
+            'http://www.pluto.tv/on-demand/series/leverage/season/1/episode/the-nigerian-job-2009-1-1',
+            'http://pluto.tv/on-demand/series/fear-factor-usa-(lf)/season/5/episode/underwater-safe-bob-car-ramp-2004-5-3',
+            'https://www.pluto.tv/on-demand/movies/dr.-no-1963-1-1',
+            'http://pluto.tv/on-demand/movies/the-last-dragon-(1985)-1-1',
+        ]
+        for url in should_match:
+            self.assertTrue(Pluto.can_handle_url(url), url)
 
-        # shouldn't match
-        self.assertFalse(Pluto.can_handle_url("https://fake.pluto.tv/live-tv/hello"))
-        self.assertFalse(Pluto.can_handle_url("http://www.pluto.tv/live-tv/channel-lineup/extra"))
-        self.assertFalse(Pluto.can_handle_url("https://www.pluto.tv/live-tv"))
-        self.assertFalse(Pluto.can_handle_url("https://pluto.tv/live-tv"))
-        self.assertFalse(Pluto.can_handle_url("https://www.pluto.com/live-tv/swag"))
-        self.assertFalse(Pluto.can_handle_url("https://youtube.com/live-tv/swag.html"))
-        self.assertFalse(Pluto.can_handle_url("http://pluto.tv/movies/dr.-no-1963-1-1"))
-        self.assertFalse(Pluto.can_handle_url("http://pluto.tv/on-demand/movies/dr.-no-1/963-1-1"))
-        self.assertFalse(Pluto.can_handle_url("http://pluto.tv/on-demand/series/dr.-no-1963-1-1"))
-        self.assertFalse(Pluto.can_handle_url("http://pluto.tv/on-demand/movies/leverage/season/1/episode/the-nigerian-job-2009-1-1"))
-        self.assertFalse(Pluto.can_handle_url("http://pluto.tv/on-demand/fear-factor-usa-(lf)/season/5/episode/underwater-safe-savetongue-bob-and-transfertruck-car-ramp-2004-5-3"))
-
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_can_handle_url_negative(self):
+        should_not_match = [
+            'https://fake.pluto.tv/live-tv/hello',
+            'http://www.pluto.tv/live-tv/channel-lineup/extra',
+            'https://www.pluto.tv/live-tv',
+            'https://pluto.tv/live-tv',
+            'https://www.pluto.com/live-tv/swag',
+            'https://youtube.com/live-tv/swag.html',
+            'http://pluto.tv/movies/dr.-no-1963-1-1',
+            'http://pluto.tv/on-demand/movies/dr.-no-1/963-1-1',
+            'http://pluto.tv/on-demand/series/dr.-no-1963-1-1',
+            'http://pluto.tv/on-demand/movies/leverage/season/1/episode/the-nigerian-job-2009-1-1',
+            'http://pluto.tv/on-demand/fear-factor-usa-(lf)/season/5/episode/underwater-safe-bob-car-ramp-2004-5-3',
+        ]
+        for url in should_not_match:
+            self.assertFalse(Pluto.can_handle_url(url), url)
