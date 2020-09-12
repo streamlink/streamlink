0c9a878d96a0b607919e221f275fb64e8be1627c
Ian Cameron <1661072+mkbloke@users.noreply.github.com>
2020-09-12 17:08:26 +0100

    Implement changes following review

--- src/streamlink/plugins/svtplay.py
+++ src/streamlink/plugins/svtplay.py
@@ -2,7 +2,6 @@ import logging
 import re
 
 from streamlink.compat import urljoin
-from streamlink.exceptions import PluginError
 from streamlink.plugin import Plugin, PluginArguments, PluginArgument
 from streamlink.plugin.api import validate
 from streamlink.stream import DASHStream, HTTPStream
@@ -19,7 +18,8 @@ class SVTPlay(Plugin):
     title = None
 
     url_re = re.compile(r'''
-        https?://(?:www\.)?(?:svtplay|oppetarkiv).se(?P<path>.*)
+        https?://(?:www\.)?(?:svtplay|oppetarkiv)\.se
+        (/(kanaler/)?.*)
     ''', re.VERBOSE)
 
     latest_episode_url_re = re.compile(r'''
@@ -29,7 +29,7 @@ class SVTPlay(Plugin):
     live_id_re = re.compile(r'.*/(?P<live_id>[^?]+)')
 
     vod_id_re = re.compile(r'''
-        (?:DATA_LAKE\s+=\s+{"content":{"id":|data-video-id=)
+        (?:DATA_LAKE\s+=\s+{"content":{"id":|"svtId":|data-video-id=)
         "(?P<vod_id>[^"]+)"
     ''', re.VERBOSE)
 
@@ -70,77 +70,79 @@ class SVTPlay(Plugin):
         if self.title is not None:
             return self.title
 
+    def _set_metadata(self, data, category):
+        if 'programTitle' in data:
+            self.author = data['programTitle']
+
+        self.category = category
+
+        if 'episodeTitle' in data:
+            self.title = data['episodeTitle']
+
+    def _get_live(self, path):
+        match = self.live_id_re.search(path)
+        if match is None:
+            return
+
+        live_id = "ch-{0}".format(match.group('live_id'))
+        log.debug("Live ID={0}".format(live_id))
+
+        res = self.session.http.get(self.api_url.format(live_id))
+        api_data = self.session.http.json(res, schema=self._video_schema)
+
+        self._set_metadata(api_data, 'Live')
+
+        for playlist in api_data['videoReferences']:
+            if playlist['format'] == 'dashhbbtv':
+                for s in DASHStream.parse_manifest(self.session, playlist['url']).items():
+                    yield s
+
+    def _get_vod(self):
+        res = self.session.http.get(self.url)
+        match = self.latest_episode_url_re.search(res.text)
+        if match:
+            res = self.session.http.get(
+                urljoin(self.url, match.group('url')),
+            )
+
+        match = self.vod_id_re.search(res.text)
+        if match is None:
+            return
+
+        vod_id = match.group('vod_id')
+        log.debug("VOD ID={0}".format(vod_id))
+
+        res = self.session.http.get(self.api_url.format(vod_id))
+        api_data = self.session.http.json(res, schema=self._video_schema)
+
+        self._set_metadata(api_data, 'VOD')
+
+        substreams = {}
+        if 'subtitleReferences' in api_data:
+            for subtitle in api_data['subtitleReferences']:
+                if subtitle['format'] == 'webvtt':
+                    log.debug("Subtitle={0}".format(subtitle['url']))
+                    substreams[subtitle['format']] = HTTPStream(
+                        self.session,
+                        subtitle['url'],
+                    )
+
+        for manifest in api_data['videoReferences']:
+            if manifest['format'] == 'dashhbbtv':
+                for q, s in DASHStream.parse_manifest(self.session, manifest['url']).items():
+                    if self.get_option('mux_subtitles') and substreams:
+                        yield q, MuxedStream(self.session, s, subtitles=substreams)
+                    else:
+                        yield q, s
+
     def _get_streams(self):
-        path = self.url_re.match(self.url).group('path')
+        path, live = self.url_re.match(self.url).groups()
         log.debug("Path={0}".format(path))
 
-        if path.startswith('/kanaler/'):
-            match = self.live_id_re.search(path)
-            if match is not None:
-                live_id = "ch-{0}".format(match.group('live_id'))
-                log.debug("Live ID={0}".format(live_id))
-            else:
-                raise PluginError("Failed to get live ID")
-
-            res = self.session.http.get(self.api_url.format(live_id))
-            api_data = self.session.http.json(res, schema=self._video_schema)
-
-            if 'programTitle' in api_data:
-                self.author = api_data['programTitle']
-
-            self.category = 'Live'
-
-            if 'episodeTitle' in api_data:
-                self.title = api_data['episodeTitle']
-
-            for playlist in api_data['videoReferences']:
-                if playlist['format'] == 'dashhbbtv':
-                    for s in DASHStream.parse_manifest(self.session, playlist['url']).items():
-                        yield s
+        if live:
+            return self._get_live(path)
         else:
-            res = self.session.http.get(self.url)
-            match = self.latest_episode_url_re.search(res.text)
-            if match:
-                res = self.session.http.get(
-                    urljoin(self.url, match.group('url')),
-                )
-
-            match = self.vod_id_re.search(res.text)
-
-            if match is not None:
-                vod_id = match.group('vod_id')
-                log.debug("VOD ID={0}".format(vod_id))
-            else:
-                raise PluginError("Failed to get VOD ID")
-
-            res = self.session.http.get(self.api_url.format(vod_id))
-            api_data = self.session.http.json(res, schema=self._video_schema)
-
-            if 'programTitle' in api_data:
-                self.author = api_data['programTitle']
-
-            self.category = 'VOD'
-
-            if 'episodeTitle' in api_data:
-                self.title = api_data['episodeTitle']
-
-            substreams = {}
-            if 'subtitleReferences' in api_data:
-                for subtitle in api_data['subtitleReferences']:
-                    if subtitle['format'] == 'webvtt':
-                        log.debug("Subtitle={0}".format(subtitle['url']))
-                        substreams[subtitle['format']] = HTTPStream(
-                            self.session,
-                            subtitle['url'],
-                        )
-
-            for manifest in api_data['videoReferences']:
-                if manifest['format'] == 'dashhbbtv':
-                    for q, s in DASHStream.parse_manifest(self.session, manifest['url']).items():
-                        if self.get_option('mux_subtitles') and substreams:
-                            yield q, MuxedStream(self.session, s, subtitles=substreams)
-                        else:
-                            yield q, s
+            return self._get_vod()
 
 
 __plugin__ = SVTPlay
