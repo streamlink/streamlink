ff9e40ae0fb56f842b5ea31c3ad49bb352d5493f
Billy2011 <kschmidt2007@googlemail.com>
2020-09-25 10:33:09 +0200

    use hello api v2 only for zattoo.com

--- src/streamlink/plugins/zattoo.py
+++ src/streamlink/plugins/zattoo.py
@@ -18,7 +18,8 @@ log = logging.getLogger(__name__)
 
 class Zattoo(Plugin):
     API_CHANNELS = '{0}/zapi/v2/cached/channels/{1}?details=False'
-    API_HELLO = '{0}/zapi/v2/session/hello'
+    API_HELLO = '{0}/zapi/session/hello'
+    API_HELLO_V2 = '{0}/zapi/v2/session/hello'
     API_HELLO_V3 = '{0}/zapi/v3/session/hello'
     API_LOGIN = '{0}/zapi/v2/account/login'
     API_LOGIN_V3 = '{0}/zapi/v3/account/login'
@@ -168,12 +169,12 @@ class Zattoo(Plugin):
         if self.base_url == 'https://www.quantum-tv.com':
             app_token = self.session.http.json(res)["session_token"]
             hello_url = self.API_HELLO_V3.format(self.base_url)
+        elif self.base_url == 'https://zattoo.com':
+            app_token = self.session.http.json(res)['app_tid']
+            hello_url = self.API_HELLO_V2.format(self.base_url)
         else:
-            if self.base_url == 'https://zattoo.com':
-                app_token = self.session.http.json(res)['app_tid']
-            else:
-                match = self._app_token_re.search(res.text)
-                app_token = match.group(1)
+            match = self._app_token_re.search(res.text)
+            app_token = match.group(1)
             hello_url = self.API_HELLO.format(self.base_url)
 
         if self._uuid:
