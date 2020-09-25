3561be7311f1b57027894ee9bb03ff756a9d1b7c
Alexis Murzeau <amubtdx@gmail.com>
2020-09-26 00:10:11 +0200

    docs: use recommonmark as an extension
    Sphinx v1.8 has deprecated source_parsers which is used for recommonmark.
    
    This commit replace the use of source_parsers for recommonmark with the
    use of recommonmark as an extension (supported by recommonmark since
    0.5.0).

--- docs/conf.py
+++ docs/conf.py
@@ -18,16 +18,10 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'ext_argparse', 'ext_github', 'ext_releaseref']
+extensions = ['sphinx.ext.autodoc', 'ext_argparse', 'ext_github', 'ext_releaseref', 'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# The suffix of source filenames.
-source_suffix = ['.rst', '.md']
-source_parsers = {
-   '.md': 'recommonmark.parser.CommonMarkParser',
-}
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
