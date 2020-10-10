175a80b929207088db4b7a1fdb128ed9a5580689
back-to <backto@protonmail.ch>
2020-10-10 12:46:42 +0200

    plugins.livestream: remove AkamaiHDStream, use only secure HLSStream

--- tests/plugins/test_livestream.py
+++ tests/plugins/test_livestream.py
@@ -8,13 +8,14 @@ class TestPluginLivestream(unittest.TestCase):
         should_match = [
             'https://livestream.com/',
             'https://www.livestream.com/',
+            'https://livestream.com/accounts/22300508/events/6675945',
         ]
         for url in should_match:
-            self.assertTrue(Livestream.can_handle_url(url))
+            self.assertTrue(Livestream.can_handle_url(url), url)
 
     def test_can_handle_url_negative(self):
         should_not_match = [
             'https://example.com/index.html',
         ]
         for url in should_not_match:
-            self.assertFalse(Livestream.can_handle_url(url))
+            self.assertFalse(Livestream.can_handle_url(url), url)
