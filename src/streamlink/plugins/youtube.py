24e25e2f23059c9b31aa7b6b223c28b05b4dd8a2
Billy2011 <kschmidt2007@googlemail.com>
2020-10-19 17:30:58 +0200

    plugins.youtube: fix live playback (#3268)
    Closes #3263
    
    check for valid canonical URL
    The video_id "live_stream" is not a live stream id

--- src/streamlink/plugins/youtube.py
+++ src/streamlink/plugins/youtube.py
@@ -255,6 +255,10 @@ class YouTube(Plugin):
                     log.debug("Video ID from currentVideoEndpoint")
                     return video_id
             for x in search_dict(data, 'videoRenderer'):
+                if x.get("viewCountText", {}).get("runs"):
+                    if x.get("videoId"):
+                        log.debug("Video ID from videoRenderer (live)")
+                        return x["videoId"]
                 for bstyle in search_dict(x.get("badges", {}), "style"):
                     if bstyle == "BADGE_STYLE_TYPE_LIVE_NOW":
                         if x.get("videoId"):
@@ -266,8 +270,12 @@ class YouTube(Plugin):
                 if link.attributes.get("rel") == "canonical":
                     canon_link = link.attributes.get("href")
                     if canon_link != url:
-                        log.debug("Re-directing to canonical URL: {0}".format(canon_link))
-                        return self._find_video_id(canon_link)
+                        if canon_link.endswith("v=live_stream"):
+                            log.debug("The video is not available")
+                            break
+                        else:
+                            log.debug("Re-directing to canonical URL: {0}".format(canon_link))
+                            return self._find_video_id(canon_link)
 
         raise PluginError("Could not find a video on this page")
 
