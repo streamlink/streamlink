065cb82794cb80dd5d2abf3f11034ae40e2308df
back-to <backto@protonmail.ch>
2020-10-31 10:04:19 +0100

    plugins.abweb: fixed login issues

--- tests/plugins/test_abweb.py
+++ tests/plugins/test_abweb.py
@@ -9,12 +9,14 @@ class TestPluginABweb(unittest.TestCase):
             'http://www.abweb.com/bis-tv-online/bistvo-tele-universal.aspx?chn=ab1',
             'http://www.abweb.com/BIS-TV-Online/bistvo-tele-universal.aspx?chn=ab1',
             'http://www.abweb.com/BIS-TV-Online/bistvo-tele-universal.aspx?chn=luckyjack',
+            'https://www.abweb.com/BIS-TV-Online/bistvo-tele-universal.aspx?chn=action',
         ]
         for url in should_match:
-            self.assertTrue(ABweb.can_handle_url(url))
+            self.assertTrue(ABweb.can_handle_url(url), url)
 
+    def test_can_handle_url_negative(self):
         should_not_match = [
             'http://www.abweb.com',
         ]
         for url in should_not_match:
-            self.assertFalse(ABweb.can_handle_url(url))
+            self.assertFalse(ABweb.can_handle_url(url), url)
