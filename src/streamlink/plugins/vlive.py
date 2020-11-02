c49593843872fd94f9703b1018f097d1b853531d
SeonjaeHyeon <hsj106@mju.ac.kr>
2020-11-02 23:24:19 +0900

    plugins.vlive: fix URL regex and plugin

--- src/streamlink/plugins/vlive.py
+++ src/streamlink/plugins/vlive.py
@@ -9,48 +9,60 @@ log = logging.getLogger(__name__)
 
 
 class Vlive(Plugin):
-    _url_re = re.compile(r"https?://(?:www.)vlive\.tv/video/(\d+)")
-    _video_status = re.compile(r'oVideoStatus = (.+)<', re.DOTALL)
-    _video_init_url = "https://www.vlive.tv/video/init/view"
+    _url_re = re.compile(r"https?://(?:www.)vlive\.tv/(?P<format>video|post)/(?P<id>[0-9\-]+)")
+    _page_info = re.compile(r'window\.__PRELOADED_STATE__\s*=\s*({.*})\s*<', re.DOTALL)
+    _playinfo_url = "https://www.vlive.tv/globalv-web/vam-web/old/v3/live/%s/playInfo"
 
     @classmethod
     def can_handle_url(cls, url):
         return cls._url_re.match(url) is not None
 
-    @property
-    def video_id(self):
-        return self._url_re.match(self.url).group(1)
-
     def _get_streams(self):
-        vinit_req = self.session.http.get(self._video_init_url,
-                                          params=dict(videoSeq=self.video_id),
-                                          headers=dict(referer=self.url))
-        if vinit_req.status_code != 200:
-            raise PluginError('Could not get video init page (HTTP Status {})'
-                              .format(vinit_req.status_code))
+        video_page = self.session.http.get(self.url, headers=dict(referer=self.url))
+        if video_page.status_code != 200:
+            raise PluginError('Could not get video page (HTTP Status {})'
+                              .format(video_page.status_code))
 
-        video_status_js = self._video_status.search(vinit_req.text)
-        if not video_status_js:
-            raise PluginError('Could not find video status information!')
+        page_info_js = self._page_info.search(video_page.text)
+        if not page_info_js:
+            raise PluginError('Could not find page info')
 
-        video_status = json.loads(video_status_js.group(1))
+        page_info = json.loads(page_info_js.group(1))
+        if not page_info['postDetail'].get('post'):
+            raise PluginError('Could not find video info')
 
-        if video_status['viewType'] == 'vod':
+        video_json = page_info['postDetail']['post']['officialVideo']
+
+        video_type = video_json['type']
+        if video_type == 'VOD':
             raise PluginError('VODs are not supported')
 
-        if 'liveStreamInfo' not in video_status:
-            raise PluginError('Stream is offline')
+        url_format, video_id = re.match(self._url_re, self.url).group('format', 'id')
+        if url_format == 'post':
+            video_id = str(video_json['videoSeq'])
 
-        stream_info = json.loads(video_status['liveStreamInfo'])
+        video_status = video_json['status']
+        if video_status == 'ENDED':
+            raise PluginError('Stream has ended')
+        elif video_status != 'ON_AIR':
+            raise PluginError('Unknown video status: %s' % video_status)
+
+        stream_info_js = self.session.http.get(self._playinfo_url % video_id,
+                                               headers=dict(referer=self.url))
+        if stream_info_js.status_code != 200:
+            raise PluginError('Could not get stream info (HTTP Status {})'
+                              .format(stream_info_js.status_code))
+
+        stream_info = json.loads(stream_info_js.text)
 
         streams = dict()
         # All "resolutions" have a variant playlist with only one entry, so just combine them
-        for i in stream_info['resolutions']:
-            res_streams = HLSStream.parse_variant_playlist(self.session, i['cdnUrl'])
+        for i in stream_info['result']['streamList']:
+            res_streams = HLSStream.parse_variant_playlist(self.session, i['serviceUrl'])
             if len(res_streams.values()) > 1:
                 log.warning('More than one stream in variant playlist, using first entry!')
 
-            streams[i['name']] = res_streams.popitem()[1]
+            streams[i['streamName']] = res_streams.popitem()[1]
 
         return streams
 
