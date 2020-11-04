8853127d2c6ccd04f3140cb71940206caab86c85
Jon Bergli Heier <snakebite@jvnv.net>
2020-11-04 22:23:02 +0100

    Update NRK plugin
    The TV and radio sites of nrk.no has been changed some time ago which
    has left the NRK plugin broken. This change makes use of PSAPI [1] to
    bring both TV and radio back to working state, for both live and
    on-demand shows.
    
    [1]: https://psapi-we.nrk.no/documentation/

--- src/streamlink/plugins/nrk.py
+++ src/streamlink/plugins/nrk.py
@@ -3,30 +3,10 @@ from urllib.parse import urljoin
 
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import validate
-from streamlink.stream import HLSStream
+from streamlink.stream import HLSStream, HTTPStream
 
-COOKIE_PARAMS = (
-    "devicetype=desktop&"
-    "preferred-player-odm=hlslink&"
-    "preferred-player-live=hlslink"
-)
-
-_id_re = re.compile(r"/(?:program|direkte|serie/[^/]+)/([^/]+)")
+_id_re = re.compile(r"/(program|direkte|serie|podkast)(?:/.+)?/([^/]+)")
 _url_re = re.compile(r"https?://(tv|radio).nrk.no/")
-_api_baseurl_re = re.compile(r'''apiBaseUrl:\s*["'](?P<baseurl>[^"']+)["']''')
-
-_schema = validate.Schema(
-    validate.transform(_api_baseurl_re.search),
-    validate.any(
-        None,
-        validate.all(
-            validate.get("baseurl"),
-            validate.url(
-                scheme="http"
-            )
-        )
-    )
-)
 
 _mediaelement_schema = validate.Schema({
     "mediaUrl": validate.url(
@@ -35,8 +15,35 @@ _mediaelement_schema = validate.Schema({
     )
 })
 
+_playable_schema = validate.Schema({
+    "playable": validate.all(
+        {
+            "assets": validate.all(
+                [{
+                    "url": validate.url(
+                        scheme="http",
+                        path=validate.any(
+                            validate.endswith(".m3u8"),
+                            validate.endswith(".mp3")
+                        ),
+                    ),
+                    "format": validate.all(validate.text),
+                }]
+            ),
+        }
+    ),
+    "statistics": {
+        "luna": {
+            "data": {
+                "title": validate.text,
+            },
+        },
+    },
+})
 
 class NRK(Plugin):
+    _psapi_url = 'https://psapi.nrk.no'
+
     @classmethod
     def can_handle_url(self, url):
         return _url_re.match(url)
@@ -44,21 +51,38 @@ class NRK(Plugin):
     def _get_streams(self):
         # Get the stream type from the url (tv/radio).
         stream_type = _url_re.match(self.url).group(1).upper()
-        cookie = {
-            "NRK_PLAYER_SETTINGS_{0}".format(stream_type): COOKIE_PARAMS
-        }
 
-        # Construct API URL for this program.
-        baseurl = self.session.http.get(self.url, cookies=cookie, schema=_schema)
-        program_id = _id_re.search(self.url).group(1)
+        # Construct manifest URL for this program.
+        program_type, program_id = _id_re.search(self.url).groups()
+        if program_type == 'direkte':
+            manifest_type = 'channel'
+        elif program_type == 'serie':
+            manifest_type = 'program'
+        elif program_type == 'podkast':
+            manifest_type = 'podcast'
+        manifest_url = urljoin(self._psapi_url, "playback/manifest/{0}/{1}".format(manifest_type, program_id))
 
         # Extract media URL.
-        json_url = urljoin(baseurl, "mediaelement/{0}".format(program_id))
-        res = self.session.http.get(json_url, cookies=cookie)
-        media_element = self.session.http.json(res, schema=_mediaelement_schema)
-        media_url = media_element["mediaUrl"]
+        res = self.session.http.get(manifest_url)
+        manifest = self.session.http.json(res, schema=_playable_schema)
+        asset = manifest['playable']['assets'][0]
 
-        return HLSStream.parse_variant_playlist(self.session, media_url)
+        # Some streams such as podcasts are not HLS but plain files.
+        if asset['format'] == 'HLS':
+            return HLSStream.parse_variant_playlist(self.session, asset['url'])
+        else:
+            return [(self._get_title(manifest), HTTPStream(self.session, asset['url']))]
 
+    def _get_title(self, manifest):
+        statistics = manifest.get("statistics")
+        if not statistics:
+            return None
+        luna = statistics.get("luna")
+        if not luna:
+            return None
+        data = luna.get("data")
+        if not data:
+            return None
+        return data.get("title")
 
 __plugin__ = NRK
