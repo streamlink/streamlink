54cba72dcbd66fd88d2ca4c4feaa728130d7fb65
azizLIGHT <azizLIGHT@users.noreply.github.com>
2020-11-18 12:53:39 -0500

    Fix broken link
    Fixed URL pointing to list of mpv format codes

--- src/streamlink_cli/argparser.py
+++ src/streamlink_cli/argparser.py
@@ -468,7 +468,7 @@ def build_parser():
             inserted inside your --title string.
 
             A full list of the format codes mpv uses is available here:
-            https://mpv.io/manual/stable/#property-expansion
+            https://mpv.io/manual/master/#property-list
 
         Formatting variables available to use in --title:
 
