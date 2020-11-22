b64588527b8e0ec4b7c0814cd254e414516652a5
Billy2011 <kschmidt2007@googlemail.com>
2020-11-22 11:45:37 +0100

    [PATCH] plugins.streann: allow different source URLs
    - fixed plugin compatibility for other domains, not just `www.ellobo106.com`
    - Added some websites where it is used for easier usage with Streamlink,
      did not add them to plugin_matrix it is just unnecessary spam there.
    - Direct iframes from `ott.streann.com` can be used with `--streann-url`
    
    streamlink#3356

--- tests/plugins/test_streann.py
+++ tests/plugins/test_streann.py
@@ -6,16 +6,24 @@ from streamlink.plugins.streann import Streann
 class TestPluginStreann(unittest.TestCase):
     def test_can_handle_url(self):
         should_match = [
-            "https://ott.streann.com/streaming/player.html?U2FsdGVkX1/BPZsbal3VWfGa7sWzTIlFtDEU+hyCEHP9Ma"
-            + "vCxtCfL1G8nXtK5LHSsgFYhG3Nw9UilT1fx3H+lf5R54SLHAU6BKP/n7kjJBDF0HBFblSUHmApiEf5BrU3coECX9RQcR"
-            + "aOY+uLtkeyEktvlrQ5nhg2QRw5x1IjkxFD0Rk5/ylTYxMqnu8snxRe2p09iJLi9E6cG8eXSHZby2We+pyV0obzGDbI2J"
-            + "pyh4dFL646strLxFraeAJq5+mfpTqK10XZQi3sXA1/ULDH5lz+I9bZW4q/wZtlEdyA0CuB9LpDvCM11imLIhthGnRz",
+            # ott.streann.com
+            "https://ott.streann.com/streaming/player.html?U2FsdGVkX1/PAwXdSYuiw+o5BxSoG10K8ShiKMDOOUEuoYiQxiZlD0gg7y+Ij07/OaI9TWk+MHp40Fx4jrOv304Z+PZwLqGJgs+b0xsnfZJMpx+UmYnjys1rzTZ8UeztNjDeYEElKdaHHGkv0HFcGBGWjyWOvQuCbjGyr4dtzLyaChewqR9lNCuil/HOMiL/eYtEMEPVjMdeUFilb5GSVdIyeunr+JnI1tvdPC5ow3rx3NWjbqIHd13qWVSnaZSl/UZ0BDmWBf+Vr+3pPAd1Mg3y01mKaYZywOxRduBW2HZwoLQe2Lok5Z/q4aJHO02ZESqEPLRKkEqMntuuqGfy1g==",  # noqa: E501
+            "https://ott.streann.com/s-secure/player.html?U2FsdGVkX19Z8gO/ThCsK3I1DTIdVzIGRKiJP36DEx2K1n9HeXKV4nmJrKptTZRlTmM4KTxC/Mi5k3kWEsC1pr9QWmQJzKAfGdROMWB6voarQ1UQqe8IMDiibG+lcNTggCIkAS8a+99Kbe/C1W++YEP+BCBC/8Ss2RYIhNyVdqjUtqvv4Exk6l1gJDWNHc6b5P51020dUrkuJCgEJCbJBE/MYFuC5xlhmzf6kcN5GlBrTuwyHYBkkVi1nvjOm1QS0iQw36UgJx9JS3DDTf7BzlAimLV5M1rXS/ME3XpllejHV0aL3sghCBzc4f4AAz1IoTsl4qEamWBxyfy2kdNJRQ==",  # noqa: E501
+            # URL with iframe
+            "http://centroecuador.ec/tv-radio/",
+            "http://crc.cr/estaciones/crc-89-1/",
+            "https://columnaestilos.com/",
+            "https://crc.cr/estaciones/azul-999/",
+            "https://evtv.online/noticias-de-venezuela/",
+            "https://telecuracao.com/",
+            "https://willax.tv/en-vivo/",
         ]
         for url in should_match:
-            self.assertTrue(Streann.can_handle_url(url))
+            self.assertTrue(Streann.can_handle_url(url), url)
 
+    def test_can_handle_url_negative(self):
         should_not_match = [
             "https://ott.streann.com",
         ]
         for url in should_not_match:
-            self.assertFalse(Streann.can_handle_url(url))
+            self.assertFalse(Streann.can_handle_url(url), url)
