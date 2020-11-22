9ea16daabbf0d443f276ceb22820c1f542ad8c90
bastimeyer <mail@bastimeyer.de>
2020-11-22 20:13:31 +0100

    stream.ffmpegmux: only close FFMPEGMuxer once

--- src/streamlink/stream/ffmpegmux.py
+++ src/streamlink/stream/ffmpegmux.py
@@ -153,6 +153,9 @@ class FFMPEGMuxer(StreamIO):
         return data
 
     def close(self):
+        if self.closed:
+            return
+
         log.debug("Closing ffmpeg thread")
         if self.process:
             # kill ffmpeg
@@ -161,10 +164,13 @@ class FFMPEGMuxer(StreamIO):
 
             # close the streams
             for stream in self.streams:
-                if hasattr(stream, "close"):
+                if hasattr(stream, "close") and callable(stream.close):
                     stream.close()
 
             log.debug("Closed all the substreams")
+
         if self.close_errorlog:
             self.errorlog.close()
             self.errorlog = None
+
+        super().close()
