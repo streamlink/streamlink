db46759c07d50182f1b449f6226f0c449a492d0b
bastimeyer <mail@bastimeyer.de>
2020-12-06 17:31:29 +0100

    plugin.api: remove support_plugin

--- tests/plugin/testplugin.py
+++ tests/plugin/testplugin.py
@@ -2,7 +2,6 @@ from io import BytesIO
 
 from streamlink import NoStreamsError
 from streamlink.options import Options
-from streamlink.plugin.api.support_plugin import testplugin_support
 from streamlink.plugins import Plugin
 from streamlink.stream import AkamaiHDStream, HLSStream, HTTPStream, RTMPStream, Stream
 
@@ -64,8 +63,6 @@ class TestPlugin(Plugin):
 
         streams["480p"] = [HTTPStream(self.session, "http://test.se/stream"),
                            RTMPStream(self.session, dict(rtmp="rtmp://test.se"))]
-
-        streams.update(testplugin_support.get_streams(self.session))
 
         return streams
 
--- tests/plugin/testplugin_support.py
+++
@@ -1,5 +0,0 @@
-from streamlink.stream import HTTPStream
-
-
-def get_streams(session):
-    return dict(support=HTTPStream(session, "http://test.se/support"))
