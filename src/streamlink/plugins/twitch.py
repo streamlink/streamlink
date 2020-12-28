600a8e1f12a62dd2419facb765e273b968bdd1a8
bastimeyer <mail@bastimeyer.de>
2020-12-28 15:12:45 +0100

    plugins.twitch: fix access_token on invalid inputs

--- src/streamlink/plugins/twitch.py
+++ src/streamlink/plugins/twitch.py
@@ -304,11 +304,17 @@ class TwitchAPI:
                 "playerType": "embed"
             }
         }
-        subschema = {
-            "value": str,
-            "signature": str,
-            "__typename": "PlaybackAccessToken"
-        }
+        subschema = validate.any(None, validate.all(
+            {
+                "value": str,
+                "signature": str,
+                "__typename": "PlaybackAccessToken"
+            },
+            validate.union((
+                validate.get("signature"),
+                validate.get("value")
+            ))
+        ))
         return self.call_gql(request, schema=validate.Schema(
             {"data": validate.any(
                 validate.all(
@@ -320,11 +326,7 @@ class TwitchAPI:
                     validate.get("videoPlaybackAccessToken")
                 )
             )},
-            validate.get("data"),
-            validate.union((
-                validate.get("signature"),
-                validate.get("value")
-            ))
+            validate.get("data")
         ))
 
     def parse_token(self, tokenstr):
@@ -561,11 +563,8 @@ class Twitch(Plugin):
     def _access_token(self, is_live, channel_or_vod):
         try:
             sig, token = self.api.access_token(is_live, channel_or_vod)
-        except PluginError as err:
-            if "404 Client Error" in str(err):
-                raise NoStreamsError(self.url)
-            else:
-                raise
+        except (PluginError, TypeError):
+            raise NoStreamsError(self.url)
 
         try:
             restricted_bitrates = self.api.parse_token(token)
