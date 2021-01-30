8dec61f9b07ea47ff5df6e7517d985d0b3154795
Vladimir Stavrinov <9163352+vstavrinov@users.noreply.github.com>
2021-01-30 10:12:48 +0300

    plugins.oneplusone: fix iframe url pattern (#3503)
    - fix for new '/' iframe URL
    - allow '//' iframe URL for future changes
    
    Co-authored-by: back-to <backto@protonmail.ch>

--- src/streamlink/plugins/oneplusone.py
+++ src/streamlink/plugins/oneplusone.py
@@ -55,9 +55,12 @@ class OnePlusOne(Plugin):
         parser = Online_Parser()
         parser.feed(res.text)
         url = parser.iframe_url
-        if url.startswith("//"):
+        log.trace(f"find_iframe url: {url}")
+        if url.startswith("/"):
             p = urlparse(self.url)
-            return "{0}:{1}".format(p.scheme, url)
+            if url.startswith("//"):
+                return "{0}:{1}".format(p.scheme, url)
+            return "{0}://{1}{2}".format(p.scheme, p.netloc, url)
         else:
             return url
 
