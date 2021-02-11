164b2579262a72bb291bd641309b703556878332
bastimeyer <mail@bastimeyer.de>
2021-02-11 15:37:34 +0100

    plugins.vtvgo: ignore duplicate params
    - add schemas for params and stream_url
    - find first script tag via itertags which includes `setplayer(`
    - validate params via schema

--- src/streamlink/plugins/vtvgo.py
+++ src/streamlink/plugins/vtvgo.py
@@ -1,19 +1,37 @@
 import logging
 import re
 
-from streamlink.exceptions import PluginError
 from streamlink.plugin import Plugin
+from streamlink.plugin.api import validate
+from streamlink.plugin.api.utils import itertags
 from streamlink.stream import HLSStream
+from streamlink.utils import parse_json
 
 log = logging.getLogger(__name__)
 
 
 class VTVgo(Plugin):
-
     AJAX_URL = 'https://vtvgo.vn/ajax-get-stream'
 
     _url_re = re.compile(r'https://vtvgo\.vn/xem-truc-tuyen-kenh-')
-    _params_re = re.compile(r'''var\s(?P<key>(?:type_)?id|time|token)\s*=\s*["']?(?P<value>[^"']+)["']?;''')
+    _params_re = re.compile(r'''var\s+(?P<key>(?:type_)?id|time|token)\s*=\s*["']?(?P<value>[^"']+)["']?;''')
+
+    _schema_params = validate.Schema(
+        validate.transform(lambda html: next(tag.text for tag in itertags(html, "script") if "setplayer(" in tag.text)),
+        validate.transform(_params_re.findall),
+        [
+            ("id", int),
+            ("type_id", str),
+            ("time", str),
+            ("token", str)
+        ]
+    )
+    _schema_stream_url = validate.Schema(
+        validate.transform(parse_json),
+        {"stream_url": [validate.url()]},
+        validate.get("stream_url"),
+        validate.get(0)
+    )
 
     @classmethod
     def can_handle_url(cls, url):
@@ -25,17 +43,10 @@ class VTVgo(Plugin):
             'Referer': self.url,
             'X-Requested-With': 'XMLHttpRequest',
         })
-        res = self.session.http.get(self.url)
-
-        params = self._params_re.findall(res.text)
-        if not params:
-            raise PluginError('No POST data')
-        elif len(params) != 4:
-            raise PluginError('Not enough POST data: {0!r}'.format(params))
+        params = self.session.http.get(self.url, schema=self._schema_params)
 
         log.trace('{0!r}'.format(params))
-        res = self.session.http.post(self.AJAX_URL, data=dict(params))
-        hls_url = self.session.http.json(res)['stream_url'][0]
+        hls_url = self.session.http.post(self.AJAX_URL, data=dict(params), schema=self._schema_stream_url)
 
         return HLSStream.parse_variant_playlist(self.session, hls_url)
 
