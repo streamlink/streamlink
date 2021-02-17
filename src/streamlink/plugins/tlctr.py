32cb2aae81c5b3c9101306bf62eaa1eb06e219f0
Ian Cameron <1661072+mkbloke@users.noreply.github.com>
2021-02-17 00:31:46 +0000

    plugins.tlctr: update/fix
    - update to use API URL
    - add API schema

--- src/streamlink/plugins/tlctr.py
+++ src/streamlink/plugins/tlctr.py
@@ -2,16 +2,22 @@ import logging
 import re
 
 from streamlink.plugin import Plugin
+from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream
 
 log = logging.getLogger(__name__)
 
 
 class TLCtr(Plugin):
-
     _url_re = re.compile(r'https?://(?:www\.)?tlctv\.com\.tr/canli-izle')
-    _hls_re = re.compile(
-        r'''["'](?P<url>https?://[^/]+/live/hls/[^"']+)["']''')
+    _token_params_re = re.compile(r'var liveUrl = ".*?tlctv(.*?)";')
+    _api_url = 'https://dygvideo.dygdigital.com/live/hls/tlctvdai{0}&json=true'
+
+    _api_schema = validate.Schema({
+        'xtra': {
+            'url': validate.url(),
+        },
+    }, validate.get('xtra'), validate.get('url'))
 
     @classmethod
     def can_handle_url(cls, url):
@@ -20,18 +26,18 @@ class TLCtr(Plugin):
     def _get_streams(self):
         res = self.session.http.get(self.url)
 
-        m = self._hls_re.search(res.text)
+        m = self._token_params_re.search(res.text)
         if not m:
-            log.error('No playlist found.')
+            log.error('Token data not found')
             return
 
-        hls_url = m.group('url')
-        log.debug('URL={0}'.format(hls_url))
-        streams = HLSStream.parse_variant_playlist(self.session, hls_url)
-        if not streams:
-            return {'live': HLSStream(self.session, hls_url)}
-        else:
-            return streams
+        api_url = self._api_url.format(m.group(1))
+        log.debug(f'API URL={api_url}')
+        res = self.session.http.get(api_url)
+        hls_url = self.session.http.json(res, schema=self._api_schema)
+        log.debug(f'HLS URL={hls_url}')
+
+        return HLSStream.parse_variant_playlist(self.session, hls_url)
 
 
 __plugin__ = TLCtr
