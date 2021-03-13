bf86370b38bb28fea8838c6d316744d69dd857ce
bururaku <rakuburu@gmail.com>
2021-03-13 15:14:13 +0900

    Fixed download problem.
    Fixed a bug where some free videos(like this:https://abema.tv/video/episode/199-12_s0_p1) were determined to be premium exclusive and could not be downloaded.

--- src/streamlink/plugins/abematv.py
+++ src/streamlink/plugins/abematv.py
@@ -192,7 +192,7 @@ class AbemaTV(Plugin):
             res = self.session.http.get(self._PRGM_API.format(vid),
                                         headers=auth_header)
             jsonres = self.session.http.json(res, schema=self._PRGM_SCHEMA)
-            return jsonres["label"].get("free", False) is True
+            return jsonres["terms"][0].get("onDemandType", False) == 3
         elif vtype == "slots":
             res = self.session.http.get(self._SLOTS_API.format(vid),
                                         headers=auth_header)
