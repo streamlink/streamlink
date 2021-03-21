ee33a4e0613b7d3e4b03d6371570b541782fa1f8
back-to <backto@protonmail.ch>
2021-03-21 13:36:56 +0100

    plugins.rtpplay: fixed string issue and geo blocked content
    - Fixed string issue
    
    ```
    file:  { fps:"" , hls: "https://.../playlist.m3u8", dash:""},
    ```
    
    - Fixed empty playlist for geo blocked content

--- src/streamlink/plugins/rtpplay.py
+++ src/streamlink/plugins/rtpplay.py
@@ -9,7 +9,11 @@ from streamlink.utils import parse_json
 
 class RTPPlay(Plugin):
     _url_re = re.compile(r"https?://www\.rtp\.pt/play/")
-    _m3u8_re = re.compile(r"""hls:\s*(?:(["'])(?P<string>.+)\1|decodeURIComponent\((?P<obfuscated>\[.*?])\.join\()""")
+    _m3u8_re = re.compile(r"""
+        hls:\s*(?:(["'])(?P<string>[^"']+)\1
+        |
+        decodeURIComponent\((?P<obfuscated>\[.*?])\.join\()
+    """, re.VERBOSE)
 
     _schema_hls = validate.Schema(
         validate.transform(_m3u8_re.search),
@@ -37,12 +41,7 @@ class RTPPlay(Plugin):
         hls_url = self.session.http.get(self.url, schema=self._schema_hls)
         if not hls_url:
             return
-
-        streams = HLSStream.parse_variant_playlist(self.session, hls_url)
-        if not streams:
-            return {"live": HLSStream(self.session, hls_url)}
-        else:
-            return streams
+        return HLSStream.parse_variant_playlist(self.session, hls_url)
 
 
 __plugin__ = RTPPlay
