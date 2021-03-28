6b454052d1a79e1aa07acedb2837f0481faf8f9a
back-to <backto@protonmail.ch>
2021-03-28 13:08:52 +0200

    plugins.radiko: use API instead of hardcoded URLs

--- src/streamlink/plugins/radiko.py
+++ src/streamlink/plugins/radiko.py
@@ -1,13 +1,17 @@
 import base64
 import datetime
 import hashlib
+import logging
 import random
 import re
 import xml.etree.ElementTree as ET
-from urllib.parse import urlencode
 
 from streamlink.plugin import Plugin
+from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream
+from streamlink.utils import parse_xml
+
+log = logging.getLogger(__name__)
 
 
 class Radiko(Plugin):
@@ -18,53 +22,55 @@ class Radiko(Plugin):
 
     @classmethod
     def can_handle_url(cls, url):
-        return cls._url_re.match(url)
+        return cls._url_re.match(url) is not None
 
     def _get_streams(self):
         match = self._url_re.match(self.url)
         state = match.group('state')
         station_id = match.group('station_id').upper()
-        if state == 'live':
-            url, token = self._live(station_id)
-        else:
+        is_timefree = '0' if state == 'live' else '1'
+
+        url = self.session.http.get(
+            f'https://radiko.jp/v3/station/stream/pc_html5/{station_id}.xml',
+            schema=validate.Schema(
+                validate.transform(parse_xml),
+                validate.xml_findall('.//url[@areafree="1"]'),
+                [validate.union({
+                    'timefree': validate.all(
+                        validate.getattr('attrib'),
+                        validate.get('timefree'),
+                    ),
+                    'url': validate.all(
+                        validate.xml_find('./playlist_create_url'),
+                        validate.getattr('text'),
+                    ),
+                })],
+                validate.filter(lambda x: x['timefree'] == is_timefree),
+                validate.get(0),
+                validate.get('url'),
+            )
+        )
+        log.debug(f'url={url}')
+        params = {
+            'station_id': station_id,
+            'l': 15,
+            'lsid': hashlib.md5(str(random.random()).encode('utf-8')).hexdigest(),
+            'type': 'b',
+        }
+        token, area_id = self._authorize()
+        log.debug(f'area_id={area_id}')
+        if is_timefree == '1':
             start_at = match.group('start_at')
-            url, token = self._timefree(station_id, start_at)
-        headers = {
-            'X-Radiko-AuthToken': token
-        }
-        self.session.http.headers = headers
-        yield from HLSStream.parse_variant_playlist(self.session, url).items()
-
-    def _live(self, station_id):
-        live_url = 'http://f-radiko.smartstream.ne.jp/{}/_definst_/simul-stream.stream/playlist.m3u8'.format(station_id)
-        token, area_id = self._authorize()
-        lsid = hashlib.md5(str(random.random()).encode('utf-8')).hexdigest()
-        live_params = {
-            'station_id': station_id,
-            'l': 15,
-            'lsid': lsid,
-            'type': 'b'
-        }
-        url = live_url + '?' + urlencode(live_params)
-        return url, token
-
-    def _timefree(self, station_id, start_at):
-        m3u8_url = 'https://tf-rpaa.smartstream.ne.jp/tf/playlist.m3u8'
-        token, area_id = self._authorize()
-        lsid = hashlib.md5(str(random.random()).encode('utf-8')).hexdigest()
-        end_at = self._get_xml(start_at, station_id)
-        m3u8_params = {
-            'station_id': station_id,
-            'start_at': start_at,
-            'ft': start_at,
-            'end_at': end_at,
-            'to': end_at,
-            'l': 15,
-            'lsid': lsid,
-            'type': 'b'
-        }
-        url = m3u8_url + '?' + urlencode(m3u8_params)
-        return url, token
+            end_at = self._get_xml(start_at, station_id)
+            params.update({
+                'start_at': start_at,
+                'ft': start_at,
+                'end_at': end_at,
+                'to': end_at,
+            })
+        log.debug(f'params={params!r}')
+        self.session.http.headers = {'X-Radiko-AuthToken': token}
+        yield from HLSStream.parse_variant_playlist(self.session, url, params=params).items()
 
     def _authorize(self):
         headers = {
@@ -97,8 +103,7 @@ class Radiko(Plugin):
             date = yesterday.strftime('%Y%m%d')
         else:
             date = today.strftime('%Y%m%d')
-        api = 'http://radiko.jp/v3/program/station/date/{}/{}.xml'.format(date, station_id)
-        r = self.session.http.get(api)
+        r = self.session.http.get(f'http://radiko.jp/v3/program/station/date/{date}/{station_id}.xml')
         tree = ET.XML(r.content)
         for x in tree[2][0][1].findall('prog'):
             if x.attrib['ft'] == start_at:
