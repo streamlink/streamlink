4d55e60ff4e9a96003f9a50b7f347cbdbb8962bb
Ian Cameron <1661072+mkbloke@users.noreply.github.com>
2021-03-31 21:09:42 +0100

    plugins.bfmtv: fix/find Brightcove video data in JS

--- src/streamlink/plugins/bfmtv.py
+++ src/streamlink/plugins/bfmtv.py
@@ -1,5 +1,6 @@
 import logging
 import re
+from urllib.parse import urljoin
 
 from streamlink.plugin import Plugin
 from streamlink.plugins.brightcove import BrightcovePlayer
@@ -22,6 +23,10 @@ class BFMTV(Plugin):
         r'<iframe.*?src=".*?/(?P<video_id>\w+)"',
         re.DOTALL
     )
+    _main_js_url_re = re.compile(r'src="([\w/]+/main\.\w+\.js)"')
+    _js_brightcove_video_re = re.compile(
+        r'i\?\(N="[^"]+",y="(?P<video_id>[0-9]+).*"data-account"\s*:\s*"(?P<account_id>[0-9]+)',
+    )
 
     @classmethod
     def can_handle_url(cls, url):
@@ -31,20 +36,34 @@ class BFMTV(Plugin):
         # Retrieve URL page and search for Brightcove video data
         res = self.session.http.get(self.url)
         match = self._brightcove_video_re.search(res.text) or self._brightcove_video_alt_re.search(res.text)
-        if match is not None:
+        if match:
             account_id = match.group('account_id')
             log.debug(f'Account ID: {account_id}')
             video_id = match.group('video_id')
             log.debug(f'Video ID: {video_id}')
             player = BrightcovePlayer(self.session, account_id)
             yield from player.get_streams(video_id)
-        else:
-            # Try to find the Dailymotion video ID
-            match = self._embed_video_id_re.search(res.text)
-            if match is not None:
+
+        # Try to find the Dailymotion video ID
+        match = self._embed_video_id_re.search(res.text)
+        if match:
+            video_id = match.group('video_id')
+            log.debug(f'Video ID: {video_id}')
+            yield from self.session.streams(self._dailymotion_url.format(video_id)).items()
+
+        # Try the JS for Brightcove video data
+        match = self._main_js_url_re.search(res.text)
+        if match:
+            log.debug(f'JS URL: {urljoin(self.url, match.group(1))}')
+            res = self.session.http.get(urljoin(self.url, match.group(1)))
+            match = self._js_brightcove_video_re.search(res.text)
+            if match:
+                account_id = match.group('account_id')
+                log.debug(f'Account ID: {account_id}')
                 video_id = match.group('video_id')
                 log.debug(f'Video ID: {video_id}')
-                yield from self.session.streams(self._dailymotion_url.format(video_id)).items()
+                player = BrightcovePlayer(self.session, account_id)
+                yield from player.get_streams(video_id)
 
 
 __plugin__ = BFMTV
