ccff0d2b00947775d77a6ce5290abcc8c9518534
Billy2011 <kschmidt2007@googlemail.com>
2021-04-16 10:32:42 +0200

    plugins.wetter: add mp4 stream type

--- src/streamlink/plugins/wetter.py
+++ src/streamlink/plugins/wetter.py
@@ -1,7 +1,6 @@
 import logging
 import re
 
-from streamlink.exceptions import PluginError
 from streamlink.plugin import Plugin
 from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream, HTTPStream, RTMPStream
@@ -11,7 +10,7 @@ log = logging.getLogger(__name__)
 
 class Wetter(Plugin):
     _url_re = re.compile(r"https?://(?:www\.)?wetter\.com/")
-    _videourl_re = re.compile(r'data-video-url-(hls|rtmp|endpoint)\s*=\s*"(.+)"')
+    _videourl_re = re.compile(r'data-video-url-(hls|rtmp|endpoint|mp4)\s*=\s*"(.+)"')
 
     _stream_schema = validate.Schema(
         validate.transform(_videourl_re.findall),
@@ -38,24 +37,21 @@ class Wetter(Plugin):
         return cls._url_re.match(url) is not None
 
     def _get_streams(self):
-        try:
-            streams = self.session.http.get(self.url, schema=self._stream_schema)
-        except PluginError as err:
-            log.debug(err)
-            return
-
+        streams = self.session.http.get(self.url, schema=self._stream_schema)
         for stream in streams:
             if stream["stream-type"] == "hls":
                 for s in HLSStream.parse_variant_playlist(self.session, stream["url"]).items():
                     yield s
             elif stream["stream-type"] == "rtmp":
-                yield "0live", RTMPStream(self.session, {"rtmp": stream["url"]})
+                yield "0_live", RTMPStream(self.session, {"rtmp": stream["url"]})
             elif stream["stream-type"] == "endpoint":
                 res = self.session.http.get(stream["url"])
                 files = self.session.http.json(res, schema=self._endpoint_schema)
                 for f in files:
                     s = HTTPStream(self.session, f["file"])
                     yield "vod", s
+            elif stream["stream-type"] == "mp4":
+                yield "vod", HTTPStream(self.session, stream["url"])
 
 
 __plugin__ = Wetter
