b3a0f64364fd0cee52e5cd62f11e783f619a2c42
Billy2011 <kschmidt2007@googlemail.com>
2021-06-01 13:15:54 +0200

    [PATCH] plugins.bfmtv: fix/find Brightcove video data in JS (#3662)
    * plugins.bfmtv: fix/find Brightcove video data in JS
    
    * plugins.bfmtv: update audio, return on match, fix regex
    
    Co-authored-by: back-to <backto@protonmail.ch>

--- src/streamlink/plugins/bfmtv.py
+++ src/streamlink/plugins/bfmtv.py
@@ -1,8 +1,11 @@
 import logging
 import re
 
+from streamlink.compat import urljoin, urlparse
 from streamlink.plugin import Plugin
+from streamlink.plugin.api.utils import itertags
 from streamlink.plugins.brightcove import BrightcovePlayer
+from streamlink.stream import HTTPStream
 
 log = logging.getLogger(__name__)
 
@@ -22,31 +25,70 @@ class BFMTV(Plugin):
         r'<iframe.*?src=".*?/(?P<video_id>\w+)"',
         re.DOTALL
     )
+    _main_js_url_re = re.compile(r'src="([\w/]+/main\.\w+\.js)"')
+    _js_brightcove_video_re = re.compile(
+        r'i\?\([A-Z]="[^"]+",y="(?P<video_id>[0-9]+).*"data-account"\s*:\s*"(?P<account_id>[0-9]+)',
+    )
 
     @classmethod
     def can_handle_url(cls, url):
         return cls._url_re.match(url) is not None
 
     def _get_streams(self):
-        # Retrieve URL page and search for Brightcove video data
         res = self.session.http.get(self.url)
-        match = self._brightcove_video_re.search(res.text) or self._brightcove_video_alt_re.search(res.text)
-        if match is not None:
-            account_id = match.group('account_id')
-            log.debug('Account ID: {0}'.format(account_id))
-            video_id = match.group('video_id')
+
+        m = self._brightcove_video_re.search(res.text) or self._brightcove_video_alt_re.search(res.text)
+        if m:
+            account_id = m.group('account_id')
+            log.debug('Account ID: {}'.format(account_id))
+            video_id = m.group('video_id')
             log.debug('Video ID: {0}'.format(video_id))
             player = BrightcovePlayer(self.session, account_id)
-            for stream in player.get_streams(video_id):
-                yield stream
-        else:
-            # Try to find the Dailymotion video ID
-            match = self._embed_video_id_re.search(res.text)
-            if match is not None:
-                video_id = match.group('video_id')
+            for s in player.get_streams(video_id):
+                yield s
+            return
+
+        # Try to find the Dailymotion video ID
+        m = self._embed_video_id_re.search(res.text)
+        if m:
+            video_id = m.group('video_id')
+            log.debug('Video ID: {0}'.format(video_id))
+            for s in self.session.streams(self._dailymotion_url.format(video_id)).items():
+                yield s
+            return
+
+        # Try the JS for Brightcove video data
+        m = self._main_js_url_re.search(res.text)
+        if m:
+            log.debug('JS URL: {}'.format(urljoin(self.url, m.group(1))))
+            res = self.session.http.get(urljoin(self.url, m.group(1)))
+            m = self._js_brightcove_video_re.search(res.text)
+            if m:
+                account_id = m.group('account_id')
+                log.debug('Account ID: {0}'.format(account_id))
+                video_id = m.group('video_id')
                 log.debug('Video ID: {0}'.format(video_id))
-                for stream in self.session.streams(self._dailymotion_url.format(video_id)).items():
-                    yield stream
+                player = BrightcovePlayer(self.session, account_id)
+                for s in player.get_streams(video_id):
+                    yield s
+                return
+
+        # Audio Live
+        audio_url = None
+        for source in itertags(res.text, 'source'):
+            url = source.attributes.get('src')
+            if url:
+                p_url = urlparse(url)
+                if p_url.path.endswith(('.mp3')):
+                    audio_url = url
+
+        # Audio VOD
+        for div in itertags(res.text, 'div'):
+            if div.attributes.get('class') == 'audio-player':
+                audio_url = div.attributes.get('data-media-url')
+
+        if audio_url:
+            yield 'audio', HTTPStream(self.session, audio_url)
 
 
 __plugin__ = BFMTV
