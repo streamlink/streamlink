012d53da688d2413c8ee3f5baedf6455b87876c2
bastimeyer <mail@bastimeyer.de>
2021-06-04 13:42:44 +0200

    plugin.api.validate: add nested lookups to get()

--- tests/test_api_validate.py
+++ tests/test_api_validate.py
@@ -84,7 +84,25 @@ class TestPluginAPIValidate(unittest.TestCase):
 
     def test_get(self):
         assert validate(get("key"), {"key": "value"}) == "value"
+        assert validate(get("key"), re.match(r"(?P<key>.+)", "value")) == "value"
+        assert validate(get("invalidkey"), {"key": "value"}) is None
         assert validate(get("invalidkey", "default"), {"key": "value"}) == "default"
+        assert validate(get(3, "default"), [0, 1, 2]) == "default"
+
+        with self.assertRaisesRegex(ValueError, "'NoneType' object is not subscriptable"):
+            validate(get("key"), None)
+
+        data = {"one": {"two": {"three": "value1"}},
+                ("one", "two", "three"): "value2"}
+        assert validate(get(("one", "two", "three")), data) == "value1", "Recursive lookup"
+        assert validate(get(("one", "two", "three"), strict=True), data) == "value2", "Strict tuple-key lookup"
+        assert validate(get(("one", "two", "invalidkey")), data) is None, "Default value is None"
+        assert validate(get(("one", "two", "invalidkey"), "default"), data) == "default", "Custom default value"
+
+        with self.assertRaisesRegex(ValueError, "Object \"{'two': {'three': 'value1'}}\" does not have item \"invalidkey\""):
+            validate(get(("one", "invalidkey", "three")), data)
+        with self.assertRaisesRegex(ValueError, "'NoneType' object is not subscriptable"):
+            validate(all(get("one"), get("invalidkey"), get("three")), data)
 
     def test_get_re(self):
         m = re.match(r"(\d+)p", "720p")
