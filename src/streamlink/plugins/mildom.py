7bd23a18af9018c354810851612c58c910084646
back-to <backto@protonmail.ch>
2021-06-04 22:40:08 +0200

    Update mildom.py

--- src/streamlink/plugins/mildom.py
+++ src/streamlink/plugins/mildom.py
@@ -2,15 +2,16 @@ import logging
 import re
 
 from streamlink.plugin import Plugin
+from streamlink.plugin.api import validate
 from streamlink.stream import HLSStream
+from streamlink.utils import parse_json
+from streamlink.utils.url import url_concat
 
 log = logging.getLogger(__name__)
 
 
 class Mildom(Plugin):
-
-    _re_url = re.compile(r"""
-        https?://(www\.)?mildom\.com/
+    _re_url = re.compile(r"""https?://(?:www\.)?mildom\.com/
         (?:
             playback/(\d+)(/(?P<video_id>(\d+)\-(\w+)))
             |
@@ -18,48 +19,101 @@ class Mildom(Plugin):
         )
     """, re.VERBOSE)
 
-    _VOD_API_URL = "https://cloudac.mildom.com/nonolive/videocontent/playback/getPlaybackDetail?v_id={}"
-
-    _STATUS_API_URL = "https://cloudac.mildom.com/nonolive/gappserv/live/enterstudio?__platform=web&user_id={}"
-
-    _LIVESERVER_API_URL = 'https://cloudac.mildom.com/nonolive/gappserv/live/liveserver'\
-                          '?__platform=web&user_id=4253873&live_server_type=hls'
-
     @classmethod
     def can_handle_url(cls, url):
         return cls._re_url.match(url)
 
     def _get_vod_streams(self, video_id):
-        res = self.session.http.get(self._VOD_API_URL.format(video_id))
-        response_json = self.session.http.json(res)
-        if response_json.get("code"):
-            log.debug("Mildom API returned an error. Vod is probably invalid")
+        data = self.session.http.get(
+            "https://cloudac.mildom.com/nonolive/videocontent/playback/getPlaybackDetail",
+            params={
+                "__platform": "web",
+                "v_id": video_id,
+            },
+            schema=validate.Schema(validate.transform(parse_json), {
+                "code": int,
+                validate.optional("message"): str,
+                validate.optional("body"): {
+                    "playback": {
+                        "video_link": [{"name": str, "url": validate.url()}],
+                    },
+                },
+            })
+        )
+        log.trace(f"{data!r}")
+        if data["code"] != 0:
+            log.debug(data.get("message", "Mildom API returned an error"))
             return
-        for stream in response_json["body"]["playback"]["video_link"]:
+        for stream in data["body"]["playback"]["video_link"]:
             yield stream["name"], HLSStream(self.session, stream["url"])
 
     def _get_live_streams(self, channel_id):
         # Get quality info and check if user is live1
-        res = self.session.http.get(self._STATUS_API_URL.format(channel_id))
-        response_json = self.session.http.json(res)
-        if response_json.get("code"):
-            log.debug("Mildom API returned an error")
+        data = self.session.http.get(
+            "https://cloudac.mildom.com/nonolive/gappserv/live/enterstudio",
+            params={
+                "__platform": "web",
+                "user_id": channel_id,
+            },
+            headers={"Accept-Language": "en"},
+            schema=validate.Schema(
+                validate.transform(parse_json),
+                {
+                    "code": int,
+                    validate.optional("message"): str,
+                    validate.optional("body"): {
+                        validate.optional("status"): int,
+                        "anchor_live": int,
+                        validate.optional("live_type"): int,
+                        "ext": {
+                            "cmode_params": [{
+                                "cmode": str,
+                                "name": str,
+                            }],
+                            validate.optional("live_mode"): int,
+                        },
+                    },
+                },
+            )
+        )
+        log.trace(f"{data!r}")
+        if data["code"] != 0:
+            log.debug(data.get("message", "Mildom API returned an error"))
             return
-        if response_json["body"].get("anchor_live") != 11:
+        if data["body"]["anchor_live"] != 11:
             log.debug("User doesn't appear to be live")
             return
         qualities = []
-        for quality_info in response_json["body"]["ext"]["cmode_params"]:
+        for quality_info in data["body"]["ext"]["cmode_params"]:
             qualities.append((quality_info["name"], "_" + quality_info["cmode"] if quality_info["cmode"] != "raw" else ""))
+
         # Create stream URLs
-        res = self.session.http.get(self._LIVESERVER_API_URL.format(channel_id))
-        response_json = self.session.http.json(res)
-        if response_json.get("code"):
-            log.debug("Mildom API returned an error")
+        data = self.session.http.get(
+            "https://cloudac.mildom.com/nonolive/gappserv/live/liveserver",
+            params={
+                "__platform": "web",
+                "user_id": channel_id,
+                "live_server_type": "hls",
+            },
+            headers={"Accept-Language": "en"},
+            schema=validate.Schema(
+                validate.transform(parse_json),
+                {
+                    "code": int,
+                    validate.optional("message"): str,
+                    validate.optional("body"): {
+                        "stream_server": validate.url(),
+                    }
+                }
+            )
+        )
+        log.trace(f"{data!r}")
+        if data["code"] != 0:
+            log.debug(data.get("message", "Mildom API returned an error"))
             return
-        base_url = response_json["body"]["stream_server"] + "/" + str(channel_id) + "{}.m3u8"
+        base_url = url_concat(data["body"]["stream_server"], f"{channel_id}{{}}.m3u8")
+        self.session.http.headers.update({"Referer": "https://www.mildom.com/"})
         for quality in qualities:
-            self.session.http.headers.update({"Referer": "https://www.mildom.com/"})
             yield quality[0], HLSStream(self.session, base_url.format(quality[1]))
 
     def _get_streams(self):
