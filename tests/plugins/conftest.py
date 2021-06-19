f7b6f15044f504914c3cc236665595f9bfaed247
bastimeyer <mail@bastimeyer.de>
2021-06-19 21:49:34 +0200

    plugins.youtube: translate embed_live URLs
    - translate embedded live-channel URLs to avoid unnecessary HTTP
      redirections and having to find the canonical URL in the metadata
    - add URL translation tests
    - fix plugins can_handle_url pytest collection function

--- tests/plugins/conftest.py
+++ tests/plugins/conftest.py
@@ -2,7 +2,7 @@ from tests.plugins import PluginCanHandleUrl, generic_negative_matches
 
 
 def pytest_generate_tests(metafunc):
-    if issubclass(metafunc.cls, PluginCanHandleUrl):  # pragma: no branch
+    if metafunc.cls is not None and issubclass(metafunc.cls, PluginCanHandleUrl):  # pragma: no branch
         if metafunc.function.__name__ == "test_can_handle_url_positive":
             metafunc.parametrize("url", metafunc.cls.should_match)
         elif metafunc.function.__name__ == "test_can_handle_url_negative":
