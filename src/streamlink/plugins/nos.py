23920009523ee78538491b577206dcf6f995b975
back-to <backto@protonmail.ch>
2021-06-20 18:05:46 +0200

    plugins.nos: Fixed Livestream and VOD
    ```
    $ streamlink https://nos.nl/collectie/13781/livestream/2385461-ek-voetbal-voorbeschouwing-italie-wales-18-00-uur
    [cli][info] Found matching plugin nos for URL https://nos.nl/collectie/13781/livestream/2385461-ek-voetbal-voorbeschouwing-italie-wales-18-00-uur
    [cli][info] Available streams: 150k (worst), 216p, 270p, 432p, 576p, 1080p (best)
    [cli][info] Opening stream: 1080p (hls)
    ```

--- src/streamlink/plugins/nos.py
+++ src/streamlink/plugins/nos.py
@@ -1,53 +1,91 @@
-"""Plugin for NOS: Nederlandse Omroep Stichting
-
-Supports:
-   MP$: http://nos.nl/uitzending/nieuwsuur.html
-   Live: http://www.nos.nl/livestream/*
-   Tour: http://nos.nl/tour/live
-"""
 import logging
 import re
-from urllib.parse import urljoin
 
 from streamlink.plugin import Plugin
+from streamlink.plugin.api import validate
 from streamlink.plugin.api.utils import itertags
 from streamlink.stream import HLSStream
+from streamlink.utils import parse_json
 
 log = logging.getLogger(__name__)
 
 
 class NOS(Plugin):
-    _url_re = re.compile(r"https?://(?:\w+\.)?nos.nl/")
+    _re_url = re.compile(r"""https?://(?:\w+\.)?nos\.nl/(?:
+        livestream
+        |
+        collectie
+        |
+        video
+        |
+        uitzendingen
+    )""", re.VERBOSE)
+    _msg_live_offline = "This livestream is offline."
+    title = None
+    vod_keys = {
+        "pages/Collection/Video/Video": "item",
+        "pages/Video/Video": "video",
+    }
 
     @classmethod
     def can_handle_url(cls, url):
-        return cls._url_re.match(url)
+        return cls._re_url.match(url) is not None
 
-    def _resolve_stream(self):
-        res = self.session.http.get(self.url)
-        for video in itertags(res.text, 'video'):
-            stream_url = video.attributes.get("data-stream")
-            log.debug("Stream data: {0}".format(stream_url))
-            return HLSStream.parse_variant_playlist(self.session, stream_url)
-
-    def _get_source_streams(self):
-        res = self.session.http.get(self.url)
-
-        for atag in itertags(res.text, 'a'):
-            if "video-play__link" in atag.attributes.get("class", ""):
-                href = urljoin(self.url, atag.attributes.get("href"))
-                log.debug("Loading embedded video page")
-                vpage = self.session.http.get(href, params=dict(ajax="true", npo_cc_skip_wall="true"))
-                for source in itertags(vpage.text, 'source'):
-                    return HLSStream.parse_variant_playlist(self.session, source.attributes.get("src"))
+    def get_title(self):
+        return self.title
 
     def _get_streams(self):
-        if "/livestream/" in self.url or "/tour/" in self.url:
-            log.debug("Finding live streams")
-            return self._resolve_stream()
-        else:
-            log.debug("Finding VOD streams")
-            return self._get_source_streams()
+        res = self.session.http.get(self.url)
+        for script in itertags(res.text, "script"):
+            _type = script.attributes.get("type")
+            if not (_type and _type == "application/json"):
+                continue
+
+            video_url = None
+            _data_ssr_name = script.attributes.get("data-ssr-name")
+            if not _data_ssr_name:
+                continue
+
+            log.trace(f"Found _data_ssr_name={_data_ssr_name}")
+            if _data_ssr_name == "pages/Broadcasts/Broadcasts":
+                self.title, video_url, is_live = parse_json(script.text, schema=validate.Schema({
+                    "currentLivestream": {
+                        "is_live": bool,
+                        "title": str,
+                        "stream": validate.url(),
+                    }},
+                    validate.get("currentLivestream"),
+                    validate.union_get("title", "stream", "is_live")))
+                if not is_live:
+                    log.error(self._msg_live_offline)
+                    continue
+            elif _data_ssr_name == "pages/Livestream/Livestream":
+                self.title, video_url, is_live = parse_json(script.text, schema=validate.Schema({
+                    "streamIsLive": bool,
+                    "title": str,
+                    "stream": validate.url(),
+                }, validate.union_get("title", "stream", "streamIsLive")))
+                if not is_live:
+                    log.error(self._msg_live_offline)
+                    continue
+            elif _data_ssr_name in self.vod_keys.keys():
+                _key = self.vod_keys[_data_ssr_name]
+                self.title, video_url = parse_json(script.text, schema=validate.Schema({
+                    _key: {
+                        "title": str,
+                        "aspect_ratios": {
+                            "profiles": validate.all([{
+                                "name": str,
+                                "url": validate.url(),
+                            }], validate.filter(lambda n: n["name"] == "hls_unencrypted"))
+                        }
+                    }},
+                    validate.get(_key),
+                    validate.union_get("title", ("aspect_ratios", "profiles", 0, "url"))))
+
+            if video_url is not None:
+                yield from HLSStream.parse_variant_playlist(self.session, video_url).items()
+                break
 
 
 __plugin__ = NOS
