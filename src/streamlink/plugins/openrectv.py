272bd2e6fa653da55f6bd9eb21fbc42ed5e3a6a0
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-07-07 19:17:46 +0900

    plugins.openrectv: update HLS URLs (#3850)

--- src/streamlink/plugins/openrectv.py
+++ src/streamlink/plugins/openrectv.py
@@ -22,9 +22,11 @@ class OPENRECtv(Plugin):
         validate.optional("id"): validate.text,
         validate.optional("title"): validate.text,
         validate.optional("movie_type"): validate.text,
+        validate.optional("onair_status"): validate.any(None, int),
         validate.optional("media"): {
             "url": validate.any(None, validate.url()),
-            "url_public": validate.any(None, validate.url())
+            "url_public": validate.any(None, validate.url()),
+            "url_ull": validate.any(None, validate.url()),
         }
     })
 
@@ -96,20 +98,30 @@ class OPENRECtv(Plugin):
         if self.get_option("email") and self.get_option("password"):
             self.login(self.get_option("email"), self.get_option("password"))
         mdata = self._get_movie_data()
+
         if mdata:
             log.debug("Found video: {0} ({1})".format(mdata["title"], mdata["id"]))
-            if mdata["media"]["url"]:
-                yield from HLSStream.parse_variant_playlist(
-                    self.session,
-                    mdata["media"]["url"]
-                ).items()
-            elif mdata["media"]["url_public"]:
-                yield from HLSStream.parse_variant_playlist(
-                    self.session,
-                    mdata["media"]["url_public"].replace("public.m3u8", "playlist.m3u8")
-                ).items()
+            m3u8_file = None
+            # streaming
+            if mdata["onair_status"] == 1:
+                m3u8_file = mdata["media"]["url_ull"]
+            # archive
+            elif mdata["onair_status"] == 2 and mdata["media"]["url_public"] is not None:
+                m3u8_file = mdata["media"]["url_public"].replace("public.m3u8", "playlist.m3u8")
+            # uploaded video
+            elif mdata["onair_status"] is None and mdata["movie_type"] == "2":
+                m3u8_file = mdata["media"]["url"]
             else:
-                log.error("You don't have the authority.")
+                log.error("There is no video file.")
+
+            if m3u8_file is not None:
+                yield from HLSStream.parse_variant_playlist(
+                    self.session,
+                    m3u8_file
+                ).items()
+
+        else:
+            log.error("You don't have the authority or no video file.")
 
 
 __plugin__ = OPENRECtv
