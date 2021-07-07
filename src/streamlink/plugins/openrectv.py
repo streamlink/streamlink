ddc23695525afe626700719da35858a9043da62e
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-07-07 18:53:43 +0900

    plugins.openrec.tv: update validation schema

--- src/streamlink/plugins/openrectv.py
+++ src/streamlink/plugins/openrectv.py
@@ -22,9 +22,11 @@ class OPENRECtv(Plugin):
         validate.optional("id"): validate.text,
         validate.optional("title"): validate.text,
         validate.optional("movie_type"): validate.text,
+        validate.optional("onair_status"): validate.any(None, int),
         validate.optional("media"): {
             "url": validate.any(None, validate.url()),
-            "url_public": validate.any(None, validate.url())
+            "url_public": validate.any(None, validate.url()),
+            "url_ull": validate.any(None, validate.url()),
         }
     })
 
@@ -99,14 +101,15 @@ class OPENRECtv(Plugin):
 
         if mdata:
             log.debug("Found video: {0} ({1})".format(mdata["title"], mdata["id"]))
+            m3u8_file = None
             # streaming
             if mdata["onair_status"] == 1:
                 m3u8_file = mdata["media"]["url_ull"]
             # archive
-            elif mdata["onair_status"] == 2:
+            elif mdata["onair_status"] == 2 and mdata["media"]["url_public"] is not None:
                 m3u8_file = mdata["media"]["url_public"].replace("public.m3u8", "playlist.m3u8")
             # uploaded video
-            elif mdata["onair_status"] is None and mdata["movie_type"] == 2:
+            elif mdata["onair_status"] is None and mdata["movie_type"] == "2":
                 m3u8_file = mdata["media"]["url"]
             else:
                 log.error("There is no video file.")
