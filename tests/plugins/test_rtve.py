2c8f0ba2af38225735a6ffb4d9402ed47ad28c8a
back-to <backto@protonmail.ch>
2021-08-06 22:54:20 +0200

    plugins.rtve: update for /play/ URLs
    - update pluginmatcher URL
    - use https
    - remove to many mirror streams
    - fix idAsset

--- tests/plugins/test_rtve.py
+++ tests/plugins/test_rtve.py
@@ -6,13 +6,16 @@ class TestPluginCanHandleUrlRtve(PluginCanHandleUrl):
     __plugin__ = Rtve
 
     should_match = [
+        'https://www.rtve.es/play/videos/directo/la-1/',
+        'https://www.rtve.es/play/videos/directo/canales-lineales/24h/',
+        'https://www.rtve.es/play/videos/rebelion-en-el-reino-salvaje/mata-reyes/5803959/',
+    ]
+
+    should_not_match = [
+        'https://www.rtve.es',
         'http://www.rtve.es/directo/la-1',
         'http://www.rtve.es/directo/la-2/',
         'http://www.rtve.es/directo/teledeporte/',
         'http://www.rtve.es/directo/canal-24h/',
         'http://www.rtve.es/infantil/directo/',
-    ]
-
-    should_not_match = [
-        'https://www.rtve.es',
     ]
