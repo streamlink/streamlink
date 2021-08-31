40b604e69c4387d1cdb7a6d680f15caca4369a23
bastimeyer <mail@bastimeyer.de>
2021-08-31 23:57:27 +0200

    plugin.api.validate: add parse_{json,html,xml,qsd}
    - add parse_{json,html,xml,qsd} validate.transform wrapper methods
    - refactor utils module and move parse* methods into utils.parse
    - refactor parse* methods and use common parsing + exception handling
    - reformat all-export list in utils

--- src/streamlink/utils/__init__.py
+++ src/streamlink/utils/__init__.py
@@ -1,17 +1,14 @@
-import json
-import re
 import zlib
 from collections import OrderedDict
 from importlib.machinery import FileFinder, SOURCE_SUFFIXES, SourceFileLoader
 from importlib.util import module_from_spec
 from typing import Dict, Generic, Optional, TypeVar
-from urllib.parse import parse_qsl, urljoin, urlparse
-
-from lxml.etree import XML
+from urllib.parse import urljoin, urlparse
 
 from streamlink.exceptions import PluginError
 from streamlink.utils.lazy_formatter import LazyFormatter
 from streamlink.utils.named_pipe import NamedPipe
+from streamlink.utils.parse import parse_html, parse_json, parse_qsd, parse_xml
 from streamlink.utils.url import update_scheme, url_equal
 
 
@@ -59,74 +56,6 @@ def prepend_www(url):
         return parsed.scheme + "://www." + parsed.netloc + parsed.path
     else:
         return url
-
-
-def parse_json(data, name="JSON", exception=PluginError, schema=None):
-    """Wrapper around json.loads.
-
-    Wraps errors in custom exception with a snippet of the data in the message.
-    """
-    try:
-        json_data = json.loads(data)
-    except ValueError as err:
-        snippet = repr(data)
-        if len(snippet) > 35:
-            snippet = snippet[:35] + " ..."
-        else:
-            snippet = data
-
-        raise exception("Unable to parse {0}: {1} ({2})".format(name, err, snippet))
-
-    if schema:
-        json_data = schema.validate(json_data, name=name, exception=exception)
-
-    return json_data
-
-
-def parse_xml(data, name="XML", ignore_ns=False, exception=PluginError, schema=None, invalid_char_entities=False):
-    """Wrapper around ElementTree.fromstring with some extras.
-
-    Provides these extra features:
-     - Handles incorrectly encoded XML
-     - Allows stripping namespace information
-     - Wraps errors in custom exception with a snippet of the data in the message
-    """
-    if isinstance(data, str):
-        data = bytes(data, "utf8")
-
-    if ignore_ns:
-        data = re.sub(br"[\t ]xmlns=\"(.+?)\"", b"", data)
-
-    if invalid_char_entities:
-        data = re.sub(br"&(?!(?:#(?:[0-9]+|[Xx][0-9A-Fa-f]+)|[A-Za-z0-9]+);)", b"&amp;", data)
-
-    try:
-        tree = XML(data)
-    except Exception as err:
-        snippet = repr(data)
-        if len(snippet) > 35:
-            snippet = snippet[:35] + " ..."
-
-        raise exception(f"Unable to parse {name}: {err} ({snippet})")
-
-    if schema:
-        tree = schema.validate(tree, name=name, exception=exception)
-
-    return tree
-
-
-def parse_qsd(data, name="query string", exception=PluginError, schema=None, **params):
-    """Parses a query string into a dict.
-
-    Unlike parse_qs and parse_qsl, duplicate keys are not preserved in
-    favor of a simpler return value.
-    """
-
-    value = dict(parse_qsl(data, **params))
-    if schema:
-        value = schema.validate(value, name=name, exception=exception)
-
-    return value
 
 
 def rtmpparse(url):
@@ -210,7 +139,15 @@ class LRUCache(Generic[TCacheKey, TCacheValue]):
             self.cache.popitem(last=False)
 
 
-__all__ = ["load_module", "swfdecompress", "update_scheme", "url_equal",
-           "verifyjson", "absolute_url", "parse_qsd", "parse_json",
-           "parse_xml", "rtmpparse", "prepend_www", "NamedPipe",
-           "escape_librtmp", "LRUCache", "LazyFormatter"]
+__all__ = [
+    "load_module",
+    "escape_librtmp", "rtmpparse", "swfdecompress",
+    "verifyjson",
+    "absolute_url", "prepend_www",
+    "search_dict",
+    "LRUCache",
+    "LazyFormatter",
+    "NamedPipe",
+    "parse_html", "parse_json", "parse_qsd", "parse_xml",
+    "update_scheme", "url_equal",
+]
---
+++ src/streamlink/utils/parse.py
@@ -0,0 +1,99 @@
+import json
+import re
+from urllib.parse import parse_qsl
+
+from lxml.etree import HTML, XML
+
+from streamlink.plugin import PluginError
+
+
+def _parse(parser, data, name, exception, schema, *args, **kwargs):
+    try:
+        parsed = parser(data, *args, **kwargs)
+    except Exception as err:
+        snippet = repr(data)
+        if len(snippet) > 35:
+            snippet = f"{snippet[:35]} ..."
+
+        raise exception(f"Unable to parse {name}: {err} ({snippet})")
+
+    if schema:
+        parsed = schema.validate(parsed, name=name, exception=exception)
+
+    return parsed
+
+
+def parse_json(
+    data,
+    name="JSON",
+    exception=PluginError,
+    schema=None,
+    *args, **kwargs
+):
+    """Wrapper around json.loads.
+
+    Provides these extra features:
+     - Wraps errors in custom exception with a snippet of the data in the message
+    """
+    return _parse(json.loads, data, name, exception, schema, *args, **kwargs)
+
+
+def parse_html(
+    data,
+    name="HTML",
+    exception=PluginError,
+    schema=None,
+    *args, **kwargs
+):
+    """Wrapper around lxml.etree.HTML with some extras.
+
+    Provides these extra features:
+     - Handles incorrectly encoded HTML
+     - Wraps errors in custom exception with a snippet of the data in the message
+    """
+    if isinstance(data, str):
+        data = bytes(data, "utf8")
+
+    return _parse(HTML, data, name, exception, schema, *args, **kwargs)
+
+
+def parse_xml(
+    data,
+    ignore_ns=False,
+    invalid_char_entities=False,
+    name="XML",
+    exception=PluginError,
+    schema=None,
+    *args, **kwargs
+):
+    """Wrapper around lxml.etree.XML with some extras.
+
+    Provides these extra features:
+     - Handles incorrectly encoded XML
+     - Allows stripping namespace information
+     - Wraps errors in custom exception with a snippet of the data in the message
+    """
+    if isinstance(data, str):
+        data = bytes(data, "utf8")
+    if ignore_ns:
+        data = re.sub(br"[\t ]xmlns=\"(.+?)\"", b"", data)
+    if invalid_char_entities:
+        data = re.sub(br"&(?!(?:#(?:[0-9]+|[Xx][0-9A-Fa-f]+)|[A-Za-z0-9]+);)", b"&amp;", data)
+
+    return _parse(XML, data, name, exception, schema, *args, **kwargs)
+
+
+def parse_qsd(
+    data,
+    name="query string",
+    exception=PluginError,
+    schema=None,
+    *args, **kwargs
+):
+    """Parses a query string into a dict.
+
+    Provides these extra features:
+     - Unlike parse_qs and parse_qsl, duplicate keys are not preserved in favor of a simpler return value
+     - Wraps errors in custom exception with a snippet of the data in the message
+    """
+    return _parse(lambda d: dict(parse_qsl(d, *args, **kwargs)), data, name, exception, schema)
