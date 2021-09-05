6576613804ff4d5b6e453f1c2907c19c5103dff5
bastimeyer <mail@bastimeyer.de>
2021-09-05 04:35:34 +0200

    cli.utils.formatter: rewrite Formatter
    Format strings via string.Formatter.parse() and build the formatted
    string with a custom method similar to string.Formatter._vformat()
    instead of calling str.format_map().

--- src/streamlink_cli/utils/formatter.py
+++ src/streamlink_cli/utils/formatter.py
@@ -1,52 +1,55 @@
-from typing import Callable, Dict, Optional, Type, Union
+from string import Formatter as StringFormatter
+from typing import Any, Callable, Dict, Optional
 
 from streamlink_cli.utils.path import replace_chars
 
 
-class _UNKNOWN:
-    pass
+# we only need string.Formatter for calling its parse() method, which returns `_string.formatter_parser(string)`.
+_stringformatter = StringFormatter()
 
 
-class Formatter(dict):
+def _identity(obj):
+    return obj
+
+
+class Formatter:
     def __init__(self, mapping: Dict[str, Callable]):
         super().__init__()
         self.mapping = mapping
-        self.cache = dict()
+        self.cache = {}
 
-    def __missing__(self, key: str) -> Union[Type[_UNKNOWN], None, str]:
-        if key not in self.mapping:
-            return _UNKNOWN
+    def _get_value(self, field_name: str, defaults: Dict[str, str]) -> Any:
+        if field_name not in self.mapping:
+            return defaults.get(field_name, f"{{{field_name}}}")
 
-        if key in self.cache:
-            return self.cache[key]
+        if field_name in self.cache:
+            value = self.cache[field_name]
+        else:
+            value = self.mapping[field_name]()
+            self.cache[field_name] = value
 
-        value = self.mapping[key]()
-        self.cache[key] = value
+        if value is None:
+            value = defaults.get(field_name, "")
 
         return value
 
-    def _format(self, string: str, wrapper: Callable[[str], str], defaults: Optional[Dict] = None) -> str:
-        return string.format_map(_Wrapper(self, wrapper, defaults))
+    def _format(self, string: str, mapper: Callable[[str], str], defaults: Dict[str, str]) -> str:
+        result = []
+
+        for literal_text, field_name, format_spec, conversion in _stringformatter.parse(string):
+            if literal_text:
+                result.append(literal_text)
+
+            if field_name is None:
+                continue
+
+            value = self._get_value(field_name, defaults)
+            result.append(mapper(str(value)))
+
+        return "".join(result)
 
     def filename(self, filename: str, charmap: Optional[str] = None) -> str:
-        return self._format(filename, lambda s: replace_chars(s, charmap))
+        return self._format(filename, lambda s: replace_chars(s, charmap), {})
 
-    def title(self, title: str, defaults: Optional[Dict] = None) -> str:
-        return self._format(title, lambda s: s, defaults)
-
-
-class _Wrapper(dict):
-    def __init__(self, formatter: Formatter, wrapper: Callable[[str], str], defaults: Optional[Dict[str, str]] = None):
-        super().__init__()
-        self.formatter = formatter
-        self.wrapper = wrapper
-        self.defaults = defaults or {}
-
-    def __missing__(self, key: str) -> str:
-        value = self.formatter[key]
-        if value is _UNKNOWN:
-            value = str(self.defaults.get(key, f"{{{key}}}"))
-        elif value is None:
-            value = str(self.defaults.get(key, ""))
-
-        return self.wrapper(value)
+    def title(self, title: str, defaults: Optional[Dict[str, str]] = None) -> str:
+        return self._format(title, _identity, defaults or {})
