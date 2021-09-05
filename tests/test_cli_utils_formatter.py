02094369b5591503188f050b535823e2582d1817
bastimeyer <mail@bastimeyer.de>
2021-09-05 04:35:34 +0200

    cli.utils.formatter: rewrite Formatter
    Format strings via string.Formatter.parse() and build the formatted
    string with a custom method similar to string.Formatter._vformat()
    instead of calling str.format_map().

--- tests/test_cli_utils_formatter.py
+++ tests/test_cli_utils_formatter.py
@@ -5,15 +5,22 @@ from streamlink_cli.utils.formatter import Formatter
 
 
 class TestFormatter(unittest.TestCase):
+    class Obj:
+        def __str__(self):
+            return "obj"
+
     def setUp(self):
         self.prop = Mock(return_value="prop")
+        self.obj = self.Obj()
         self.formatter = Formatter({
             "prop": self.prop,
+            "obj": lambda: self.obj,
             "empty": lambda: "",
             "none": lambda: None
         })
 
     def test_unknown(self):
+        self.assertEqual(self.formatter.title("{}"), "{}")
         self.assertEqual(self.formatter.title("some {unknown} variable"), "some {unknown} variable")
         self.assertEqual(self.formatter.title("some {unknown} variable", {"unknown": "known"}), "some known variable")
         self.assertEqual(self.formatter.cache, dict())
@@ -23,7 +30,13 @@ class TestFormatter(unittest.TestCase):
         self.assertEqual(self.formatter.cache, dict(prop="prop", empty="", none=None))
         self.assertEqual(self.prop.call_count, 1)
 
-        self.assertEqual(self.formatter.title("text '{prop}' '{empty}' '{none}'"), "text 'prop' '' ''")
+        self.assertEqual(self.formatter.title("text '{prop}' '{obj}' '{empty}' '{none}'"), "text 'prop' 'obj' '' ''")
+        self.assertEqual(self.formatter.cache, dict(prop="prop", obj=self.obj, empty="", none=None))
+        self.assertEqual(self.prop.call_count, 1)
+
+        defaults = dict(prop="PROP", obj="OBJ", empty="EMPTY", none="NONE")
+        self.assertEqual(self.formatter.title("'{prop}' '{obj}' '{empty}' '{none}'", defaults), "'prop' 'obj' '' 'NONE'")
+        self.assertEqual(self.formatter.cache, dict(prop="prop", obj=self.obj, empty="", none=None))
         self.assertEqual(self.prop.call_count, 1)
 
     @patch("streamlink_cli.utils.formatter.replace_chars")
@@ -36,12 +49,11 @@ class TestFormatter(unittest.TestCase):
         self.assertEqual(mock_replace_chars.call_args_list, [
             call("prop", None), call("", None), call("", None)
         ])
-
         mock_replace_chars.reset_mock()
 
-        self.assertEqual(self.formatter.filename("text '{prop}' '{empty}' '{none}'", "foo"), "text 'PROP' '' ''")
-        self.assertEqual(self.formatter.cache, dict(prop="prop", empty="", none=None))
+        self.assertEqual(self.formatter.filename("text '{prop}' '{obj}' '{empty}' '{none}'", "foo"), "text 'PROP' 'OBJ' '' ''")
+        self.assertEqual(self.formatter.cache, dict(prop="prop", obj=self.obj, empty="", none=None))
         self.assertEqual(self.prop.call_count, 1)
         self.assertEqual(mock_replace_chars.call_args_list, [
-            call("prop", "foo"), call("", "foo"), call("", "foo")
+            call("prop", "foo"), call("obj", "foo"), call("", "foo"), call("", "foo")
         ])
