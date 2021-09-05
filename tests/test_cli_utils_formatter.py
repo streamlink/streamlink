3fb959311389d71b09687119908773ff0362cf65
bastimeyer <mail@bastimeyer.de>
2021-09-05 05:07:16 +0200

    cli.utils.formatter: implement format_spec

--- tests/test_cli_utils_formatter.py
+++ tests/test_cli_utils_formatter.py
@@ -1,9 +1,13 @@
 import unittest
+from datetime import datetime
 from unittest.mock import Mock, call, patch
+
+from freezegun import freeze_time
 
 from streamlink_cli.utils.formatter import Formatter
 
 
+@freeze_time(datetime(2000, 1, 2, 3, 4, 5, 6, None))
 class TestFormatter(unittest.TestCase):
     class Obj:
         def __str__(self):
@@ -12,12 +16,18 @@ class TestFormatter(unittest.TestCase):
     def setUp(self):
         self.prop = Mock(return_value="prop")
         self.obj = self.Obj()
-        self.formatter = Formatter({
-            "prop": self.prop,
-            "obj": lambda: self.obj,
-            "empty": lambda: "",
-            "none": lambda: None
-        })
+        self.formatter = Formatter(
+            {
+                "prop": self.prop,
+                "obj": lambda: self.obj,
+                "time": datetime.now,
+                "empty": lambda: "",
+                "none": lambda: None
+            },
+            {
+                "time": lambda dt, fmt: dt.strftime(fmt)
+            }
+        )
 
     def test_unknown(self):
         self.assertEqual(self.formatter.title("{}"), "{}")
@@ -57,3 +67,18 @@ class TestFormatter(unittest.TestCase):
         self.assertEqual(mock_replace_chars.call_args_list, [
             call("prop", "foo"), call("obj", "foo"), call("", "foo"), call("", "foo")
         ])
+
+    def test_format_spec(self):
+        self.assertEqual(self.formatter.title("{time}"), "2000-01-02 03:04:05.000006")
+        self.assertEqual(self.formatter.cache, dict(time=datetime(2000, 1, 2, 3, 4, 5, 6, None)))
+        self.assertEqual(self.formatter.title("{time:%Y}"), "2000")
+        self.assertEqual(self.formatter.title("{time:%Y-%m-%d}"), "2000-01-02")
+        self.assertEqual(self.formatter.title("{time:%H:%M:%S}"), "03:04:05")
+        with patch("datetime.datetime.strftime", side_effect=ValueError):
+            self.assertEqual(self.formatter.title("{time:foo:bar}"), "{time:foo:bar}")
+        self.assertEqual(self.formatter.cache, dict(time=datetime(2000, 1, 2, 3, 4, 5, 6, None)))
+
+        self.assertEqual(self.formatter.title("{prop:foo}"), "prop")
+        self.assertEqual(self.formatter.title("{none:foo}"), "")
+        self.assertEqual(self.formatter.title("{unknown:format}"), "{unknown:format}")
+        self.assertEqual(self.formatter.title("{unknown:format}", {"unknown": "known"}), "known")
