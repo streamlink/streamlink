8fcf7b8a20588df799f45e0c8d19c9ca68918d68
bastimeyer <mail@bastimeyer.de>
2021-09-12 04:43:53 +0200

    utils: replace LazyFormatter with new Formatter
    - Replace utils.lazy_formatter with cli.utils.formatter, add the generic
      `format` method and create a Formatter subclass in cli.utils.formatter
      with additional `title` and `filename` methods for usage in cli.main.
    - Update HLSStreamWriter.create_decryptor, which was the last remaining
      code using the old LazyFormatter.

--- src/streamlink/utils/__init__.py
+++ src/streamlink/utils/__init__.py
@@ -5,7 +5,7 @@ from importlib.util import module_from_spec
 from typing import Dict, Generic, Optional, TypeVar
 from urllib.parse import urljoin, urlparse
 
-from streamlink.utils.lazy_formatter import LazyFormatter
+from streamlink.utils.formatter import Formatter
 from streamlink.utils.named_pipe import NamedPipe
 from streamlink.utils.parse import parse_html, parse_json, parse_qsd, parse_xml
 from streamlink.utils.url import update_scheme, url_equal
@@ -134,7 +134,7 @@ __all__ = [
     "absolute_url", "prepend_www",
     "search_dict",
     "LRUCache",
-    "LazyFormatter",
+    "Formatter",
     "NamedPipe",
     "parse_html", "parse_json", "parse_qsd", "parse_xml",
     "update_scheme", "url_equal",
---
+++ src/streamlink/utils/formatter.py
@@ -0,0 +1,58 @@
+from string import Formatter as StringFormatter
+from typing import Any, Callable, Dict, Optional
+
+
+# we only need string.Formatter for calling its parse() method, which returns `_string.formatter_parser(string)`.
+_stringformatter = StringFormatter()
+
+
+def _identity(obj):
+    return obj
+
+
+class Formatter:
+    def __init__(self, mapping: Dict[str, Callable], formatting: Optional[Dict[str, Callable]] = None):
+        super().__init__()
+        self.mapping = mapping
+        self.formatting = formatting or {}
+        self.cache = {}
+
+    def _get_value(self, field_name: str, format_spec: str, defaults: Dict[str, str]) -> Any:
+        if field_name not in self.mapping:
+            return defaults.get(field_name, f"{{{field_name}}}" if not format_spec else f"{{{field_name}:{format_spec}}}")
+
+        if field_name in self.cache:
+            value = self.cache[field_name]
+        else:
+            value = self.mapping[field_name]()
+            self.cache[field_name] = value
+
+        if value is None:
+            value = defaults.get(field_name, "")
+
+        if format_spec and field_name in self.formatting:
+            # noinspection PyBroadException
+            try:
+                return self.formatting[field_name](value, format_spec)
+            except Exception:
+                return f"{{{field_name}:{format_spec}}}"
+
+        return value
+
+    def _format(self, string: str, mapper: Callable[[str], str], defaults: Dict[str, str]) -> str:
+        result = []
+
+        for literal_text, field_name, format_spec, conversion in _stringformatter.parse(string):
+            if literal_text:
+                result.append(literal_text)
+
+            if field_name is None:
+                continue
+
+            value = self._get_value(field_name, format_spec, defaults)
+            result.append(mapper(str(value)))
+
+        return "".join(result)
+
+    def format(self, title: str, defaults: Optional[Dict[str, str]] = None) -> str:
+        return self._format(title, _identity, defaults or {})
