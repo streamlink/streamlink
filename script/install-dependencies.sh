5c304453cc8bb544e10b643afbb84fea498ac805
Billy2011 <kschmidt2007@googlemail.com>
2021-09-16 12:06:29 +0200

    [PATCH] tests: fix Accept-Encoding headers in stream_json (#4022)

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -8,4 +8,6 @@ python -m pip install --disable-pip-version-check --upgrade pip setuptools
 python -m pip install --upgrade -r dev-requirements.txt
 python -m pip install 'pycountry==19.8.18;python_version<"3.0"'
 python -m pip install 'pycountry;python_version>="3.0"'
+# https://github.com/streamlink/streamlink/issues/4021
+python -m pip install brotli
 python -m pip install -e .
