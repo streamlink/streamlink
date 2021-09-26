c8f0e432368a11a43eaf4bef4bd9882760ae5318
back-to <backto@protonmail.ch>
2021-09-26 13:02:06 +0200

    plugins.oneplusone: cleanup and add auto session reload (#4049)

--- tests/plugins/test_oneplusone.py
+++ tests/plugins/test_oneplusone.py
@@ -6,14 +6,16 @@ class TestPluginCanHandleUrlOnePlusOne(PluginCanHandleUrl):
     __plugin__ = OnePlusOne
 
     should_match = [
-        'https://1plus1.video/tvguide/1plus1/online',
-        'https://1plus1.video/tvguide/2plus2/online',
-        'https://1plus1.video/tvguide/tet/online',
-        'https://1plus1.video/tvguide/plusplus/online',
-        'https://1plus1.video/tvguide/bigudi/online',
-        'https://1plus1.video/tvguide/uniantv/online',
+        "https://1plus1.video/ru/tvguide/plusplus/online",
+        "https://1plus1.video/tvguide/1plus1/online",
+        "https://1plus1.video/tvguide/2plus2/online",
+        "https://1plus1.video/tvguide/bigudi/online",
+        "https://1plus1.video/tvguide/plusplus/online",
+        "https://1plus1.video/tvguide/sport/online",
+        "https://1plus1.video/tvguide/tet/online",
+        "https://1plus1.video/tvguide/uniantv/online",
     ]
 
     should_not_match = [
-        'https://1plus1.video/',
+        "https://1plus1.video/",
     ]
