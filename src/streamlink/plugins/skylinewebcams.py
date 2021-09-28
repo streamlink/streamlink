b23b2878d386502d44e403da66fa9cefd913fb59
Billy2011 <kschmidt2007@googlemail.com>
2021-09-28 16:09:07 +0200

    plugins.skylinewebcams: add yt supp.

--- src/streamlink/plugins/skylinewebcams.py
+++ src/streamlink/plugins/skylinewebcams.py
@@ -12,21 +12,26 @@ log = logging.getLogger(__name__)
     r"https?://(?:www\.)?skylinewebcams\.com/(?:[\w-]+/){5}.+\.html"
 ))
 class SkylineWebcams(Plugin):
-    _source_re = re.compile(r"source:\s*'(.+?)'")
+    _source_re = re.compile(r"(source):\s*'(.+?)'")
+    _yt_source_re = re.compile(r"YT\.Player\('live'.*?(videoId):\s*'(.+?)'")
     _HD_AUTH_BASE = "https://hd-auth.skylinewebcams.com/"
 
     def _get_streams(self):
         res = self.session.http.get(self.url)
-        m = self._source_re.search(res.text)
-        if m and ".m3u8" in m.group(1):
-            url = urljoin(self._HD_AUTH_BASE, m.group(1))
-            url = url.replace("livee.", "live.")
-            log.debug(url)
-            streams = HLSStream.parse_variant_playlist(self.session, url)
-            if not streams:
-                return {"live": HLSStream(self.session, url)}
-            else:
-                return streams
+        m = self._source_re.search(res.text) or self._yt_source_re.search(res.text)
+        if m:
+            if m.group(1) == "source" and ".m3u8" in m.group(2):
+                url = urljoin(self._HD_AUTH_BASE, m.group(2))
+                url = url.replace("livee.", "live.")
+                log.debug(url)
+                streams = HLSStream.parse_variant_playlist(self.session, url)
+                if not streams:
+                    return {"live": HLSStream(self.session, url)}
+                else:
+                    return streams
+            elif m.group(1) == "videoId":
+                url = "youtube.com/embed/{0}".format(m.group(2))
+                return self.session.streams(url)
         elif m:
             raise PluginError("Unexpected source format: {0}".format(m.group(1)))
         else:
