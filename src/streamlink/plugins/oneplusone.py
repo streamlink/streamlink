8a12e1908e3674dce34abaac3f8d231a7ae56626
Billy2011 <kschmidt2007@googlemail.com>
2021-09-29 13:15:47 +0200

    [PATCH] plugins.oneplusone: cleanup and add auto session reload (#4049)

--- src/streamlink/plugins/oneplusone.py
+++ src/streamlink/plugins/oneplusone.py
@@ -1,105 +1,118 @@
 import logging
 import re
 from base64 import b64decode
+from datetime import datetime
+from time import time
 
-from streamlink.compat import urlparse
-from streamlink.plugin import Plugin, PluginError, pluginmatcher
+from streamlink.compat import urljoin, urlparse
+from streamlink.exceptions import NoStreamsError, PluginError
+from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
-from streamlink.utils.parse import parse_json
-
-try:
-    from html.parser import HTMLParser
-except ImportError:
-    from HTMLParser import HTMLParser
 
 log = logging.getLogger(__name__)
 
 
-class Online_Parser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        if tag == 'iframe':
-            attrs = dict(attrs)
-            if 'name' in attrs and attrs['name'] == 'twttrHubFrameSecure':
-                self.iframe_url = attrs['src']
+class OnePlusOneHLS(HLSStream):
+    __shortname__ = "hls-oneplusone"
+
+    def __init__(self, session_, url, self_url=None, **args):
+        super(OnePlusOneHLS, self).__init__(session_, url, None, **args)
+        self._url = url
+
+        first_parsed = urlparse(self._url)
+        self._first_netloc = first_parsed.netloc
+        self._first_path_chunklist = first_parsed.path.split("/")[-1]
+        self.watch_timeout = int(first_parsed.path.split("/")[2]) - 15
+        self.api = OnePlusOneAPI(session_, self_url)
+
+    def _next_watch_timeout(self):
+        _next = datetime.fromtimestamp(self.watch_timeout).isoformat(" ")
+        log.debug("next watch_timeout at {0}".format(_next))
+
+    def open(self):
+        self._next_watch_timeout()
+        return super(OnePlusOneHLS, self).open()
+
+    @property
+    def url(self):
+        if int(time()) >= self.watch_timeout:
+            log.debug("Reloading HLS URL")
+            _hls_url = self.api.get_hls_url()
+            if not _hls_url:
+                self.watch_timeout += 10
+                return self._url
+            parsed = urlparse(_hls_url)
+            path_parts = parsed.path.split("/")
+            path_parts[-1] = self._first_path_chunklist
+            self.watch_timeout = int(path_parts[2]) - 15
+            self._next_watch_timeout()
+
+            self._url = parsed._replace(
+                netloc=self._first_netloc,
+                path="/".join([p for p in path_parts])
+            ).geturl()
+        return self._url
 
 
-class Iframe_Parser(HTMLParser):
-    js = False
+class OnePlusOneAPI:
+    def __init__(self, session, url):
+        self.session = session
+        self.url = url
+        self._re_data = re.compile(r"ovva-player\",\"([^\"]*)\"\)")
 
-    def handle_starttag(self, tag, attrs):
-        if tag == 'script':
-            attrs = dict(attrs)
-            if 'type' in attrs and attrs['type'] == 'text/javascript':
-                self.js = True
+    def get_hls_url(self):
+        self.session.http.cookies.clear()
+        url_parts = self.session.http.get(
+            url=self.url,
+            schema=validate.Schema(
+                validate.parse_html(),
+                validate.xml_xpath_string(".//iframe[@src][@name='twttrHubFrameSecure']/@src")))
+        if not url_parts:
+            raise NoStreamsError("Missing url_parts")
 
-    def handle_data(self, data):
-        if self.js and data.startswith('window.onload'):
-            self.data = data
+        log.trace("url_parts={0}".format(url_parts))
+        self.session.http.headers.update({"Referer": self.url})
+
+        try:
+            url_ovva = self.session.http.get(
+                url=urljoin(self.url, url_parts),
+                schema=validate.Schema(
+                    validate.parse_html(),
+                    validate.xml_xpath_string(".//script[@type='text/javascript'][contains(text(),'ovva-player')]/text()"),
+                    validate.text,
+                    validate.transform(self._re_data.search),
+                    validate.get(1),
+                    validate.transform(lambda x: b64decode(x).decode()),
+                    validate.parse_json(),
+                    {"balancer": validate.url()},
+                    validate.get("balancer")
+                ))
+        except (PluginError, TypeError) as err:
+            log.error("ovva-player: {0}".format(err))
+            return
+
+        log.debug("url_ovva={0}".format(url_ovva))
+        url_hls = self.session.http.get(
+            url=url_ovva,
+            schema=validate.Schema(
+                validate.transform(lambda x: x.split("=")),
+                ["302", validate.url(path=validate.endswith(".m3u8"))],
+                validate.get(1)))
+        return url_hls
 
 
 @pluginmatcher(re.compile(
-    r"https?://1plus1\.video/tvguide/.*/online"
+    r"https?://1plus1\.video/(?:\w{2}/)?tvguide/[^/]+/online"
 ))
 class OnePlusOne(Plugin):
-    data_re = re.compile(r"ovva-player\",\"([^\"]*)\"\)")
-    ovva_data_schema = validate.Schema({
-        "balancer": validate.url()
-    }, validate.get("balancer"))
-    ovva_redirect_schema = validate.Schema(validate.all(
-        validate.transform(lambda x: x.split("=")),
-        ['302', validate.url()],
-        validate.get(1)
-    ))
-
-    def find_iframe(self, res):
-        parser = Online_Parser()
-        parser.feed(res.text)
-        url = parser.iframe_url
-        log.trace("find_iframe url: {0}".format(url))
-        if url.startswith("/"):
-            p = urlparse(self.url)
-            if url.startswith("//"):
-                return "{0}:{1}".format(p.scheme, url)
-            return "{0}://{1}{2}".format(p.scheme, p.netloc, url)
-        else:
-            return url
-
-    def get_data(self, res):
-        parser = Iframe_Parser()
-        parser.feed(res.text)
-        if hasattr(parser, "data"):
-            m = self.data_re.search(parser.data)
-            if m:
-                data = m.group(1)
-                return data
-
     def _get_streams(self):
-        res = self.session.http.get(self.url)
-        iframe_url = self.find_iframe(res)
-        if iframe_url:
-            log.debug("Found iframe: {0}".format(iframe_url))
-            res = self.session.http.get(
-                iframe_url,
-                headers={"Referer": self.url})
-            data = self.get_data(res)
-            if data:
-                try:
-                    ovva_url = parse_json(
-                        b64decode(data).decode(),
-                        schema=self.ovva_data_schema)
-                    log.debug("Found ovva: {0}".format(ovva_url))
-
-                    stream_url = self.session.http.get(
-                        ovva_url,
-                        schema=self.ovva_redirect_schema,
-                        headers={"Referer": iframe_url})
-                    log.debug("Found stream: {0}".format(stream_url))
-
-                except PluginError as e:
-                    log.error("Could not find stream URL: {0}".format(e))
-                else:
-                    return HLSStream.parse_variant_playlist(self.session, stream_url)
+        self.api = OnePlusOneAPI(self.session, self.url)
+        url_hls = self.api.get_hls_url()
+        if not url_hls:
+            return
+        for q, s in HLSStream.parse_variant_playlist(self.session, url_hls).items():
+            yield q, OnePlusOneHLS(self.session, s.url, self_url=self.url)
 
 
 __plugin__ = OnePlusOne
