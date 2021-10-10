af260a64f647a807422952eeadf7b4bb22495fb3
bastimeyer <mail@bastimeyer.de>
2021-10-10 20:10:48 +0200

    plugin.api: remove StreamMapper

--- src/streamlink/plugin/api/__init__.py
+++ src/streamlink/plugin/api/__init__.py
@@ -1,4 +1,3 @@
 from streamlink.plugin.api.http_session import HTTPSession
-from streamlink.plugin.api.mapper import StreamMapper
 
-__all__ = ["HTTPSession", "StreamMapper"]
+__all__ = ["HTTPSession"]
