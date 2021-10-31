0dce95e7faab069b0af88b279904c6c6da9e5f70
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-10-31 15:42:12 +0900

    plugins.openrectv: be able to get subscription video (#4130)

--- src/streamlink/plugins/openrectv.py
+++ src/streamlink/plugins/openrectv.py
@@ -15,7 +15,8 @@ class OPENRECtv(Plugin):
     _stores_re = re.compile(r"window.stores\s*=\s*({.*?});", re.DOTALL | re.MULTILINE)
     _config_re = re.compile(r"window.sharedConfig\s*=\s*({.*?});", re.DOTALL | re.MULTILINE)
 
-    api_url = "https://public.openrec.tv/external/api/v5/movies/{id}"
+    movie_info_url = "https://public.openrec.tv/external/api/v5/movies/{id}"
+    subscription_info_url = "https://apiv5.openrec.tv/api/v5/movies/{id}/detail"
     login_url = "https://www.openrec.tv/viewapp/v4/mobile/user/login"
 
     _info_schema = validate.Schema({
@@ -23,10 +24,22 @@ class OPENRECtv(Plugin):
         validate.optional("title"): validate.text,
         validate.optional("movie_type"): validate.text,
         validate.optional("onair_status"): validate.any(None, int),
+        validate.optional("public_type"): validate.text,
         validate.optional("media"): {
             "url": validate.any(None, validate.url()),
             "url_public": validate.any(None, validate.url()),
             "url_ull": validate.any(None, validate.url()),
+        }
+    })
+
+    _subscription_schema = validate.Schema({
+        validate.optional("status"): int,
+        validate.optional("data"): {
+            "items": [{
+                "media": {
+                    "url": validate.any(None, validate.url())
+                }
+            }]
         }
     })
 
@@ -68,7 +81,7 @@ class OPENRECtv(Plugin):
         return data["status"] == 0
 
     def _get_movie_data(self):
-        url = self.api_url.format(id=self.video_id)
+        url = self.movie_info_url.format(id=self.video_id)
         res = self.session.http.get(url, headers={
             "access-token": self.session.http.cookies.get("access_token"),
             "uuid": self.session.http.cookies.get("uuid")
@@ -77,6 +90,20 @@ class OPENRECtv(Plugin):
 
         if data["id"]:
             log.debug("Got valid detail response")
+            return data
+        else:
+            log.error("Failed to get video stream: {0}".format(data["message"]))
+
+    def _get_subscription_movie_data(self):
+        url = self.subscription_info_url.format(id=self.video_id)
+        res = self.session.http.get(url, headers={
+            "access-token": self.session.http.cookies.get("access_token"),
+            "uuid": self.session.http.cookies.get("uuid")
+        })
+        data = self.session.http.json(res, schema=self._subscription_schema)
+
+        if data["status"] == 0:
+            log.debug("Got valid subscription info")
             return data
         else:
             log.error("Failed to get video stream: {0}".format(data["message"]))
@@ -100,8 +127,12 @@ class OPENRECtv(Plugin):
         if mdata:
             log.debug("Found video: {0} ({1})".format(mdata["title"], mdata["id"]))
             m3u8_file = None
+            # subscription
+            if mdata["public_type"] == "member":
+                subs_data = self._get_subscription_movie_data()
+                m3u8_file = subs_data["data"]["items"][0]["media"]["url"]
             # streaming
-            if mdata["onair_status"] == 1:
+            elif mdata["onair_status"] == 1:
                 m3u8_file = mdata["media"]["url_ull"]
             # archive
             elif mdata["onair_status"] == 2 and mdata["media"]["url_public"] is not None:
