114b4fd92ba6c64e3a47db6b788d3fab12ff7148
Ian Cameron <1661072+mkbloke@users.noreply.github.com>
2021-11-01 03:49:55 +0000

    plugins.picarto: update URL regex and logic

--- tests/plugins/test_picarto.py
+++ tests/plugins/test_picarto.py
@@ -8,7 +8,15 @@ class TestPluginCanHandleUrlPicarto(PluginCanHandleUrl):
     should_match = [
         'https://picarto.tv/example',
         'https://www.picarto.tv/example',
+        'https://www.picarto.tv/example/videos/123456',
         'https://www.picarto.tv/streampopout/example/public',
         'https://www.picarto.tv/videopopout/123456',
-        'https://www.picarto.tv/example?tab=videos&id=123456',
+    ]
+
+    should_not_match = [
+        'https://picarto.tv/',
+        'https://www.picarto.tv/example/',
+        'https://www.picarto.tv/example/videos/abc123',
+        'https://www.picarto.tv/streampopout/example/notpublic',
+        'https://www.picarto.tv/videopopout/abc123',
     ]
