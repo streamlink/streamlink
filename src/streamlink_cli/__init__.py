08ca58294ae1cdbe062e299f91a9620b38f8cd2e
bastimeyer <mail@bastimeyer.de>
2021-11-20 14:29:17 +0100

    cli: override default signal handlers
    - Don't raise a KeyboardInterrupt until streamlink_cli has been fully
      initialized. This prevents a stack call from being printed to stderr
      when streamlink_cli gets interrupted early.
    - Restore default signal handlers once streamlink_cli has finished its
      initialization, so that KeyboardInterrupt exceptions can be caught to
      perform clean up tasks.
    - Restore default signal handlers in test immediately after importing
      streamlink_cli once as early as possible, to be able to cancel the
      test runners regularly via a KeyboardInterrupt.
    - Refactor CLI logging tests, which test the log output of the
      initialization and need to stop execution at some point.

---
+++ src/streamlink_cli/__init__.py
@@ -0,0 +1,12 @@
+from signal import SIGINT, SIGTERM, signal
+from sys import exit
+
+
+def _exit(*_):
+    # don't raise a KeyboardInterrupt until streamlink_cli has been fully initialized
+    exit(128 | 2)
+
+
+# override default SIGINT handler (and set SIGTERM handler) as early as possible
+signal(SIGINT, _exit)
+signal(SIGTERM, _exit)
