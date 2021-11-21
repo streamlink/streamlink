133400faa3f5fcb2f74efdd69bde32c09317864b
back-to <backto@protonmail.ch>
2021-11-21 19:07:47 +0100

    plugins.tviplayer: new plugin
    https://en.wikipedia.org/wiki/Televis%C3%A3o_Independente
    
    ```
    $ streamlink https://tviplayer.iol.pt/direto/TVI24
    [cli][info] Found matching plugin tviplayer for URL https://tviplayer.iol.pt/direto/TVI24
    [cli][info] Available streams: 240p (worst), 360p, 480p, 720p (best)
    [cli][info] Opening stream: 720p
    ```
    
    ```
    $ streamlink https://tviplayer.iol.pt/programa/prisioneira/5c890ae00cf2f1892ed73779/episodio/t2e219
    [cli][info] Found matching plugin tviplayer for URL https://tviplayer.iol.pt/programa/prisioneira/5c890ae00cf2f1892ed73779/episodio/t2e219
    [cli][info] Available streams: 240p (worst), 360p, 576p (best)
    [cli][info] Opening stream: 576p
    ```
    
    ```
    $ streamlink https://tviplayer.iol.pt/direto/TVI_AFRICA
    [cli][info] Found matching plugin tviplayer for URL https://tviplayer.iol.pt/direto/TVI_AFRICA
    [plugins.tviplayer][error] Country is blocked
    error: No playable streams found on this URL: https://tviplayer.iol.pt/direto/TVI_AFRICA
    ```

---
+++ src/streamlink/plugins/tviplayer.py
@@ -0,0 +1,73 @@
+import logging
+import re
+
+from streamlink.plugin import Plugin, pluginmatcher
+from streamlink.plugin.api import validate
+from streamlink.stream.hls import HLSStream
+from streamlink.utils.url import update_qsd
+
+log = logging.getLogger(__name__)
+
+
+@pluginmatcher(re.compile(
+    r"https://tviplayer\.iol\.pt/(?:direto|programa)/",
+))
+class TVIPlayer(Plugin):
+    _re_jsonData = re.compile(r"jsonData\s*=\s*(?P<json>{.+?})\s*;", re.DOTALL)
+
+    def _get_streams(self):
+        self.session.http.headers.update({"Referer": "https://tviplayer.iol.pt/"})
+        data = self.session.http.get(
+            self.url,
+            schema=validate.Schema(
+                validate.parse_html(),
+                validate.xml_xpath_string(".//script[contains(text(),'.m3u8')]/text()"),
+                str,
+                validate.transform(self._re_jsonData.search),
+                validate.any(None, validate.all(
+                    validate.get("json"),
+                    validate.parse_json(),
+                    {
+                        "id": str,
+                        "liveType": str,
+                        "videoType": str,
+                        "videoUrl": validate.url(path=validate.endswith(".m3u8")),
+                        validate.optional("channel"): str,
+                    }
+                ))
+            )
+        )
+        if not data:
+            return
+        log.debug(f"{data!r}")
+
+        if data["liveType"].upper() == "DIRETO" and data["videoType"].upper() == "LIVE":
+            geo_path = "live"
+        else:
+            geo_path = "vod"
+        data_geo = self.session.http.get(
+            f"https://services.iol.pt/direitos/rights/{geo_path}?id={data['id']}",
+            acceptable_status=(200, 403),
+            schema=validate.Schema(
+                validate.parse_json(),
+                {
+                    "code": str,
+                    "error": validate.any(None, str),
+                    "detail": str,
+                }
+            )
+        )
+        log.debug(f"{data_geo!r}")
+        if data_geo["detail"] != "ok":
+            log.error(f"{data_geo['detail']}")
+            return
+
+        wmsAuthSign = self.session.http.get(
+            "https://services.iol.pt/matrix?userId=",
+            schema=validate.Schema(str)
+        )
+        hls_url = update_qsd(data["videoUrl"], {"wmsAuthSign": wmsAuthSign})
+        return HLSStream.parse_variant_playlist(self.session, hls_url)
+
+
+__plugin__ = TVIPlayer
