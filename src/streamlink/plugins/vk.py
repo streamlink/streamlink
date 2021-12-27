ffaa6ff54f0a51c086230c960d5abdf1b7f5d3a0
Billy2011 <kschmidt2007@googlemail.com>
2021-12-27 11:08:29 +0100

    [PATCH] plugins.vk: rewrite and remove itertags (#4267)
    - drop IPHONE6 user agent
    - update matcher regexes
    - rewrite redirection logic
    - rewrite validation schemas, remove itertags
    - add id, author and title metadata
    - drop HTTPStream and add DASHStream, prefer HLSStream
    - update tests

--- src/streamlink/plugins/vk.py
+++ src/streamlink/plugins/vk.py
@@ -2,79 +2,102 @@
 import logging
 import re
 
-from streamlink.compat import html_unescape, parse_qsl, unquote, urlparse
+from streamlink.compat import parse_qsl, unquote, urlparse
 from streamlink.exceptions import NoStreamsError
-from streamlink.plugin import Plugin, pluginmatcher
-from streamlink.plugin.api import useragents
-from streamlink.plugin.api.utils import itertags
+from streamlink.plugin import Plugin, PluginError, pluginmatcher
+from streamlink.plugin.api import validate
+from streamlink.stream.dash import DASHStream
 from streamlink.stream.hls import HLSStream
-from streamlink.stream.http import HTTPStream
-from streamlink.utils.url import update_scheme
 
 log = logging.getLogger(__name__)
 
 
 @pluginmatcher(re.compile(
-    r"https?://(?:\w+\.)?vk\.com/video(?:\?z=video)?(?P<video_id>-?\d*_\d*)"
+    r"https?://(?:\w+\.)?vk\.com/videos?(?:\?z=video)?(?P<video_id>-?\d+_\d+)"
 ))
 @pluginmatcher(re.compile(
-    r"https?://(\w+\.)?vk\.com/videos-?\d*"
+    r"https?://(\w+\.)?vk\.com/.+"
 ))
 class VK(Plugin):
-    API_URL = 'https://vk.com/al_video.php'
+    API_URL = "https://vk.com/al_video.php"
 
-    _vod_quality_re = re.compile(r"\.([0-9]*?)\.mp4")
+    def _has_video_id(self):
+        return any(m for m in self.matches[:-1])
 
     def follow_vk_redirect(self):
-        # If this is a 'videos' catalog URL
-        # with an video ID in the GET request, get that instead
-        if self.matches[1]:
-            try:
-                parsed_url = urlparse(self.url)
-                true_path = next(unquote(v).split('/')[0] for k, v in parse_qsl(parsed_url.query) if k == "z")
-                self.url = parsed_url.scheme + '://' + parsed_url.netloc + '/' + true_path
-            except StopIteration:
-                raise NoStreamsError(self.url)
+        if self._has_video_id():
+            return
+
+        try:
+            parsed_url = urlparse(self.url)
+            true_path = next(unquote(v).split("/")[0] for k, v in parse_qsl(parsed_url.query) if k == "z" and len(v) > 0)
+            self.url = "{0}://{1}/{2}".format(parsed_url.scheme, parsed_url.netloc, true_path)
+            if self._has_video_id():
+                return
+        except StopIteration:
+            pass
+
+        try:
+            self.url = self.session.http.get(self.url, schema=validate.Schema(
+                validate.parse_html(),
+                validate.xml_xpath_string(".//head/meta[@property='og:url'][@content]/@content"),
+                validate.text
+            ))
+        except PluginError:
+            pass
+        if self._has_video_id():
+            return
+
+        raise NoStreamsError(self.url)
 
     def _get_streams(self):
-        self.session.http.headers.update({'User-Agent': useragents.IPHONE_6})
-
         self.follow_vk_redirect()
 
-        video_id = self.match.group('video_id')
-        log.debug('video ID: {0}'.format(video_id))
+        video_id = self.match.group("video_id")
+        if not video_id:
+            return
 
-        params = {
-            'act': 'show_inline',
-            'al': '1',
-            'video': video_id,
-        }
-        res = self.session.http.post(self.API_URL, params=params)
+        log.debug("video ID: {0}".format(video_id))
+        try:
+            data = self.session.http.post(
+                self.API_URL,
+                params={
+                    "act": "show_inline",
+                    "al": "1",
+                    "video": video_id,
+                },
+                schema=validate.Schema(
+                    validate.transform(lambda text: re.sub(r"^\s*<!--\s*", "", text)),
+                    validate.parse_json(),
+                    {"payload": list},
+                    validate.get(("payload", -1)),
+                    list,
+                    validate.get(-1),
+                    {"player": {"params": [dict]}},
+                    validate.get(("player", "params", 0)),
+                    {
+                        validate.optional("hls"): validate.url(),
+                        validate.optional("manifest"): validate.startswith("<?xml"),
+                        validate.optional("md_author"): validate.any(validate.text, None),
+                        validate.optional("md_title"): validate.any(validate.text, None),
+                    }
+                )
+            )
+        except PluginError:
+            log.error("Could not parse API response")
+            return
 
-        for _i in itertags(res.text, 'iframe'):
-            if _i.attributes.get('src'):
-                iframe_url = update_scheme("https://", _i.attributes["src"])
-                log.debug('Found iframe: {0}'.format(iframe_url))
-                for s in self.session.streams(iframe_url).items():
-                    yield s
+        self.id = video_id
+        self.author = data.get("md_author")
+        self.title = data.get("md_title")
 
-        for _i in itertags(res.text.replace('\\', ''), 'source'):
-            if _i.attributes.get('type') == 'application/vnd.apple.mpegurl':
-                video_url = html_unescape(_i.attributes['src'])
-                streams = HLSStream.parse_variant_playlist(self.session,
-                                                           video_url)
-                if not streams:
-                    yield 'live', HLSStream(self.session, video_url)
-                else:
-                    for s in streams.items():
-                        yield s
-            elif _i.attributes.get('type') == 'video/mp4':
-                q = 'vod'
-                video_url = _i.attributes['src']
-                m = self._vod_quality_re.search(video_url)
-                if m:
-                    q = '{0}p'.format(m.group(1))
-                yield q, HTTPStream(self.session, video_url)
+        hls = data.get("hls")
+        if hls:
+            return HLSStream.parse_variant_playlist(self.session, hls)
+
+        dash_manifest = data.get("manifest")
+        if dash_manifest:
+            return DASHStream.parse_manifest(self.session, dash_manifest)
 
 
 __plugin__ = VK
--- tests/plugins/test_vk.py
+++ tests/plugins/test_vk.py
@@ -1,8 +1,9 @@
 import pytest
+import requests_mock as rm
 
+from streamlink import Streamlink
 from streamlink.exceptions import NoStreamsError
 from streamlink.plugins.vk import VK
-from tests.mock import Mock
 from tests.plugins import PluginCanHandleUrl
 
 
@@ -19,40 +20,57 @@ class TestPluginCanHandleUrlVK(PluginCanHandleUrl):
         "https://vk.com/video132886594_167211693",
         "https://vk.com/videos132886594?z=video132886594_167211693",
         "https://vk.com/video-73154028_456239128",
+        "https://vk.com/dota2?w=wall-126093223_1057924",
+        "https://vk.com/search?c%5Bq%5D=dota&c%5Bsection%5D=auto&z=video295508334_171402661",
     ]
 
     should_not_match = [
         "https://vk.com/",
-        "https://vk.com/restore",
     ]
 
 
 @pytest.mark.parametrize("url,newurl,raises", [
-    ("https://vk.com/videos-24136539?z=video-24136539_456241176%2Fclub24136539%2Fpl_-24136539_-2",
+    # canonical video URL
+    ("https://vk.com/video-24136539_456241176",
      "https://vk.com/video-24136539_456241176",
      False),
+
+    # video ID from URL
     ("https://vk.com/videos-24136539?z=video-24136539_456241181%2Fpl_-24136539_-2",
      "https://vk.com/video-24136539_456241181",
      False),
     ("https://vk.com/videos132886594?z=video132886594_167211693",
      "https://vk.com/video132886594_167211693",
      False),
-    ("http://vk.com/",
-     "http://vk.com/",
+    ("https://vk.com/search?c%5Bq%5D=dota&c%5Bsection%5D=auto&z=video295508334_171402661",
+     "https://vk.com/video295508334_171402661",
      False),
-    ("http://vk.com/videos-24136539",
-     "http://vk.com/videos-24136539",
+
+    # video ID from HTTP request
+    ("https://vk.com/dota2?w=wall-126093223_1057924",
+     "https://vk.com/video-126093223_1057924",
+     False),
+
+    # no video ID
+    ("https://vk.com/videos-0123456789?z=",
+     "",
      True),
-    ("http://vk.com/videos-24136539?z=",
-     "http://vk.com/videos-24136539?z=",
+    ("https://vk.com/video/for_kids?z=video%2Fpl_-22277933_51053000",
+     "",
      True),
 ])
-def test_url_redirect(url, newurl, raises):
-    VK.bind(Mock(), "tests.plugins.test_vk")
+def test_url_redirect(url, newurl, raises, requests_mock):
+    session = Streamlink()
+    VK.bind(session, "tests.plugins.test_vk")
     plugin = VK(url)
+    requests_mock.register_uri(rm.ANY, rm.ANY, exc=rm.exceptions.InvalidRequest)
+    requests_mock.get(
+        url,
+        text="""<!DOCTYPE html><html><head><meta property="og:url" content="{0}"/></head></html>""".format(newurl)
+    )
     try:
         plugin.follow_vk_redirect()
         assert not raises
+        assert plugin.url == newurl
     except NoStreamsError:
         assert raises
-    assert plugin.url == newurl
