682aaafaa9f630d19836db9904e5a0754062420c
bastimeyer <mail@bastimeyer.de>
2022-02-02 14:28:41 +0100

    [PATCH] plugins.pluzz: fix workflow value in API schema

--- src/streamlink/plugins/pluzz.py
+++ src/streamlink/plugins/pluzz.py
@@ -98,7 +98,7 @@ class Pluzz(Plugin):
             validate.parse_json(),
             {
                 "video": {
-                    "workflow": "token-akamai",
+                    "workflow": validate.any("token-akamai", "dai"),
                     "format": validate.any("dash", "hls"),
                     "token": validate.url(),
                     "url": validate.url()
