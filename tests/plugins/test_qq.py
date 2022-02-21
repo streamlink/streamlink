19fdc30689bc83286db9ce48af0e98a6d71ae3db
bastimeyer <mail@bastimeyer.de>
2022-02-21 21:23:53 +0100

    tests.plugins: test URL capture groups
    - move old custom capture group tests to should_match_group fixtures
    - fix URL regex in Twitch plugin and replace regular URL fixtures
      with capture group fixtures

--- tests/plugins/test_qq.py
+++ tests/plugins/test_qq.py
@@ -1,7 +1,3 @@
-from unittest.mock import Mock
-
-import pytest
-
 from streamlink.plugins.qq import QQ
 from tests.plugins import PluginCanHandleUrl
 
@@ -18,19 +14,17 @@ class TestPluginCanHandleUrlQQ(PluginCanHandleUrl):
         "http://m.live.qq.com/10039165"
     ]
 
+    should_match_groups = [
+        ("http://live.qq.com/10003715", {
+            "room_id": "10003715"
+        }),
+        ("http://m.live.qq.com/10039165", {
+            "room_id": "10039165"
+        }),
+    ]
+
     should_not_match = [
         "http://live.qq.com/",
         "http://qq.com/",
         "http://www.qq.com/"
     ]
-
-
-@pytest.mark.parametrize("url,group,expected", [
-    ("http://live.qq.com/10003715", "room_id", "10003715"),
-    ("http://m.live.qq.com/10039165", "room_id", "10039165")
-])
-def test_match_url(url, group, expected):
-    QQ.bind(Mock(), "tests.plugins.test_qq")
-    plugin = QQ(url)
-    assert plugin.match is not None
-    assert plugin.match.group(group) == expected
