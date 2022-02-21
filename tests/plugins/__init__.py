d6b952d26e00256e4935f55a035f9284f9627bd2
bastimeyer <mail@bastimeyer.de>
2022-02-21 21:19:33 +0100

    tests.plugins: implement should_match_groups
    Add parametrized tests for plugin URL capture groups via the newly added
    should_match_groups attribute in PluginCanHandleUrl. Capture groups can
    be tested either with dictionaries (named) or tuples (unnamed).
    
    Capture group URL fixtures automatically get appended to the regular
    positive URL fixtures.
    
    - Call register_assert_rewrite("tests.plugins"), so that pytest properly
      prints diffs of given and expected values in assertion failures raised
      by parametrized tests in tests.plugins.
    - Implement should_match_groups logic in PluginCanHandleUrl class
    - Add short descriptions to each URL test fixtures list
    - Rename parametrized test_capture_groups test
    - Remove skipped parametrized tests from the collection, so that plugins
      without capture group tests or with only capture group tests don't
      unnecessarily emit a warning.

--- tests/plugins/__init__.py
+++ tests/plugins/__init__.py
@@ -11,18 +11,30 @@ generic_negative_matches = [
 class PluginCanHandleUrl:
     __plugin__ = None
 
+    # A list of URLs that should match any of the plugin's URL regexes.
+    #   ["https://foo", "https://bar"]
     should_match = []
+
+    # A list of URL+capturegroup tuples, where capturegroup can be a dict (re.Match.groupdict()) or a tuple (re.Match.groups()).
+    # URLs defined in this list automatically get appended to the should_match list.
+    # Values in capturegroup dictionaries that are None get ignored when comparing and can be omitted in the test fixtures.
+    #   [("https://foo", {"foo": "foo"}), ("https://bar", ("bar", None))]
+    should_match_groups = []
+
+    # A list of URLs that should not match any of the plugin's URL regexes.
+    #   ["https://foo", "https://bar"]
     should_not_match = []
 
     def test_class_setup(self):
         assert issubclass(self.__plugin__, Plugin), "Test has a __plugin__ that is a subclass of streamlink.plugin.Plugin"
-        assert len(self.should_match) > 0, "Test has at least one positive URL"
+        assert len(self.should_match + self.should_match_groups) > 0, "Test has at least one positive URL"
 
     def test_matchers(self):
+        should_match = self.should_match + [url for url, groups in self.should_match_groups]
         assert all(
             any(
                 matcher.pattern.match(url)
-                for url in self.should_match
+                for url in should_match
             )
             for matcher in self.__plugin__.matchers
         ), "All plugin matchers should match"
@@ -40,3 +52,17 @@ class PluginCanHandleUrl:
             matcher.pattern.match(url)
             for matcher in self.__plugin__.matchers
         ), "URL does not match"
+
+    # parametrized dynamically via conftest.py
+    def test_capture_groups(self, url, groups):
+        for matcher in self.__plugin__.matchers:
+            match = matcher.pattern.match(url)
+            if match:  # pragma: no branch
+                res = (
+                    # ignore None values in capture group dicts
+                    {k: v for k, v in match.groupdict().items() if v is not None}
+                    if type(groups) is dict else
+                    # capture group tuples
+                    match.groups()
+                )
+                assert res == groups, "URL capture groups match"
--- tests/plugins/conftest.py
+++ tests/plugins/conftest.py
@@ -1,9 +1,24 @@
 from tests.plugins import PluginCanHandleUrl, generic_negative_matches
 
 
-def pytest_generate_tests(metafunc):
-    if metafunc.cls is not None and issubclass(metafunc.cls, PluginCanHandleUrl):  # pragma: no branch
+def pytest_collection_modifyitems(session, config, items):  # pragma: no cover
+    # remove empty parametrized tests
+    session.items = list(filter(lambda item: not any(
+        marker.name == "skip" and str(marker.kwargs.get("reason", "")).startswith("got empty parameter set")
+        for marker in item.own_markers
+    ), items))
+
+
+def pytest_generate_tests(metafunc):  # pragma: no cover
+    if metafunc.cls is not None and issubclass(metafunc.cls, PluginCanHandleUrl):
         if metafunc.function.__name__ == "test_can_handle_url_positive":
-            metafunc.parametrize("url", metafunc.cls.should_match)
+            metafunc.parametrize("url", metafunc.cls.should_match + [url for url, groups in metafunc.cls.should_match_groups])
+
         elif metafunc.function.__name__ == "test_can_handle_url_negative":
             metafunc.parametrize("url", metafunc.cls.should_not_match + generic_negative_matches)
+
+        elif metafunc.function.__name__ == "test_capture_groups":
+            metafunc.parametrize("url,groups", metafunc.cls.should_match_groups, ids=[
+                f"URL={url} GROUPS={groups}"
+                for url, groups in metafunc.cls.should_match_groups
+            ])
