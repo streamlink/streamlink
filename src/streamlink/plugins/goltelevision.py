bc042fef89e7904891fb08e4beac176a819d090f
bastimeyer <mail@bastimeyer.de>
2022-02-27 01:41:39 +0100

    plugins.goltelevision: fix API URL
    and remove unneeded HLSStream subclass

--- src/streamlink/plugins/goltelevision.py
+++ src/streamlink/plugins/goltelevision.py
@@ -5,27 +5,20 @@ from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
 
 
-class GOLTelevisionHLSStream(HLSStream):
-    @classmethod
-    def _get_variant_playlist(cls, res):
-        res.encoding = "UTF-8"
-        return super()._get_variant_playlist(res)
-
-
 @pluginmatcher(re.compile(
     r"https?://(?:www\.)?goltelevision\.com/en-directo"
 ))
 class GOLTelevision(Plugin):
     def _get_streams(self):
         url = self.session.http.get(
-            "https://www.goltelevision.com/api/manifest/live",
+            "https://play.goltelevision.com/api/stream/live",
             schema=validate.Schema(
                 validate.parse_json(),
                 {"manifest": validate.url()},
                 validate.get("manifest")
             )
         )
-        return GOLTelevisionHLSStream.parse_variant_playlist(self.session, url)
+        return HLSStream.parse_variant_playlist(self.session, url)
 
 
 __plugin__ = GOLTelevision
