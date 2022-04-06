9790ebc479e9653df68969e9d64fb6b6bd7089a5
bastimeyer <mail@bastimeyer.de>
2022-04-06 17:22:54 +0200

    plugins.dash: add support for parameters
    This allows passing parameters to `DASHStream` and `requests.request`,
    similar to the HLS protocol plugin. See `DASHStream.parse_manifest`.

--- src/streamlink/plugins/dash.py
+++ src/streamlink/plugins/dash.py
@@ -2,7 +2,7 @@ import logging
 import re
 
 from streamlink.plugin import Plugin, pluginmatcher
-from streamlink.plugin.plugin import LOW_PRIORITY, stream_weight
+from streamlink.plugin.plugin import LOW_PRIORITY, parse_params, stream_weight
 from streamlink.stream.dash import DASHStream
 from streamlink.utils.url import update_scheme
 
@@ -10,10 +10,10 @@ log = logging.getLogger(__name__)
 
 
 @pluginmatcher(re.compile(
-    r"dash://(?P<url>.+)"
+    r"dash://(?P<url>\S+)(?:\s(?P<params>.+))?"
 ))
 @pluginmatcher(priority=LOW_PRIORITY, pattern=re.compile(
-    r"(?P<url>.+\.mpd(?:\?.*)?)"
+    r"(?P<url>\S+\.mpd(?:\?\S*)?)(?:\s(?P<params>.+))?"
 ))
 class MPEGDASH(Plugin):
     @classmethod
@@ -29,10 +29,12 @@ class MPEGDASH(Plugin):
             return stream_weight(stream)
 
     def _get_streams(self):
-        url = update_scheme("https://", self.match.group(1), force=False)
-        log.debug(f"Parsing MPD URL: {url}")
+        data = self.match.groupdict()
+        url = update_scheme("https://", data.get("url"), force=False)
+        params = parse_params(data.get("params"))
+        log.debug(f"URL={url}; params={params}")
 
-        return DASHStream.parse_manifest(self.session, url)
+        return DASHStream.parse_manifest(self.session, url, **params)
 
 
 __plugin__ = MPEGDASH
