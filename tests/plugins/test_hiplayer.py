baf1e24625f33589b6ecd3b0e66cbdbb3af40775
Ian Cameron <1661072+mkbloke@users.noreply.github.com>
2022-05-02 18:38:42 +0100

    plugins.hiplayer: new plugin
    * replaces rotana with rewrite/fix
    * add support for cnbcarabia.com, media.gov.kw

---
+++ tests/plugins/test_hiplayer.py
@@ -0,0 +1,15 @@
+from streamlink.plugins.hiplayer import HiPlayer
+from tests.plugins import PluginCanHandleUrl
+
+
+class TestPluginCanHandleUrlHiPlayer(PluginCanHandleUrl):
+    __plugin__ = HiPlayer
+
+    should_match = [
+        "https://www.cnbcarabia.com/any",
+        "https://www.cnbcarabia.com/any/path",
+        "https://www.media.gov.kw/any",
+        "https://www.media.gov.kw/any/path",
+        "https://rotana.net/any",
+        "https://rotana.net/any/path",
+    ]
