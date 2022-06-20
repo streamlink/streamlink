3d4563104a4de03a8b9fd30b819954d2c9bc524c
Ian Cameron <1661072+mkbloke@users.noreply.github.com>
2022-06-20 01:35:04 +0100

    [PATCH] plugins.vk: fix

--- src/streamlink/plugins/vk.py
+++ src/streamlink/plugins/vk.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-$description Russian live streaming and video hosting social platform.
+$description Russian live-streaming and video hosting social platform.
 $url vk.com
 $type live, vod
 """
@@ -68,10 +68,11 @@ class VK(Plugin):
             data = self.session.http.post(
                 self.API_URL,
                 params={
-                    "act": "show_inline",
+                    "act": "show",
                     "al": "1",
                     "video": video_id,
                 },
+                headers={"Referer": self.url},
                 schema=validate.Schema(
                     validate.transform(lambda text: re.sub(r"^\s*<!--\s*", "", text)),
                     validate.parse_json(),
