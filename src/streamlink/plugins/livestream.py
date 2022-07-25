2bedb6330f13e47c573906b551c3ed25b932fe12
LaneSh4d0w <lanesh4d0w@gmail.com>
2022-07-25 19:00:21 +0200

    correct fix

--- src/streamlink/plugins/livestream.py
+++ src/streamlink/plugins/livestream.py
@@ -14,6 +14,7 @@ from streamlink.stream.hls import HLSStream
 log = logging.getLogger(__name__)
 
 URL_API = "https://api.new.livestream.com/accounts/{}/events"
+URL_API_EVENT = "https://api.new.livestream.com/accounts/{}/events/{}"
 
 
 @pluginmatcher(re.compile(
@@ -24,21 +25,26 @@ URL_API = "https://api.new.livestream.com/accounts/{}/events"
 class Livestream(Plugin):
 
     def _get_streams(self):
-        # If the channel is "hidden", you will most likely be with a precise api.new.livestream.com JSON url,
-        # including event number (only getting into the /events endpoint will show an almost blank JSON, no stream attached).
+        # If the channel is hidden, you most likely will be with a precise api.new.livestream.com JSON url,
+        # including event number (only getting into the /events endpoint will show nothing.
         # This workaround permits you to get access to the hidden streams via Streamlink.
         if self.url.__contains__("api.new."):
             res = self.session.http.get(self.url).json()
+            # API verification from given URL if there's a live event / working VOD stream.
+            if "stream_info" not in res:
+                log.debug("No stream has been found with this URL.")
+                return
+            stream_info = res["stream_info"]
         else:
-            account_no = self.match.group(2)
-            res = self.session.http.get(URL_API.format(account_no)).json()
-
-        # API verification from given URL if there's a live event / working VOD stream.
-        if "stream_info" not in res:
-            log.debug("No stream has been found with this URL.")
-            return
-        
-        stream_info = res["stream_info"]
+            account_no = self.match.group(1)
+            events = self.session.http.get(URL_API.format(account_no)).json()
+            # API verification from given URL if there's a live event / working VOD stream.
+            if "data" not in events:
+                log.debug("No stream has been found with this URL.")
+                return
+            stream_no = events["data"][0]["id"]
+            res = self.session.http.get(URL_API_EVENT.format(account_no, stream_no)).json()
+            stream_info = res["stream_info"]
 
         log.debug("stream_info: {0!r}".format(stream_info))
         if not (stream_info and stream_info["is_live"]):
