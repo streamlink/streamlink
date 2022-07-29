8aa839082fbd70530ce3ac371ac3ca88d0e8c54f
Brandon Emlaw <brandonemlaw@gmail.com>
2022-07-28 22:05:11 -0500

    Cleaned up test changes

--- tests/plugins/test_lura.py
+++ tests/plugins/test_lura.py
@@ -7,6 +7,6 @@ class TestPluginCanHandleUrlLura(PluginCanHandleUrl):
     __plugin__ = Lura
 
     should_match_groups = [
-              ("https://w3.mp.lura.live/player/prod/v3/anvload.html?key=somekey", {"params": "key=somekey"}),
-              ("http://w3.mp.lura.live/player/prod/v3/anvload.html?key=somekey", {"params": "key=somekey"}),
-          ]
+        ("https://w3.mp.lura.live/player/prod/v3/anvload.html?key=somekey", {"params": "key=somekey"}),
+        ("http://w3.mp.lura.live/player/prod/v3/anvload.html?key=somekey", {"params": "key=somekey"}),
+    ]
