f7cbfae2621d6dd2f9f1189cf98a59ba64ff9721
bastimeyer <mail@bastimeyer.de>
2022-08-04 07:49:32 +0200

    plugins: move and refactor validation schemas
    Refactor validation schemas of plugins where schemas are defined as
    class attributes and where no major changes are needed:
    
    - Move validation schemas from class attributes to schema definitions
    - Apply minor validation schema adjustments (eg. union_get)
    - Use `validate.none_or_all(...)`
    - Replace `validate.transform(pattern.search)` with `pattern` and fix
      schemas using `validate.regex(pattern)` where a regex has to match
      - Move pattern definitions from class attributes to schema definitions
      - Fix some patterns in regards to quotation matching
    - Fix minor style issues where it makes sense
      - use double quotes
      - use f-strings
      - add trailing commas
      - fix indentation

--- src/streamlink/plugins/cbsnews.py
+++ src/streamlink/plugins/cbsnews.py
@@ -15,23 +15,26 @@ from streamlink.stream.hls import HLSStream
     r"https?://www\.cbsnews\.com/live/"
 ))
 class CBSNews(Plugin):
-    _re_default_payload = re.compile(r"CBSNEWS.defaultPayload = (\{.*)")
-
-    _schema_items = validate.Schema(
-        validate.transform(_re_default_payload.search),
-        validate.any(None, validate.all(
-            validate.get(1),
-            validate.parse_json(),
-            {"items": [validate.all({
-                "video": validate.url(),
-                "format": "application/x-mpegURL"
-            }, validate.get("video"))]},
-            validate.get("items")
-        ))
-    )
-
     def _get_streams(self):
-        items = self.session.http.get(self.url, schema=self._schema_items)
+        items = self.session.http.get(self.url, schema=validate.Schema(
+            re.compile(r"CBSNEWS.defaultPayload = (\{.*)"),
+            validate.none_or_all(
+                validate.get(1),
+                validate.parse_json(),
+                {
+                    "items": [
+                        validate.all(
+                            {
+                                "video": validate.url(),
+                                "format": "application/x-mpegURL",
+                            },
+                            validate.get("video"),
+                        ),
+                    ],
+                },
+                validate.get("items"),
+            ),
+        ))
         if items:
             for hls_url in items:
                 yield from HLSStream.parse_variant_playlist(self.session, hls_url).items()
--- src/streamlink/plugins/vtvgo.py
+++ src/streamlink/plugins/vtvgo.py
@@ -15,46 +15,47 @@ log = logging.getLogger(__name__)
 
 
 @pluginmatcher(re.compile(
-    r'https?://vtvgo\.vn/xem-truc-tuyen-kenh-'
+    r"https?://vtvgo\.vn/xem-truc-tuyen-kenh-"
 ))
 class VTVgo(Plugin):
-    AJAX_URL = 'https://vtvgo.vn/ajax-get-stream'
-
-    _params_re = re.compile(r'''var\s+(?P<key>(?:type_)?id|time|token)\s*=\s*["']?(?P<value>[^"']+)["']?;''')
-
-    _schema_params = validate.Schema(
-        validate.parse_html(),
-        validate.xml_xpath_string(".//script[contains(text(),'setplayer(')][1]/text()"),
-        validate.any(None, validate.all(
-            str,
-            validate.transform(_params_re.findall),
-            [
-                ("id", int),
-                ("type_id", str),
-                ("time", str),
-                ("token", str)
-            ]
-        ))
-    )
-    _schema_stream_url = validate.Schema(
-        validate.parse_json(),
-        {"stream_url": [validate.url()]},
-        validate.get("stream_url"),
-        validate.get(0)
-    )
+    AJAX_URL = "https://vtvgo.vn/ajax-get-stream"
 
     def _get_streams(self):
         self.session.http.headers.update({
-            'Origin': 'https://vtvgo.vn',
-            'Referer': self.url,
-            'X-Requested-With': 'XMLHttpRequest',
+            "Origin": "https://vtvgo.vn",
+            "Referer": self.url,
+            "X-Requested-With": "XMLHttpRequest",
         })
-        params = self.session.http.get(self.url, schema=self._schema_params)
+
+        params = self.session.http.get(self.url, schema=validate.Schema(
+            validate.parse_html(),
+            validate.xml_xpath_string(".//script[contains(text(),'setplayer(')][1]/text()"),
+            validate.none_or_all(
+                validate.regex(
+                    re.compile(r"""var\s+(?P<key>(?:type_)?id|time|token)\s*=\s*["']?(?P<value>[^"']+)["']?;"""),
+                    method="findall",
+                ),
+                [
+                    ("id", int),
+                    ("type_id", str),
+                    ("time", str),
+                    ("token", str),
+                ],
+            ),
+        ))
         if not params:
             return
 
         log.trace(f"{params!r}")
-        hls_url = self.session.http.post(self.AJAX_URL, data=dict(params), schema=self._schema_stream_url)
+        hls_url = self.session.http.post(
+            self.AJAX_URL,
+            data=dict(params),
+            schema=validate.Schema(
+                validate.parse_json(),
+                {"stream_url": [validate.url()]},
+                validate.get(("stream_url", 0)),
+            ),
+        )
 
         return HLSStream.parse_variant_playlist(self.session, hls_url)
 
