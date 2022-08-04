c59b946b1821be0d45f30bb727f5af3475dc902b
bastimeyer <mail@bastimeyer.de>
2022-08-04 09:55:36 +0200

    plugins.dlive: refactor

--- src/streamlink/plugins/dlive.py
+++ src/streamlink/plugins/dlive.py
@@ -1,24 +1,18 @@
 """
-$description Global live streaming platform owned by BitTorrent, Inc.
+$description Global live-streaming platform owned by BitTorrent, Inc.
 $url dlive.tv
 $type live, vod
 """
 
-import json
 import logging
 import re
 from urllib.parse import unquote_plus
 
-from streamlink.plugin import Plugin, PluginError, pluginmatcher
+from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
 
 log = logging.getLogger(__name__)
-
-
-QUALITY_WEIGHTS = {
-    "src": 1080,
-}
 
 
 @pluginmatcher(re.compile(r"""
@@ -30,85 +24,75 @@ QUALITY_WEIGHTS = {
     )
 """, re.VERBOSE))
 class DLive(Plugin):
-    _re_videoPlaybackUrl = re.compile(r'"playbackUrl"\s*:\s*"([^"]+\.m3u8)"')
+    URL_LIVE = "https://live.prd.dlive.tv/hls/live/{username}.m3u8"
 
-    _schema_userByDisplayName = validate.Schema({
-        "data": {
-            "userByDisplayName": {
-                "livestream": validate.any(None, {
-                    "title": validate.text
-                }),
-                "username": validate.text
-            }
-        }},
-        validate.get("data"),
-        validate.get("userByDisplayName")
-    )
-    _schema_videoPlaybackUrl = validate.Schema(
-        validate.transform(_re_videoPlaybackUrl.search),
-        validate.any(None, validate.all(
-            validate.get(1),
-            validate.transform(unquote_plus),
-            validate.transform(lambda url: bytes(url, "utf-8").decode("unicode_escape")),
-            validate.url()
-        ))
-    )
+    QUALITY_WEIGHTS = {
+        "src": 1080,
+    }
 
     @classmethod
     def stream_weight(cls, key):
-        weight = QUALITY_WEIGHTS.get(key)
+        weight = cls.QUALITY_WEIGHTS.get(key)
         if weight:
             return weight, "dlive"
 
-        return Plugin.stream_weight(key)
+        return super().stream_weight(key)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.video = self.match.group("video")
-        self.channel = self.match.group("channel")
-
-    def _get_streams_video(self):
-        log.debug("Getting video HLS streams for {0}".format(self.video))
-        try:
-            hls_url = self.session.http.get(self.url, schema=self._schema_videoPlaybackUrl)
-            if hls_url is None:
-                return
-        except PluginError:
-            return
+    def _get_streams_video(self, video):
+        log.debug(f"Getting video HLS streams for {video}")
+        hls_url = self.session.http.get(self.url, schema=validate.Schema(
+            validate.regex(re.compile(r'"playbackUrl"\s*:\s*"([^"]+\.m3u8)"')),
+            validate.get(1),
+            validate.transform(unquote_plus),
+            validate.transform(lambda url: bytes(url, "utf-8").decode("unicode_escape")),
+            validate.url(),
+        ))
 
         return HLSStream.parse_variant_playlist(self.session, hls_url)
 
-    def _get_streams_live(self):
-        log.debug("Getting live HLS streams for {0}".format(self.channel))
-        try:
-            data = json.dumps({"query": """query {{
-                userByDisplayName(displayname:"{displayname}") {{
-                    livestream {{
-                        title
-                    }}
-                    username
+    def _get_streams_live(self, channel):
+        log.debug(f"Getting live HLS streams for {channel}")
+        query = f"""query {{
+            userByDisplayName(displayname:"{channel}") {{
+                livestream {{
+                    title
                 }}
-            }}""".format(displayname=self.channel)})
-            res = self.session.http.post("https://graphigo.prd.dlive.tv/", data=data)
-            res = self.session.http.json(res, schema=self._schema_userByDisplayName)
-            if res["livestream"] is None:
-                return
-        except PluginError:
-            return
+                username
+            }}
+        }}"""
+        livestream, username = self.session.http.post(
+            "https://graphigo.prd.dlive.tv/",
+            json={"query": query},
+            schema=validate.Schema(
+                validate.parse_json(),
+                {
+                    "data": {
+                        "userByDisplayName": {
+                            "livestream": {
+                                "title": str,
+                            },
+                            "username": str,
+                        },
+                    },
+                },
+                validate.get(("data", "userByDisplayName")),
+                validate.union_get("livestream", "username"),
+            ),
+        )
 
-        self.author = self.channel
-        self.title = res["livestream"]["title"]
+        self.author = channel
+        self.title = livestream["title"]
 
-        hls_url = "https://live.prd.dlive.tv/hls/live/{0}.m3u8".format(res["username"])
-
-        return HLSStream.parse_variant_playlist(self.session, hls_url)
+        return HLSStream.parse_variant_playlist(self.session, self.URL_LIVE.format(username=username))
 
     def _get_streams(self):
-        if self.video:
-            return self._get_streams_video()
-        elif self.channel:
-            return self._get_streams_live()
+        video = self.match.group("video")
+        channel = self.match.group("channel")
+
+        if video:
+            return self._get_streams_video(video)
+        elif channel:
+            return self._get_streams_live(channel)
 
 
 __plugin__ = DLive
