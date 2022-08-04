17b1b21726ccf2046f18e2c3df68e1942e21efff
bastimeyer <mail@bastimeyer.de>
2022-08-04 09:42:56 +0200

    [PATCH] plugins.huajiao: rewrite and fix plugin

--- tests/plugins/test_huajiao.py
+++ tests/plugins/test_huajiao.py
@@ -6,5 +6,6 @@ class TestPluginCanHandleUrlHuajiao(PluginCanHandleUrl):
     __plugin__ = Huajiao
 
     should_match = [
-        'http://www.huajiao.com/l/123123123',
+        "http://www.huajiao.com/l/123123123",
+        "https://www.huajiao.com/l/123123123",
     ]
