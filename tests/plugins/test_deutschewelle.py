9af8bb10364bfba245d9725b625c20f1ac9b10a1
bastimeyer <mail@bastimeyer.de>
2022-08-10 02:13:10 +0200

    plugins.deutschewelle: rewrite and fix plugin

--- tests/plugins/test_deutschewelle.py
+++ tests/plugins/test_deutschewelle.py
@@ -6,10 +6,26 @@ class TestPluginCanHandleUrlDeutscheWelle(PluginCanHandleUrl):
     __plugin__ = DeutscheWelle
 
     should_match = [
-        "http://www.dw.com/en/media-center/live-tv/s-100825",
-        "http://www.dw.com/fr/médiathèque/direct-tv/s-100948?channel=5",
-        "http://www.dw.com/de/tim-bendzko-deutsch-pop-darling-auf-tour/av-39101077",
-        "http://www.dw.com/de/trump-sagt-nein-die-welt-schüttelt-den-kopf/av-39096724",
-        "http://www.dw.com/el/ο-τραμπ-θεωρεί-την-ευρώπη-ανταγωνιστή-όχι-εταίρο/av-39103742",
-        "http://www.dw.com/ar/أي-البروتينات-صحية-الحيوانية-أم-النباتية/av-39095317",
+        # Live: en/EN (no channel selection)
+        "https://www.dw.com/en/live-tv/s-100825",
+
+        # Live: de/DE (default channel)
+        "https://www.dw.com/de/live-tv/s-100817",
+        # Live: de/DE (selected channel)
+        "https://www.dw.com/de/live-tv/s-100817?channel=5",
+        # Live: de/EN (selected channel)
+        "https://www.dw.com/de/live-tv/s-100817?channel=1",
+
+        # Live: es/ES (default channel)
+        "https://www.dw.com/es/tv-en-vivo/s-100837",
+        # Live: es/ES (selected channel)
+        "https://www.dw.com/es/tv-en-vivo/s-100837?channel=3",
+        # Live: es/DE (selected channel)
+        "https://www.dw.com/es/tv-en-vivo/s-100837?channel=5",
+
+        # VOD
+        "https://www.dw.com/en/top-stories-in-90-seconds/av-49496622",
+
+        # Audio
+        "https://www.dw.com/en/womens-euros-2022-the-end/av-62738085",
     ]
