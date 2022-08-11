9fdc8d2a821e801acddc77b0ba1763195842ba2b
bastimeyer <mail@bastimeyer.de>
2022-08-11 15:55:48 +0200

    plugins.bloomberg: fix default US live stream

--- tests/plugins/test_bloomberg.py
+++ tests/plugins/test_bloomberg.py
@@ -5,18 +5,14 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlBloomberg(PluginCanHandleUrl):
     __plugin__ = Bloomberg
 
-    should_match = [
-        "https://www.bloomberg.com/live/us",
-        "https://www.bloomberg.com/live/europe",
-        "https://www.bloomberg.com/live/asia",
-        "https://www.bloomberg.com/live/stream",
-        "https://www.bloomberg.com/live/emea",
-        "https://www.bloomberg.com/live/asia_stream",
-        "https://www.bloomberg.com/news/videos/2017-04-17/wozniak-science-fiction-finally-becoming-reality-video",
-        "http://www.bloomberg.com/news/videos/2017-04-17/russia-s-stake-in-a-u-s-north-korea-conflict-video"
+    should_match_groups = [
+        ("https://www.bloomberg.com/live", {"live": "live"}),
+        ("https://www.bloomberg.com/live/", {"live": "live"}),
+        ("https://www.bloomberg.com/live/europe", {"live": "live", "channel": "europe"}),
+        ("https://www.bloomberg.com/live/europe/", {"live": "live", "channel": "europe"}),
+        ("https://www.bloomberg.com/news/videos/2022-08-10/-bloomberg-surveillance-early-edition-full-08-10-22", {}),
     ]
 
     should_not_match = [
-        "https://www.bloomberg.com/live/",
         "https://www.bloomberg.com/politics/articles/2017-04-17/french-race-up-for-grabs-days-before-voters-cast-first-ballots",
     ]
