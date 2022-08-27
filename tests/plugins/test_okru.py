f219e7ff733149a6330b150cba288ab15e29e1f9
bastimeyer <mail@bastimeyer.de>
2022-08-27 22:47:19 +0200

    plugins.okru: support mobile page

--- tests/plugins/test_okru.py
+++ tests/plugins/test_okru.py
@@ -1,5 +1,3 @@
-import pytest
-
 from streamlink.plugins.okru import OKru
 from tests.plugins import PluginCanHandleUrl
 
@@ -15,13 +13,3 @@ class TestPluginCanHandleUrlOKru(PluginCanHandleUrl):
         "https://www.ok.ru/live/12345",
         "https://ok.ru/video/266205792931",
     ]
-
-
-class TestOKru:
-    @pytest.mark.parametrize("url,expected", [
-        ("https://m.ok.ru/live/12345", "https://ok.ru/live/12345"),
-        ("https://mobile.ok.ru/live/12345", "https://ok.ru/live/12345"),
-    ])
-    def test_url_mobile(self, url, expected):
-        plugin = OKru(url)
-        assert plugin.url == expected
