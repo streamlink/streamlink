04fb1f702d96b0c0955600ec3f121fc8265ed5cb
bastimeyer <mail@bastimeyer.de>
2022-09-08 17:22:11 +0200

    ci: install custom lxml wheel on cp311 win-amd64
    Install temporary custom lxml wheel on Windows built in an external
    repo, as official wheels are missing for Python 3.11. Building it
    from source requires building libxml2 and libxslt first and patching
    the wheel build config, which is slow and tedious.

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -8,4 +8,9 @@ python -m pip install --disable-pip-version-check --upgrade pip setuptools
 python -m pip install --upgrade -r dev-requirements.txt
 # https://github.com/streamlink/streamlink/issues/4021
 python -m pip install brotli
+# Temporary custom lxml wheel for cp311 on Windows: https://github.com/streamlink/streamlink/pull/4806#issue-1364468477
+[[ "$(uname)" != "Linux" ]] \
+  && [[ "$(python -V)" =~ "Python 3.11."* ]] \
+  && python -m pip install https://github.com/streamlink/temp-wheel-for-lxml-cp311-win-amd64/releases/download/lxml-4.9.1-1/lxml-4.9.1-cp311-cp311-win_amd64.whl \
+  || true
 python -m pip install -e .
