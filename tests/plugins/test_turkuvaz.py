351b92df158b6a1d6501221d417b93d622b825c8
Ömer Görür <102440553+omergorur@users.noreply.github.com>
2022-09-10 23:52:17 +0300

    plugins.turkuvaz: rewrite, update supported sites (#4819)
    Co-authored-by: bastimeyer <mail@bastimeyer.de>

--- tests/plugins/test_turkuvaz.py
+++ tests/plugins/test_turkuvaz.py
@@ -6,17 +6,15 @@ class TestPluginCanHandleUrlTurkuvaz(PluginCanHandleUrl):
     __plugin__ = Turkuvaz
 
     should_match = [
-        'http://www.atv.com.tr/a2tv/canli-yayin',
-        'https://www.atv.com.tr/a2tv/canli-yayin',
-        'https://www.atv.com.tr/webtv/canli-yayin',
-        'http://www.a2tv.com.tr/webtv/canli-yayin',
-        'http://www.ahaber.com.tr/video/canli-yayin',
-        'https://www.ahaber.com.tr/video/canli-yayin',
-        'https://www.ahaber.com.tr/webtv/canli-yayin',
-        'https://www.aspor.com.tr/webtv/canli-yayin',
-        'http://www.anews.com.tr/webtv/live-broadcast',
-        'http://www.atvavrupa.tv/webtv/canli-yayin',
-        'http://www.minikacocuk.com.tr/webtv/canli-yayin',
-        'http://www.minikago.com.tr/webtv/canli-yayin',
-        'https://www.sabah.com.tr/apara/canli-yayin',
+        "https://www.ahaber.com.tr/video/canli-yayin",
+        "https://www.ahaber.com.tr/webtv/canli-yayin",
+        "https://www.anews.com.tr/webtv/live-broadcast",
+        "https://www.apara.com.tr/canli-yayin",
+        "https://www.aspor.com.tr/webtv/canli-yayin",
+        "https://www.atv.com.tr/a2tv/canli-yayin",
+        "https://www.atv.com.tr/webtv/canli-yayin",
+        "https://www.atvavrupa.tv/webtv/canli-yayin",
+        "https://www.minikacocuk.com.tr/webtv/canli-yayin",
+        "https://www.minikago.com.tr/webtv/canli-yayin",
+        "https://www.vavtv.com.tr/canli-yayin",
     ]
