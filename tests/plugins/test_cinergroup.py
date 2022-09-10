c7bb80b5e677a847819e83fd8765b31c41815668
bastimeyer <mail@bastimeyer.de>
2022-09-10 13:19:11 +0200

    plugins.cinergroup: fix regex, add bloomberght

--- tests/plugins/test_cinergroup.py
+++ tests/plugins/test_cinergroup.py
@@ -6,18 +6,15 @@ class TestPluginCanHandleUrlCinerGroup(PluginCanHandleUrl):
     __plugin__ = CinerGroup
 
     should_match = [
-        "https://showtv.com.tr/canli-yayin",
-        "https://showtv.com.tr/canli-yayin/showtv",
+        "https://bloomberght.com/tv",
         "https://haberturk.com/canliyayin",
         "https://haberturk.com/tv/canliyayin",
         "http://haberturk.tv/canliyayin",
-        "https://www.bloomberght.com/tv",
-        "http://www.bloomberght.com/tv",
-        "https://bloomberght.com/tv",
-        "http://bloomberght.com/tv",
         "http://showmax.com.tr/canliyayin",
         "http://showmax.com.tr/canli-yayin",
         "https://showturk.com.tr/canli-yayin/showturk",
         "https://showturk.com.tr/canli-yayin",
         "https://showturk.com.tr/canliyayin",
+        "https://showtv.com.tr/canli-yayin",
+        "https://showtv.com.tr/canli-yayin/showtv",
     ]
