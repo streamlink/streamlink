43d9db7f66e5fa54c7bc1e160a7ae28c6e321b53
Ömer Görür <102440553+omergorur@users.noreply.github.com>
2022-09-10 23:52:17 +0300

    plugins.turkuvaz: rewrite, update supported sites (#4819)
    Co-authored-by: bastimeyer <mail@bastimeyer.de>

--- src/streamlink/plugins/turkuvaz.py
+++ src/streamlink/plugins/turkuvaz.py
@@ -1,75 +1,81 @@
 """
 $description Turkish live TV channels from Turkuvaz Media Group, including Ahaber, ATV, Minika COCUK and MinikaGO.
-$url atv.com.tr
 $url a2tv.com.tr
 $url ahaber.com.tr
 $url anews.com.tr
+$url apara.com.tr
 $url aspor.com.tr
+$url atv.com.tr
 $url atvavrupa.tv
 $url minikacocuk.com.tr
 $url minikago.com.tr
-$url sabah.com.tr
+$url vavtv.com.tr
 $type live
 $region various
 """
 
-import logging
 import re
+from urllib.parse import urlparse
 
 from streamlink.plugin import Plugin, pluginmatcher
-from streamlink.plugin.api import useragents, validate
+from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
 
-log = logging.getLogger(__name__)
 
-
-@pluginmatcher(re.compile(r"""https?://(?:www\.)?
+@pluginmatcher(re.compile(r"""
+    https?://(?:www\.)?
     (?:
         (?:
-            (atvavrupa)\.tv
+            atvavrupa\.tv
             |
-            (atv|a2tv|ahaber|aspor|minikago|minikacocuk|anews)\.com\.tr
+            (?:atv|a2tv|ahaber|aspor|minikago|minikacocuk|anews)\.com\.tr
         )/webtv/(?:live-broadcast|canli-yayin)
         |
-        (ahaber)\.com\.tr/video/canli-yayin
+        ahaber\.com\.tr/video/canli-yayin
         |
-        atv\.com\.tr/(a2tv)/canli-yayin
+        (?:apara|vavtv)\.com\.tr/canli-yayin
         |
-        sabah\.com\.tr/(apara)/canli-yayin
+        atv\.com\.tr/a2tv/canli-yayin
     )
 """, re.VERBOSE))
 class Turkuvaz(Plugin):
-    _hls_url = "https://trkvz-live.ercdn.net/{channel}/{channel}.m3u8"
-    _token_url = "https://securevideotoken.tmgrup.com.tr/webtv/secure"
-    _token_schema = validate.Schema(validate.all(
-        {
-            "Success": True,
-            "Url": validate.url(),
-        },
-        validate.get("Url"))
-    )
+    _URL_HLS = "https://trkvz-live.ercdn.net/{channel}/{channel}.m3u8"
+    _URL_TOKEN = "https://securevideotoken.tmgrup.com.tr/webtv/secure"
+
+    _MAP_HOST_TO_CHANNEL = {
+        "atv": "atvhd",
+        "ahaber": "ahaberhd",
+        "apara": "aparahd",
+        "aspor": "asporhd",
+        "anews": "anewshd",
+        "vavtv": "vavtv",
+        "minikacocuk": "minikagococuk",
+    }
 
     def _get_streams(self):
-        url_m = self.match
-        domain = url_m.group(1) or url_m.group(2) or url_m.group(3) or url_m.group(4) or url_m.group(5)
+        hostname = re.sub(r"^www\.", "", urlparse(self.url).netloc).split(".")[0]
+
         # remap the domain to channel
-        channel = {"atv": "atvhd",
-                   "ahaber": "ahaberhd",
-                   "apara": "aparahd",
-                   "aspor": "asporhd",
-                   "anews": "anewshd",
-                   "minikacocuk": "minikagococuk"}.get(domain, domain)
-        hls_url = self._hls_url.format(channel=channel)
+        channel = self._MAP_HOST_TO_CHANNEL.get(hostname, hostname)
+        hls_url = self._URL_HLS.format(channel=channel)
+
         # get the secure HLS URL
-        res = self.session.http.get(self._token_url,
-                                    params="url={0}".format(hls_url),
-                                    headers={"Referer": self.url,
-                                             "User-Agent": useragents.CHROME})
+        secure_hls_url = self.session.http.get(
+            self._URL_TOKEN,
+            params=f"url={hls_url}",
+            headers={"Referer": self.url},
+            schema=validate.Schema(
+                validate.parse_json(),
+                {
+                    "Success": True,
+                    "Url": validate.url(),
+                },
+                validate.get("Url"),
+            ),
+        )
 
-        secure_hls_url = self.session.http.json(res, schema=self._token_schema)
-
-        log.debug("Found HLS URL: {0}".format(secure_hls_url))
-        return HLSStream.parse_variant_playlist(self.session, secure_hls_url)
+        if secure_hls_url:
+            return HLSStream.parse_variant_playlist(self.session, secure_hls_url)
 
 
 __plugin__ = Turkuvaz
