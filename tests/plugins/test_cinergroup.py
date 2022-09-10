cd3fd7c3fba25e02dffb84ceef0f801fd6be5e7c
Ömer Görür <102440553+omergorur@users.noreply.github.com>
2022-09-10 14:32:06 +0300

    plugins.cinergroup: rewrite, add bloomberght (#4813)
    Co-authored-by: Sebastian Meyer <mail@bastimeyer.de>

--- tests/plugins/test_cinergroup.py
+++ tests/plugins/test_cinergroup.py
@@ -6,11 +6,15 @@ class TestPluginCanHandleUrlCinerGroup(PluginCanHandleUrl):
     __plugin__ = CinerGroup
 
     should_match = [
-        "https://showtv.com.tr/canli-yayin",
-        "https://showtv.com.tr/canli-yayin/showtv",
+        "https://bloomberght.com/tv",
         "https://haberturk.com/canliyayin",
         "https://haberturk.com/tv/canliyayin",
         "http://haberturk.tv/canliyayin",
         "http://showmax.com.tr/canliyayin",
+        "http://showmax.com.tr/canli-yayin",
         "https://showturk.com.tr/canli-yayin/showturk",
+        "https://showturk.com.tr/canli-yayin",
+        "https://showturk.com.tr/canliyayin",
+        "https://showtv.com.tr/canli-yayin",
+        "https://showtv.com.tr/canli-yayin/showtv",
     ]
