bf2b107fd0302b8776236c0f63bdf2f81c28f007
bastimeyer <mail@bastimeyer.de>
2022-09-12 19:43:05 +0200

    plugins.tv3cat: rewrite

--- src/streamlink/plugins/tv3cat.py
+++ src/streamlink/plugins/tv3cat.py
@@ -8,7 +8,7 @@ $region Spain
 import logging
 import re
 
-from streamlink.plugin import Plugin, PluginError, pluginmatcher
+from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
 
@@ -16,33 +16,54 @@ log = logging.getLogger(__name__)
 
 
 @pluginmatcher(re.compile(
-    r"https?://(?:www\.)?ccma\.cat/tv3/directe/(.+?)/"
+    r"https?://(?:www\.)?ccma\.cat/tv3/directe/(?P<ident>.+?)/"
 ))
 class TV3Cat(Plugin):
-    _stream_info_url = "http://dinamics.ccma.cat/pvideo/media.jsp" \
-                       "?media=video&version=0s&idint={ident}&profile=pc&desplacament=0"
-    _media_schema = validate.Schema({
-        "geo": validate.text,
-        "url": validate.url(scheme=validate.any("http", "https"))
-    })
-    _channel_schema = validate.Schema({
-        "media": validate.any([_media_schema], _media_schema)},
-        validate.get("media"),
-        # If there is only one item, it's not a list ... silly
-        validate.transform(lambda x: x if isinstance(x, list) else [x])
-    )
+    _URL_STREAM_INFO = "https://dinamics.ccma.cat/pvideo/media.jsp"
+
+    _MAP_CHANNELS = {
+        "tv3": "tvi",
+    }
 
     def _get_streams(self):
-        if self.match:
-            ident = self.match.group(1)
-            data_url = self._stream_info_url.format(ident=ident)
-            stream_infos = self.session.http.json(self.session.http.get(data_url), schema=self._channel_schema)
+        ident = self.match.group("ident")
 
-            for stream in stream_infos:
-                try:
-                    return HLSStream.parse_variant_playlist(self.session, stream['url'], name_fmt="{pixels}_{bitrate}")
-                except PluginError:
-                    log.debug("Failed to get streams for: {0}".format(stream['geo']))
+        schema_media = {
+            "geo": str,
+            "url": validate.url(path=validate.endswith(".m3u8")),
+        }
+
+        stream_infos = self.session.http.get(
+            self._URL_STREAM_INFO,
+            params={
+                "media": "video",
+                "versio": "vast",
+                "idint": self._MAP_CHANNELS.get(ident, ident),
+                "profile": "pc",
+                "desplacament": "0",
+                "broadcast": "false",
+            },
+            schema=validate.Schema(
+                validate.parse_json(),
+                {
+                    "media": validate.any(
+                        [schema_media],
+                        validate.all(
+                            schema_media,
+                            validate.transform(lambda item: [item]),
+                        ),
+                    ),
+                },
+                validate.get("media"),
+            ),
+        )
+
+        for stream in stream_infos:
+            log.info(f"Accessing stream from region {stream['geo']}")
+            try:
+                return HLSStream.parse_variant_playlist(self.session, stream["url"], name_fmt="{pixels}_{bitrate}")
+            except OSError:
+                pass
 
 
 __plugin__ = TV3Cat
