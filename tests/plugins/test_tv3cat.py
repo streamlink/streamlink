2ab1724cbee9eaedf69b12d762cfe56aa632ef9f
bastimeyer <mail@bastimeyer.de>
2022-09-12 19:43:05 +0200

    plugins.tv3cat: rewrite

--- tests/plugins/test_tv3cat.py
+++ tests/plugins/test_tv3cat.py
@@ -5,13 +5,9 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlTV3Cat(PluginCanHandleUrl):
     __plugin__ = TV3Cat
 
-    should_match = [
-        'http://ccma.cat/tv3/directe/tv3/',
-        'http://ccma.cat/tv3/directe/324/',
-        'https://ccma.cat/tv3/directe/tv3/',
-        'https://ccma.cat/tv3/directe/324/',
-        'http://www.ccma.cat/tv3/directe/tv3/',
-        'http://www.ccma.cat/tv3/directe/324/',
-        'https://www.ccma.cat/tv3/directe/tv3/',
-        'https://www.ccma.cat/tv3/directe/324/',
+    should_match_groups = [
+        ("https://ccma.cat/tv3/directe/tv3/", {"ident": "tv3"}),
+        ("https://ccma.cat/tv3/directe/324/", {"ident": "324"}),
+        ("https://www.ccma.cat/tv3/directe/tv3/", {"ident": "tv3"}),
+        ("https://www.ccma.cat/tv3/directe/324/", {"ident": "324"}),
     ]
