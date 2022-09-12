5df028bae7a38fef0eb67a8c55ecb391b940518c
bastimeyer <mail@bastimeyer.de>
2022-09-12 18:44:34 +0200

    plugins.dailymotion: rewrite and fix plugin

--- src/streamlink/plugins/dailymotion.py
+++ src/streamlink/plugins/dailymotion.py
@@ -1,5 +1,5 @@
 """
-$description Global live streaming and video on-demand hosting platform.
+$description Global live-streaming and video on-demand hosting platform.
 $url dailymotion.com
 $type live, vod
 """
@@ -7,41 +7,13 @@ $type live, vod
 import logging
 import re
 
-from streamlink.exceptions import NoStreamsError
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
 from streamlink.stream.http import HTTPStream
 
+
 log = logging.getLogger(__name__)
-
-COOKIES = {
-    "family_filter": "off",
-    "ff": "off"
-}
-STREAM_INFO_URL = "https://www.dailymotion.com/player/metadata/video/{0}"
-USER_INFO_URL = "https://api.dailymotion.com/user/{0}"
-
-_media_schema = validate.Schema(validate.any(
-    {"error": {"title": validate.text}},
-    # "stream_chromecast_url": validate.url(),
-    # Chromecast URL is already available in qualities subdict
-    {"qualities": validate.any({
-        validate.text: validate.all([{
-            "type": validate.text,
-            "url": validate.url()
-        }])
-    })
-    }))
-_live_id_schema = validate.Schema(
-    {
-        "total": int,
-        "list": validate.any(
-            [],
-            [{"id": validate.text}]
-        )
-    }
-)
 
 
 @pluginmatcher(re.compile(r"""
@@ -49,61 +21,110 @@ _live_id_schema = validate.Schema(
     (?:
         (/embed)?/(video|live)/(?P<media_id>[^_?/]+)
         |
-        /(?P<channel_name>[\w-]+)
+        /(?P<user>[\w-]+)
     )
 """, re.VERBOSE))
 class DailyMotion(Plugin):
-    def _get_streams_from_media(self, media_id):
-        res = self.session.http.get(STREAM_INFO_URL.format(media_id), cookies=COOKIES)
-        media = self.session.http.json(res, schema=_media_schema)
+    _URL_API_USER_VIDEO = "https://api.dailymotion.com/user/{user}/videos"
+    _URL_STREAM_INFO = "https://www.dailymotion.com/player/metadata/video/{media_id}"
 
-        if media.get("error"):
-            log.error("Failed to get stream: {0}".format(media["error"]["title"]))
+    def _get_streams_from_media(self, media_id):
+        media = self.session.http.get(
+            self._URL_STREAM_INFO.format(media_id=media_id),
+            cookies={
+                "family_filter": "off",
+                "ff": "off",
+            },
+            schema=validate.Schema(
+                validate.parse_json(),
+                validate.any(
+                    {
+                        "error": {"type": "not_found"},
+                    },
+                    {
+                        "error": {"title": str},
+                    },
+                    {
+                        "owner.username": str,
+                        "title": str,
+                        "qualities": {
+                            str: [{
+                                "type": str,
+                                "url": validate.url(),
+                            }],
+                        },
+                    },
+                ),
+            ),
+        )
+
+        error = media.get("error")
+        if error:
+            if error.get("type") == "not_found":
+                log.error(f"Unknown media ID: {media_id}")
+            else:
+                log.error(f"Failed to get stream: {error['title']}")
             return
 
-        for quality, streams in media['qualities'].items():
+        self.id = media_id
+        self.author = media["owner.username"]
+        self.title = media["title"]
+
+        for quality, streams in media["qualities"].items():
             for stream in streams:
-                if stream['type'] == 'application/x-mpegURL':
-                    if quality != 'auto':
+                if stream["type"] == "application/x-mpegURL":
+                    if quality != "auto":
                         # Avoid duplicate HLS streams with bitrate selector in the URL query
                         continue
-                    yield from HLSStream.parse_variant_playlist(self.session, stream['url']).items()
-                elif stream['type'] == 'video/mp4':
+                    yield from HLSStream.parse_variant_playlist(self.session, stream["url"]).items()
+                elif stream["type"] == "video/mp4":
                     # Drop FPS in quality
-                    resolution = re.sub('@[0-9]+', '', quality) + 'p'
-                    yield resolution, HTTPStream(self.session, stream['url'])
+                    quality = re.sub(r"@\d+", "", quality)
+                    resolution = f"{quality}p"
+                    yield resolution, HTTPStream(self.session, stream["url"])
 
-    def get_live_id(self, username):
-        """Get the livestream videoid from a username.
-           https://developer.dailymotion.com/tools/apiexplorer#/user/videos/list
-        """
-        params = {
-            "flags": "live_onair"
-        }
-        api_user_videos = f"{USER_INFO_URL.format(username)}/videos"
-        try:
-            res = self.session.http.get(
-                api_user_videos.format(username),
-                params=params
-            )
-        except Exception:
-            log.error("invalid username")
-            raise NoStreamsError(self.url)
+    def _get_media_id(self, user):
+        # https://developers.dailymotion.com/tools/
+        data = self.session.http.get(
+            self._URL_API_USER_VIDEO.format(user=user),
+            params={
+                "fields": "id",
+                "flags": "live_onair",
+                "family_filter": "false",
+            },
+            acceptable_status=(200, 404),
+            schema=validate.Schema(
+                validate.parse_json(),
+                validate.any(
+                    {
+                        "error": {"message": str},
+                    },
+                    {
+                        "list": [{"id": str}],
+                    },
+                ),
+            ),
+        )
 
-        data = self.session.http.json(res, schema=_live_id_schema)
-        if data["total"] > 0:
-            return data["list"][0]["id"]
-        return False
+        if data.get("error"):
+            log.error(f"Error while retrieving media ID: {data['error']['message']}")
+            return
+
+        if not data["list"]:
+            log.error(f"No live streams found for channel {user}")
+            return
+
+        return data["list"][0]["id"]
 
     def _get_streams(self):
         media_id = self.match.group("media_id")
-        username = self.match.group("channel_name")
+        user = self.match.group("user")
 
-        if not media_id and username:
-            media_id = self.get_live_id(username)
+        if not media_id and user:
+            media_id = self._get_media_id(user)
 
         if media_id:
-            log.debug("Found media ID: {0}".format(media_id))
+            log.debug(f"Found media ID: {media_id}")
             return self._get_streams_from_media(media_id)
 
 
