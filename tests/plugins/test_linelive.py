33ba25a2a84a3230c8eaf24bae7575869851bbbd
bastimeyer <mail@bastimeyer.de>
2022-09-12 19:23:22 +0200

    plugins.linelive: rewrite

--- tests/plugins/test_linelive.py
+++ tests/plugins/test_linelive.py
@@ -5,11 +5,12 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlLineLive(PluginCanHandleUrl):
     __plugin__ = LineLive
 
-    should_match = [
-        'http://live.line.me/channels/123/broadcast/12345678',
-        'https://live.line.me/channels/123/broadcast/12345678',
+    should_match_groups = [
+        ("https://live.line.me/channels/123/broadcast/456", {"channel": "123", "broadcast": "456"}),
     ]
 
     should_not_match = [
-        'https://live.line.me/channels/123/upcoming/12345678',
+        "https://live.line.me/",
+        "https://live.line.me/channels/123/",
+        "https://live.line.me/channels/123/upcoming/456",
     ]
