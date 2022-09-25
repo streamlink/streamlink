ee230e47ca54545f65aba2af8c41df161b350032
bastimeyer <mail@bastimeyer.de>
2022-09-25 02:06:43 +0200

    plugins.vinhlongtv: rewrite and fix plugin

--- tests/plugins/test_vinhlongtv.py
+++ tests/plugins/test_vinhlongtv.py
@@ -1,3 +1,7 @@
+from unittest.mock import Mock
+
+from freezegun import freeze_time
+
 from streamlink.plugins.vinhlongtv import VinhLongTV
 from tests.plugins import PluginCanHandleUrl
 
@@ -6,7 +10,17 @@ class TestPluginCanHandleUrlVinhLongTV(PluginCanHandleUrl):
     __plugin__ = VinhLongTV
 
     should_match = [
-        'http://thvli.vn/live/thvl1-hd/aab94d1f-44e1-4992-8633-6d46da08db42',
-        'http://thvli.vn/live/thvl2-hd/bc60bddb-99ac-416e-be26-eb4d0852f5cc',
-        'http://thvli.vn/live/phat-thanh/c87174ba-7aeb-4cb4-af95-d59de715464c',
+        "https://www.thvli.vn/live/thvl1-hd",
+        "https://www.thvli.vn/live/thvl2-hd",
+        "https://www.thvli.vn/live/thvl3-hd",
+        "https://www.thvli.vn/live/thvl4-hd",
     ]
+
+
+@freeze_time("2022-09-25T00:04:45Z")
+def test_headers():
+    # noinspection PyUnresolvedReferences
+    assert VinhLongTV(Mock(), "")._get_headers() == {
+        "X-SFD-Date": "20220925000445",
+        "X-SFD-Key": "3507c190ae8befda3bfa8e2c00af3c7a",
+    }
