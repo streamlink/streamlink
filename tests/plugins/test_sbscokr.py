02e8bc9dd7c20001937d05fbe5e45bf2dc62dc52
bastimeyer <mail@bastimeyer.de>
2022-10-04 03:20:25 +0200

    plugins.sbscokr: rewrite and fix plugin

--- tests/plugins/test_sbscokr.py
+++ tests/plugins/test_sbscokr.py
@@ -5,7 +5,11 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlSBScokr(PluginCanHandleUrl):
     __plugin__ = SBScokr
 
-    should_match = [
-        'https://play.sbs.co.kr/onair/pc/index.html',
-        'http://play.sbs.co.kr/onair/pc/index.html',
+    should_match_groups = [
+        # regular channels
+        ("https://www.sbs.co.kr/live/S01", {"channel": "S01"}),
+        ("https://www.sbs.co.kr/live/S01?div=live_list", {"channel": "S01"}),
+        # "virtual" channels
+        ("https://www.sbs.co.kr/live/S21", {"channel": "S21"}),
+        ("https://www.sbs.co.kr/live/S21?div=live_list", {"channel": "S21"}),
     ]
