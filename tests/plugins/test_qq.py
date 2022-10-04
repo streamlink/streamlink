49f54d886d62628444c2231d96fd35a6681a9b36
bastimeyer <mail@bastimeyer.de>
2022-10-04 09:27:29 +0200

    plugins.qq: rewrite and fix plugin

--- tests/plugins/test_qq.py
+++ tests/plugins/test_qq.py
@@ -5,26 +5,8 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlQQ(PluginCanHandleUrl):
     __plugin__ = QQ
 
-    should_match = [
-        "http://live.qq.com/10003715",
-        "http://live.qq.com/10007266",
-        "http://live.qq.com/10039165",
-        "http://m.live.qq.com/10003715",
-        "http://m.live.qq.com/10007266",
-        "http://m.live.qq.com/10039165"
-    ]
-
     should_match_groups = [
-        ("http://live.qq.com/10003715", {
-            "room_id": "10003715"
-        }),
-        ("http://m.live.qq.com/10039165", {
-            "room_id": "10039165"
-        }),
-    ]
-
-    should_not_match = [
-        "http://live.qq.com/",
-        "http://qq.com/",
-        "http://www.qq.com/"
+        ("https://live.qq.com/", {}),
+        ("https://live.qq.com/123456789", {"room_id": "123456789"}),
+        ("https://m.live.qq.com/123456789", {"room_id": "123456789"}),
     ]
