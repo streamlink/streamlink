52b95c792f3c2daff58e7267c7693759c0e95ac4
bastimeyer <mail@bastimeyer.de>
2022-10-18 14:22:42 +0200

    plugins.tf1: rewrite plugin, add "stream" VODs
    Co-Authored-By: BZHDeveloper <inizan.yannick@gmail.com>

--- src/streamlink/plugins/tf1.py
+++ src/streamlink/plugins/tf1.py
@@ -1,6 +1,7 @@
 """
 $description French live TV channels from TF1 Group, including LCI and TF1.
 $url tf1.fr
+$url tf1info.fr
 $url lci.fr
 $type live
 $region France
@@ -10,57 +11,88 @@ import logging
 import re
 
 from streamlink.plugin import Plugin, PluginError, pluginmatcher
-from streamlink.plugin.api import useragents
-from streamlink.stream.dash import DASHStream
+from streamlink.plugin.api import useragents, validate
 from streamlink.stream.hls import HLSStream
 
 log = logging.getLogger(__name__)
 
 
-@pluginmatcher(re.compile(
-    r"https?://(?:www\.)?(?:tf1\.fr/([\w-]+)/direct|(lci)\.fr/direct)/?"
-))
+@pluginmatcher(re.compile(r"""
+    https?://(?:www\.)?
+    (?:
+        tf1\.fr/(?:
+            (?P<live>[\w-]+)/direct/?
+            |
+            stream/(?P<stream>[\w-]+)
+        )
+        |
+        (?P<lci>tf1info|lci)\.fr/direct/?
+    )
+""", re.VERBOSE))
 class TF1(Plugin):
-    api_url = "https://mediainfo.tf1.fr/mediainfocombo/{}?context=MYTF1&pver=4001000"
+    _URL_API = "https://mediainfo.tf1.fr/mediainfocombo/{channel_id}"
 
-    def api_call(self, channel, useragent=useragents.CHROME):
-        url = self.api_url.format(f"L_{channel.upper()}")
-        req = self.session.http.get(url,
-                                    headers={"User-Agent": useragent})
-        return self.session.http.json(req)
+    def _get_channel(self):
+        if self.match["live"]:
+            channel = self.match["live"]
+            channel_id = f"L_{channel.upper()}"
+        elif self.match["lci"]:
+            channel = "LCI"
+            channel_id = "L_LCI"
+        elif self.match["stream"]:
+            channel = self.match["stream"]
+            channel_id = f"L_FAST_v2l-{channel}"
+        else:  # pragma: no cover
+            raise PluginError("Invalid channel")
 
-    def get_stream_urls(self, channel):
-        for useragent in [useragents.CHROME, useragents.IPHONE_6]:
-            data = self.api_call(channel, useragent)
+        return channel, channel_id
 
-            if 'delivery' not in data or 'url' not in data['delivery']:
-                continue
-
-            log.debug("Got {format} stream {url}".format(**data['delivery']))
-            yield data['delivery']['format'], data['delivery']['url']
+    def _api_call(self, channel_id):
+        return self.session.http.get(
+            self._URL_API.format(channel_id=channel_id),
+            params={
+                "context": "MYTF1",
+                "pver": "4001000",
+            },
+            headers={
+                # forces HLS streams
+                "User-Agent": useragents.IPHONE,
+            },
+            schema=validate.Schema(
+                validate.parse_json(),
+                {
+                    "delivery": validate.any(
+                        validate.all(
+                            {
+                                "code": 200,
+                                "format": "hls",
+                                "url": validate.url(),
+                            },
+                            validate.union_get("code", "url"),
+                        ),
+                        validate.all(
+                            {
+                                "code": int,
+                                "error": str,
+                            },
+                            validate.union_get("code", "error"),
+                        ),
+                    ),
+                },
+                validate.get("delivery"),
+            ),
+        )
 
     def _get_streams(self):
-        m = self.match
-        if m:
-            channel = m.group(1) or m.group(2)
-            log.debug("Found channel {0}".format(channel))
-            for sformat, url in self.get_stream_urls(channel):
-                try:
-                    if sformat == "dash":
-                        yield from DASHStream.parse_manifest(
-                            self.session,
-                            url,
-                            headers={"User-Agent": useragents.CHROME}
-                        ).items()
-                    if sformat == "hls":
-                        yield from HLSStream.parse_variant_playlist(
-                            self.session,
-                            url,
-                            headers={"User-Agent": useragents.IPHONE},
-                        ).items()
-                except PluginError as e:
-                    log.error("Could not open {0} stream".format(sformat))
-                    log.debug("Failed with error: {0}".format(e))
+        channel, channel_id = self._get_channel()
+        log.debug(f"Found channel {channel} ({channel_id})")
+
+        code, data = self._api_call(channel_id)
+        if code != 200:
+            log.error(data)
+            return
+
+        return HLSStream.parse_variant_playlist(self.session, data)
 
 
 __plugin__ = TF1
--- tests/plugins/test_tf1.py
+++ tests/plugins/test_tf1.py
@@ -5,14 +5,19 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlTF1(PluginCanHandleUrl):
     __plugin__ = TF1
 
-    should_match = [
-        "http://tf1.fr/tf1/direct/",
-        "http://tf1.fr/tfx/direct/",
-        "http://tf1.fr/tf1-series-films/direct/",
-        "http://lci.fr/direct",
-        "http://www.lci.fr/direct",
-        "http://tf1.fr/tmc/direct",
-        "http://tf1.fr/lci/direct",
+    should_match_groups = [
+        ("https://tf1.fr/tf1/direct", {"live": "tf1"}),
+        ("https://www.tf1.fr/tf1/direct", {"live": "tf1"}),
+        ("https://www.tf1.fr/tfx/direct", {"live": "tfx"}),
+        ("https://www.tf1.fr/tf1-series-films/direct", {"live": "tf1-series-films"}),
+
+        ("https://lci.fr/direct", {"lci": "lci"}),
+        ("https://www.lci.fr/direct", {"lci": "lci"}),
+        ("https://tf1info.fr/direct/", {"lci": "tf1info"}),
+        ("https://www.tf1info.fr/direct/", {"lci": "tf1info"}),
+
+        ("https://www.tf1.fr/stream/chante-69061019", {"stream": "chante-69061019"}),
+        ("https://www.tf1.fr/stream/arsene-lupin-39652174", {"stream": "arsene-lupin-39652174"}),
     ]
 
     should_not_match = [
