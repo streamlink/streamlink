2a0dab6eab959a31b1bb370c98d4ddbe6407c2c1
bastimeyer <mail@bastimeyer.de>
2022-11-07 20:19:36 +0100

    chore: fix new mypy warnings and errors

--- src/streamlink_cli/main.py
+++ src/streamlink_cli/main.py
@@ -672,7 +672,11 @@ def load_plugins(dirs: List[Path], showwarning: bool = True):
             log.warning(f"Plugin path {directory} does not exist or is not a directory!")
 
 
-def setup_args(parser: argparse.ArgumentParser, config_files: List[Path] = None, ignore_unknown: bool = False):
+def setup_args(
+    parser: argparse.ArgumentParser,
+    config_files: Optional[List[Path]] = None,
+    ignore_unknown: bool = False,
+):
     """Parses arguments."""
     global args
     arglist = sys.argv[1:]
