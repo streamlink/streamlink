55673f3d1823d70e65aa5605867a2ae89496707e
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2022-11-09 00:48:22 +0800

    plugins.niconicochannelplus: use double quotes for strings

--- src/streamlink/plugins/niconicochannelplus.py
+++ src/streamlink/plugins/niconicochannelplus.py
@@ -17,67 +17,67 @@ log = logging.getLogger(__name__)
 
 @pluginmatcher(
     re.compile(
-        r'^https?://nicochannel\.jp/(?P<channel>[a-z0-9_-]+)/(?:video|live)/(?P<id>sm[a-zA-Z0-9]+)$'
+        r"^https?://nicochannel\.jp/(?P<channel>[a-z0-9_-]+)/(?:video|live)/(?P<id>sm[a-zA-Z0-9]+)$"
     )
 )
 class NicoNicoChannelPlus(Plugin):
-    _URL_API_VIDEO_PAGE_INFO = 'https://nfc-api.nicochannel.jp/fc/video_pages/{video_id}'
-    _URL_API_SESSION_ID = 'https://nfc-api.nicochannel.jp/fc/video_pages/{video_id}/session_ids'
-    _URL_API_MASTER_PLAYLIST = 'https://hls-auth.cloud.stream.co.jp/auth/index.m3u8?session_id={session_id}'
+    _URL_API_VIDEO_PAGE_INFO = "https://nfc-api.nicochannel.jp/fc/video_pages/{video_id}"
+    _URL_API_SESSION_ID = "https://nfc-api.nicochannel.jp/fc/video_pages/{video_id}/session_ids"
+    _URL_API_MASTER_PLAYLIST = "https://hls-auth.cloud.stream.co.jp/auth/index.m3u8?session_id={session_id}"
 
     def get_video_page_info(self, video_id: str) -> dict:
         return self.session.http.get(
             self._URL_API_VIDEO_PAGE_INFO.format(video_id=video_id),
             schema=validate.Schema(
                 validate.parse_json(),
-                {'data': {'video_page': {
-                    'title': str,
-                    'type': str,
-                    'live_started_at': validate.any(str, None),
-                    'live_finished_at': validate.any(str, None),
+                {"data": {"video_page": {
+                    "title": str,
+                    "type": str,
+                    "live_started_at": validate.any(str, None),
+                    "live_finished_at": validate.any(str, None),
                 }}},
-                validate.get(('data', 'video_page')),
+                validate.get(("data", "video_page")),
             ),
         )
 
     def get_master_playlist_url(self, video_id: str, video_info: dict) -> str:
-        video_type = video_info['type']
+        video_type = video_info["type"]
 
-        if video_type == 'vod':
+        if video_type == "vod":
             payload: dict = {}
-        elif video_type == 'live':
-            if not video_info['live_started_at']:
-                raise PluginError('Live is not started yet.')
+        elif video_type == "live":
+            if not video_info["live_started_at"]:
+                raise PluginError("Live is not started yet.")
 
-            if not video_info['live_finished_at']:
+            if not video_info["live_finished_at"]:
                 # Live is available.
                 payload = {}
             else:
                 # Live is already ended, try DVR.
-                payload = {'broadcast_type': 'dvr'}
+                payload = {"broadcast_type": "dvr"}
         else:
-            raise PluginError(f'Unknown video type: {video_type}')
+            raise PluginError(f"Unknown video type: {video_type}")
 
         session_id = self.session.http.post(
             self._URL_API_SESSION_ID.format(video_id=video_id),
             json=payload,
             schema=validate.Schema(
                 validate.parse_json(),
-                {'data': {'session_id': str}},
-                validate.get(('data', 'session_id')),
+                {"data": {"session_id": str}},
+                validate.get(("data", "session_id")),
             ),
         )
 
         return self._URL_API_MASTER_PLAYLIST.format(session_id=session_id)
 
     def _get_streams(self):
-        self.id = self.match.group('id')
-        self.author = self.match.group('channel')
+        self.id = self.match.group("id")
+        self.author = self.match.group("channel")
 
         video_info = self.get_video_page_info(self.id)
         playlist_url = self.get_master_playlist_url(self.id, video_info)
 
-        self.title = video_info['title']
+        self.title = video_info["title"]
 
         return HLSStream.parse_variant_playlist(self.session, playlist_url)
 
--- tests/plugins/test_niconicochannelplus.py
+++ tests/plugins/test_niconicochannelplus.py
@@ -8,55 +8,55 @@ class TestPluginCanHandleUrlNicoNicoChannelPlus(PluginCanHandleUrl):
     should_match = [
         # video urls
         #   normal channel name.
-        'https://nicochannel.jp/olchannel/video/smq9UriUQ9PU65jTjVxh2PVo',
+        "https://nicochannel.jp/olchannel/video/smq9UriUQ9PU65jTjVxh2PVo",
         #   numbers in channel name.
-        'https://nicochannel.jp/dateno8noba/video/smVGqtKpdmva4Mcrw7rbeQ8Y',
+        "https://nicochannel.jp/dateno8noba/video/smVGqtKpdmva4Mcrw7rbeQ8Y",
         #   hyphens in channel name.
-        'https://nicochannel.jp/mimimoto-ayaka/video/smRS96YpmDJr24YpWFZziG4L',
+        "https://nicochannel.jp/mimimoto-ayaka/video/smRS96YpmDJr24YpWFZziG4L",
         #   underscores in channel name.
-        'https://nicochannel.jp/sakaguchi_kugimiya/video/smnSFttxbkjZDBwZMMj8CgsJ',
+        "https://nicochannel.jp/sakaguchi_kugimiya/video/smnSFttxbkjZDBwZMMj8CgsJ",
 
         # live urls
         #   normal channel name.
-        'https://nicochannel.jp/example/live/sm3Xample',
+        "https://nicochannel.jp/example/live/sm3Xample",
     ]
 
     should_match_groups = [
         # video urls
         (
-            'https://nicochannel.jp/olchannel/video/smq9UriUQ9PU65jTjVxh2PVo',
+            "https://nicochannel.jp/olchannel/video/smq9UriUQ9PU65jTjVxh2PVo",
             {
-                'id': 'smq9UriUQ9PU65jTjVxh2PVo',
-                'channel': 'olchannel',
+                "id": "smq9UriUQ9PU65jTjVxh2PVo",
+                "channel": "olchannel",
             }
         ),
         (
-            'https://nicochannel.jp/dateno8noba/video/smVGqtKpdmva4Mcrw7rbeQ8Y',
+            "https://nicochannel.jp/dateno8noba/video/smVGqtKpdmva4Mcrw7rbeQ8Y",
             {
-                'id': 'smVGqtKpdmva4Mcrw7rbeQ8Y',
-                'channel': 'dateno8noba',
+                "id": "smVGqtKpdmva4Mcrw7rbeQ8Y",
+                "channel": "dateno8noba",
             }
         ),
         (
-            'https://nicochannel.jp/mimimoto-ayaka/video/smRS96YpmDJr24YpWFZziG4L',
+            "https://nicochannel.jp/mimimoto-ayaka/video/smRS96YpmDJr24YpWFZziG4L",
             {
-                'id': 'smRS96YpmDJr24YpWFZziG4L',
-                'channel': 'mimimoto-ayaka',
+                "id": "smRS96YpmDJr24YpWFZziG4L",
+                "channel": "mimimoto-ayaka",
             }
         ),
         (
-            'https://nicochannel.jp/sakaguchi_kugimiya/video/smnSFttxbkjZDBwZMMj8CgsJ',
+            "https://nicochannel.jp/sakaguchi_kugimiya/video/smnSFttxbkjZDBwZMMj8CgsJ",
             {
-                'id': 'smnSFttxbkjZDBwZMMj8CgsJ',
-                'channel': 'sakaguchi_kugimiya',
+                "id": "smnSFttxbkjZDBwZMMj8CgsJ",
+                "channel": "sakaguchi_kugimiya",
             }
         ),
         # live urls
         (
-            'https://nicochannel.jp/example/live/sm3Xample',
+            "https://nicochannel.jp/example/live/sm3Xample",
             {
-                'id': 'sm3Xample',
-                'channel': 'example',
+                "id": "sm3Xample",
+                "channel": "example",
             }
         ),
     ]
@@ -64,22 +64,22 @@ class TestPluginCanHandleUrlNicoNicoChannelPlus(PluginCanHandleUrl):
     should_not_match = [
         # Niconico Channel Plus
         #   portal
-        'https://portal.nicochannel.jp/',
+        "https://portal.nicochannel.jp/",
         #   channel home pages
-        'https://nicochannel.jp/example',
+        "https://nicochannel.jp/example",
         #   channel live list
-        'https://nicochannel.jp/example/lives',
+        "https://nicochannel.jp/example/lives",
         #   channel video list
-        'https://nicochannel.jp/example/videos',
+        "https://nicochannel.jp/example/videos",
         #   invalid urls
-        'https://nicochannel.jp/'
-        'https://nicochannel.jp/example/live',
-        'https://nicochannel.jp/example/video',
+        "https://nicochannel.jp/"
+        "https://nicochannel.jp/example/live",
+        "https://nicochannel.jp/example/video",
 
         # Niconico Channel
         #   portal
-        'https://ch.nicovideo.jp/',
+        "https://ch.nicovideo.jp/",
         #   channel home pages
-        'https://ch.nicovideo.jp/ch0000000',
-        'https://ch.nicovideo.jp/example',
+        "https://ch.nicovideo.jp/ch0000000",
+        "https://ch.nicovideo.jp/example",
     ]
