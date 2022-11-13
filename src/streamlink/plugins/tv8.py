925847e24d741bf4a76be51e85faedf0af3f45ca
fenopa <62562166+fenopa@users.noreply.github.com>
2022-11-14 00:34:28 +0300

    Update tv8.py

--- src/streamlink/plugins/tv8.py
+++ src/streamlink/plugins/tv8.py
@@ -16,17 +16,18 @@ log = logging.getLogger(__name__)
 
 class TV8HLSStreamWriter(HLSStreamWriter):
     ad_re = re.compile(r"/ad/|/crea/")
-    
+
     def should_filter_sequence(self, sequence):
         return self.ad_re.search(sequence.segment.uri) is not None or super().should_filter_sequence(sequence)
 
-    
+
 class TV8HLSStreamReader(HLSStreamReader):
     __writer__ = TV8HLSStreamWriter
 
 
 class TV8HLSStream(HLSStream):
     __reader__ = TV8HLSStreamReader
+
 
 @pluginmatcher(re.compile(
     r"https?://www\.tv8\.com\.tr/canli-yayin"
