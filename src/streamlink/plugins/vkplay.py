708aa5ec3cc6fa2ad831abe39ccfe32916380f0e
bastimeyer <mail@bastimeyer.de>
2022-12-29 19:11:33 +0100

    plugins.vkplay: clean up

--- src/streamlink/plugins/vkplay.py
+++ src/streamlink/plugins/vkplay.py
@@ -1,5 +1,5 @@
 """
-$description Russian live-streaming platform.
+$description Russian live-streaming platform for gaming and esports, owned by VKontakte.
 $url vkplay.live
 $type live
 """
@@ -7,8 +7,7 @@ $type live
 import logging
 import re
 
-from streamlink.exceptions import NoStreamsError
-from streamlink.plugin import Plugin, PluginError, pluginmatcher
+from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
 
@@ -16,40 +15,75 @@ log = logging.getLogger(__name__)
 
 
 @pluginmatcher(re.compile(
-    r"https?://vkplay\.live/(?P<channel_name>[a-zA-Z0-9_]+)/?$"
+    r"https?://vkplay\.live/(?P<channel_name>\w+)/?$",
 ))
 class VKplay(Plugin):
     API_URL = "https://api.vkplay.live/v1"
 
     def _get_streams(self):
-        channel_name = self.match.group("channel_name")
-        if not channel_name:
+        self.author = self.match.group("channel_name")
+        log.debug(f"Channel name: {self.author}")
+
+        data = self.session.http.get(
+            f"{self.API_URL}/blog/{self.author}/public_video_stream",
+            headers={"Referer": self.url},
+            acceptable_status=(200, 404),
+            schema=validate.Schema(
+                validate.parse_json(),
+                validate.any(
+                    validate.all(
+                        {"error": str, "error_description": str},
+                        validate.get("error_description"),
+                    ),
+                    validate.all(
+                        {
+                            "category": {
+                                "title": str,
+                            },
+                            "title": str,
+                            "data": validate.any(
+                                [
+                                    validate.all(
+                                        {
+                                            "vid": str,
+                                            "playerUrls": [
+                                                validate.all(
+                                                    {
+                                                        "type": str,
+                                                        "url": validate.any("", validate.url()),
+                                                    },
+                                                    validate.union_get("type", "url"),
+                                                ),
+                                            ],
+                                        },
+                                        validate.union_get("vid", "playerUrls"),
+                                    ),
+                                ],
+                                [],
+                            ),
+                        },
+                        validate.union_get(
+                            ("category", "title"),
+                            "title",
+                            ("data", 0),
+                        ),
+                    ),
+                ),
+            ),
+        )
+        if type(data) is str:
+            log.error(data)
             return
 
-        log.debug(f"Channel name: {channel_name}")
-        try:
-            res = self.session.http.get(
-                f"{self.API_URL}/blog/{channel_name}/public_video_stream",
-                headers={"Referer": self.url},
-                schema=validate.Schema(
-                    validate.parse_json(),
-                    {
-                        "data":
-                            [{
-                                "playerUrls": validate.all(
-                                    [{
-                                        "url": validate.any(validate.url(), ""),
-                                        "type": validate.any("live_hls", validate.text)
-                                    }],
-                                    validate.filter(lambda item: item["type"] == "live_hls")),
+        self.category, self.title, streamdata = data
+        if not streamdata:
+            return
 
-                            }]
+        self.id, streams = streamdata
 
-                    }, validate.get("data"), validate.get(0), validate.get("playerUrls"), validate.get(0), validate.get("url"))
-            )
-        except PluginError:
-            raise NoStreamsError(self.url)
-        yield from HLSStream.parse_variant_playlist(self.session, res).items()
+        for streamtype, streamurl in streams:
+            if streamurl and streamtype == "live_hls":
+                return HLSStream.parse_variant_playlist(self.session, streamurl)
 
 
 __plugin__ = VKplay
--- tests/plugins/test_vkplay.py
+++ tests/plugins/test_vkplay.py
@@ -6,15 +6,12 @@ class TestPluginCanHandleUrlVKplay(PluginCanHandleUrl):
     __plugin__ = VKplay
 
     should_match = [
-        'https://vkplay.live/dsadasllda',
-        'https://vkplay.live/dsadas231',
-        'https://vkplay.live/p4ds_flob3k',
-        'https://vkplay.live/901237691',
+        "https://vkplay.live/Channel_Name_123",
     ]
 
     should_not_match = [
-        'https://vkplay.live/',
-        'https://support.vkplay.ru/vkp_live',
-        'https://vkplay.live/app/settings/edit',
-        'https://vkplay.live/app/settings/external-apps'
+        "https://vkplay.live/",
+        "https://support.vkplay.ru/vkp_live",
+        "https://vkplay.live/app/settings/edit",
+        "https://vkplay.live/app/settings/external-apps",
     ]
