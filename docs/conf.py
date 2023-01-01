578467b131feddf41bf19d5987fa50799ab7cffa
bastimeyer <mail@bastimeyer.de>
2023-01-01 00:00:00 +0000

    2023

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Streamlink'
-copyright = '2022, Streamlink'
+copyright = '2023, Streamlink'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
