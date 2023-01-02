cfa5575031e6ccf8b2e68ae3a02c0f91598507a4
bastimeyer <mail@bastimeyer.de>
2023-01-02 17:53:05 +0100

    plugins.euronews: fix and rewrite plugin

--- src/streamlink/plugins/euronews.py
+++ src/streamlink/plugins/euronews.py
@@ -4,73 +4,110 @@ $url euronews.com
 $type live
 """
 
+import logging
 import re
-from urllib.parse import urlparse
 
-from streamlink.plugin import Plugin, pluginmatcher
+from streamlink.plugin import Plugin, PluginError, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
 from streamlink.stream.http import HTTPStream
-from streamlink.utils.url import update_scheme
+
+
+log = logging.getLogger(__name__)
 
 
 @pluginmatcher(re.compile(
-    r'https?://(?:(?P<subdomain>\w+)\.)?euronews\.com/'
+    r"https?://(?:(?P<subdomain>\w+)\.)?euronews\.com/(?P<live>live$)?"
 ))
 class Euronews(Plugin):
-    API_URL = "https://{subdomain}.euronews.com/api/watchlive.json"
+    API_URL = "https://{subdomain}.euronews.com/api/live/data"
 
-    def _get_vod_stream(self):
-        root = self.session.http.get(self.url, schema=validate.Schema(
-            validate.parse_html()
-        ))
+    def _get_live(self):
+        if not self.match["live"] or not self.match["subdomain"]:
+            return
 
-        video_url = root.xpath("string(.//meta[@property='og:video'][1]/@content)")
-        if video_url:
-            return dict(vod=HTTPStream(self.session, video_url))
+        try:
+            log.debug("Querying live API")
+            stream_url = self.session.http.get(
+                self.API_URL.format(subdomain=self.match["subdomain"]),
+                params={"locale": self.match["subdomain"]},
+                schema=validate.Schema(
+                    validate.parse_json(),
+                    {"videoPrimaryUrl": validate.url(path=validate.endswith(".m3u8"))},
+                    validate.get("videoPrimaryUrl"),
+                ),
+            )
+        except PluginError:
+            pass
+        else:
+            return HLSStream.parse_variant_playlist(self.session, stream_url)
 
-        video_id = root.xpath("string(.//div[@data-google-src]/@data-video-id)")
-        if video_id:
-            return self.session.streams(f"https://www.youtube.com/watch?v={video_id}")
-
-        video_url = root.xpath("string(.//iframe[@id='pfpPlayer'][starts-with(@src,'https://www.youtube.com/')][1]/@src)")
-        if video_url:
-            return self.session.streams(video_url)
-
-    def _get_live_streams(self):
-        video_id = self.session.http.get(self.url, schema=validate.Schema(
-            validate.parse_html(),
-            validate.xml_xpath_string(".//div[@data-google-src]/@data-video-id")
-        ))
-
-        if video_id:
-            return self.session.streams(f"https://www.youtube.com/watch?v={video_id}")
-
-        info_url = self.session.http.get(self.API_URL.format(subdomain=self.match.group("subdomain")), schema=validate.Schema(
-            validate.parse_json(),
-            {"url": validate.url()},
-            validate.get("url"),
-            validate.transform(lambda url: update_scheme("https://", url))
-        ))
-        hls_url = self.session.http.get(info_url, schema=validate.Schema(
+    def _get_embed(self, root):
+        schema_video_id = validate.Schema(
+            validate.xml_xpath_string(".//div[@data-video][1]/@data-video"),
+            str,
             validate.parse_json(),
             {
-                "status": "ok",
-                "protocol": "hls",
-                "primary": validate.url()
+                "player": "pfp",
+                "videoId": str,
             },
-            validate.get("primary")
-        ))
+            validate.get("videoId"),
+        )
+        try:
+            log.debug("Looking for YouTube video ID")
+            video_id = schema_video_id.validate(root)
+        except PluginError:
+            pass
+        else:
+            return self.session.streams(f"https://www.youtube.com/watch?v={video_id}")
 
-        return HLSStream.parse_variant_playlist(self.session, hls_url)
+        schema_video_url = validate.Schema(
+            validate.xml_xpath_string(".//iframe[@id='pfpPlayer'][starts-with(@src,'https://www.youtube.com/')][1]/@src"),
+            str,
+        )
+        try:
+            log.debug("Looking for embedded YouTube iframe")
+            video_url = schema_video_url.validate(root)
+        except PluginError:
+            pass
+        else:
+            return self.session.streams(video_url)
+
+    def _get_vod(self, root):
+        schema_vod = validate.Schema(
+            validate.any(
+                validate.all(
+                    validate.xml_xpath_string(".//meta[@property='og:video'][1]/@content"),
+                    str,
+                ),
+                validate.all(
+                    validate.xml_xpath_string(".//div[@data-video][1]/@data-video"),
+                    str,
+                    validate.parse_json(),
+                    {"url": str},
+                    validate.get("url"),
+                ),
+            ),
+            validate.url(),
+        )
+        try:
+            log.debug("Looking for VOD URL")
+            video_url = schema_vod.validate(root)
+        except PluginError:
+            pass
+        else:
+            return dict(vod=HTTPStream(self.session, video_url))
 
     def _get_streams(self):
-        parsed = urlparse(self.url)
+        live = self._get_live()
+        if live:
+            return live
 
-        if parsed.path == "/live":
-            return self._get_live_streams()
-        else:
-            return self._get_vod_stream()
+        root = self.session.http.get(self.url, schema=validate.Schema(
+            validate.parse_html(),
+        ))
+
+        return self._get_embed(root) or self._get_vod(root)
 
 
 __plugin__ = Euronews
