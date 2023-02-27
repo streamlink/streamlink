344821203bf9ef6ed7147f54433c7794e81d890a
bastimeyer <mail@bastimeyer.de>
2023-02-27 01:04:31 +0100

    plugins.tv5monde: rewrite and fix plugin

--- src/streamlink/plugins/tv5monde.py
+++ src/streamlink/plugins/tv5monde.py
@@ -3,80 +3,93 @@ $description Live TV channel and video on-demand service from TV5Monde, a French
 $url tv5monde.com
 $url tivi5mondeplus.com
 $type live, vod
-$region France, Belgium, Switzerland
+$region France, Belgium, Switzerland, Africa
 """
 
 import re
-from urllib.parse import urlparse
+from urllib.parse import quote
 
-from streamlink.plugin import Plugin, PluginError, pluginmatcher
+from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
+from streamlink.stream.dash import DASHStream
 from streamlink.stream.hls import HLSStream
 from streamlink.stream.http import HTTPStream
 from streamlink.utils.url import update_scheme
 
 
-@pluginmatcher(re.compile(r"""
-    https?://(?:[\w-]+\.)*(?:tv5monde|tivi5mondeplus)\.com/
-""", re.VERBOSE))
+@pluginmatcher(re.compile(
+    r"https?://(?:[\w-]+\.)*(?:tv5monde|tivi5mondeplus)\.com/",
+))
 class TV5Monde(Plugin):
-    def _get_hls(self, root):
-        schema_live = validate.Schema(
-            validate.xml_xpath_string(".//*[contains(@data-broadcast,'m3u8')]/@data-broadcast"),
-            str,
-            validate.parse_json(),
-            validate.any(
-                validate.all({"files": list}, validate.get("files")),
-                list,
+    _URL_API = "https://api.tv5monde.com/player/asset/{asset}/resolve"
+
+    _MAP_STREAMTYPES = {
+        "application/x-mpegURL": HLSStream.parse_variant_playlist,
+        "application/dash+xml": DASHStream.parse_manifest,
+        "video/mp4": lambda *args: {"vod": HTTPStream(*args)},
+    }
+
+    def _find_streams(self, data):
+        schema_streams = validate.Schema([
+            validate.all(
+                {
+                    "type": str,
+                    "url": validate.url(),
+                },
+                validate.union_get("type", "url"),
             ),
+        ])
+        streams = schema_streams.validate(data)
+
+        for mimetype, streamfactory in self._MAP_STREAMTYPES.items():
+            for streamtype, streamurl in streams:
+                if streamtype == mimetype:
+                    url = update_scheme("https://", streamurl)
+                    return streamfactory(self.session, url)
+
+    def _get_playlist(self, data):
+        schema_playlist = validate.Schema(
+            validate.parse_json(),
+            [{"sources": list}],
+            validate.get((0, "sources")),
+        )
+        playlist = schema_playlist.validate(data)
+
+        return self._find_streams(playlist)
+
+    def _get_broadcast(self, data):
+        schema_broadcast = validate.Schema(
+            validate.parse_json(),
             [{
-                "url": validate.url(path=validate.endswith(".m3u8")),
+                "type": str,
+                "url": str,
             }],
-            validate.get((0, "url")),
-            validate.transform(lambda content_url: update_scheme("https://", content_url)),
         )
-        try:
-            live = schema_live.validate(root)
-        except PluginError:
-            return
+        broadcast = schema_broadcast.validate(data)
 
-        return HLSStream.parse_variant_playlist(self.session, live)
+        deferred = next((item["url"] for item in broadcast if item["type"] == "application/deferred"), None)
+        if deferred:
+            url = self._URL_API.format(asset=quote(deferred))
+            broadcast = self.session.http.get(url, schema=validate.Schema(
+                validate.parse_json(),
+            ))
 
-    def _get_vod(self, root):
-        schema_vod = validate.Schema(
-            validate.xml_xpath_string(".//script[@type='application/ld+json'][contains(text(),'VideoObject')][1]/text()"),
-            str,
-            validate.transform(lambda jsonlike: re.sub(r"[\r\n]+", "", jsonlike)),
-            validate.parse_json(),
-            validate.any(
-                validate.all(
-                    {"@graph": [dict]},
-                    validate.get("@graph"),
-                    validate.filter(lambda obj: obj["@type"] == "VideoObject"),
-                    validate.get(0),
-                ),
-                dict,
-            ),
-            {"contentUrl": validate.url()},
-            validate.get("contentUrl"),
-            validate.transform(lambda content_url: update_scheme("https://", content_url)),
-        )
-        try:
-            vod = schema_vod.validate(root)
-        except PluginError:
-            return
-
-        if urlparse(vod).path.endswith(".m3u8"):
-            return HLSStream.parse_variant_playlist(self.session, vod)
-
-        return {"vod": HTTPStream(self.session, vod)}
+        return self._find_streams(broadcast)
 
     def _get_streams(self):
-        root = self.session.http.get(self.url, schema=validate.Schema(
+        playlist, broadcast = self.session.http.get(self.url, schema=validate.Schema(
             validate.parse_html(),
+            validate.union((
+                validate.xml_xpath_string(".//*[@data-playlist][1]/@data-playlist"),
+                validate.xml_xpath_string(".//*[@data-broadcast][1]/@data-broadcast"),
+            )),
         ))
 
-        return self._get_hls(root) or self._get_vod(root)
+        # data-playlist needs higher priority (data-broadcast attribute exists on the same DOM node)
+        if playlist:
+            return self._get_playlist(playlist)
+        if broadcast:
+            return self._get_broadcast(broadcast)
 
 
 __plugin__ = TV5Monde
