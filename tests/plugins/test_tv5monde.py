8c0113653749b7640c8a30789b7441e0c36ec465
bastimeyer <mail@bastimeyer.de>
2023-02-27 01:04:31 +0100

    plugins.tv5monde: rewrite and fix plugin

--- tests/plugins/test_tv5monde.py
+++ tests/plugins/test_tv5monde.py
@@ -6,11 +6,11 @@ class TestPluginCanHandleUrlTV5Monde(PluginCanHandleUrl):
     __plugin__ = TV5Monde
 
     should_match = [
-        "http://live.tv5monde.com/fbs.html",
-        "https://www.tv5monde.com/emissions/episode/version-francaise-vf-83",
-        "https://revoir.tv5monde.com/toutes-les-videos/cinema/je-ne-reve-que-de-vous",
-        "https://revoir.tv5monde.com/toutes-les-videos/documentaires/des-russes-blancs-des-russes-blancs",
+        "https://live.tv5monde.com/fbs.html",
+        "https://europe.tv5monde.com/fr/direct",
+        "https://maghreb-orient.tv5monde.com/fr/direct",
+        "https://revoir.tv5monde.com/toutes-les-videos/documentaires/sauver-notre-dame-sauver-notre-dame-14-04-2020",
         "https://information.tv5monde.com/video/la-diplomatie-francaise-est-elle-en-crise",
-        "https://afrique.tv5monde.com/videos/exclusivites-web/les-tutos-de-magloire/season-1/episode-1",
+        "https://afrique.tv5monde.com/videos/afrykas-et-la-boite-magique",
         "https://www.tivi5mondeplus.com/conte-nous/episode-25",
     ]
