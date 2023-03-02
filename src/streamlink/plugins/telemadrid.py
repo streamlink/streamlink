1b9ce004545253ad38c27f61c341f16ea556b2f4
Pablo Hoffman <pablohoffman@gmail.com>
2023-03-02 19:16:39 +0100

    plugins.telemadrid: new plugin (#5212)

---
+++ src/streamlink/plugins/telemadrid.py
@@ -0,0 +1,31 @@
+"""
+$description Spanish live TV channel for Telemadrid, a public regional television station.
+$url telemadrid.es
+$type live, vod
+$region Spain
+"""
+
+import re
+
+from streamlink.plugin import Plugin, pluginmatcher
+from streamlink.plugin.api import validate
+from streamlink.plugins.brightcove import BrightcovePlayer
+
+
+@pluginmatcher(re.compile(
+    r"https?://(?:www\.)?telemadrid\.es/",
+))
+class Telemadrid(Plugin):
+
+    def _get_streams(self):
+        data = self.session.http.get(self.url, schema=validate.Schema(
+            validate.parse_html(),
+            validate.xml_find(".//video[@class='video-js'][@data-video-id][@data-account][@data-player][1]"),
+            validate.union_get("data-video-id", "data-account", "data-player"),
+        ))
+        data_video_id, data_account, data_player = data
+        player = BrightcovePlayer(self.session, data_account, f"{data_player}_default")
+        return player.get_streams(data_video_id)
+
+
+__plugin__ = Telemadrid
