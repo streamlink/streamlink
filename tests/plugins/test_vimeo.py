bece17f5a01ecf976977e9edc9de5b6763b468f8
Pascal Romahn <pascal.romahn@mailbox.org>
2023-05-07 16:48:00 +0200

    Replace dead test URLs

--- tests/plugins/test_vimeo.py
+++ tests/plugins/test_vimeo.py
@@ -6,10 +6,10 @@ class TestPluginCanHandleUrlVimeo(PluginCanHandleUrl):
     __plugin__ = Vimeo
 
     should_match = [
-        "https://vimeo.com/237163735",
+        "https://vimeo.com/783455878",
         "https://vimeo.com/channels/music/176894130",
         "https://vimeo.com/album/3706071/video/148903960",
-        "https://vimeo.com/ondemand/surveyopenspace/92630739",
+        "https://vimeo.com/ondemand/worldoftomorrow3/467204924",
         "https://vimeo.com/ondemand/100footsurfingdays",
         "https://player.vimeo.com/video/176894130",
         "https://vimeo.com/771745400/840d05200c",
