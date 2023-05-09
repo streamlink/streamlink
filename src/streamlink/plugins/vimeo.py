25caa3585ec6ad5140bd0a3483a9c9367cb9601b
bastimeyer <mail@bastimeyer.de>
2023-05-09 15:06:08 +0200

    plugins.vimeo: refactor plugin
    - Split pluginmatcher into two
    - Rewrite validation schemas
    - Move data retrieval into separate methods
    - Always query API on non-player URLs and don't try to find embedded
      config data first
    - Add stream metadata
    - Clean up stream collection and only include streams from at most
      one CDN in order to avoid unnecessary duplicates

--- src/streamlink/plugins/vimeo.py
+++ src/streamlink/plugins/vimeo.py
@@ -7,7 +7,6 @@ $notes Password protected streams are not supported
 
 import logging
 import re
-from html import unescape as html_unescape
 from urllib.parse import urljoin, urlparse
 
 from streamlink.plugin import Plugin, pluginmatcher
@@ -22,49 +21,101 @@ from streamlink.utils.url import update_scheme
 log = logging.getLogger(__name__)
 
 
-@pluginmatcher(re.compile(
-    r"https?://(player\.vimeo\.com/video/\d+|(www\.)?vimeo\.com/.+)",
-))
+@pluginmatcher(
+    name="default",
+    pattern=re.compile(r"https?://(?:www\.)?vimeo\.com/.+"),
+)
+@pluginmatcher(
+    name="player",
+    pattern=re.compile(r"https?://player\.vimeo\.com/video/\d+"),
+)
 class Vimeo(Plugin):
     VIEWER_URL = "https://vimeo.com/_next/viewer"
     OEMBED_URL = "https://vimeo.com/api/oembed.json"
-    _config_url_re = re.compile(r'(?:"config_url"|\bdata-config-url)\s*[:=]\s*(".+?")')
-    _config_re = re.compile(r"playerConfig\s*=\s*({.+?})\s*var")
-    _config_url_schema = validate.Schema(
-        validate.transform(_config_url_re.search),
-        validate.any(
-            None,
-            validate.Schema(
-                validate.get(1),
-                validate.parse_json(),
-                validate.transform(html_unescape),
-                validate.url(),
-            ),
-        ),
-    )
-    _config_schema = validate.Schema(
-        validate.parse_json(),
-        {
-            "request": {
-                "files": {
-                    validate.optional("dash"): {"cdns": {str: {"url": validate.url()}}},
-                    validate.optional("hls"): {"cdns": {str: {"url": validate.url()}}},
-                    validate.optional("progressive"): validate.all(
-                        [{"url": validate.url(), "quality": str}],
+
+    @staticmethod
+    def _schema_config(config):
+        schema_cdns = validate.all(
+            {
+                "cdns": {
+                    str: validate.all(
+                        {"url": validate.url()},
+                        validate.get("url"),
                     ),
                 },
-                validate.optional("text_tracks"): validate.all(
-                    [{"url": str, "lang": str}],
+            },
+            validate.get("cdns"),
+        )
+        schema_config = validate.Schema(
+            {
+                "request": {
+                    "files": {
+                        validate.optional("hls"): schema_cdns,
+                        validate.optional("dash"): schema_cdns,
+                        validate.optional("progressive"): [
+                            validate.all(
+                                {
+                                    "url": validate.url(),
+                                    "quality": str,
+                                },
+                                validate.union_get("quality", "url"),
+                            ),
+                        ],
+                    },
+                    validate.optional("text_tracks"): [
+                        validate.all(
+                            {
+                                "url": str,
+                                "lang": str,
+                            },
+                            validate.union_get("lang", "url"),
+                        ),
+                    ],
+                },
+                validate.optional("video"): validate.none_or_all(
+                    {
+                        "id": int,
+                        "title": str,
+                        "owner": {
+                            "name": str,
+                        },
+                    },
+                    validate.union_get(
+                        "id",
+                        ("owner", "name"),
+                        "title",
+                    ),
                 ),
             },
-        },
-    )
-    _player_schema = validate.Schema(
-        validate.transform(_config_re.search),
-        validate.any(None, validate.Schema(validate.get(1), _config_schema)),
-    )
+            validate.union_get(
+                ("request", "files", "hls"),
+                ("request", "files", "dash"),
+                ("request", "files", "progressive"),
+                ("request", "text_tracks"),
+                "video",
+            ),
+        )
 
-    def get_config_url(self) -> str:
+        return schema_config.validate(config)
+
+    def _get_dash_url(self, url):
+        return self.session.http.get(url, schema=validate.Schema(
+            validate.parse_json(),
+            {"url": validate.url()},
+            validate.get("url"),
+        ))
+
+    def _query_player(self):
+        return self.session.http.get(self.url, schema=validate.Schema(
+            re.compile(r"playerConfig\s*=\s*({.+?})\s*var"),
+            validate.none_or_all(
+                validate.get(1),
+                validate.parse_json(),
+                validate.transform(self._schema_config),
+            ),
+        ))
+
+    def _query_api(self):
         jwt, api_url = self.session.http.get(
             self.VIEWER_URL,
             schema=validate.Schema(
@@ -86,8 +137,7 @@ class Vimeo(Plugin):
             ),
         )
         player_config_url = urljoin(update_scheme("https://", api_url), uri)
-
-        return self.session.http.get(
+        config_url = self.session.http.get(
             player_config_url,
             params={"fields": "config_url"},
             headers={"Authorization": f"jwt {jwt}"},
@@ -98,54 +148,54 @@ class Vimeo(Plugin):
             ),
         )
 
+        return self.session.http.get(config_url, schema=validate.Schema(
+            validate.parse_json(),
+            validate.transform(self._schema_config),
+        ))
+
     def _get_streams(self):
-        if "player.vimeo.com" in self.url:
-            data = self.session.http.get(self.url, schema=self._player_schema)
+        if self.matches["player"]:
+            data = self._query_player()
         else:
-            api_url = self.session.http.get(self.url, schema=self._config_url_schema)
+            data = self._query_api()
 
-            if not api_url:
-                api_url = self.get_config_url()
+        if not data:
+            return
 
-            if not api_url:
-                return
-            data = self.session.http.get(api_url, schema=self._config_schema)
+        hls, dash, progressive, text_tracks, metadata = data
+        if metadata:
+            self.id, self.author, self.title = metadata
 
-        videos = data["request"]["files"]
         streams = []
 
-        for stream_type in ("hls", "dash"):
-            if stream_type not in videos:
+        for url in hls.values():
+            streams.extend(HLSStream.parse_variant_playlist(self.session, url).items())
+            break
+
+        for url in dash.values():
+            p = urlparse(url)
+            if p.path.endswith("dash.mpd"):
+                # LIVE
+                url = self._get_dash_url(url)
+            elif p.path.endswith("master.json"):
+                # VOD
+                url = url.replace("master.json", "master.mpd")
+            else:
+                log.error(f"Unsupported DASH path: {p.path}")
                 continue
-            for _, video_data in videos[stream_type]["cdns"].items():
-                log.trace("{0!r}".format(video_data))
-                url = video_data.get("url")
-                if stream_type == "hls":
-                    streams.extend(HLSStream.parse_variant_playlist(self.session, url).items())
 
-                elif stream_type == "dash":
-                    p = urlparse(url)
-                    if p.path.endswith("dash.mpd"):
-                        # LIVE
-                        url = self.session.http.get(url).json()["url"]
-                    elif p.path.endswith("master.json"):
-                        # VOD
-                        url = url.replace("master.json", "master.mpd")
-                    else:
-                        log.error("Unsupported DASH path: {0}".format(p.path))
-                        continue
-
-                    streams.extend(DASHStream.parse_manifest(self.session, url).items())
+            streams.extend(DASHStream.parse_manifest(self.session, url).items())
+            break
 
         streams.extend(
-            (stream["quality"], HTTPStream(self.session, stream["url"]))
-            for stream in videos.get("progressive", [])
+            (quality, HTTPStream(self.session, url))
+            for quality, url in progressive or []
         )
 
-        if self.session.get_option("mux-subtitles") and data["request"].get("text_tracks"):
+        if text_tracks and self.session.get_option("mux-subtitles"):
             substreams = {
-                s["lang"]: HTTPStream(self.session, "https://vimeo.com" + s["url"])
-                for s in data["request"]["text_tracks"]
+                lang: HTTPStream(self.session, urljoin("https://vimeo.com/", url))
+                for lang, url in text_tracks
             }
             for quality, stream in streams:
                 yield quality, MuxedStream(self.session, stream, subtitles=substreams)
--- tests/plugins/test_vimeo.py
+++ tests/plugins/test_vimeo.py
@@ -6,13 +6,13 @@ class TestPluginCanHandleUrlVimeo(PluginCanHandleUrl):
     __plugin__ = Vimeo
 
     should_match = [
-        "https://vimeo.com/783455878",
-        "https://vimeo.com/channels/music/176894130",
-        "https://vimeo.com/album/3706071/video/148903960",
-        "https://vimeo.com/ondemand/worldoftomorrow3/467204924",
-        "https://vimeo.com/ondemand/100footsurfingdays",
-        "https://player.vimeo.com/video/176894130",
-        "https://vimeo.com/771745400/840d05200c",
+        ("default", "https://vimeo.com/783455878"),
+        ("default", "https://vimeo.com/channels/music/176894130"),
+        ("default", "https://vimeo.com/album/3706071/video/148903960"),
+        ("default", "https://vimeo.com/ondemand/worldoftomorrow3/467204924"),
+        ("default", "https://vimeo.com/ondemand/100footsurfingdays"),
+        ("default", "https://vimeo.com/771745400/840d05200c"),
+        ("player", "https://player.vimeo.com/video/176894130"),
     ]
 
     should_not_match = [
