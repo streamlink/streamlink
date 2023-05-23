f32164844a07bfe9af3531552482cbe966c86d01
bastimeyer <mail@bastimeyer.de>
2023-05-23 19:12:31 +0200

    session: fix interface option setter
    - Fix KeyError when unsetting interface option when it's already unset
    - Refactor tests using pytest and the existing session fixture

--- src/streamlink/session.py
+++ src/streamlink/session.py
@@ -87,7 +87,7 @@ class StreamlinkOptions(Options):
             if scheme not in ("http://", "https://"):
                 continue
             if not value:
-                adapter.poolmanager.connection_pool_kw.pop("source_address")
+                adapter.poolmanager.connection_pool_kw.pop("source_address", None)
             else:
                 # https://docs.python.org/3/library/socket.html#socket.create_connection
                 adapter.poolmanager.connection_pool_kw.update(source_address=(value, 0))
