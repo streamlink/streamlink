639b6e66fda321a197c8b004ec0a7053a3475824
bastimeyer <mail@bastimeyer.de>
2023-05-25 14:57:02 +0200

    tests.plugins: rewrite HTTPStream plugin tests
    - Remove HTTPStream plugin tests from `tests.plugins.test_stream`
    - Add dedicated HTTPStream plugin test module similar to the DASH plugin

---
+++ tests/plugins/test_http.py
@@ -0,0 +1,68 @@
+from unittest.mock import Mock, call
+
+import pytest
+
+from streamlink.plugins.http import HTTPStreamPlugin
+from streamlink.session import Streamlink
+from streamlink.stream.http import HTTPStream
+from tests.plugins import PluginCanHandleUrl
+
+
+class TestPluginCanHandleUrlHTTPStreamPlugin(PluginCanHandleUrl):
+    __plugin__ = HTTPStreamPlugin
+
+    should_match = [
+        "httpstream://example.com/foo",
+        "httpstream://http://example.com/foo",
+        "httpstream://https://example.com/foo",
+    ]
+
+
+@pytest.mark.parametrize(("url", "expected"), [
+    ("httpstream://example.com/foo", "https://example.com/foo"),
+    ("httpstream://http://example.com/foo", "http://example.com/foo"),
+    ("httpstream://https://example.com/foo", "https://example.com/foo"),
+])
+def test_get_streams(
+    monkeypatch: pytest.MonkeyPatch,
+    session: Streamlink,
+    url: str,
+    expected: str,
+):
+    mock_httpstream_init = Mock(return_value=None)
+    monkeypatch.setattr("streamlink.stream.http.HTTPStream.__init__", mock_httpstream_init)
+
+    plugin = HTTPStreamPlugin(session, url)
+    result = plugin.streams()
+    result.pop("worst", None)
+    result.pop("best", None)
+
+    assert list(result.keys()) == ["live"]
+    assert isinstance(result["live"], HTTPStream)
+    assert mock_httpstream_init.call_args_list == [call(session, expected)]
+
+
+def test_parameters(monkeypatch: pytest.MonkeyPatch, session: Streamlink):
+    mock_httpstream_init = Mock(return_value=None)
+    monkeypatch.setattr("streamlink.stream.http.HTTPStream.__init__", mock_httpstream_init)
+
+    plugin = HTTPStreamPlugin(
+        session,
+        (
+            "httpstream://example.com/foo"
+            + " auth=('foo', 'bar')"
+            + " verify=False"
+            + " referer=https://example2.com/bar"
+            + " params={'key': 'a value'}"
+        ),
+    )
+    plugin.streams()
+
+    assert mock_httpstream_init.call_args_list == [call(
+        session,
+        "https://example.com/foo",
+        auth=("foo", "bar"),
+        verify=False,
+        referer="https://example2.com/bar",
+        params={"key": "a value"},
+    )]
