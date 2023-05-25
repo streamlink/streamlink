fe8d357efa5f07b3de093e18c9730ece50e9b4e6
bastimeyer <mail@bastimeyer.de>
2023-05-25 14:57:02 +0200

    tests.plugins: rewrite HTTPStream plugin tests
    - Remove HTTPStream plugin tests from `tests.plugins.test_stream`
    - Add dedicated HTTPStream plugin test module similar to the DASH plugin

--- tests/plugins/test_stream.py
+++ tests/plugins/test_stream.py
@@ -1,46 +1,7 @@
-import unittest
-
-import requests_mock
-
-from streamlink import Streamlink
 from streamlink.plugin.plugin import parse_params, stream_weight
-from streamlink.stream.http import HTTPStream
 
 
-class TestPluginStream(unittest.TestCase):
-    def setUp(self):
-        self.mocker = requests_mock.Mocker()
-        self.mocker.start()
-        self.mocker.register_uri(requests_mock.ANY, requests_mock.ANY, text="")
-        self.session = Streamlink()
-
-    def tearDown(self):
-        self.mocker.stop()
-
-    def assertDictHas(self, a, b):
-        for key, value in a.items():
-            assert b[key] == value
-
-    def _test_http(self, surl, url, params):
-        streams = self.session.streams(surl)
-
-        assert "live" in streams
-
-        stream = streams["live"]
-        assert isinstance(stream, HTTPStream)
-        assert stream.url == url
-        self.assertDictHas(params, stream.args)
-
-    def test_plugin_http(self):
-        self._test_http("httpstream://hostname.se/auth.php auth=('test','test2')",
-                        "https://hostname.se/auth.php", dict(auth=("test", "test2")))
-        self._test_http("httpstream://http://hostname.se/auth.php auth=('test','test2')",
-                        "http://hostname.se/auth.php", dict(auth=("test", "test2")))
-        self._test_http("httpstream://https://hostname.se/auth.php auth=('test','test2')",
-                        "https://hostname.se/auth.php", dict(auth=("test", "test2")))
-        self._test_http("httpstream://https://hostname.se/auth.php verify=False params={'key': 'a value'}",
-                        "https://hostname.se/auth.php?key=a+value", dict(verify=False, params=dict(key="a value")))
-
+class TestPluginStream:
     def test_parse_params(self):
         assert parse_params() \
                == {}
