984202a17b7ea0d0cbaff7c1194a4ded77739e2b
bastimeyer <mail@bastimeyer.de>
2023-05-27 19:05:08 +0200

    tests.stream: refactor FileStream tests
    Use pytest and the existing session fixture

--- tests/stream/test_file.py
+++ tests/stream/test_file.py
@@ -1,22 +1,18 @@
-import unittest
-from unittest.mock import Mock, mock_open, patch
+from unittest.mock import Mock, call, mock_open, patch
 
 from streamlink import Streamlink
 from streamlink.stream.file import FileStream
 
 
-class TestFileStream(unittest.TestCase):
-    def setUp(self):
-        self.session = Streamlink()
+class TestFileStream:
+    def test_open_path(self, session: Streamlink):
+        mock = mock_open()
+        stream = FileStream(session, path="/test/path")
+        with patch("streamlink.stream.file.open", mock, create=True):
+            stream.open()
+        assert mock.call_args_list == [call("/test/path")]
 
-    def test_open_file_path(self):
-        m = mock_open()
-        s = FileStream(self.session, path="/test/path")
-        with patch("streamlink.stream.file.open", m, create=True):
-            s.open()
-            m.assert_called_with("/test/path")
-
-    def test_open_fileobj(self):
+    def test_open_fileobj(self, session: Streamlink):
         fileobj = Mock()
-        s = FileStream(self.session, fileobj=fileobj)
-        assert s.open() is fileobj
+        stream = FileStream(session, fileobj=fileobj)
+        assert stream.open() is fileobj
