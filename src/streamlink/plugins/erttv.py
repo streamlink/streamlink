ac5d4ebbb6871b46d32b00a79e3b1b80e1ec69f0
George Dhmosxakhs <14020938+un1c0rn-the-pwnie@users.noreply.github.com>
2023-06-17 14:40:25 +0300

    erttv plugin added

---
+++ src/streamlink/plugins/erttv.py
@@ -0,0 +1,29 @@
+"""
+$description Greek national live TV channels owned by The Hellenic Broadcasting Corporation.
+$url ert.gr
+$type live
+"""
+
+import re
+
+from streamlink.plugin import Plugin, pluginmatcher
+from streamlink.plugin.api import validate
+from streamlink.stream.hls import HLSStream
+
+# example inputs urls:
+# https://www.ert.gr/webtv/ert/tv/live-glm/ert1.html
+# https://www.ert.gr/webtv/ert/tv/live-glm/ert2.html
+# https://www.ert.gr/webtv/ert/tv/live-glm/ert3.html 
+@pluginmatcher(re.compile(
+    r"https?://(?:www\.)?ert.gr/",
+))
+class ERTTV(Plugin):
+    def _get_streams(self):
+        re_streams = re.compile(r"var\s*streamww\s*\=\s*\'(https:\/\/.*playlist_dvr\.m3u8*)\'")
+        stream_url = self.session.http.get(self.url, schema=validate.Schema(
+            validate.transform(re_streams.findall),
+        ))[0]
+        return HLSStream.parse_variant_playlist(self.session, stream_url)
+
+
+__plugin__ = ERTTV
