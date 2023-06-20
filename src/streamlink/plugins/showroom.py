d214bf908ee88f269bd2de95e5ee44196bc58953
bastimeyer <mail@bastimeyer.de>
2023-06-21 01:38:06 +0200

    plugins.showroom: fix room-id schema

--- src/streamlink/plugins/showroom.py
+++ src/streamlink/plugins/showroom.py
@@ -6,6 +6,7 @@ $type live
 
 import logging
 import re
+from urllib.parse import parse_qsl, urlparse
 
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
@@ -30,15 +31,17 @@ class Showroom(Plugin):
             self.url,
             schema=validate.Schema(
                 validate.parse_html(),
-                validate.xml_xpath_string(".//script[contains(text(),'share_url:\"https:')][1]/text()"),
+                validate.xml_xpath_string(".//nav//a[contains(@href,'/room/profile?')]/@href"),
                 validate.none_or_all(
-                    re.compile(r"share_url:\"https:[^?]+?\?room_id=(?P<room_id>\d+)\""),
-                    validate.any(None, validate.get("room_id")),
+                    validate.transform(lambda _url_profile: dict(parse_qsl(urlparse(_url_profile).query))),
+                    validate.get("room_id"),
                 ),
             ),
         )
         if not room_id:
             return
+
+        log.debug(f"Room ID: {room_id}")
 
         live_status, self.title = self.session.http.get(
             "https://www.showroom-live.com/api/live/live_info",
