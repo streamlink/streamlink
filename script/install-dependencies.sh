8b45e31774ea15b68f6c5424269bbd623d8c4b37
bastimeyer <mail@bastimeyer.de>
2023-06-22 13:12:24 +0200

    ci: update custom lxml cp312 wheel

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -10,8 +10,8 @@ python -m pip install --upgrade -r dev-requirements.txt
 # install a custom-built lxml wheel for the py312 CI runner until an official one is available
 [[ "${GITHUB_ACTIONS}" && "$(uname)" == Linux && "$(python -V)" =~ 3.12. ]] \
   && python -m pip install --require-hashes -r /dev/stdin <<EOF
-    https://github.com/streamlink/temp-lxml-wheel-DO-NOT-USE/releases/download/lxml-c6b7e62-1/lxml-5.0.0a0-cp312-cp312-linux_x86_64.whl \
-      --hash=sha256:52c42777484b9ab5dbc604583d007048a7d1e7cb0fd101ac063972da1b8e50fb
+    https://github.com/streamlink/temp-lxml-wheel-DO-NOT-USE/releases/download/lxml-ec0b59b-1/lxml-5.0.0a0-cp312-cp312-linux_x86_64.whl \
+      --hash=sha256:b2049d4dfc3fba2d8bb998f909ee98c183cb02b8a277ea8ef323238c88aac744
 EOF
 
 # https://github.com/streamlink/streamlink/issues/4021
