a9e3a73aa4085ade0653a7888bc34e9cbcec7d72
bastimeyer <mail@bastimeyer.de>
2023-07-15 13:20:28 +0200

    plugins.rtvs: fix plugin

--- src/streamlink/plugins/rtvs.py
+++ src/streamlink/plugins/rtvs.py
@@ -6,47 +6,52 @@ $region Slovakia
 """
 
 import re
+from urllib.parse import urlparse
 
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
-from streamlink.utils.parse import parse_json
 
 
 @pluginmatcher(re.compile(
-    r"https?://www\.rtvs\.sk/televizia/live-[\w-]+",
+    r"https?://www\.rtvs\.sk/televizia/(?:live-|sport)",
 ))
 class Rtvs(Plugin):
-    _re_channel_id = re.compile(r"'stream':\s*'live-(\d+)'")
-
     def _get_streams(self):
-        res = self.session.http.get(self.url)
-        m = self._re_channel_id.search(res.text)
-        if not m:
+        channel = self.session.http.get(self.url, schema=validate.Schema(
+            validate.parse_html(),
+            validate.xml_xpath_string(".//iframe[@id='player_live']//@src"),
+            validate.url(path=validate.startswith("/embed/live/")),
+            validate.transform(lambda embed: urlparse(embed).path[len("/embed/live/"):]),
+        ))
+        if not channel:
             return
 
-        res = self.session.http.get(
+        videos = self.session.http.get(
             "https://www.rtvs.sk/json/live5f.json",
             params={
-                "c": m.group(1),
+                "c": channel,
                 "b": "mozilla",
                 "p": "win",
                 "f": "0",
                 "d": "1",
             },
+            schema=validate.Schema(
+                validate.parse_json(),
+                {
+                    "clip": {
+                        "sources": [{
+                            "src": validate.url(),
+                            "type": str,
+                        }],
+                    },
+                },
+                validate.get(("clip", "sources")),
+                validate.filter(lambda n: n["type"] == "application/x-mpegurl"),
+            ),
         )
-        videos = parse_json(res.text, schema=validate.Schema({
-            "clip": {
-                "sources": [{
-                    "src": validate.url(),
-                    "type": str,
-                }],
-            }},
-            validate.get(("clip", "sources")),
-            validate.filter(lambda n: n["type"] == "application/x-mpegurl"),
-        ))
         for video in videos:
-            yield from HLSStream.parse_variant_playlist(self.session, video["src"]).items()
+            return HLSStream.parse_variant_playlist(self.session, video["src"])
 
 
 __plugin__ = Rtvs
