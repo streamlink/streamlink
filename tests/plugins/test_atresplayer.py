ad62fb5b23b39725b3196ab408b13fe76014d595
Pablo Hoffman <pablohoffman@gmail.com>
2023-08-03 14:47:48 +0200

    plugins.atresplayer: fix player API URL
    Co-Authored-By: bastimeyer <mail@bastimeyer.de>

--- tests/plugins/test_atresplayer.py
+++ tests/plugins/test_atresplayer.py
@@ -10,9 +10,12 @@ class TestPluginCanHandleUrlAtresPlayer(PluginCanHandleUrl):
     __plugin__ = AtresPlayer
 
     should_match = [
-        "http://www.atresplayer.com/directos/antena3/",
-        "http://www.atresplayer.com/directos/lasexta/",
         "https://www.atresplayer.com/directos/antena3/",
+        "https://www.atresplayer.com/directos/lasexta/",
+        "https://www.atresplayer.com/directos/antena3-internacional/",
+    ]
+
+    should_not_match = [
         "https://www.atresplayer.com/flooxer/programas/unas/temporada-1/dario-eme-hache-sindy-takanashi-entrevista_123/",
     ]
 
