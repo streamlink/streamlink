304f234212c6aba517b0698fe8270c1bae6ed4b9
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2023-08-21 22:08:16 +0800

    plugins.piaulizaportal: apply code cleanups
    - max line length
    - the `Referer` header with the current URL should be set on the player
      JS request
    - the order of query string parameters can be arbitrary, so matching
      `expires` in the pluginmatcher is not the way to go
    
    Co-Authored-By: bastimeyer <mail@bastimeyer.de>

--- src/streamlink/plugins/piaulizaportal.py
+++ src/streamlink/plugins/piaulizaportal.py
@@ -9,6 +9,7 @@ $notes Tickets purchased at "PIA LIVE STREAM" are used for this platform.
 import logging
 import re
 import time
+from urllib.parse import parse_qsl, urlparse
 
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
@@ -18,36 +19,34 @@ from streamlink.stream.hls import HLSStream
 log = logging.getLogger(__name__)
 
 
-@pluginmatcher(
-    re.compile(
-        r"https://ulizaportal\.jp/pages/(?P<id>[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12})(\?expires=(?P<expires>\d+).*)?",
-    ),
-)
+@pluginmatcher(re.compile(
+    r"https://ulizaportal\.jp/pages/(?P<id>[\da-f]{8}-[\da-f]{4}-[\da-f]{4}-[\da-f]{4}-[\da-f]{12})",
+))
 class PIAULIZAPortal(Plugin):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.session.http.headers.update({"Referer": "https://ulizaportal.jp/"})
+    _URL_PLAYER_DATA = "https://player-api.p.uliza.jp/v1/players/"
+    _URL_PLAYLIST = "https://vms-api.p.uliza.jp/v1/prog-index.m3u8"
 
     def _get_streams(self):
         self.id = self.match.group("id")
 
-        expires = self.match.group("expires")
-        if expires and int(expires) <= time.time():
-            log.error("The link is expired")
+        try:
+            expires = int(dict(parse_qsl(urlparse(self.url).query)).get("expires", 0))
+        except ValueError:
+            expires = 0
+        if 0 < expires <= time.time():
+            log.error("The URL has expired")
             return None
 
         self.title, player_data_url = self.session.http.get(
             self.url,
             schema=validate.Schema(
                 validate.parse_html(),
-                validate.union(
-                    (
-                        validate.xml_xpath_string(".//head/title[1]/text()"),
-                        validate.xml_xpath_string(
-                            ".//script[@type='text/javascript'][contains(@src,'https://player-api.p.uliza.jp/v1/players/')]/@src",
-                        ),
+                validate.union((
+                    validate.xml_xpath_string(".//head/title[1]/text()"),
+                    validate.xml_xpath_string(
+                        f".//script[@type='text/javascript'][contains(@src,'{self._URL_PLAYER_DATA}')][1]/@src",
                     ),
-                ),
+                )),
             ),
         )
         if not player_data_url:
@@ -56,8 +55,11 @@ class PIAULIZAPortal(Plugin):
 
         m3u8_url = self.session.http.get(
             player_data_url,
+            headers={
+                "Referer": self.url,
+            },
             schema=validate.Schema(
-                re.compile(r"""https://vms-api.p.uliza.jp/v1/prog-index.m3u8[^"]+"""),
+                re.compile(rf"""{re.escape(self._URL_PLAYLIST)}[^"']+"""),
                 validate.none_or_all(
                     validate.get(0),
                     validate.url(),
