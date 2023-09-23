ff3c47d91361b62e655462e9879406377e073701
bastimeyer <mail@bastimeyer.de>
2023-09-23 17:35:16 +0200

    ci.github: add py312-dev test runner on Windows

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -5,8 +5,13 @@
 set -ex
 
 python -m pip install --disable-pip-version-check --upgrade pip setuptools
-python -m pip install --upgrade -r dev-requirements.txt
 
+# temporary dependency workarounds
+[[ "$(python -V)" =~ 3.12. && "$(uname)" != Linux ]] \
+  && python -m pip install --upgrade --force-reinstall \
+    'https://github.com/streamlink/temp-dependency-builds-DO-NOT-USE/releases/download/cffi-20230923-1/cffi-1.15.1-cp312-cp312-win_amd64.whl'
+
+python -m pip install --upgrade -r dev-requirements.txt
 # https://github.com/streamlink/streamlink/issues/4021
 python -m pip install brotli
 python -m pip install -e .
