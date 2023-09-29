f8f956cd615dde9bae9fd3f65ea14e415c389bd6
bastimeyer <mail@bastimeyer.de>
2023-09-29 11:31:38 +0200

    build: add Python 3.12 to package classifiers

--- script/install-dependencies.sh
+++ script/install-dependencies.sh
@@ -6,11 +6,6 @@ set -ex
 
 python -m pip install --disable-pip-version-check --upgrade pip setuptools
 
-# temporary dependency workarounds
-[[ "$(python -V)" =~ 3.12. && "$(uname)" != Linux ]] \
-  && python -m pip install --upgrade --force-reinstall \
-    'https://github.com/streamlink/temp-dependency-builds-DO-NOT-USE/releases/download/cffi-20230923-1/cffi-1.15.1-cp312-cp312-win_amd64.whl'
-
 python -m pip install --upgrade -r dev-requirements.txt
 # https://github.com/streamlink/streamlink/issues/4021
 python -m pip install brotli
