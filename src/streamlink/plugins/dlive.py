bc25d859e7cca2ac0423984d7471028ef873381c
bastimeyer <mail@bastimeyer.de>
2023-10-18 14:30:46 +0200

    plugins.dlive: rewrite and fix VODs

--- src/streamlink/plugins/dlive.py
+++ src/streamlink/plugins/dlive.py
@@ -2,13 +2,15 @@
 $description Global live-streaming platform owned by BitTorrent, Inc.
 $url dlive.tv
 $type live, vod
+$metadata id
 $metadata author
+$metadata category
 $metadata title
 """
 
 import logging
 import re
-from urllib.parse import unquote_plus
+from textwrap import dedent
 
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
@@ -18,15 +20,20 @@ from streamlink.stream.hls import HLSStream
 log = logging.getLogger(__name__)
 
 
-@pluginmatcher(re.compile(r"""
-    https?://(?:www\.)?dlive\.tv/
-    (?:
-        p/(?P<video>[^/]+)
-        |
-        (?P<channel>[^/]+)
-    )
-""", re.VERBOSE))
+@pluginmatcher(
+    name="live",
+    pattern=re.compile(
+        r"https?://(?:www\.)?dlive\.tv/(?P<channel>[^/?#]+)(?:$|[?#])",
+    ),
+)
+@pluginmatcher(
+    name="vod",
+    pattern=re.compile(
+        r"https?://(?:www\.)?dlive\.tv/p/(?P<video>[^/?#]+)(?:$|[?#])",
+    ),
+)
 class DLive(Plugin):
+    URL_API = "https://graphigo.prd.dlive.tv/"
     URL_LIVE = "https://live.prd.dlive.tv/hls/live/{username}.m3u8"
 
     QUALITY_WEIGHTS = {
@@ -43,59 +50,103 @@ class DLive(Plugin):
 
     def _get_streams_video(self, video):
         log.debug(f"Getting video HLS streams for {video}")
-        hls_url = self.session.http.get(self.url, schema=validate.Schema(
-            validate.regex(re.compile(r'"playbackUrl"\s*:\s*"([^"]+\.m3u8)"')),
-            validate.get(1),
-            validate.transform(unquote_plus),
-            validate.transform(lambda url: bytes(url, "utf-8").decode("unicode_escape")),
-            validate.url(),
-        ))
+
+        self.id = video
+        hls_url, self.author, self.category, self.title = self.session.http.post(
+            self.URL_API,
+            json={
+                "query": dedent(f"""
+                    query {{
+                        pastBroadcast(permlink:"{video}") {{
+                            playbackUrl
+                            creator {{
+                                username
+                            }}
+                            category {{
+                                title
+                            }}
+                            title
+                        }}
+                    }}
+                """),
+            },
+            schema=validate.Schema(
+                validate.parse_json(),
+                {
+                    "data": {
+                        "pastBroadcast": {
+                            "playbackUrl": validate.url(path=validate.endswith(".m3u8")),
+                            "creator": {
+                                "username": str,
+                            },
+                            "category": {
+                                "title": str,
+                            },
+                            "title": str,
+                        },
+                    },
+                },
+                validate.get(("data", "pastBroadcast")),
+                validate.union_get(
+                    "playbackUrl",
+                    ("creator", "username"),
+                    ("category", "title"),
+                    "title",
+                ),
+            ),
+        )
 
         return HLSStream.parse_variant_playlist(self.session, hls_url)
 
     def _get_streams_live(self, channel):
         log.debug(f"Getting live HLS streams for {channel}")
-        query = f"""query {{
-            userByDisplayName(displayname:"{channel}") {{
-                livestream {{
-                    title
-                }}
-                username
-            }}
-        }}"""
-        livestream, username = self.session.http.post(
-            "https://graphigo.prd.dlive.tv/",
-            json={"query": query},
+
+        self.author = channel
+        username, self.title = self.session.http.post(
+            self.URL_API,
+            json={
+                "query": dedent(f"""
+                    query {{
+                        userByDisplayName(displayname:"{channel}") {{
+                            username
+                            livestream {{
+                                title
+                            }}
+                        }}
+                    }}
+                """),
+            },
             schema=validate.Schema(
                 validate.parse_json(),
                 {
                     "data": {
                         "userByDisplayName": {
+                            "username": str,
                             "livestream": {
                                 "title": str,
                             },
-                            "username": str,
                         },
                     },
                 },
                 validate.get(("data", "userByDisplayName")),
-                validate.union_get("livestream", "username"),
+                validate.union_get(
+                    "username",
+                    ("livestream", "title"),
+                ),
             ),
         )
 
-        self.author = channel
-        self.title = livestream["title"]
-
-        return HLSStream.parse_variant_playlist(self.session, self.URL_LIVE.format(username=username), headers={"Referer": "https://dlive.tv/"})
+        return HLSStream.parse_variant_playlist(self.session, self.URL_LIVE.format(username=username))
 
     def _get_streams(self):
-        video = self.match.group("video")
-        channel = self.match.group("channel")
+        self.session.http.headers.update({
+            "Referer": "https://dlive.tv/",
+        })
 
-        if video:
-            return self._get_streams_video(video)
-        elif channel:
-            return self._get_streams_live(channel)
+        if self.matches["live"]:
+            return self._get_streams_live(self.match["channel"])
+        if self.matches["vod"]:
+            return self._get_streams_video(self.match["video"])
 
 
 __plugin__ = DLive
--- tests/plugins/test_dlive.py
+++ tests/plugins/test_dlive.py
@@ -5,9 +5,13 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlDLive(PluginCanHandleUrl):
     __plugin__ = DLive
 
-    should_match = [
-        "https://dlive.tv/pewdiepie",
-        "https://dlive.tv/p/pdp+K6DqqtYWR",
+    should_match_groups = [
+        (("live", "https://dlive.tv/cryptokaprika"), {"channel": "cryptokaprika"}),
+        (("live", "https://dlive.tv/cryptokaprika?query"), {"channel": "cryptokaprika"}),
+        (("live", "https://dlive.tv/cryptokaprika#hash"), {"channel": "cryptokaprika"}),
+        (("vod", "https://dlive.tv/p/countrycafebgky+oLCFcknSR"), {"video": "countrycafebgky+oLCFcknSR"}),
+        (("vod", "https://dlive.tv/p/countrycafebgky+oLCFcknSR?query"), {"video": "countrycafebgky+oLCFcknSR"}),
+        (("vod", "https://dlive.tv/p/countrycafebgky+oLCFcknSR#hash"), {"video": "countrycafebgky+oLCFcknSR"}),
     ]
 
     should_not_match = [
