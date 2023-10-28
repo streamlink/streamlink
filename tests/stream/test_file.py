258b053305b688330de7a80a871b4cc1c4e58138
bastimeyer <mail@bastimeyer.de>
2023-10-28 17:14:43 +0200

    stream.file: fix read mode

--- tests/stream/test_file.py
+++ tests/stream/test_file.py
@@ -1,18 +1,34 @@
-from unittest.mock import Mock, call, mock_open, patch
+from io import BytesIO
+from unittest.mock import Mock, call
+
+import pytest
 
 from streamlink import Streamlink
 from streamlink.stream.file import FileStream
 
 
 class TestFileStream:
-    def test_open_path(self, session: Streamlink):
-        mock = mock_open()
-        stream = FileStream(session, path="/test/path")
-        with patch("streamlink.stream.file.open", mock, create=True):
-            stream.open()
-        assert mock.call_args_list == [call("/test/path")]
+    @pytest.fixture(autouse=True)
+    def mock_open(self, monkeypatch: pytest.MonkeyPatch) -> Mock:
+        mock_open = Mock(return_value=BytesIO(b"foo"))
+        monkeypatch.setattr("builtins.open", mock_open)
 
-    def test_open_fileobj(self, session: Streamlink):
-        fileobj = Mock()
+        return mock_open
+
+    def test_open_path(self, session: Streamlink, mock_open: Mock):
+        stream = FileStream(session, path="/test/path")
+        streamio = stream.open()
+        assert mock_open.call_args_list == [call("/test/path", "rb")]
+        assert streamio.read() == b"foo"
+
+        streamio.close()
+
+    def test_open_fileobj(self, session: Streamlink, mock_open: Mock):
+        fileobj = BytesIO(b"bar")
         stream = FileStream(session, fileobj=fileobj)
-        assert stream.open() is fileobj
+        streamio = stream.open()
+        assert streamio is fileobj
+        assert mock_open.call_args_list == []
+        assert streamio.read() == b"bar"
+
+        streamio.close()
