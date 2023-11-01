ec0cef5b38966f6d3f2f220ededa9165607008b9
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2023-11-02 00:25:17 +0800

    plugins.joqrag: new plugin

---
+++ src/streamlink/plugins/joqrag.py
@@ -0,0 +1,66 @@
+"""
+$description Japanese Internet radio "Super! A&G+" operated by Nippon Cultural Broadcasting (JOQR).
+$url https://www.uniqueradio.jp/agplayer5/player.php
+$url https://www.uniqueradio.jp/agplayer5/inc-player-hls.php
+$url https://www.joqr.co.jp/ag/
+$url https://www.joqr.co.jp/qr/agdailyprogram/
+$url https://www.joqr.co.jp/qr/agregularprogram/
+$type live
+$metadata author
+$metadata title
+"""
+
+import logging
+import re
+from urllib.parse import unquote_plus, urljoin
+
+from streamlink.exceptions import NoStreamsError
+from streamlink.plugin import Plugin, pluginmatcher
+from streamlink.plugin.api import validate
+from streamlink.stream.hls import HLSStream
+
+
+log = logging.getLogger(__name__)
+
+
+@pluginmatcher(re.compile(r"https?://www\.uniqueradio\.jp/agplayer5/player\.php"))
+@pluginmatcher(re.compile(r"https?://www\.uniqueradio\.jp/agplayer5/inc-player-hls\.php"))
+@pluginmatcher(re.compile(r"https?://(?:www\.)?joqr\.co\.jp/ag/"))
+@pluginmatcher(re.compile(r"https?://(?:www\.)?joqr\.co\.jp/qr/(?:agdailyprogram|agregularprogram)/"))
+class JoqrAg(Plugin):
+    _URL_HOST = "https://www.uniqueradio.jp"
+    _URL_METADATA = f"{_URL_HOST}/aandg"
+    _URL_PLAYER = f"{_URL_HOST}/agplayer5/inc-player-hls.php"
+
+    def _get_streams(self):
+        self.id = "live"
+        self.author = "超!A&G+"
+
+        self.title = self.session.http.get(
+            self._URL_METADATA,
+            schema=validate.Schema(
+                validate.regex(re.compile(r"""var\s+Program_name\s*=\s*["\']([^"\']+)["\']""")),
+                validate.none_or_all(
+                    validate.get(1),
+                    validate.transform(unquote_plus),
+                ),
+            ),
+        )
+        if self.title == "放送休止":
+            raise NoStreamsError
+
+        m3u8_url = self.session.http.get(
+            self._URL_PLAYER,
+            schema=validate.Schema(
+                validate.regex(re.compile(r"""<source\s[^>]*\bsrc="([^"]+)"\s*""")),
+                validate.none_or_all(
+                    validate.get(1),
+                    validate.transform(lambda m3u8_path: urljoin(self._URL_HOST, m3u8_path)),
+                ),
+            ),
+        )
+
+        return HLSStream.parse_variant_playlist(self.session, m3u8_url)
+
+
+__plugin__ = JoqrAg
---
+++ tests/plugins/test_joqrag.py
@@ -0,0 +1,28 @@
+from streamlink.plugins.joqrag import JoqrAg
+from tests.plugins import PluginCanHandleUrl
+
+
+class TestPluginCanHandleUrlKugou(PluginCanHandleUrl):
+    __plugin__ = JoqrAg
+
+    should_match = [
+        "https://www.uniqueradio.jp/agplayer5/player.php",
+        "https://www.uniqueradio.jp/agplayer5/inc-player-hls.php",
+        "https://joqr.co.jp/ag/article/103760/",
+        "https://www.joqr.co.jp/ag/article/103760/",
+        "https://joqr.co.jp/qr/agdailyprogram/",
+        "https://www.joqr.co.jp/qr/agdailyprogram/",
+        "https://joqr.co.jp/qr/agregularprogram/",
+        "https://www.joqr.co.jp/qr/agregularprogram/",
+    ]
+
+    should_not_match = [
+        "https://uniqueradio.jp/agplayer5/player.php",
+        "https://uniqueradio.jp/agplayer5/inc-player-hls.php",
+        "https://www.joqr.co.jp/",
+        "https://www.joqr.co.jp/qr/article/0/",
+        "https://www.joqr.co.jp/ic/",
+        "https://www.joqr.co.jp/ic/article/0/",
+        "https://www.joqr.co.jp/qr/dailyprogram/",
+        "https://www.joqr.co.jp/qr/regularprogram/",
+    ]
