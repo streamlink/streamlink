defa02f4435c7a5c05f5ca8a9c85eabbb5d03a14
Gianluca Caronte <148002157+g-caronte@users.noreply.github.com>
2023-11-13 16:37:03 +0100

    plugin.raiplay: add VOD and authentication support
    Co-authored-by: bastimeyer <mail@bastimeyer.de>

--- tests/plugins/test_raiplay.py
+++ tests/plugins/test_raiplay.py
@@ -6,11 +6,12 @@ class TestPluginCanHandleUrlRaiPlay(PluginCanHandleUrl):
     __plugin__ = RaiPlay
 
     should_match = [
-        "http://www.raiplay.it/dirette/rai1",
-        "http://www.raiplay.it/dirette/rai2",
-        "http://www.raiplay.it/dirette/rai3",
-        "http://raiplay.it/dirette/rai3",
-        "https://raiplay.it/dirette/rai3",
-        "http://www.raiplay.it/dirette/rainews24",
-        "https://www.raiplay.it/dirette/rainews24",
+        ("live", "https://raiplay.it/dirette/rai1"),
+        ("live", "https://www.raiplay.it/dirette/rai1"),
+        ("live", "https://www.raiplay.it/dirette/rai2"),
+        ("live", "https://www.raiplay.it/dirette/rai3"),
+        ("live", "https://www.raiplay.it/dirette/rainews24"),
+
+        ("vod", "https://raiplay.it/video/2023/11/Un-posto-al-sole---Puntata-del-08112023-EP6313-1377bcf9-db3f-40f7-aa05-fefeb086ec68.html"),
+        ("vod", "https://www.raiplay.it/video/2023/11/Un-posto-al-sole---Puntata-del-08112023-EP6313-1377bcf9-db3f-40f7-aa05-fefeb086ec68.html"),
     ]
