ad701455716108b1ceb616cfecacb3846ca5741e
bastimeyer <mail@bastimeyer.de>
2024-01-13 22:30:03 +0100

    plugins.twitch: add --twitch-supported-codecs
    Add the temporary `--twitch-supported-codecs` plugin argument
    for being able to set the client's video codec preference.
    
    Set the default value to "h264" to ensure that no compatibility
    issues arise on clients without AV1/HEVC decode capabilities.

--- src/streamlink/plugins/twitch.py
+++ src/streamlink/plugins/twitch.py
@@ -258,18 +258,22 @@ class TwitchHLSStream(HLSStream):
 
 
 class UsherService:
-    def __init__(self, session):
+    def __init__(self, session: Streamlink, supported_codecs: list[str] | None = None):
         self.session = session
+        self.supported_codecs = supported_codecs or ["h264"]
 
     def _create_url(self, endpoint, **extra_params):
         url = f"https://usher.ttvnw.net{endpoint}"
         params = {
+            "platform": "web",
             "player": "twitchweb",
             "p": int(random() * 999999),
             "type": "any",
             "allow_source": "true",
             "allow_audio_only": "true",
             "allow_spectre": "false",
+            "playlist_include_framerate": "true",
+            "supported_codecs": ",".join(self.supported_codecs),
         }
         params.update(extra_params)
 
@@ -750,6 +754,23 @@ class TwitchClientIntegrity:
     """,
 )
 @pluginargument(
+    "supported-codecs",
+    type="comma_list_filter",
+    type_kwargs={
+        "acceptable": ["h264", "h265", "av1"],
+        "unique": True,
+    },
+    default=["h264"],
+    help="""
+        A comma-separated list of codec names which signals Twitch the client's stream codec preference.
+        Which streams and which codecs are available depends on the specific channel and broadcast.
+
+        Default is "h264".
+
+        Supported codecs are "h264", "h265", "av1". Set to "h264,h265,av1" to enable all codecs.
+    """,
+)
+@pluginargument(
     "api-header",
     metavar="KEY=VALUE",
     type="keyvalue",
@@ -815,7 +836,10 @@ class Twitch(Plugin):
             api_header=self.get_option("api-header"),
             access_token_param=self.get_option("access-token-param"),
         )
-        self.usher = UsherService(session=self.session)
+        self.usher = UsherService(
+            session=self.session,
+            supported_codecs=self.get_option("supported-codecs"),
+        )
 
         self._checked_metadata = False
 
