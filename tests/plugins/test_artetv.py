6d5117569c41246a7cfe06ee93e1260ca0fc59bb
bastimeyer <mail@bastimeyer.de>
2024-01-14 20:39:26 +0100

    plugins.artetv: rewrite and fix plugin

--- tests/plugins/test_artetv.py
+++ tests/plugins/test_artetv.py
@@ -5,30 +5,51 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlArteTV(PluginCanHandleUrl):
     __plugin__ = ArteTV
 
-    should_match = [
-        # new url
-        "http://www.arte.tv/fr/direct/",
-        "http://www.arte.tv/de/live/",
-        "http://www.arte.tv/de/videos/074633-001-A/gesprach-mit-raoul-peck",
-        "http://www.arte.tv/en/videos/071437-010-A/sunday-soldiers",
-        "http://www.arte.tv/fr/videos/074633-001-A/entretien-avec-raoul-peck",
-        "http://www.arte.tv/pl/videos/069873-000-A/supermama-i-businesswoman",
+    should_match_groups = [
+        # live
+        (
+            ("live", "https://www.arte.tv/fr/direct"),
+            {"language": "fr"},
+        ),
+        (
+            ("live", "https://www.arte.tv/fr/direct/"),
+            {"language": "fr"},
+        ),
+        (
+            ("live", "https://www.arte.tv/de/live"),
+            {"language": "de"},
+        ),
+        (
+            ("live", "https://www.arte.tv/de/live/"),
+            {"language": "de"},
+        ),
 
-        # old url - some of them get redirected and some are 404
-        "http://www.arte.tv/guide/fr/direct",
-        "http://www.arte.tv/guide/de/live",
-        "http://www.arte.tv/guide/fr/024031-000-A/le-testament-du-docteur-mabuse",
-        "http://www.arte.tv/guide/de/024031-000-A/das-testament-des-dr-mabuse",
-        "http://www.arte.tv/guide/en/072544-002-A/christmas-carols-from-cork",
-        "http://www.arte.tv/guide/es/068380-000-A/una-noche-en-florencia",
-        "http://www.arte.tv/guide/pl/068916-006-A/belle-and-sebastian-route-du-rock",
+        # vod
+        (
+            ("vod", "https://www.arte.tv/de/videos/097372-001-A/mysterium-satoshi-bitcoin-wie-alles-begann-1-6/"),
+            {"language": "de", "video_id": "097372-001-A"},
+        ),
+        (
+            ("vod", "https://www.arte.tv/en/videos/097372-001-A/the-satoshi-mystery-the-story-of-bitcoin/"),
+            {"language": "en", "video_id": "097372-001-A"},
+        ),
+
+        # old vod URLs with redirects
+        (
+            ("vod", "https://www.arte.tv/guide/de/097372-001-A/mysterium-satoshi-bitcoin-wie-alles-begann-1-6/"),
+            {"language": "de", "video_id": "097372-001-A"},
+        ),
+        (
+            ("vod", "https://www.arte.tv/guide/en/097372-001-A/the-satoshi-mystery-the-story-of-bitcoin/"),
+            {"language": "en", "video_id": "097372-001-A"},
+        ),
     ]
 
     should_not_match = [
-        # shouldn't match
-        "http://www.arte.tv/guide/fr/plus7/",
-        "http://www.arte.tv/guide/de/plus7/",
+        "https://www.arte.tv/guide/de/live/",
+        "https://www.arte.tv/guide/fr/plus7/",
+        "https://www.arte.tv/guide/de/plus7/",
         # shouldn't match - playlists without video ids in url
-        "http://www.arte.tv/en/videos/RC-014457/the-power-of-forests/",
-        "http://www.arte.tv/en/videos/RC-013118/street-art/",
+        "https://www.arte.tv/en/videos/RC-014457/the-power-of-forests/",
+        "https://www.arte.tv/en/videos/RC-013118/street-art/",
     ]
