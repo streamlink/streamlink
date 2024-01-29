d4d351e4dd66a844ac857efad7203e9b69924939
bastimeyer <mail@bastimeyer.de>
2024-01-29 20:08:24 +0100

    utils.module: fix load_module(), add exec_module()
    - Use global finder cache via `pkgutil.get_importer()`
      instead of always creating a new `FileFinder` object for each module
    - Add `exec_module()` for being able to reuse `PathEntryFinder` objects
      returned by `pkgutil.iter_modules()`
    - Add support for pathlib path objects
    - Update tests

--- src/streamlink/utils/module.py
+++ src/streamlink/utils/module.py
@@ -1,15 +1,30 @@
-from importlib.machinery import SOURCE_SUFFIXES, FileFinder, SourceFileLoader
+from importlib.abc import PathEntryFinder
+from importlib.machinery import FileFinder
 from importlib.util import module_from_spec
+from pathlib import Path
+from pkgutil import get_importer
+from types import ModuleType
+from typing import Union
 
 
-_loader_details = [(SourceFileLoader, SOURCE_SUFFIXES)]
+def load_module(name: str, path: Union[Path, str]) -> ModuleType:
+    path = str(path)
+    finder = get_importer(path)
+    if not finder:
+        raise ImportError(f"Not a package path: {path}", path=path)
+
+    return exec_module(finder, name)
 
 
-def load_module(name, path=None):
-    finder = FileFinder(path, *_loader_details)
+def exec_module(finder: PathEntryFinder, name: str) -> ModuleType:
     spec = finder.find_spec(name)
     if not spec or not spec.loader:
-        raise ImportError(f"no module named {name}")
+        raise ImportError(
+            f"No module named '{name}'",
+            name=name,
+            path=finder.path if isinstance(finder, FileFinder) else None,
+        )
     mod = module_from_spec(spec)
     spec.loader.exec_module(mod)
+
     return mod
