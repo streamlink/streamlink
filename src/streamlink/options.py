8643019cb177b5c74b7091276434432cb9d78243
bastimeyer <mail@bastimeyer.de>
2024-02-11 14:28:50 +0100

    options: compare equality of Arguments objects
    ac45740

--- src/streamlink/options.py
+++ src/streamlink/options.py
@@ -279,6 +279,12 @@ class Arguments:
         # iterate in reverse order due to add() being called by multiple pluginargument decorators in reverse order
         return reversed(self.arguments.values())
 
+    def __hash__(self):
+        return hash(tuple(self.arguments.items()))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and hash(self) == hash(other)
+
     def add(self, argument: Argument) -> None:
         self.arguments[argument.name] = argument
 
