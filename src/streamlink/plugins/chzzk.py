caa5fc264689f12128aec47c9d5552ca380f93f6
fml09 <webh4ckpwn@gmail.com>
2024-02-17 22:53:00 +0900

    plugin.chzzk: fix the expiration time issue

--- src/streamlink/plugins/chzzk.py
+++ src/streamlink/plugins/chzzk.py
@@ -6,31 +6,216 @@ $type live, vod
 
 import logging
 import re
+import time
+from typing import Any, Dict, List, Optional, Union
+from streamlink.exceptions import StreamError
 
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
+from streamlink.session import Streamlink
 from streamlink.stream.dash import DASHStream
+from streamlink.stream.ffmpegmux import FFMPEGMuxer, MuxedStream
 from streamlink.stream.hls import HLSStream
+from streamlink.stream.hls.hls import HLSStreamReader, HLSStreamWorker, MuxedHLSStream
+from streamlink.stream.hls.m3u8 import M3U8, parse_m3u8
+
+from streamlink.stream.hls.segment import Media
 
 
 log = logging.getLogger(__name__)
 
 
-@pluginmatcher(
-    name="live",
-    pattern=re.compile(r"https?://chzzk\.naver\.com/live/(?P<channel_id>[^/?]+)"),
-)
-@pluginmatcher(
-    name="video",
-    pattern=re.compile(r"https?://chzzk\.naver\.com/video/(?P<video_id>[^/?]+)"),
-)
-class Chzzk(Plugin):
-    _API_CHANNELS_LIVE_DETAIL = "https://api.chzzk.naver.com/service/v2/channels/{channel_id}/live-detail"
-    _API_VIDEOS = "https://api.chzzk.naver.com/service/v2/videos/{video_id}"
-    _API_VOD_PLAYBACK_URL = "https://apis.naver.com/neonplayer/vodplay/v2/playback/{video_id}?key={in_key}"
+class ChzzkHLSStreamWorker(HLSStreamWorker):
+    stream: "ChzzkHLSStream"
+
+    def _fetch_playlist(self):
+        try:
+            return super()._fetch_playlist()
+        except StreamError as err:
+            if err.err.response.status_code >= 400:
+                self.stream.refresh_playlist()
+                log.debug(
+                    f"Force-reloading the channel playlist on error: {err}")
+            raise err
+
+
+class ChzzkHLSStreamReader(HLSStreamReader):
+    __worker__ = ChzzkHLSStreamWorker
+
+
+class ChzzkHLSStream(HLSStream):
+    __shortname__ = "hls-chzzk"
+    __reader__ = ChzzkHLSStreamReader
+
+    _EXPIRE = re.compile(r"exp=(\d+)")
+    _REFRESH_BEFORE = 3 * 60 * 60  # 3 hours
+
+    def __init__(self, session, url, channel, *args, **kwargs):
+        super().__init__(session, url, *args, **kwargs)
+        self._url = url
+        self._channel = channel
+        self._api = ChzzkAPI(session)
+
+    def refresh_playlist(self):
+        log.debug("Refreshing the stream URL to get a new token.")
+        datatype, data = self._api.get_live_detail(self._channel)
+        if datatype == "error":
+            log.error(data)
+            return self._url
+        media, status, *_ = data
+        if status != "OPEN":
+            log.error("The stream is unavailable")
+            return self._url
+        for media_id, media_protocol, media_path in media:
+            if media_protocol == "HLS" and media_id == "HLS":
+                media_uri = self._get_media_uri(media_path)
+                self._replace_token_from(media_uri)
+                log.debug(f"Refreshed the stream URL to {self._url}")
+                break
+
+    def _get_media_uri(self, media_path):
+        res = self._fetch_variant_playlist(self.session, media_path)
+        m3u8 = parse_m3u8(res)
+        return m3u8.media[0].uri
+
+    def _get_token_from(self, path):
+        return path.split("/")[-2]
+
+    def _replace_token_from(self, media_uri):
+        prev_token = self._get_token_from(self._url)
+        current_token = self._get_token_from(media_uri)
+        self._url = self._url.replace(prev_token, current_token)
+
+    def _should_refresh(self):
+        return self._expire is not None and time.time() >= self._expire - \
+            self._REFRESH_BEFORE
+
+    @property
+    def _expire(self):
+        match = self._EXPIRE.search(self._url)
+        if match:
+            return int(match.group(1))
+        return None
+
+    @property
+    def url(self):
+        if self._should_refresh():
+            self.refresh_playlist()
+        return self._url
+
+    @classmethod
+    def parse_variant_playlist(cls,session: Streamlink,url: str,channel: str,**kwargs) -> Dict[str, HLSStream | MuxedHLSStream]:
+        stream_name: Optional[str]
+        stream: Union["HLSStream", "MuxedHLSStream"]
+        streams: Dict[str, Union["HLSStream", "MuxedHLSStream"]] = {}
+
+        request_args = session.http.valid_request_args(**kwargs)
+        res = cls._fetch_variant_playlist(session, url, **request_args)
+
+        try:
+            multivariant = parse_m3u8(res, parser=cls.__parser__)
+        except ValueError as err:
+            raise OSError(f"Failed to parse playlist: {err}") from err
+
+        for playlist in multivariant.playlists:
+            if playlist.is_iframe:
+                continue
+
+            audio_streams = []
+
+            for media in playlist.media:
+                if media.type == "AUDIO":
+                    audio_streams.append(media)
+
+            if playlist.stream_info.resolution and playlist.stream_info.resolution.height:
+                pixels = f"{playlist.stream_info.resolution.height}p"
+
+            stream_name = pixels
+            if not stream_name:
+                continue
+
+            if audio_streams and FFMPEGMuxer.is_usable(session):
+                stream = MuxedChzzkHLSStream(
+                    session,
+                    video=playlist.uri,
+                    audio=[x.uri for x in audio_streams if x.uri],
+                    channel=channel,
+                    multivariant=multivariant,
+                    **kwargs,
+                )
+            else:
+                stream = cls(
+                    session,
+                    playlist.uri,
+                    multivariant=multivariant,
+                    channel=channel,
+                    **kwargs,
+                )
+
+            streams[stream_name] = stream
+
+        return streams
+
+
+class MuxedChzzkHLSStream(MuxedStream["HLSStream"]):
+    __shortname__ = "hls-multi-chzzk"
+
+    def __init__(
+        self,
+        session: Streamlink,
+        video: str,
+        audio: Union[str, List[str]],
+        channel: str,
+        url_master: Optional[str] = None,
+        multivariant: Optional[M3U8] = None,
+        force_restart: bool = False,
+        ffmpeg_options: Optional[Dict[str, Any]] = None,
+        **kwargs,
+    ):
+        tracks = [video]
+        maps = ["0:v?", "0:a?"]
+        if audio:
+            if isinstance(audio, list):
+                tracks.extend(audio)
+            else:
+                tracks.append(audio)
+        maps.extend(f"{i}:a" for i in range(1, len(tracks)))
+        substreams = [
+            ChzzkHLSStream(
+                session,
+                url,
+                channel,
+                force_restart=force_restart,
+                **kwargs) for url in tracks]
+        ffmpeg_options = ffmpeg_options or {}
+
+        super().__init__(
+            session,
+            *substreams,
+            format="mpegts",
+            maps=maps,
+            **ffmpeg_options)
+        self._url_master = url_master
+        self.multivariant = multivariant if multivariant and multivariant.is_master else None
+
+    def to_manifest_url(self):
+        url = self.multivariant.uri if self.multivariant and self.multivariant.uri else self.url_master
+
+        if url is None:
+            return super().to_manifest_url()
+
+        return url
+
+
+class ChzzkAPI:
+    _CHANNELS_LIVE_DETAIL_URL = "https://api.chzzk.naver.com/service/v2/channels/{channel_id}/live-detail"
+    _VIDEOS_URL = "https://api.chzzk.naver.com/service/v2/videos/{video_id}"
+
+    def __init__(self, session):
+        self._session = session
 
     def _query_api(self, url, *schemas):
-        return self.session.http.get(
+        return self._session.http.get(
             url,
             acceptable_status=(200, 404),
             schema=validate.Schema(
@@ -56,9 +241,9 @@ class Chzzk(Plugin):
             ),
         )
 
-    def _api_channels_live_detail(self, channel_id):
+    def get_live_detail(self, channel_id):
         return self._query_api(
-            self._API_CHANNELS_LIVE_DETAIL.format(channel_id=channel_id),
+            self._CHANNELS_LIVE_DETAIL_URL.format(channel_id=channel_id),
             {
                 "status": str,
                 "liveId": int,
@@ -72,37 +257,31 @@ class Chzzk(Plugin):
                     str,
                     validate.parse_json(),
                     {
-                        "media": [
-                            validate.all(
+                        "media": 
+                            [
                                 {
                                     "mediaId": str,
                                     "protocol": str,
                                     "path": validate.url(),
-                                },
-                                validate.union_get(
-                                    "mediaId",
-                                    "protocol",
-                                    "path",
-                                ),
-                            ),
-                        ],
+                                }],  
                     },
                     validate.get("media"),
+                    validate.transform(lambda media: [(m["mediaId"], m["protocol"], m["path"]) for m in media]),
                 ),
             },
             validate.union_get(
+                "livePlaybackJson",
                 "status",
                 "liveId",
                 "channel",
                 "liveCategory",
                 "liveTitle",
-                "livePlaybackJson",
             ),
         )
 
-    def _api_videos(self, video_id):
+    def get_videos(self, video_id):
         return self._query_api(
-            self._API_VIDEOS.format(video_id=video_id),
+            self._VIDEOS_URL.format(video_id=video_id),
             {
                 "inKey": str,
                 "videoId": str,
@@ -122,27 +301,40 @@ class Chzzk(Plugin):
             ),
         )
 
+
+@pluginmatcher(name="live", pattern=re.compile(
+    r"https?://chzzk\.naver\.com/live/(?P<channel_id>[^/?]+)"), )
+@pluginmatcher(name="video", pattern=re.compile(
+    r"https?://chzzk\.naver\.com/video/(?P<video_id>[^/?]+)"), )
+class Chzzk(Plugin):
+    _API_VOD_PLAYBACK_URL = "https://apis.naver.com/neonplayer/vodplay/v2/playback/{video_id}?key={in_key}"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._api = ChzzkAPI(self.session)
+
     def _get_live(self, channel_id):
-        datatype, data = self._api_channels_live_detail(channel_id)
+        datatype, data = self._api.get_live_detail(channel_id)
         if datatype == "error":
             log.error(data)
             return
 
-        status, self.id, self.author, self.category, self.title, media = data
+        media, status, self.id, self.author, self.category, self.title = data
         if status != "OPEN":
             log.error("The stream is unavailable")
             return
-
         for media_id, media_protocol, media_path in media:
             if media_protocol == "HLS" and media_id == "HLS":
-                return HLSStream.parse_variant_playlist(
-                    self.session,
-                    media_path,
-                    ffmpeg_options={"copyts": True},
-                )
+                return ChzzkHLSStream.parse_variant_playlist(
+                            self.session,
+                            media_path,
+                            channel=channel_id,
+                            ffmpeg_options={"copyts": True},
+                        )
+
 
     def _get_video(self, video_id):
-        datatype, data = self._api_videos(video_id)
+        datatype, data = self._api.get_videos(video_id)
         if datatype == "error":
             log.error(data)
             return
