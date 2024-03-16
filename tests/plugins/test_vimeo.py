9ff6e1124081793b6729664a8747f2a9f06c9f89
bastimeyer <mail@bastimeyer.de>
2024-03-16 15:38:33 +0100

    plugins.vimeo: fix event and player URLs

--- tests/plugins/test_vimeo.py
+++ tests/plugins/test_vimeo.py
@@ -5,14 +5,16 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlVimeo(PluginCanHandleUrl):
     __plugin__ = Vimeo
 
-    should_match = [
-        ("default", "https://vimeo.com/783455878"),
-        ("default", "https://vimeo.com/channels/music/176894130"),
-        ("default", "https://vimeo.com/album/3706071/video/148903960"),
-        ("default", "https://vimeo.com/ondemand/worldoftomorrow3/467204924"),
-        ("default", "https://vimeo.com/ondemand/100footsurfingdays"),
-        ("default", "https://vimeo.com/771745400/840d05200c"),
-        ("player", "https://player.vimeo.com/video/176894130"),
+    should_match_groups = [
+        (("default", "https://vimeo.com/783455878"), {}),
+        (("default", "https://vimeo.com/channels/music/176894130"), {}),
+        (("default", "https://vimeo.com/ondemand/worldoftomorrow3/467204924"), {}),
+        (("default", "https://vimeo.com/ondemand/100footsurfingdays"), {}),
+
+        (("player", "https://player.vimeo.com/video/176894130"), {}),
+
+        (("event", "https://vimeo.com/event/4154130"), {"event_id": "4154130"}),
+        (("event", "https://vimeo.com/event/4154130/embed"), {"event_id": "4154130"}),
     ]
 
     should_not_match = [
