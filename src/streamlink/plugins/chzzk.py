eec60d29df68108008d368a7a6329b59335a1201
Clap2rap <gusdn23@gmail.com>
2024-04-29 11:53:42 +0900

    Add: plugin Chzzk

--- src/streamlink/plugins/chzzk.py
+++ src/streamlink/plugins/chzzk.py
@@ -6,14 +6,91 @@ $type live, vod
 
 import logging
 import re
+import time
 
+from streamlink.exceptions import StreamError
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.dash import DASHStream
-from streamlink.stream.hls import HLSStream
+from streamlink.stream.hls import HLSStream, HLSStreamReader, HLSStreamWorker, parse_m3u8
 
 
 log = logging.getLogger(__name__)
+
+
+class ChzzkHLSStreamWorker(HLSStreamWorker):
+    stream: "ChzzkHLSStream"
+
+    def _fetch_playlist(self):
+        try:
+            return super()._fetch_playlist()
+        except StreamError as err:
+            if err.err.response.status_code >= 400:
+                self.stream.refresh_playlist()
+                log.debug(f"Force-reloading the channel playlist on error: {err}")
+            raise err
+
+
+class ChzzkHLSStreamReader(HLSStreamReader):
+    __worker__ = ChzzkHLSStreamWorker
+
+
+class ChzzkHLSStream(HLSStream):
+    __shortname__ = "hls-chzzk"
+    __reader__ = ChzzkHLSStreamReader
+
+    _EXPIRE = re.compile(r"exp=(\d+)")
+    _REFRESH_BEFORE = 3 * 60 * 60  # 3 hours
+
+    def __init__(self, session, url, channel_id, *args, **kwargs):
+        super().__init__(session, url, *args, **kwargs)
+        self._url = url
+        self._channel_id = channel_id
+        self._api = ChzzkAPI(session)
+
+    def refresh_playlist(self):
+        log.debug("Refreshing the stream URL to get a new token.")
+        datatype, data = self._api.get_live_detail(self._channel_id)
+        if datatype == "error":
+            raise StreamError(data)
+        media, status, *_ = data
+        if status != "OPEN" or media is None:
+            raise StreamError("Error occurred while refreshing the stream URL.")
+        for media_id, media_protocol, media_path in media:
+            if media_protocol == "HLS" and media_id == "HLS":
+                media_uri = self._get_media_uri(media_path)
+                self._replace_token_from(media_uri)
+                log.debug(f"Refreshed the stream URL to {self._url}")
+                break
+
+    def _get_media_uri(self, media_path):
+        res = self._fetch_variant_playlist(self.session, media_path)
+        m3u8 = parse_m3u8(res)
+        return m3u8.media[0].uri
+
+    def _get_token_from(self, path):
+        return path.split("/")[-2]
+
+    def _replace_token_from(self, media_uri):
+        prev_token = self._get_token_from(self._url)
+        current_token = self._get_token_from(media_uri)
+        self._url = self._url.replace(prev_token, current_token)
+
+    def _should_refresh(self):
+        return self._expire is not None and time.time() >= self._expire - self._REFRESH_BEFORE
+
+    @property
+    def _expire(self):
+        match = self._EXPIRE.search(self._url)
+        if match:
+            return int(match.group(1))
+        return None
+
+    @property
+    def url(self):
+        if self._should_refresh():
+            self.refresh_playlist()
+        return self._url
 
 
 class ChzzkAPI:
@@ -36,13 +113,6 @@ class ChzzkAPI:
                             "message": str,
                         },
                         validate.transform(lambda data: ("error", data["message"])),
-                    ),
-                    validate.all(
-                        {
-                            "code": 200,
-                            "content": None,
-                        },
-                        validate.transform(lambda _: ("success", None)),
                     ),
                     validate.all(
                         {
@@ -70,26 +140,29 @@ class ChzzkAPI:
                     {"channelName": str},
                     validate.get("channelName"),
                 ),
-                "livePlaybackJson": validate.none_or_all(
-                    str,
-                    validate.parse_json(),
-                    {
-                        "media": [
-                            validate.all(
-                                {
-                                    "mediaId": str,
-                                    "protocol": str,
-                                    "path": validate.url(),
-                                },
-                                validate.union_get(
-                                    "mediaId",
-                                    "protocol",
-                                    "path",
+                "livePlaybackJson": validate.any(
+                    None,
+                    validate.all(
+                        str,
+                        validate.parse_json(),
+                        {
+                            "media": [
+                                validate.all(
+                                    {
+                                        "mediaId": str,
+                                        "protocol": str,
+                                        "path": validate.url(),
+                                    },
+                                    validate.union_get(
+                                        "mediaId",
+                                        "protocol",
+                                        "path",
+                                    ),
                                 ),
-                            ),
-                        ],
-                    },
-                    validate.get("media"),
+                            ],
+                        },
+                        validate.get("media"),
+                    ),
                 ),
             },
             validate.union_get(
@@ -128,22 +201,10 @@ class ChzzkAPI:
         )
 
 
-@pluginmatcher(
-    name="live",
-    pattern=re.compile(
-        r"https?://chzzk\.naver\.com/live/(?P<channel_id>[^/?]+)",
-    ),
-)
-@pluginmatcher(
-    name="video",
-    pattern=re.compile(
-        r"https?://chzzk\.naver\.com/video/(?P<video_id>[^/?]+)",
-    ),
-)
+@pluginmatcher(name="live", pattern=re.compile(r"https?://chzzk\.naver\.com/live/(?P<channel_id>[^/?]+)"))
+@pluginmatcher(name="video", pattern=re.compile(r"https?://chzzk\.naver\.com/video/(?P<video_id>[^/?]+)"))
 class Chzzk(Plugin):
     _API_VOD_PLAYBACK_URL = "https://apis.naver.com/neonplayer/vodplay/v2/playback/{video_id}?key={in_key}"
-
-    _STATUS_OPEN = "OPEN"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -154,20 +215,23 @@ class Chzzk(Plugin):
         if datatype == "error":
             log.error(data)
             return
-        if data is None:
-            return
 
         media, status, self.id, self.author, self.category, self.title, adult = data
-        if status != self._STATUS_OPEN:
+        if status != "OPEN":
             log.error("The stream is unavailable")
             return
         if media is None:
-            log.error(f"This stream is for {'adults only' if adult else 'unavailable'}")
+            if adult:
+                log.error(
+                    "This stream is for adults only.",
+                )
+            else:
+                log.error("This stream is unavailable")
             return
 
         for media_id, media_protocol, media_path in media:
             if media_protocol == "HLS" and media_id == "HLS":
-                return HLSStream.parse_variant_playlist(
+                return ChzzkHLSStream.parse_variant_playlist(
                     self.session,
                     media_path,
                     channel_id=channel_id,
@@ -184,7 +248,12 @@ class Chzzk(Plugin):
         in_key, vod_id, self.author, self.category, self.title, adult = data
 
         if in_key is None or vod_id is None:
-            log.error(f"This stream is for {'adults only' if adult else 'unavailable'}")
+            if adult:
+                log.error(
+                    "This video is for adults only.",
+                )
+            else:
+                log.error("This video is unavailable")
             return
 
         for name, stream in DASHStream.parse_manifest(
