af15d6c4dc0d1d78f5ca1bcf62d9485ef85b3d68
xueli12 <51438016+xueli12@users.noreply.github.com>
2024-05-02 14:33:27 +0800

    Update streamrunner.py

--- src/streamlink_cli/streamrunner.py
+++ src/streamlink_cli/streamrunner.py
@@ -10,24 +10,30 @@ from streamlink.stream.stream import StreamIO
 from streamlink_cli.output import FileOutput, HTTPOutput, Output, PlayerOutput
 from streamlink_cli.utils.progress import Progress
 
+
 # Use the main Streamlink CLI module as logger
 log = logging.getLogger("streamlink.cli")
+
 
 ACCEPTABLE_ERRNO = errno.EPIPE, errno.EINVAL, errno.ECONNRESET
 with suppress(AttributeError):
     ACCEPTABLE_ERRNO += (errno.WSAECONNABORTED,)  # type: ignore[assignment,attr-defined]
 
+
 def _noop(_):
     return None
 
+
 class _ReadError(BaseException):
     pass
+
 
 class PlayerPollThread(Thread):
     """
     Poll the player process in a separate thread, to isolate it from the stream's read-loop in the main thread.
     Reading the stream can stall indefinitely when filtering content.
     """
+
     POLLING_INTERVAL: float = 0.5
 
     def __init__(self, stream: StreamIO, output: PlayerOutput):
@@ -61,23 +67,22 @@ class PlayerPollThread(Thread):
             self._stream.close()
             break
 
+
 class StreamRunner:
     """Read data from a stream and write it to the output."""
+
     playerpoller: Optional[PlayerPollThread] = None
     progress: Optional[Progress] = None
-    max_size_gb: Optional[float] = None
 
     def __init__(
         self,
         stream: StreamIO,
         output: Output,
         show_progress: bool = False,
-        max_size_gb: Optional[float] = None,
     ):
         self.stream = stream
         self.output = output
         self.is_http = isinstance(output, HTTPOutput)
-        self.max_size_gb = max_size_gb
 
         filename: Optional[Path] = None
 
@@ -100,9 +105,6 @@ class StreamRunner:
         prebuffer: bytes,
         chunk_size: int = 8192,
     ) -> None:
-        total_size_bytes = len(prebuffer)
-        max_size_bytes = self.max_size_gb * 1024 ** 3 if self.max_size_gb else None
-
         read = self.stream.read
         write = self.output.write
         progress = self.progress.write if self.progress else _noop
@@ -111,15 +113,15 @@ class StreamRunner:
             self.playerpoller.start()
         if self.progress:
             self.progress.start()
+
         # TODO: Fix error messages (s/when/while/) and only log "Stream ended" when it ended on its own (data == b"").
         #       These are considered breaking changes of the CLI output, which is parsed by 3rd party tools.
         try:
             write(prebuffer)
             progress(prebuffer)
             del prebuffer
-            
-            # Don't check for stream.closed, so the buffer's contents can be fully read after the stream ended or was closed
 
+            # Don't check for stream.closed, so the buffer's contents can be fully read after the stream ended or was closed
             while True:
                 try:
                     data = read(chunk_size)
@@ -128,14 +130,8 @@ class StreamRunner:
                 except OSError as err:
                     raise _ReadError() from err
 
-                if max_size_bytes and total_size_bytes + len(data) > max_size_bytes:
-                    log.warning(f"Max video size of {self.max_size_gb}GB exceeded, stopping download")
-                    #print(f"Max video size of {self.max_size_gb}GB exceeded, stopping download")
-                    break  # 超过大小限制则停止下载
-
                 write(data)
                 progress(data)
-                total_size_bytes += len(data)  # 更新已下载的大小
 
         except _ReadError as err:
             raise OSError(f"Error when reading from stream: {err.__context__}, exiting") from err.__context__
@@ -147,6 +143,7 @@ class StreamRunner:
                 log.info("HTTP connection closed")
             else:
                 raise OSError(f"Error when writing to output: {err}, exiting") from err
+
         finally:
             if self.playerpoller:
                 self.playerpoller.close()
@@ -154,5 +151,6 @@ class StreamRunner:
             if self.progress:
                 self.progress.close()
                 self.progress.join()
+
             self.stream.close()
             log.info("Stream ended")
