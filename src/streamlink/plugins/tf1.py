5d75e6c71c79aafc5efd922f6fba35da8f3d6afb
W4RdZ <74306224+W4RdZ@users.noreply.github.com>
2024-05-09 12:30:00 +0200

    Update tf1.py with authentication part
    To be transparent, I'm not a dev, so i took this modified code from BellezaEmporium code to make a pull request : https://raw.githubusercontent.com/BellezaEmporium/streamlink/master/src/streamlink/plugins/tf1.py
    
    Also this code seems to works before lazy update.

--- src/streamlink/plugins/tf1.py
+++ src/streamlink/plugins/tf1.py
@@ -7,11 +7,13 @@ $type live
 $region France
 """
 
+import json
 import logging
 import re
 
 from streamlink.plugin import Plugin, PluginError, pluginmatcher
 from streamlink.plugin.api import useragents, validate
+from streamlink.plugin.plugin import pluginargument
 from streamlink.stream.hls import HLSStream
 
 
@@ -30,8 +32,28 @@ log = logging.getLogger(__name__)
         (?P<lci>tf1info|lci)\.fr/direct/?
     )
 """, re.VERBOSE))
+@pluginargument(
+    "username",
+    required=True,
+    requires=["password"],
+    metavar="USERNAME",
+    help="Your TF1 account username",
+)
+@pluginargument(
+    "password",
+    required=True,
+    sensitive=True,
+    metavar="PASSWORD",
+    help="Your TF1 account password",
+)
+
 class TF1(Plugin):
     _URL_API = "https://mediainfo.tf1.fr/mediainfocombo/{channel_id}"
+    # Necessary for login.
+    auth_url = "https://compte.tf1.fr/accounts.login"
+    session_url = "https://www.tf1.fr/token/gigya/web"
+    gigya_api_key = '3_hWgJdARhz_7l1oOp3a8BDLoR9cuWZpUaKG4aqF7gum9_iK3uTZ2VlDBl8ANf8FVk'
+
 
     def _get_channel(self):
         if self.match["live"]:
@@ -52,12 +74,13 @@ class TF1(Plugin):
         return self.session.http.get(
             self._URL_API.format(channel_id=channel_id),
             params={
-                "context": "MYTF1",
-                "pver": "4001000",
+                "pver": "5015000",
+                "context": "MYTF1"
             },
             headers={
                 # forces HLS streams
-                "User-Agent": useragents.IPHONE,
+                "User-Agent": "iPhone",
+                "authorization": "Bearer {token}".format(token=self.user_token)
             },
             schema=validate.Schema(
                 validate.parse_json(),
@@ -83,8 +106,57 @@ class TF1(Plugin):
                 validate.get("delivery"),
             ),
         )
+    
+    def login(self, username, password):
+        """
+        Create session using Gigya's API for TF1.
+        :return: Whether authentication was successful
+        :rtype: bool
+        """
+
+        def auth_check():
+            res = self.session.http.post(
+            self.auth_url,
+            data=dict(
+                loginID=username,
+                password=password,
+                APIKey=self.gigya_api_key,
+                includeUserInfo="true"
+            ),
+            headers={"Referer": self.url, "User-Agent": useragents.IPHONE})
+
+            # If TF1 login is successful, get Gigya token.
+            if res.status_code == 200:
+                # make the session request to get the correct cookies
+                session_res = self.session.http.post(
+                    self.session_url,
+                    data=json.dumps({
+                        "uid": res.json()['userInfo']['UID'],
+                        "signature": res.json()['userInfo']['UIDSignature'],
+                        "timestamp": int(res.json()['userInfo']['signatureTimestamp']),
+                        "consent_ids": ["1", "2", "3", "4", "10001", "10003", "10005", "10007", "10013", "10015", "10017", "10019", "10009", "10011", "13002", "13001", "10004", "10014", "10016", "10018", "10020", "10010", "10012", "10006", "10008"]
+                    })
+                )
+                if session_res.status_code == 200:
+                    self.user_token = session_res.json()['token']
+                    return True
+                else:
+                    return False
+            else:
+                return False
+
+        
+        if auth_check() == True:
+            log.debug("Already authenticated, skipping authentication")
+            return True
+        else:
+            return False
 
     def _get_streams(self):
+        if not self.login(self.get_option("username"), self.get_option("password")):
+            log.error("Failed to login, check username and password")
+            return
+
         channel, channel_id = self._get_channel()
         log.debug(f"Found channel {channel} ({channel_id})")
 
