870b1a4562ffa9a4e792a3209065b03eec7968d1
Clap2rap <gusdn23@gmail.com>
2024-05-24 00:52:35 +0900

    Fix: Chzzk live detail api response validation

--- src/streamlink/plugins/chzzk.py
+++ src/streamlink/plugins/chzzk.py
@@ -40,6 +40,13 @@ class ChzzkAPI:
                     validate.all(
                         {
                             "code": 200,
+                            "content": None,
+                        },
+                        validate.transform(lambda _: ("success", None)),
+                    ),
+                    validate.all(
+                        {
+                            "code": 200,
                             "content": dict,
                         },
                         validate.get("content"),
@@ -146,6 +153,8 @@ class Chzzk(Plugin):
         datatype, data = self._api.get_live_detail(channel_id)
         if datatype == "error":
             log.error(data)
+            return
+        if data is None:
             return
 
         media, status, self.id, self.author, self.category, self.title, adult = data
