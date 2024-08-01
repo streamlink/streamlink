c3afa417bce9399a171101b79c187a6eb53fdcbe
Silvan Gümüsdere <silvan@trollbox.org>
2024-08-01 23:31:15 +0200

    plugins.sportschau: add schema validation and metadata extraction

--- src/streamlink/plugins/sportschau.py
+++ src/streamlink/plugins/sportschau.py
@@ -1,7 +1,9 @@
 """
-$description German sports magazine live stream, owned by ARD.
+$description German sports magazine live streams and VOD content, owned by ARD.
 $url sportschau.de
 $type live
+$metadata id
+$metadata title
 """
 
 import logging
@@ -15,33 +17,62 @@ from streamlink.stream.http import HTTPStream
 log = logging.getLogger(__name__)
 
 
-@pluginmatcher(
-    re.compile(
-        r"https?://(?:\w+\.)*sportschau\.de/",
-    )
-)
+@pluginmatcher(re.compile(r"https?://(?:\w+\.)*sportschau\.de/"))
 class Sportschau(Plugin):
-    def _get_streams(self):
-        streams = self.session.http.get(
-            self.url,
-            schema=validate.Schema(
-                validate.parse_html(),
-                validate.xml_xpath_string("(//*[@data-v-type='MediaPlayer'])[1]/@data-v"),
-                validate.parse_json(),
-                validate.get("mc"),
-                validate.get("streams"),
+    _schema_media = validate.Schema(
+        validate.parse_html(),
+        validate.xml_xpath_string(".//*[@data-v-type='MediaPlayer'][@data-v][1]/@data-v"),
+        validate.none_or_all(
+            validate.parse_json(),
+            {
+                "mc": {
+                    validate.optional("id"): str,
+                    "meta": {
+                        "title": str,
+                    },
+                    "streams": [
+                        validate.all(
+                            {
+                                "media": [
+                                    validate.all(
+                                        {
+                                            "mimeType": str,
+                                            "url": validate.url(),
+                                        },
+                                        validate.union_get(
+                                            "mimeType",
+                                            "url",
+                                        ),
+                                    ),
+                                ],
+                            },
+                            validate.get("media"),
+                        ),
+                    ],
+                },
+            },
+            validate.get("mc"),
+            validate.union_get(
+                "id",
+                ("meta", "title"),
+                "streams",
             ),
-        )
-        for stream in streams:
-            for media in stream.get("media"):
-                url = media.get("url")
-                is_hls = media.get("mimeType") == "application/vnd.apple.mpegurl"
-                is_audio_only = stream.get("isAudioOnly")
-                if is_hls:
+        ),
+    )
+
+    def _get_streams(self):
+        data = self.session.http.get(self.url, schema=self._schema_media)
+        if not data:
+            return
+
+        self.id, self.title, streams = data
+
+        for media in streams:
+            for mime_type, url in media:
+                if mime_type == "application/vnd.apple.mpegurl":
                     yield from HLSStream.parse_variant_playlist(self.session, url).items()
                 else:
-                    media_type = "audio" if is_audio_only else "video"
-                    yield media_type, HTTPStream(self.session, url)
+                    yield "audio", HTTPStream(self.session, url)
 
 
 __plugin__ = Sportschau
