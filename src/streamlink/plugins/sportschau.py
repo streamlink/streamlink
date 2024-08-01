7354deb14731d7be92583aa1b098e401a383b9c8
Silvan Gümüsdere <silvan@trollbox.org>
2024-08-01 21:26:49 +0200

    plugins.sportschau: fix sportschau

--- src/streamlink/plugins/sportschau.py
+++ src/streamlink/plugins/sportschau.py
@@ -11,44 +11,37 @@ from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
 from streamlink.stream.hls import HLSStream
 from streamlink.stream.http import HTTPStream
-from streamlink.utils.url import update_scheme
-
 
 log = logging.getLogger(__name__)
 
 
-@pluginmatcher(re.compile(
-    r"https?://(?:\w+\.)*sportschau\.de/",
-))
+@pluginmatcher(
+    re.compile(
+        r"https?://(?:\w+\.)*sportschau\.de/",
+    )
+)
 class Sportschau(Plugin):
     def _get_streams(self):
-        player_js = self.session.http.get(self.url, schema=validate.Schema(
-            re.compile(r"https?:(//deviceids-medp.wdr.de/ondemand/\S+\.js)"),
-            validate.none_or_all(
-                validate.get(1),
-                validate.transform(lambda url: update_scheme("https://", url)),
+        streams = self.session.http.get(
+            self.url,
+            schema=validate.Schema(
+                validate.parse_html(),
+                validate.xml_xpath_string("(//*[@data-v-type='MediaPlayer'])[1]/@data-v"),
+                validate.parse_json(),
+                validate.get("mc"),
+                validate.get("streams"),
             ),
-        ))
-        if not player_js:
-            return
-
-        log.debug(f"Found player js {player_js}")
-        data = self.session.http.get(player_js, schema=validate.Schema(
-            validate.regex(re.compile(r"\$mediaObject\.jsonpHelper\.storeAndPlay\(({.+})\);?")),
-            validate.get(1),
-            validate.parse_json(),
-            validate.get("mediaResource"),
-            validate.get("dflt"),
-            {
-                validate.optional("audioURL"): validate.url(),
-                validate.optional("videoURL"): validate.url(),
-            },
-        ))
-
-        if data.get("videoURL"):
-            yield from HLSStream.parse_variant_playlist(self.session, update_scheme("https:", data.get("videoURL"))).items()
-        if data.get("audioURL"):
-            yield "audio", HTTPStream(self.session, update_scheme("https:", data.get("audioURL")))
+        )
+        for stream in streams:
+            for media in stream.get("media"):
+                url = media.get("url")
+                is_hls = media.get("mimeType") == "application/vnd.apple.mpegurl"
+                is_audio_only = stream.get("isAudioOnly")
+                if is_hls:
+                    yield from HLSStream.parse_variant_playlist(self.session, url).items()
+                else:
+                    media_type = "audio" if is_audio_only else "video"
+                    yield media_type, HTTPStream(self.session, url)
 
 
 __plugin__ = Sportschau
