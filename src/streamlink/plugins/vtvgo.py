ac9bab3cf1d9e0e4360f1e63df79b8e15622a75f
bastimeyer <mail@bastimeyer.de>
2024-08-02 18:24:14 +0200

    plugins.vtvgo: acquire AWS WAF token

--- src/streamlink/plugins/vtvgo.py
+++ src/streamlink/plugins/vtvgo.py
@@ -9,45 +9,55 @@ import re
 
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
+from streamlink.plugin.api.aws_waf import AWSWAF
 from streamlink.stream.hls import HLSStream
 
 
 log = logging.getLogger(__name__)
 
 
-@pluginmatcher(re.compile(
-    r"https?://vtvgo\.vn/xem-truc-tuyen-kenh-",
-))
+@pluginmatcher(re.compile(r"https?://vtvgo\.vn/xem-truc-tuyen-kenh-"))
 class VTVgo(Plugin):
     AJAX_URL = "https://vtvgo.vn/ajax-get-stream"
 
     def _get_streams(self):
-        # get cookies
-        self.session.http.get("https://vtvgo.vn/")
+        if (
+            not self.session.http.cookies.get(AWSWAF.TOKEN)
+            or not self.session.http.get(self.url).content
+        ):
+            log.info("Getting new AWS WAF token (bot-detection)")
+            if not AWSWAF(self.session).acquire(self.url):
+                return
+            self.save_cookies()
 
-        self.session.http.headers.update({
-            "Origin": "https://vtvgo.vn",
-            "Referer": self.url,
-            "Sec-Fetch-Site": "same-origin",
-            "X-Requested-With": "XMLHttpRequest",
-        })
+        self.session.http.headers.update(
+            {
+                "Origin": "https://vtvgo.vn",
+                "Referer": self.url,
+                "Sec-Fetch-Site": "same-origin",
+                "X-Requested-With": "XMLHttpRequest",
+            },
+        )
 
-        params = self.session.http.get(self.url, schema=validate.Schema(
-            validate.parse_html(),
-            validate.xml_xpath_string(".//script[contains(text(),'setplayer(')][1]/text()"),
-            validate.none_or_all(
-                validate.regex(
-                    re.compile(r"""var\s+(?P<key>(?:type_)?id|time|token)\s*=\s*["']?(?P<value>[^"']+)["']?;"""),
-                    method="findall",
+        params = self.session.http.get(
+            self.url,
+            schema=validate.Schema(
+                validate.parse_html(),
+                validate.xml_xpath_string(".//script[contains(text(),'setplayer(')][1]/text()"),
+                validate.none_or_all(
+                    validate.regex(
+                        re.compile(r"""var\s+(?P<key>(?:type_)?id|time|token)\s*=\s*["']?(?P<value>[^"']+)["']?;"""),
+                        method="findall",
+                    ),
+                    [
+                        ("id", int),
+                        ("type_id", str),
+                        ("time", str),
+                        ("token", str),
+                    ],
                 ),
-                [
-                    ("id", int),
-                    ("type_id", str),
-                    ("time", str),
-                    ("token", str),
-                ],
             ),
-        ))
+        )
         if not params:
             return
 
