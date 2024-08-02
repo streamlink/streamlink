e630520fd04260e3700894799d5d94fd52b818de
gsilvan <silvan@trollbox.org>
2024-08-02 12:29:26 +0200

    plugins.sportschau: use test urls with diverse media
    Co-authored-by: Sebastian Meyer <mail@bastimeyer.de>

--- tests/plugins/test_sportschau.py
+++ tests/plugins/test_sportschau.py
@@ -8,4 +8,5 @@ class TestPluginCanHandleUrlSportschau(PluginCanHandleUrl):
     should_match = [
         "https://www.sportschau.de/fussball/uefa-euro-2024/spaniens-europameister-begeistert-empfangen,em-spanien-feier-100.html",
         "https://www.sportschau.de/olympia/live/schwimmen-finals-m-f,livestream-olympia-schwimmen-110.html",
+        "https://www.sportschau.de/podcasts/sportschau-olympia-podcast/tag-6-vier-gewinnt,audio-tag-6-vier-gewinnt-100.html",
     ]
