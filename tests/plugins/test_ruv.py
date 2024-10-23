d75341ab1eef76958347e0c7950433403be567c5
bastimeyer <mail@bastimeyer.de>
2024-10-23 14:17:41 +0200

    plugins.ruv: rewrite and fix plugin

--- tests/plugins/test_ruv.py
+++ tests/plugins/test_ruv.py
@@ -5,21 +5,12 @@ from tests.plugins import PluginCanHandleUrl
 class TestPluginCanHandleUrlRuv(PluginCanHandleUrl):
     __plugin__ = Ruv
 
-    should_match = [
-        "http://ruv.is/ruv",
-        "http://ruv.is/ruv",
-        "http://ruv.is/ruv/",
-        "https://ruv.is/ruv/",
-        "http://www.ruv.is/ruv",
-        "http://www.ruv.is/ruv/",
-        "http://ruv.is/ruv2",
-        "http://ruv.is/ras1",
-        "http://ruv.is/ras2",
-        "http://ruv.is/rondo",
-        "http://www.ruv.is/spila/ruv/ol-2018-ishokki-karla/20180217",
-        "http://www.ruv.is/spila/ruv/frettir/20180217",
-    ]
-
-    should_not_match = [
-        "http://ruv.is/ruvnew",
+    should_match_groups = [
+        (("live", "https://www.ruv.is/sjonvarp/beint/ruv"), {"channel": "ruv"}),
+        (("live", "https://www.ruv.is/sjonvarp/beint/ruv2"), {"channel": "ruv2"}),
+        (("live", "https://www.ruv.is/utvarp/beint/ras1"), {"channel": "ras1"}),
+        (("live", "https://www.ruv.is/utvarp/beint/ras2"), {"channel": "ras2"}),
+        (("vod", "https://www.ruv.is/sjonvarp/spila/vedur/30766/agcf5m"), {"id": "30766", "episode": "agcf5m"}),
+        (("vod", "https://www.ruv.is/sjonvarp/spila/kastljos/35422/ahpu2g"), {"id": "35422", "episode": "ahpu2g"}),
+        (("vod", "https://www.ruv.is/utvarp/spila/laesi/37102/b1qk71"), {"id": "37102", "episode": "b1qk71"}),
     ]
