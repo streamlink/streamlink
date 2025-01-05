6b9aca24de9397eb44fce8dd43d018d805327569
bastimeyer <mail@bastimeyer.de>
2025-01-05 11:12:07 +0100

    plugins.tiktok: rewrite plugin, fix live, add VODs

--- src/streamlink/plugins/tiktok.py
+++ src/streamlink/plugins/tiktok.py
@@ -11,6 +11,7 @@ from __future__ import annotations
 
 import logging
 import re
+import sys
 
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
@@ -21,16 +22,17 @@ log = logging.getLogger(__name__)
 
 
 @pluginmatcher(
-    re.compile(
-        r"https?://(?:www\.)?tiktok\.com/@(?P<channel>[^/?]+)",
-    ),
+    name="live",
+    pattern=re.compile(r"https?://(?:www\.)?tiktok\.com/@(?P<channel>[^/?]+)(?:$|/live)"),
+)
+@pluginmatcher(
+    name="video",
+    pattern=re.compile(r"https?://(?:www\.)?tiktok\.com/@(?P<channel>[^/?]+)/video/(?P<id>\d+)"),
 )
 class TikTok(Plugin):
     QUALITY_WEIGHTS: dict[str, int] = {}
 
-    _URL_WEB_LIVE = "https://www.tiktok.com/@{channel}/live"
-    _URL_API_LIVE_DETAIL = "https://www.tiktok.com/api/live/detail/?aid=1988&roomID={room_id}"
-    _URL_WEBCAST_ROOM_INFO = "https://webcast.tiktok.com/webcast/room/info/?aid=1988&room_id={room_id}"
+    _URL_API_LIVE = "https://www.tiktok.com/api-live/user/room"
 
     _STATUS_OFFLINE = 4
 
@@ -42,114 +44,187 @@ class TikTok(Plugin):
 
         return super().stream_weight(key)
 
-    def _get_streams(self):
-        self.id = self.session.http.get(
-            self._URL_WEB_LIVE.format(channel=self.match["channel"]),
-            allow_redirects=False,
-            schema=validate.Schema(
-                validate.parse_html(),
-                validate.any(
-                    validate.all(
-                        validate.xml_xpath_string(
-                            ".//head/meta[@property='al:android:url'][contains(@content,'live?room_id=')]/@content",
-                        ),
-                        str,
-                        re.compile(r"room_id=(\d+)"),
-                        validate.get(1),
-                    ),
-                    validate.all(
-                        validate.xml_xpath_string(
-                            ".//script[@type='application/json'][@id='SIGI_STATE'][1]/text()",
-                        ),
-                        str,
-                        validate.parse_json(),
-                        {
-                            "LiveRoom": {
-                                "liveRoomUserInfo": {
-                                    "user": {
-                                        "roomId": str,
-                                    },
-                                },
-                            },
-                        },
-                        validate.get(("LiveRoom", "liveRoomUserInfo", "user", "roomId")),
-                    ),
-                    validate.transform(lambda *_: None),
-                ),
-            ),
-        )
-        if not self.id:
-            log.error("Could not find room ID")
-            return
+    def _query_api(self, url, **kwargs):
+        schema = kwargs.pop("schema")
 
-        log.debug(f"room_id={self.id}")
-
-        live_detail = self.session.http.get(
-            self._URL_API_LIVE_DETAIL.format(room_id=self.id),
+        success, data = self.session.http.get(
+            url,
             schema=validate.Schema(
                 validate.parse_json(),
+                validate.any(
+                    validate.all(
+                        {
+                            "statusCode": 0,
+                            "data": schema,
+                        },
+                        validate.transform(lambda data: (True, data["data"])),
+                    ),
+                    validate.all(
+                        {
+                            "message": str,
+                        },
+                        validate.transform(lambda data: (False, data["message"])),
+                    ),
+                ),
+            ),
+            **kwargs,
+        )
+
+        if not success:
+            log.error(data or "Error while querying API")
+            return None
+
+        return data
+
+    def _get_streams_live(self):
+        self.author = self.match["channel"]
+
+        data = self._query_api(
+            self._URL_API_LIVE,
+            params={
+                "aid": 1988,
+                "sourceType": 54,
+                "uniqueId": self.author,
+            },
+            headers={
+                "Referer": self.url,
+            },
+            schema=validate.Schema(
                 {
-                    "status_code": 0,
-                    "LiveRoomInfo": {
+                    "liveRoom": {
                         "status": int,
+                        validate.optional("streamId"): str,
                         "title": str,
-                        "ownerInfo": {"nickname": str},
+                        validate.optional("streamData"): validate.all(
+                            {
+                                "pull_data": {
+                                    "stream_data": validate.all(
+                                        str,
+                                        validate.parse_json(),
+                                        {
+                                            "data": dict,
+                                        },
+                                        validate.get("data"),
+                                        {
+                                            str: validate.all(
+                                                {
+                                                    "main": {
+                                                        "flv": validate.url(),
+                                                        "sdk_params": validate.all(
+                                                            validate.parse_json(),
+                                                            {
+                                                                "vbitrate": int,
+                                                            },
+                                                        ),
+                                                    },
+                                                },
+                                                validate.union_get(
+                                                    ("main", "flv"),
+                                                    ("main", "sdk_params", "vbitrate"),
+                                                ),
+                                            ),
+                                        },
+                                    ),
+                                },
+                            },
+                            validate.get(("pull_data", "stream_data")),
+                        ),
                     },
                 },
-                validate.get("LiveRoomInfo"),
+                validate.get("liveRoom"),
                 validate.union_get(
                     "status",
-                    ("ownerInfo", "nickname"),
+                    "streamId",
                     "title",
+                    "streamData",
                 ),
             ),
         )
-        status, self.author, self.title = live_detail
+        if not data:
+            return
+
+        status, self.id, self.title, stream_data = data
         if status == self._STATUS_OFFLINE:
             log.info("The channel is currently offline")
             return
 
-        streams = self.session.http.get(
-            self._URL_WEBCAST_ROOM_INFO.format(room_id=self.id),
+        if not stream_data:
+            log.error("The stream is inaccessible")
+            return
+
+        streams = {}
+        for name, (url, vbitrate) in stream_data.items():
+            self.QUALITY_WEIGHTS[name] = vbitrate
+            streams[name] = HTTPStream(self.session, url)
+
+        self.QUALITY_WEIGHTS["origin"] = sys.maxsize
+
+        return streams
+
+    def _get_streams_video(self):
+        self.id = self.match["id"]
+
+        data = self.session.http.get(
+            self.url,
             schema=validate.Schema(
-                validate.parse_json(),
-                {"data": {validate.optional("stream_url"): {"live_core_sdk_data": {"pull_data": {"stream_data": str}}}}},
-                validate.get(("data", "stream_url")),
+                validate.parse_html(),
+                validate.xml_xpath_string(
+                    ".//script[@type='application/json'][@id='__UNIVERSAL_DATA_FOR_REHYDRATION__'][1]/text()",
+                ),
                 validate.none_or_all(
-                    validate.get(("live_core_sdk_data", "pull_data", "stream_data")),
                     validate.parse_json(),
                     {
-                        "data": {
-                            str: validate.all(
-                                {
-                                    "main": {
-                                        "flv": validate.url(),
-                                        "sdk_params": validate.all(
-                                            validate.parse_json(),
-                                            {
-                                                "vbitrate": int,
+                        "__DEFAULT_SCOPE__": {
+                            "webapp.video-detail": validate.any(
+                                validate.all(
+                                    {
+                                        "statusCode": 0,
+                                        "itemInfo": {
+                                            "itemStruct": {
+                                                "author": {
+                                                    "uniqueId": str,
+                                                },
+                                                "video": {
+                                                    "downloadAddr": validate.url(),
+                                                },
                                             },
-                                        ),
+                                        },
                                     },
-                                },
-                                validate.union_get(
-                                    ("main", "flv"),
-                                    ("main", "sdk_params", "vbitrate"),
+                                    validate.get(("itemInfo", "itemStruct")),
+                                    validate.union_get(
+                                        ("author", "uniqueId"),
+                                        ("video", "downloadAddr"),
+                                    ),
+                                    validate.transform(lambda data: (True, data)),
+                                ),
+                                validate.all(
+                                    {
+                                        "statusMsg": str,
+                                    },
+                                    validate.transform(lambda data: (False, data["statusMsg"])),
                                 ),
                             ),
                         },
                     },
-                    validate.get("data"),
+                    validate.get(("__DEFAULT_SCOPE__", "webapp.video-detail")),
                 ),
             ),
         )
-        if not streams:
-            log.error("The stream is inaccessible")
+        if not data:
+            return
+        if not data[0]:
+            log.error(data[1] or "The video is inaccessible")
             return
 
-        for name, (url, vbitrate) in streams.items():
-            self.QUALITY_WEIGHTS[name] = vbitrate
-            yield name, HTTPStream(self.session, url)
+        self.author, url = data[1]
+
+        return {"video": HTTPStream(self.session, url)}
+
+    def _get_streams(self):
+        if self.matches["live"]:
+            return self._get_streams_live()
+        elif self.matches["video"]:
+            return self._get_streams_video()
 
 
 __plugin__ = TikTok
