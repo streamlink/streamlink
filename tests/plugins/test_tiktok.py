f88dfae18f330659f69d6deecd3cb3e8bb095406
bastimeyer <mail@bastimeyer.de>
2025-01-05 11:12:07 +0100

    plugins.tiktok: rewrite plugin, fix live, add VODs

--- tests/plugins/test_tiktok.py
+++ tests/plugins/test_tiktok.py
@@ -6,8 +6,9 @@ class TestPluginCanHandleUrlTikTok(PluginCanHandleUrl):
     __plugin__ = TikTok
 
     should_match_groups = [
-        ("https://www.tiktok.com/@CHANNEL", {"channel": "CHANNEL"}),
-        ("https://www.tiktok.com/@CHANNEL/live", {"channel": "CHANNEL"}),
+        (("live", "https://www.tiktok.com/@LIVE"), {"channel": "LIVE"}),
+        (("live", "https://www.tiktok.com/@LIVE/live"), {"channel": "LIVE"}),
+        (("video", "https://www.tiktok.com/@VIDEO/video/0123456789"), {"channel": "VIDEO", "id": "0123456789"}),
     ]
 
     should_not_match = [
