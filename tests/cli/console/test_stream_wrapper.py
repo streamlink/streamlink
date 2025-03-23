038508838ae4c9ef0fe4ee3938f78d2e9d19cf2e
bastimeyer <mail@bastimeyer.de>
2025-03-23 22:58:22 +0100

    cli.console: add ConsoleOutputStream
    - Add `StreamWrapper` base class which passes through everything
      to the given `TextIOWrapper` object. Don't subclass from
      `TextIOWrapper` or `TextIO`.
    - Wrap and replace `sys.stdout` / `sys.stderr`
    - Restore on `ConsoleOutput.close()`
    - Fix typing in `ConsoleOutput`

---
+++ tests/cli/console/test_stream_wrapper.py
@@ -0,0 +1,53 @@
+import sys
+from io import BytesIO, TextIOWrapper
+
+import pytest
+
+from streamlink_cli.console.stream import ConsoleOutputStream
+
+
+def test_wrap_error():
+    with pytest.raises(AttributeError):
+        ConsoleOutputStream.wrap(sys, "version")
+
+
+def test_wrap(capsys: pytest.CaptureFixture[str]):
+    orig_stdout = sys.stdout
+    orig_stderr = sys.stderr
+
+    streamwrapper = ConsoleOutputStream.wrap(sys, "stdout")
+    assert streamwrapper is not orig_stdout
+    assert sys.stdout is streamwrapper
+    assert sys.stderr is orig_stderr
+    assert sys.__stdout__ is not streamwrapper
+    assert sys.__stderr__ is not streamwrapper
+
+    streamwrapper.write("foo")
+    streamwrapper.write("bar")
+    streamwrapper.writelines(["abc", "def"])
+    print("123", end="456")  # noqa: T201
+    streamwrapper.flush()
+
+    out, err = capsys.readouterr()
+    assert out == "foobarabcdef123456"
+    assert err == ""
+
+    streamwrapper.restore()
+    assert sys.stdout is orig_stdout
+    assert sys.stderr is orig_stderr
+    assert sys.__stdout__ is not streamwrapper
+    assert sys.__stderr__ is not streamwrapper
+
+
+def test_destruct():
+    stream = TextIOWrapper(BytesIO(), encoding="utf-8")
+    wrapper = ConsoleOutputStream(stream)
+    assert wrapper.buffer is stream.buffer
+    assert not wrapper.closed
+    assert not wrapper.buffer.closed
+    assert not stream.closed
+    assert not stream.buffer.closed
+
+    del wrapper
+    assert not stream.closed
+    assert not stream.buffer.closed
