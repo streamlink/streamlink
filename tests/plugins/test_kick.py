c6528dae14b62fbadbc4519f8ceb8902df7354dd
bastimeyer <mail@bastimeyer.de>
2025-03-31 19:49:23 +0200

    plugins.kick: refactor, fix clips + 403 API issues
    - Fix clips matcher
    - Refactor API requests
    - Add missing API request headers and parse the User-Agent string
    
    Some API requests might fail randomly and solving a JS challenge
    is required, similar to Twitch's client-integrity token.
    The JS challenge requirement however seems to be rare and subsequent
    API request attempts are almost always successful.

--- tests/plugins/test_kick.py
+++ tests/plugins/test_kick.py
@@ -6,8 +6,28 @@ class TestPluginCanHandleUrlKick(PluginCanHandleUrl):
     __plugin__ = Kick
 
     should_match_groups = [
-        (("live", "https://kick.com/LIVE_CHANNEL"), {"channel": "LIVE_CHANNEL"}),
-        (("vod", "https://kick.com/video/VIDEO_ID"), {"vod": "VIDEO_ID"}),
-        (("vod", "https://kick.com/VIDEO_CHANNEL/videos/VIDEO_ID"), {"vod": "VIDEO_ID"}),
-        (("clip", "https://kick.com/CLIP_CHANNEL?clip=CLIP_ID"), {"channel": "CLIP_CHANNEL", "clip": "CLIP_ID"}),
+        (
+            ("live", "https://kick.com/LIVE_CHANNEL"),
+            {"channel": "LIVE_CHANNEL"},
+        ),
+        (
+            ("vod", "https://kick.com/video/VIDEO_ID"),
+            {"vod": "VIDEO_ID"},
+        ),
+        (
+            ("vod", "https://kick.com/VIDEO_CHANNEL/videos/VIDEO_ID"),
+            {"vod": "VIDEO_ID"},
+        ),
+        (
+            ("clip", "https://kick.com/CLIP_CHANNEL?clip=CLIP_ID&foo=bar"),
+            {"channel": "CLIP_CHANNEL", "clip": "CLIP_ID"},
+        ),
+        (
+            ("clip", "https://kick.com/CLIP_CHANNEL/clips/CLIP_ID?foo=bar"),
+            {"channel": "CLIP_CHANNEL", "clip": "CLIP_ID"},
+        ),
+        (
+            ("clip", "https://kick.com/CLIP_CHANNEL/clips/CLIP_ID?clip=CLIP_ID_FROM_QUERY_STRING&foo=bar"),
+            {"channel": "CLIP_CHANNEL", "clip": "CLIP_ID"},
+        ),
     ]
