2a914558138f3d78b11b747c8c6bdfda8be9d9ce
bastimeyer <mail@bastimeyer.de>
2025-06-10 14:42:30 +0200

    stream.hls: make MuxedHLSStream generic

--- src/streamlink/stream/hls/hls.py
+++ src/streamlink/stream/hls/hls.py
@@ -6,7 +6,7 @@ import struct
 from collections.abc import Mapping
 from concurrent.futures import Future
 from datetime import datetime, timedelta
-from typing import Any, ClassVar
+from typing import Any, ClassVar, TypeVar
 from urllib.parse import urlparse
 
 from requests import Response
@@ -548,7 +548,10 @@ class HLSStreamReader(FilteredStream, SegmentedStreamReader[HLSSegment, Response
         super().__init__(stream)
 
 
-class MuxedHLSStream(MuxedStream["HLSStream"]):
+TMuxedHLSStream_co = TypeVar("TMuxedHLSStream_co", bound="HLSStream", covariant=True)
+
+
+class MuxedHLSStream(MuxedStream[TMuxedHLSStream_co]):
     """
     Muxes multiple HLS video and audio streams into one output stream.
     """
@@ -560,7 +563,7 @@ class MuxedHLSStream(MuxedStream["HLSStream"]):
         session: Streamlink,
         video: str,
         audio: str | list[str],
-        hlsstream: type[HLSStream] | None = None,
+        hlsstream: type[TMuxedHLSStream_co] | None = None,
         url_master: str | None = None,
         multivariant: M3U8 | None = None,
         force_restart: bool = False,
@@ -588,8 +591,9 @@ class MuxedHLSStream(MuxedStream["HLSStream"]):
                 tracks.append(audio)
         maps.extend(f"{i}:a" for i in range(1, len(tracks)))
 
-        hlsstream = hlsstream or HLSStream
-        substreams = [hlsstream(session, url, force_restart=force_restart, **kwargs) for url in tracks]
+        # https://github.com/python/mypy/issues/18017
+        TStream: type[TMuxedHLSStream_co] = hlsstream if hlsstream is not None else HLSStream  # type: ignore[assignment]
+        substreams = [TStream(session, url, force_restart=force_restart, **kwargs) for url in tracks]
         ffmpeg_options = ffmpeg_options or {}
 
         super().__init__(session, *substreams, format="mpegts", maps=maps, **ffmpeg_options)
