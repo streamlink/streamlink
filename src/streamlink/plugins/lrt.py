449f8c06657bd86158a6807255cffb000f83a838
bastimeyer <mail@bastimeyer.de>
2025-07-13 17:39:43 +0200

    plugins.lrt: rewrite and fix stream URL schema

--- src/streamlink/plugins/lrt.py
+++ src/streamlink/plugins/lrt.py
@@ -2,9 +2,12 @@
 $description Live TV channels from LRT, a Lithuanian public, state-owned broadcaster.
 $url lrt.lt
 $type live
+$metadata id
+$metadata title
 """
 
 import re
+from urllib.parse import urlparse
 
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
@@ -16,18 +19,46 @@ from streamlink.stream.hls import HLSStream
 )
 class LRT(Plugin):
     def _get_streams(self):
-        token_url = self.session.http.get(
+        path = urlparse(self.url).path
+        channels = self.session.http.get(
             self.url,
             schema=validate.Schema(
-                re.compile(r"""var\s+tokenURL\s*=\s*(?P<q>["'])(?P<url>https://\S+)(?P=q)"""),
-                validate.none_or_all(validate.get("url")),
+                validate.parse_html(),
+                validate.xml_xpath_string(".//script[contains(text(),'/get_live_url.php?channel=')][1]/text()"),
+                re.compile(r"""\s*self\.__next_f\.push\(\[\d+,\s*(?P<payload>"\d+:\[.+")]\)"""),
+                validate.none_or_all(
+                    validate.get("payload"),
+                    validate.parse_json(),
+                    str,
+                    validate.transform(lambda data: data.split(":", maxsplit=1)[-1]),
+                    validate.parse_json(),
+                    list,
+                    validate.get(3),
+                    {
+                        "channels": [
+                            {
+                                "href": str,
+                                "channel": str,
+                                "title": str,
+                                "stream_url": validate.url(path=validate.endswith("/get_live_url.php")),
+                            },
+                        ],
+                    },
+                    validate.get("channels"),
+                ),
             ),
         )
-        if not token_url:
+        for channel in channels:
+            if channel["href"] == path:
+                break
+        else:
             return
 
+        self.id = channel["channel"]
+        self.title = channel["title"]
+
         hls_url = self.session.http.get(
-            token_url,
+            channel["stream_url"],
             schema=validate.Schema(
                 validate.parse_json(),
                 {
