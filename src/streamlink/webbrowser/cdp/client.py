86ae40c7d28d58d31e2f0cf3ac69c170b0b188d2
bastimeyer <mail@bastimeyer.de>
2025-09-03 20:21:07 +0200

    webbrowser.cdp.client: fix page.navigate ret type

--- src/streamlink/webbrowser/cdp/client.py
+++ src/streamlink/webbrowser/cdp/client.py
@@ -291,7 +291,9 @@ class CDPClientSession:
             await self.cdp_session.send(page.enable())
 
             try:
-                frame_id, _loader_id, error = await self.cdp_session.send(page.navigate(url=url, referrer=referrer))
+                frame_id, _loader_id, error, _is_download = await self.cdp_session.send(
+                    page.navigate(url=url, referrer=referrer),
+                )
                 if error:
                     raise CDPError(f"Navigation error: {error}")
 
