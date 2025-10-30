4274168c458ffe0274c02ce747f6258448bf851e
bastimeyer <mail@bastimeyer.de>
2025-10-30 20:00:15 +0100

    plugins.twitch: set ad segment duration to zero

--- tests/plugins/test_twitch.py
+++ tests/plugins/test_twitch.py
@@ -285,6 +285,7 @@ class TestTwitchHLSStream(TestMixinStreamHLS, unittest.TestCase):
         data = self.await_read(read_all=True)
         assert data == self.content(segments, cond=lambda s: s.num >= 4), "Filters out preroll ad segments"
         assert all(self.called(s) for s in segments.values()), "Downloads all segments"
+        assert self.thread.reader.worker.duration == 2.0, "Ad segments don't affect the output duration"
         assert mock_log.info.mock_calls == [
             call("Will skip ad segments"),
             call("Waiting for pre-roll ads to finish, be patient"),
@@ -311,6 +312,7 @@ class TestTwitchHLSStream(TestMixinStreamHLS, unittest.TestCase):
         data = self.await_read(read_all=True)
         assert data == self.content(segments, cond=lambda s: s.num != 2 and s.num != 3), "Filters out mid-stream ad segments"
         assert all(self.called(s) for s in segments.values()), "Downloads all segments"
+        assert self.thread.reader.worker.duration == 4.0, "Ad segments don't affect the output duration"
         assert mock_log.info.mock_calls == [
             call("Will skip ad segments"),
             call("Detected advertisement break of 2 seconds"),
@@ -360,6 +362,7 @@ class TestTwitchHLSStream(TestMixinStreamHLS, unittest.TestCase):
         data = self.await_read(read_all=True)
         assert data == self.content(segments, cond=lambda s: s.num not in (0, 1, 4, 5, 6, 7, 8)), "Filters out all ad segments"
         assert all(self.called(s) for s in segments.values()), "Downloads all segments"
+        assert self.thread.reader.worker.duration == 3.0, "Ad segments don't affect the output duration"
         assert mock_log.info.mock_calls == [
             call("Will skip ad segments"),
             call("Waiting for pre-roll ads to finish, be patient"),
