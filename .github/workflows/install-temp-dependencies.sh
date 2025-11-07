ce25d39a39b66297e92167098becda8e298a8595
bastimeyer <mail@bastimeyer.de>
2025-11-07 19:10:42 +0100

    ci.github: use upstream brotli wheel on cp314

--- .github/workflows/install-temp-dependencies.sh
+++ .github/workflows/install-temp-dependencies.sh
@@ -8,9 +8,7 @@ PY=$(python -c 'import platform,sysconfig;v="".join(platform.python_version_tupl
 
 
 BASE=https://github.com/streamlink/temp-dependency-builds-DO-NOT-USE/releases/download
-DEPS=(
-    'brotli-20251007-1/brotli-1.2.0'
-)
+DEPS=()
 
 if [[ "${PLATFORM}" == linux_x86_64 ]]; then
     DEPS+=()
@@ -20,6 +18,7 @@ fi
 
 if [[ "${PY}" == *t ]]; then
     DEPS+=(
+        'brotli-20251007-1/brotli-1.2.0'
         'pycryptodome-20251017-1/pycryptodome-3.23.0'
     )
 fi
@@ -29,4 +28,6 @@ for dep in "${DEPS[@]}"; do
     deps+=("${BASE}/${dep}-${PY}-${PLATFORM}.whl")
 done
 
-python -m pip install -U "${deps[@]}"
+if [[ ${#deps[@]} != 0 ]]; then
+    python -m pip install -U "${deps[@]}"
+fi
