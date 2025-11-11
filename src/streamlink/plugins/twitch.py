a020b7147c369745f6fd8edbd1a560264f3175c3
bastimeyer <mail@bastimeyer.de>
2025-11-11 13:19:48 +0100

    plugins.twitch: add --twitch-supported-codecs
    Add the temporary `--twitch-supported-codecs` plugin argument
    for being able to set the client's video codec preference.
    
    Set the default value to "h264" to ensure that no compatibility
    issues arise on clients without AV1/HEVC decode capabilities.
    
    Update usher API endpoint request parameters.
    
    Add "Higher quality streams" section to the Twitch plugin docs page.

--- src/streamlink/plugins/twitch.py
+++ src/streamlink/plugins/twitch.py
@@ -9,6 +9,7 @@ $metadata category
 $metadata title
 $notes See the :ref:`Authentication <cli/plugins/twitch:Authentication>` docs on how to prevent ads.
 $notes Read more about :ref:`embedded ads <cli/plugins/twitch:Embedded ads>` here.
+$notes :ref:`Higher quality streams <cli/plugins/twitch:Higher quality streams>` are supported.
 $notes :ref:`Low latency streaming <cli/plugins/twitch:Low latency streaming>` is supported.
 $notes Acquires a :ref:`client-integrity token <cli/plugins/twitch:Client-integrity token>` on streaming access token failure.
 """
@@ -267,18 +268,21 @@ class TwitchHLSStream(HLSStream):
 
 
 class UsherService:
-    def __init__(self, session):
+    SUPPORTED_CODECS_DEFAULT = ["h264"]
+
+    def __init__(self, session: Streamlink, supported_codecs: list[str] | None = None):
         self.session = session
+        self.supported_codecs = supported_codecs or self.SUPPORTED_CODECS_DEFAULT
 
     def _create_url(self, endpoint, **extra_params):
         url = f"https://usher.ttvnw.net{endpoint}"
         params = {
-            "player": "twitchweb",
+            "platform": "web",
             "p": int(random() * 999999),
-            "type": "any",
             "allow_source": "true",
             "allow_audio_only": "true",
-            "allow_spectre": "false",
+            "playlist_include_framerate": "true",
+            "supported_codecs": ",".join(self.supported_codecs),
         }
         params.update(extra_params)
 
@@ -749,6 +753,27 @@ class TwitchClientIntegrity:
     """,
 )
 @pluginargument(
+    "supported-codecs",
+    metavar="CODECS",
+    type="comma_list_filter",
+    type_kwargs={
+        "acceptable": ["h264", "h265", "av1"],
+        "unique": True,
+    },
+    default=["h264"],
+    help="""
+        A comma-separated list of codec names which signals Twitch the client's stream codec preference.
+        Which streams and which codecs are available depends on the specific channel and broadcast.
+
+        Default is "h264".
+
+        Supported codecs are h264, h265 and av1. Set to "h264,h265,av1" to enable all codecs.
+
+        Higher quality streams may only be available by enabling h265 or av1.
+        Lower quality streams which are re-encoded on Twitch's end may still be h264, even if not requested.
+    """,
+)
+@pluginargument(
     "api-header",
     metavar="KEY=VALUE",
     type="keyvalue",
@@ -814,7 +839,10 @@ class Twitch(Plugin):
             api_header=self.get_option("api-header"),
             access_token_param=self.get_option("access-token-param"),
         )
-        self.usher = UsherService(session=self.session)
+        self.usher = UsherService(
+            session=self.session,
+            supported_codecs=self.get_option("supported-codecs"),
+        )
 
         self._checked_metadata = False
 
