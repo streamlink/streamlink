5b93a213c21f9706890cd9a9c600f83b8a6bec74
bastimeyer <mail@bastimeyer.de>
2026-02-11 18:57:10 +0100

    stream: deprecate package exports

--- src/streamlink/stream/__init__.py
+++ src/streamlink/stream/__init__.py
@@ -1,6 +1,20 @@
-from streamlink.stream.dash import DASHStream
-from streamlink.stream.ffmpegmux import MuxedStream
-from streamlink.stream.hls import HLSStream, MuxedHLSStream
-from streamlink.stream.http import HTTPStream
-from streamlink.stream.stream import Stream, StreamIO
-from streamlink.stream.wrappers import StreamIOIterWrapper, StreamIOThreadWrapper, StreamIOWrapper
+# ruff: noqa: RUF067
+from streamlink.compat import deprecated
+
+
+_msg = "Importing from the 'streamlink.stream' package has been deprecated. Import from its submodules instead."
+
+deprecated({
+    "DASHStream": ("streamlink.stream.dash.dash", None, _msg),
+    "MuxedStream": ("streamlink.stream.ffmpegmux", None, _msg),
+    "HLSStream": ("streamlink.stream.hls.hls", None, _msg),
+    "MuxedHLSStream": ("streamlink.stream.hls.hls", None, _msg),
+    "HTTPStream": ("streamlink.stream.http", None, _msg),
+    "Stream": ("streamlink.stream.stream", None, _msg),
+    "StreamIO": ("streamlink.stream.stream", None, _msg),
+    "StreamIOIterWrapper": ("streamlink.stream.wrappers", None, _msg),
+    "StreamIOThreadWrapper": ("streamlink.stream.wrappers", None, _msg),
+    "StreamIOWrapper": ("streamlink.stream.wrappers", None, _msg),
+})
+
+del _msg
