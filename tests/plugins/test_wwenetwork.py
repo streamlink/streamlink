839717a09f2afbfa6e10431898d5e1fe0086adb8
bastimeyer <mail@bastimeyer.de>
2026-02-13 22:56:38 +0100

    plugins.wwenetwork: fix typing and update matcher

--- tests/plugins/test_wwenetwork.py
+++ tests/plugins/test_wwenetwork.py
@@ -6,6 +6,6 @@ class TestPluginCanHandleUrlWWENetwork(PluginCanHandleUrl):
     __plugin__ = WWENetwork
 
     should_match_groups = [
-        ("https://network.wwe.com/video/3622", {"stream_id": "3622"}),
-        ("https://network.wwe.com/live/3622", {"stream_id": "3622"}),
+        ("https://network.wwe.com/video/3622", {"content_type": "video", "stream_id": "3622"}),
+        ("https://network.wwe.com/live/3622", {"content_type": "live", "stream_id": "3622"}),
     ]
