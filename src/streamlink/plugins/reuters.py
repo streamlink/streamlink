0da0be573f1c360908e380f0a60828e060200a9d
bastimeyer <mail@bastimeyer.de>
2026-02-28 14:19:58 +0100

    chore: fix UP034

--- src/streamlink/plugins/reuters.py
+++ src/streamlink/plugins/reuters.py
@@ -79,7 +79,7 @@ class Reuters(Plugin):
                 validate.parse_json(),
                 {"videohub-by-guid-v1": {str: {"data": {"result": {"videos": list}}}}},
                 validate.get("videohub-by-guid-v1"),
-                validate.transform(lambda obj: obj[next(iter((obj.keys())))]),
+                validate.transform(lambda obj: obj[next(iter(obj.keys()))]),
                 validate.get(("data", "result", "videos", 0)),
                 schema_video,
             )
