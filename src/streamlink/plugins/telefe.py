728518aaccd33724d0b6200fcfc68f92a4fd115e
bastimeyer <mail@bastimeyer.de>
2026-04-07 14:53:22 +0200

    plugins.telefe: rewrite plugin

--- src/streamlink/plugins/telefe.py
+++ src/streamlink/plugins/telefe.py
@@ -7,7 +7,7 @@ $region Argentina
 """
 
 import re
-from urllib.parse import urljoin
+from urllib.parse import urlparse
 
 from streamlink.logger import getLogger
 from streamlink.plugin import Plugin, pluginmatcher
@@ -18,47 +18,70 @@ from streamlink.stream.hls import HLSStream
 log = getLogger(__name__)
 
 
-@pluginmatcher(re.compile(r"https://mitelefe\.com/vivo"))
+@pluginmatcher(re.compile(r"https://mitelefe\.com/(?:telefe-en-)?vivo"))
 class Telefe(Plugin):
+    _URL_TOKENIZE = "https://mitelefe.com/vidya/tokenize"
+
     def _get_streams(self):
-        self.title, hls_url = self.session.http.get(
+        self.title, stream_url = self.session.http.get(
             self.url,
             schema=validate.Schema(
                 validate.parse_html(),
-                validate.xml_xpath_string(".//script[contains(text(), 'HLS')]/text()"),
-                validate.none_or_all(
-                    re.compile(r"=\s*(\{.+?});", re.DOTALL | re.MULTILINE),
-                    validate.none_or_all(
-                        validate.get(1),
-                        validate.parse_json(),
+                validate.union((
+                    validate.xml_xpath_string(".//meta[@property='og:title'][@content][1]/@content"),
+                    validate.all(
+                        validate.xml_xpath_string(".//*[@data-player-url][1]/@data-player-url"),
+                        validate.none_or_all(
+                            validate.url(),
+                        ),
+                    ),
+                )),
+            ),
+        )
+        if not stream_url:
+            log.error("Could not find stream URL")
+            return None
+
+        parsed = urlparse(stream_url)
+        netloc = parsed.netloc.removeprefix("www.")
+        if netloc in ("youtube.com", "youtu.be", "dailymotion.com"):
+            return self.session.streams(stream_url)
+
+        headers = {
+            "Origin": "https://mitelefe.com",
+            "Referer": self.url,
+        }
+
+        hls_url = self.session.http.post(
+            self._URL_TOKENIZE,
+            headers={
+                "Content-Type": "application/json",
+                **headers,
+            },
+            json={
+                "url": stream_url,
+            },
+            schema=validate.Schema(
+                validate.parse_json(),
+                validate.any(
+                    validate.all(
                         {
-                            str: {
-                                "children": {
-                                    "top": {
-                                        "model": {
-                                            "videos": [
-                                                {
-                                                    "title": str,
-                                                    "sources": validate.all(
-                                                        [{"url": str, "type": str}],
-                                                        validate.filter(lambda p: p["type"].lower() == "hls"),
-                                                        validate.get((0, "url")),
-                                                    ),
-                                                },
-                                            ],
-                                        },
-                                    },
-                                },
-                            },
+                            "url": validate.url(path=validate.endswith(".m3u8")),
                         },
-                        validate.transform(lambda k: next(iter(k.values()))),
-                        validate.get(("children", "top", "model", "videos", 0)),
-                        validate.union_get("title", "sources"),
+                        validate.get("url"),
+                    ),
+                    validate.all(
+                        str,
+                        validate.url(path=validate.endswith(".m3u8")),
                     ),
                 ),
             ),
         )
-        return HLSStream.parse_variant_playlist(self.session, urljoin(self.url, hls_url))
+        if self.session.http.head(hls_url, raise_for_status=False).status_code >= 400:
+            log.error("Access restricted")
+            return None
+
+        return HLSStream.parse_variant_playlist(self.session, hls_url, headers=headers)
 
 
 __plugin__ = Telefe
