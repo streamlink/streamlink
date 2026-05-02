c94b509f400e7135b31d85dc2ae65d2a79ba7a7c
bastimeyer <mail@bastimeyer.de>
2026-05-02 15:58:42 +0200

    plugins.goltelevision: rewrite and fix plugin

--- src/streamlink/plugins/goltelevision.py
+++ src/streamlink/plugins/goltelevision.py
@@ -6,30 +6,63 @@ $region Spain
 """
 
 import re
+from urllib.parse import urlparse
 
+from streamlink.logger import getLogger
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import validate
-from streamlink.stream.hls import HLSStream
+
+
+log = getLogger(__name__)
 
 
 @pluginmatcher(
     re.compile(r"https?://(?:www\.)?goltelevision\.com/en-directo"),
 )
 class GOLTelevision(Plugin):
-    def _get_streams(self):
-        self.session.http.headers.update({
-            "Origin": "https://goltelevision.com",
-            "Referer": "https://goltelevision.com/",
-        })
-        url = self.session.http.get(
-            "https://play.goltelevision.com/api/stream/live",
+    _provider_dailymotion = "https://www.dailymotion.com/embed/video/{video}"
+
+    def _get_stream_data(self):
+        return self.session.http.get(
+            self.url,
             schema=validate.Schema(
-                validate.parse_json(),
-                {"manifest": validate.url()},
-                validate.get("manifest"),
+                validate.parse_html(),
+                validate.xml_xpath_string(".//script[@id='__NEXT_DATA__'][@type='application/json'][1]/text()"),
+                validate.none_or_all(
+                    validate.parse_json(),
+                    {
+                        "props": {
+                            "pageProps": {
+                                "channel": {
+                                    "stream": {
+                                        "dataVideo": str,
+                                        "key": str,
+                                        "metaContent": validate.url(),
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    validate.get(("props", "pageProps", "channel", "stream")),
+                    validate.union_get("dataVideo", "key", "metaContent"),
+                ),
             ),
         )
-        return HLSStream.parse_variant_playlist(self.session, url)
+
+    def _get_streams(self):
+        if not (data := self._get_stream_data()):
+            log.error("No stream data found")
+            return
+
+        data_video, key, meta_content = data
+
+        match urlparse(meta_content).netloc.split("."), data_video, key:
+            case host, *_ if host[-2:] == ["dailymotion", "com"] or host == ["dai", "ly"]:
+                log.info("Found embedded dailymotion stream")
+                return self.session.streams(self._provider_dailymotion.format(video=data_video))
+            case _:
+                log.error("No matching stream provider found for the embedded stream")
+                return
 
 
 __plugin__ = GOLTelevision
