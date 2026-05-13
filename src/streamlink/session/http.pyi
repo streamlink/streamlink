c88dc83c0feaa009db05c86fb935b144d3d91ea7
bastimeyer <mail@bastimeyer.de>
2026-05-13 19:00:50 +0200

    session.http: add schema method overloads
    Use the right return type for `request()` and various HTTP-verb methods
    based on the `schema` input parameter: instead of returning `Any`
    in all cases, return `requests.Response` if no `schema` was set.

--- src/streamlink/session/http.pyi
+++ src/streamlink/session/http.pyi
@@ -1,13 +1,14 @@
 import socket
 import ssl
-from collections.abc import Sequence
+from collections.abc import Mapping, Sequence
 from pathlib import Path
-from typing import Any, TypedDict
+from typing import Any, TypedDict, overload
 
 # noinspection PyProtectedMember
 import requests._types as _requeststypes  # noqa: PLC2701
 from requests import PreparedRequest, Response, Session
 from requests.adapters import HTTPAdapter
+from requests.cookies import CookieJar, RequestsCookieJar
 from typing_extensions import Unpack
 
 from streamlink.plugin.api.validate import Schema
@@ -20,19 +21,33 @@ class _StreamlinkKwargs(TypedDict, total=False):
     retries: float
     retry_backoff: float
     retry_max_backoff: float
-    schema: Schema
     session: HTTPSession
 
+class _SchemaKwargs(TypedDict):
+    schema: Schema
+
 class _RequestKwargs(_requeststypes.RequestKwargs, _StreamlinkKwargs):
+    pass
+
+class _RequestKwargsWithSchema(_requeststypes.RequestKwargs, _StreamlinkKwargs, _SchemaKwargs):
     pass
 
 class _GetKwargs(_requeststypes.GetKwargs, _StreamlinkKwargs):
     pass
 
+class _GetKwargsWithSchema(_requeststypes.GetKwargs, _StreamlinkKwargs, _SchemaKwargs):
+    pass
+
 class _PostKwargs(_requeststypes.PostKwargs, _StreamlinkKwargs):
     pass
 
+class _PostKwargsWithSchema(_requeststypes.PostKwargs, _StreamlinkKwargs, _SchemaKwargs):
+    pass
+
 class _DataKwargs(_requeststypes.DataKwargs, _StreamlinkKwargs):
+    pass
+
+class _DataKwargsWithSchema(_requeststypes.DataKwargs, _StreamlinkKwargs, _SchemaKwargs):
     pass
 
 # ----
@@ -81,50 +96,155 @@ class HTTPSession(Session):
     @staticmethod
     def valid_request_args(**req_keywords) -> dict[str, Any]: ...
     def prepare_new_request(self, **req_keywords) -> PreparedRequest: ...
+    @overload
     def request(
         self,
         method: str,
         url: _requeststypes.UriType,
-        *args,
-        **kwargs: Unpack[_RequestKwargs],
+        params: _requeststypes.ParamsType = None,
+        data: _requeststypes.DataType = None,
+        headers: Mapping[str, str | bytes] | None = None,
+        cookies: RequestsCookieJar | CookieJar | dict[str, str] | None = None,
+        files: _requeststypes.FilesType = None,
+        auth: _requeststypes.AuthType = None,
+        timeout: _requeststypes.TimeoutType = None,
+        allow_redirects: bool = True,
+        proxies: dict[str, str] | None = None,
+        hooks: _requeststypes.HooksInputType | None = None,
+        stream: bool | None = None,
+        verify: _requeststypes.VerifyType | None = None,
+        cert: _requeststypes.CertType = None,
+        json: _requeststypes.JsonType = None,
+        # streamlink options
+        acceptable_status: Sequence[int] | None = None,
+        encoding: str | None = None,
+        exception: type[Exception] | None = None,
+        raise_for_status: bool = True,
+        retries: int = 0,
+        retry_backoff: float = 0.3,
+        retry_max_backoff: float = 10.0,
+        schema: Schema | None = None,
+        session: HTTPSession | None = None,
     ) -> Any: ...
+    @overload
+    def request(
+        self,
+        url: _requeststypes.UriType,
+        params: _requeststypes.ParamsType = None,
+        data: _requeststypes.DataType = None,
+        headers: Mapping[str, str | bytes] | None = None,
+        cookies: RequestsCookieJar | CookieJar | dict[str, str] | None = None,
+        files: _requeststypes.FilesType = None,
+        auth: _requeststypes.AuthType = None,
+        timeout: _requeststypes.TimeoutType = None,
+        allow_redirects: bool = True,
+        proxies: dict[str, str] | None = None,
+        hooks: _requeststypes.HooksInputType | None = None,
+        stream: bool | None = None,
+        verify: _requeststypes.VerifyType | None = None,
+        cert: _requeststypes.CertType = None,
+        json: _requeststypes.JsonType = None,
+        # streamlink options
+        acceptable_status: Sequence[int] | None = None,
+        encoding: str | None = None,
+        exception: type[Exception] | None = None,
+        raise_for_status: bool = True,
+        retries: int = 0,
+        retry_backoff: float = 0.3,
+        retry_max_backoff: float = 10.0,
+        session: HTTPSession | None = None,
+    ) -> Response: ...
+    @overload
+    def get(
+        self,
+        url: _requeststypes.UriType,
+        params: _requeststypes.ParamsType | None = None,
+        **kwargs: Unpack[_GetKwargsWithSchema],
+    ) -> Any: ...
+    @overload
     def get(
         self,
         url: _requeststypes.UriType,
         params: _requeststypes.ParamsType | None = None,
         **kwargs: Unpack[_GetKwargs],
+    ) -> Response: ...
+    @overload
+    def options(
+        self,
+        url: _requeststypes.UriType,
+        **kwargs: Unpack[_RequestKwargsWithSchema],
     ) -> Any: ...
+    @overload
     def options(
         self,
         url: _requeststypes.UriType,
         **kwargs: Unpack[_RequestKwargs],
+    ) -> Response: ...
+    @overload
+    def head(
+        self,
+        url: _requeststypes.UriType,
+        **kwargs: Unpack[_RequestKwargsWithSchema],
     ) -> Any: ...
+    @overload
     def head(
         self,
         url: _requeststypes.UriType,
         **kwargs: Unpack[_RequestKwargs],
+    ) -> Response: ...
+    @overload
+    def post(
+        self,
+        url: _requeststypes.UriType,
+        data: _requeststypes.DataType = None,
+        json: _requeststypes.JsonType = None,
+        **kwargs: Unpack[_PostKwargsWithSchema],
     ) -> Any: ...
+    @overload
     def post(
         self,
         url: _requeststypes.UriType,
         data: _requeststypes.DataType = None,
         json: _requeststypes.JsonType = None,
         **kwargs: Unpack[_PostKwargs],
+    ) -> Response: ...
+    @overload
+    def put(
+        self,
+        url: _requeststypes.UriType,
+        data: _requeststypes.DataType = None,
+        **kwargs: Unpack[_DataKwargsWithSchema],
     ) -> Any: ...
+    @overload
     def put(
         self,
         url: _requeststypes.UriType,
         data: _requeststypes.DataType = None,
         **kwargs: Unpack[_DataKwargs],
+    ) -> Response: ...
+    @overload
+    def patch(
+        self,
+        url: _requeststypes.UriType,
+        data: _requeststypes.DataType = None,
+        **kwargs: Unpack[_DataKwargsWithSchema],
     ) -> Any: ...
+    @overload
     def patch(
         self,
         url: _requeststypes.UriType,
         data: _requeststypes.DataType = None,
         **kwargs: Unpack[_DataKwargs],
+    ) -> Response: ...
+    @overload
+    def delete(
+        self,
+        url: _requeststypes.UriType,
+        **kwargs: Unpack[_RequestKwargsWithSchema],
     ) -> Any: ...
+    @overload
     def delete(
         self,
         url: _requeststypes.UriType,
         **kwargs: Unpack[_RequestKwargs],
-    ) -> Any: ...
+    ) -> Response: ...
