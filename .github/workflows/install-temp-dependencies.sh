7118c73f48fe0b489990248feb9109e77c8ba883
bastimeyer <mail@bastimeyer.de>
2026-05-23 14:40:43 +0200

    ci.github: update temp-dependencies install script

--- .github/workflows/install-temp-dependencies.sh
+++ .github/workflows/install-temp-dependencies.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 PY=$(python -c 'import platform,sysconfig;v="".join(platform.python_version_tuple()[:2]);t="t" if sysconfig.get_config_var("Py_GIL_DISABLED") else "";print(f"cp{v}-cp{v}{t}")')
-[[ "${PY}" == cp314-cp314 || "${PY}" == cp314-cp314t ]] || exit 0
+[[ "${PY}" == cp314-cp314t || "${PY}" == cp315-cp315 ]] || exit 0
 
 if [[ "$(uname)" == Linux ]]; then
     PLATFORM=linux_x86_64
@@ -18,27 +18,29 @@ fi
 BASE=https://github.com/streamlink/temp-dependency-builds-DO-NOT-USE/releases/download
 DEPS=()
 
-# individual platform dependencies
-if [[ "${PLATFORM}" == linux_x86_64 ]]; then
-    DEPS+=()
-elif [[ "${PLATFORM}" == macosx_10_15_universal2 ]]; then
-    DEPS+=()
-elif [[ "${PLATFORM}" == win_amd64 ]]; then
-    DEPS+=()
+if [[ "${PY}" == cp314-cp314t ]]; then
+    DEPS+=(
+        "brotli-20260523-1/brotli-1.2.0-${PY}-${PLATFORM}.whl"
+        "pycryptodome-20260424-1/pycryptodome-3.24.0b0-${PY}-${PLATFORM}.whl"
+    )
+
+elif [[ "${PY}" == cp315-cp315 ]]; then
+    DEPS+=(
+        "brotli-20260523-1/brotli-1.2.0-${PY}-${PLATFORM}.whl"
+        "lxml-20260523-1/lxml-6.1.1-${PY}-${PLATFORM}.whl"
+    )
+    if [[ "${PLATFORM}" == win_amd64 ]]; then
+        DEPS+=(
+            "cffi-20260523-1/cffi-2.0.0-${PY}-${PLATFORM}.whl"
+        )
+    fi
 fi
 
-# no-GIL / free-threaded dependencies
-if [[ "${PY}" == *t ]]; then
-    DEPS+=(
-        'brotli-20260424-1/brotli-1.2.0'
-        'pycryptodome-20260424-1/pycryptodome-3.24.0b0'
-    )
-fi
 
 if [[ ${#DEPS[@]} != 0 ]]; then
     deps=()
     for dep in "${DEPS[@]}"; do
-        deps+=("${BASE}/${dep}-${PY}-${PLATFORM}.whl")
+        deps+=("${BASE}/${dep}")
     done
 
     python -m pip install -U "${deps[@]}"
