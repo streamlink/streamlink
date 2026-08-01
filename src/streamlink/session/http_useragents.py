72b58897df2837977dbde327fc10427a29d33317
streamlinkbot <streamlinkbot@users.noreply.github.com>
2026-08-01 00:26:46 +0000

    session.http_useragents: update useragents

--- src/streamlink/session/http_useragents.py
+++ src/streamlink/session/http_useragents.py
@@ -1,21 +1,21 @@
 # ruff: file-ignore[line-too-long]
 
-ANDROID = "Mozilla/5.0 (Linux; Android 17) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.7827.201 Mobile Safari/537.36"
-CHROME = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36"
-CHROME_OS = "Mozilla/5.0 (X11; CrOS x86_64 16667.55.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.7827.226 Safari/537.36"
-FIREFOX = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:152.0) Gecko/20100101 Firefox/152.0"
+ANDROID = "Mozilla/5.0 (Linux; Android 17) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.7871.187 Mobile Safari/537.36"
+CHROME = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36"
+CHROME_OS = "Mozilla/5.0 (X11; CrOS x86_64 16700.46.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.7871.150 Safari/537.36"
+FIREFOX = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:153.0) Gecko/20100101 Firefox/153.0"
 IE_11 = "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko"
 IPHONE = "Mozilla/5.0 (iPhone; CPU iPhone OS 18_7_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0 Mobile/15E148 Safari/604.1"
-OPERA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36 OPR/132.0.0.0"
-SAFARI = "Mozilla/5.0 (Macintosh; Intel Mac OS X 15_7_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0 Safari/605.1.15"
+OPERA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 OPR/133.0.0.0"
+SAFARI = "Mozilla/5.0 (Macintosh; Intel Mac OS X 15_7_8) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0 Safari/605.1.15"
 
 ANDROID_VERSION = (17,)
-CHROME_VERSION = (149, 0, 7827, 199)
-CHROME_OS_VERSION = (149, 0, 7827, 226)
-FIREFOX_VERSION = (152, 0, 4)
+CHROME_VERSION = (150, 0, 7871, 188)
+CHROME_OS_VERSION = (150, 0, 7871, 150)
+FIREFOX_VERSION = (153, 0, 1)
 IE_11_VERSION = (11,)
 IPHONE_VERSION = (18, 7, 8)
-OPERA_VERSION = (132, 0, 5905, 102)
+OPERA_VERSION = (133, 0, 5932, 85)
 SAFARI_VERSION = (26,)
 
 # Backwards compatibility
