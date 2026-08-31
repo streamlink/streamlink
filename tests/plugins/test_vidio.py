61305d8edbe3e0909c9a60b4af1bbfb0a4751751
bastimeyer <mail@bastimeyer.de>
2026-08-31 19:07:10 +0200

    plugins.vidio: rewrite plugin
    - Rewrite plugin based on new APIs
    - Remove VOD support
    - Add VidioAPI class
    - Work around stream expiration by re-fetching multivariant playlist
      from the API and copying media playlist parameters

--- tests/plugins/test_vidio.py
+++ tests/plugins/test_vidio.py
@@ -6,12 +6,11 @@ class TestPluginCanHandleUrlVidio(PluginCanHandleUrl):
     __plugin__ = Vidio
 
     should_match = [
-        "https://www.vidio.com/live/204-sctv-tv-stream",
-        "https://www.vidio.com/live/5075-dw-tv-stream",
+        "https://www.vidio.com/live/6412-euronews",
         "https://www.vidio.com/live/777-metro-tv",
-        "https://www.vidio.com/watch/766861-5-rekor-fantastis-zidane-bersama-real-madrid",
     ]
 
     should_not_match = [
         "https://www.vidio.com/",
+        "https://www.vidio.com/watch/12345-asdf",
     ]
