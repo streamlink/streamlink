dbac81b079f13e33c2c99e8b64aaf23d7fa82a13
fl-henry <fl.henry.jefferson@gmail.com>
2026-09-01 09:29:04 -0300

    plugins.vimeo: add DASH stream support and HLS URLs re-fetch
    - Implement VimeoHLSStream to re-fetch stream URLs when needed
    - Add support for DASH streams by converting Vimeo's custom JSON
    manifest to MPD format
    - Handle data: URI initialization segments in DASH streams
    - Refactor Vimeo plugin by moving API-related code into a new VimeoAPI
    class
    - Update test URLs and remove dead links

--- tests/plugins/test_vimeo.py
+++ tests/plugins/test_vimeo.py
@@ -6,13 +6,12 @@ class TestPluginCanHandleUrlVimeo(PluginCanHandleUrl):
     __plugin__ = Vimeo
 
     should_match_groups = [
-        (("default", "https://vimeo.com/783455878"), {}),
-        (("default", "https://vimeo.com/channels/music/176894130"), {}),
-        (("default", "https://vimeo.com/ondemand/worldoftomorrow3/467204924"), {}),
-        (("default", "https://vimeo.com/ondemand/100footsurfingdays"), {}),
-        (("player", "https://player.vimeo.com/video/176894130"), {}),
-        (("event", "https://vimeo.com/event/4154130"), {"event_id": "4154130"}),
-        (("event", "https://vimeo.com/event/4154130/embed"), {"event_id": "4154130"}),
+        (("default", "https://vimeo.com/783455878"), {"video_id": "783455878"}),
+        (("default", "https://vimeo.com/channels/music/176894130"), {"video_id": "176894130"}),
+        (("default", "https://vimeo.com/ondemand/worldoftomorrow3/467204924"), {"video_id": "467204924"}),
+        (("default", "https://player.vimeo.com/video/176894130"), {"video_id": "176894130"}),
+        (("event", "https://vimeo.com/event/6149301"), {"event_id": "6149301"}),
+        (("event", "https://vimeo.com/event/6149301/embed"), {"event_id": "6149301"}),
     ]
 
     should_not_match = [
