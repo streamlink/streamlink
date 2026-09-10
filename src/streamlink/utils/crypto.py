8bf51b9f4c8a2e0f48a360b9e398fac6a6d4f39b
bastimeyer <mail@bastimeyer.de>
2026-09-11 01:21:05 +0200

    utils.crypto: add encrypt_openssl()
    - Add `encrypt_openssl()`, similar to the legacy `decrypt_openssl()`
      function, with hardcoded AES-256-CBC and legacy `EVP_BytesToKey` KDF
    - Switch to pycryptodome's MD5 implementation for `evp_bytestokey`
    - Update typing
    - Rewrite tests

--- src/streamlink/utils/crypto.py
+++ src/streamlink/utils/crypto.py
@@ -1,5 +1,7 @@
 # ruff: file-ignore[unused-import]
-import hashlib
+from __future__ import annotations
+
+from os import urandom
 
 
 # re-export pycryptodome / pycryptodomex stuff in a single place
@@ -7,18 +9,18 @@ import hashlib
 try:
     # pycryptodome (drop-in replacement for the old PyCrypto library)
     from Crypto.Cipher import AES, PKCS1_v1_5
-    from Crypto.Hash import SHA256
+    from Crypto.Hash import MD5, SHA256
     from Crypto.PublicKey import RSA
     from Crypto.Util.Padding import pad, unpad
 except ImportError:  # pragma: no cover
     # pycryptodomex (independent of the old PyCrypto library)
     from Cryptodome.Cipher import AES, PKCS1_v1_5  # type: ignore
-    from Cryptodome.Hash import SHA256  # type: ignore
+    from Cryptodome.Hash import MD5, SHA256  # type: ignore
     from Cryptodome.PublicKey import RSA  # type: ignore
     from Cryptodome.Util.Padding import pad, unpad  # type: ignore
 
 
-def evp_bytestokey(password, salt, key_len, iv_len):
+def evp_bytestokey(password: bytes, salt: bytes, key_len: int, iv_len: int) -> tuple[bytes, bytes]:
     """
     Python implementation of OpenSSL's EVP_BytesToKey()
     :param password: or passphrase
@@ -29,18 +31,36 @@ def evp_bytestokey(password, salt, key_len, iv_len):
     """
     d = d_i = b""
     while len(d) < key_len + iv_len:
-        d_i = hashlib.md5(d_i + password + salt).digest()
+        d_i = MD5.new(d_i + password + salt).digest()
         d += d_i
     return d[:key_len], d[key_len : key_len + iv_len]
 
 
-def decrypt_openssl(data, passphrase, key_length=32):
-    if data.startswith(b"Salted__"):
-        salt = data[len(b"Salted__") : AES.block_size]
-        key, iv = evp_bytestokey(passphrase, salt, key_length, AES.block_size)
-        d = AES.new(key, AES.MODE_CBC, iv)
-        out = d.decrypt(data[AES.block_size :])
-        return unpad_pkcs5(out)
+def encrypt_openssl(data: bytes, passphrase: bytes, key_length: int = 32) -> bytes:
+    salt = urandom(8)
+    key, iv = evp_bytestokey(passphrase, salt, key_length, AES.block_size)
+    padded = pad_pkcs5(data, AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    encrypted = cipher.encrypt(padded)
+
+    return b"Salted__" + salt + encrypted
+
+
+def decrypt_openssl(data: bytes, passphrase: bytes, key_length: int = 32) -> bytes | None:
+    if not data.startswith(b"Salted__"):
+        return None
+
+    salt = data[len(b"Salted__") : AES.block_size]
+    key, iv = evp_bytestokey(passphrase, salt, key_length, AES.block_size)
+    d = AES.new(key, AES.MODE_CBC, iv)
+    out = d.decrypt(data[AES.block_size :])
+
+    return unpad_pkcs5(out)
+
+
+def pad_pkcs5(data: bytes, block_size: int = AES.block_size) -> bytes:
+    padding_len = block_size - (len(data) % block_size)
+    return data + bytes(padding_len * [padding_len])
 
 
 def unpad_pkcs5(padded):
