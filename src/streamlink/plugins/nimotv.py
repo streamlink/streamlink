3f0cdc570995f899cfabe713ca7db41f28b5106a
Mehavoid <63477090+Mehavoid@users.noreply.github.com>
2026-09-13 12:53:31 +0300

    plugins.nimotv: restore access for streams

--- src/streamlink/plugins/nimotv.py
+++ src/streamlink/plugins/nimotv.py
@@ -8,18 +8,23 @@ $metadata title
 """
 
 import re
+from base64 import b64decode
+from hashlib import md5
+from time import time
+from urllib.parse import unquote
 
 from streamlink.logger import getLogger
 from streamlink.plugin import Plugin, pluginmatcher
 from streamlink.plugin.api import useragents, validate
-from streamlink.stream.http import HTTPStream
+from streamlink.stream.hls import HLSStream
+from streamlink.utils.url import update_scheme
 
 
 log = getLogger(__name__)
 
 
 @pluginmatcher(
-    re.compile(r"https?://(?:www\.|m\.)?nimo\.tv/(?P<username>.*)"),
+    re.compile(r"https?://(?:www\.|m\.)?nimo\.tv/(?P<channel>(?:live/\d+)?[^/?#]+)"),
 )
 class NimoTV(Plugin):
     data_url = "https://m.nimo.tv/{0}"
@@ -28,7 +33,7 @@ class NimoTV(Plugin):
         250: "240p",
         500: "360p",
         1000: "480p",
-        2500: "720p",
+        2000: "720p",
         6000: "1080p",
     }
 
@@ -36,42 +41,64 @@ class NimoTV(Plugin):
     _re_domain = re.compile(rb"(https?:\/\/[A-Za-z]{2,3}.hls[A-Za-z\.\/]+)(?:V|&)")
     _re_id = re.compile(rb"id=([^|\\]+)")
     _re_tp = re.compile(rb"tp=(\d+)")
-    _re_wsSecret = re.compile(rb"wsSecret=(\w+)")
+    _re_fm = re.compile(rb"fm=([^&]+)")
+    _re_ctype = re.compile(rb"ctype=([^A-Z]+)")
     _re_wsTime = re.compile(rb"wsTime=(\w+)")
 
+    def _get_secret(self, fm: str, stream_name: str, ws_time: str) -> dict:
+        uid = 0
+        now = int(time() * 1000)
+        seqid = uid + now
+        prefix = b64decode(unquote(fm).encode()).decode().split("_")[0]
+        secret = md5(f"{prefix}_{uid}_{stream_name}_{seqid}_{ws_time}".encode()).hexdigest()
+        return {
+            "wsSecret": secret,
+            "seqid": seqid,
+            "u": uid,
+        }
+
     def _get_streams(self):
-        username = self.match.group("username")
-        if not username:
-            return
+        self.session.http.headers.update({
+            "User-Agent": useragents.ANDROID,
+            "Referer": "https://m.nimo.tv/",
+        })
 
         data = self.session.http.get(
-            self.data_url.format(username),
-            headers={
-                "User-Agent": useragents.ANDROID,
-            },
+            self.data_url.format(self.match["channel"]),
             schema=validate.Schema(
                 re.compile(r"<script>var G_roomBaseInfo = ({.*?});</script>"),
                 validate.none_or_all(
                     validate.get(1),
                     validate.parse_json(),
                     {
-                        "title": str,
                         "nickname": str,
                         "game": str,
+                        "title": str,
                         "liveStreamStatus": int,
                         validate.optional("mStreamPkg"): str,
                     },
+                    validate.union_get(
+                        "nickname",
+                        "game",
+                        "title",
+                        "liveStreamStatus",
+                        "mStreamPkg",
+                    ),
                 ),
             ),
         )
 
-        if data["liveStreamStatus"] == 0:
+        if not data:
+            return
+
+        self.author, self.category, self.title, online, mStreamPkg = data
+
+        if online == 0:
             log.info("This stream is currently offline")
             return
 
-        mStreamPkg = data.get("mStreamPkg")
         if not mStreamPkg:
-            log.debug("missing mStreamPkg")
+            log.error("missing mStreamPkg")
             return
 
         mStreamPkg = bytes.fromhex(mStreamPkg)
@@ -80,40 +107,34 @@ class NimoTV(Plugin):
             domain = self._re_domain.search(mStreamPkg).group(1).decode("utf-8")  # type: ignore[ty:unresolved-attribute]
             id_ = self._re_id.search(mStreamPkg).group(1).decode("utf-8")  # type: ignore[ty:unresolved-attribute]
             tp = self._re_tp.search(mStreamPkg).group(1).decode("utf-8")  # type: ignore[ty:unresolved-attribute]
-            ws_secret = self._re_wsSecret.search(mStreamPkg).group(1).decode("utf-8")  # type: ignore[ty:unresolved-attribute]
+            fm = self._re_fm.search(mStreamPkg).group(1).decode("utf-8")  # type: ignore[ty:unresolved-attribute]
+            ctype = self._re_ctype.search(mStreamPkg).group(1).decode("utf-8")  # type: ignore[ty:unresolved-attribute]
             ws_time = self._re_wsTime.search(mStreamPkg).group(1).decode("utf-8")  # type: ignore[ty:unresolved-attribute]
         except AttributeError:
             log.error("invalid mStreamPkg")
             return
 
+        secret = self._get_secret(fm, id_, ws_time)
+
         params = {
             "appid": appid,
+            "ctype": ctype,
             "id": id_,
             "tp": tp,
-            "wsSecret": ws_secret,
             "wsTime": ws_time,
-            "u": "0",
-            "t": "100",
-            "needwm": 1,
+            "t": "110",
+            "sv": 2411271811,
+            **secret,
         }
-        url = f"{domain}{id_}.flv"
-        url = url.replace("hls.nimo.tv", "flv.nimo.tv")
+
+        url = update_scheme("https://", f"{domain}{id_}.m3u8")
         log.debug(f"URL={url}")
         for k, v in self.video_qualities.items():
             params = params.copy()
             params["ratio"] = k
-            if v == "1080p":
-                params["needwm"] = 0
-            elif v in ("720p", "480p", "360p"):
-                params["sphd"] = 1
-
             log.trace("%s params=%r", v, params)
             # some qualities might not exist, but it will select a different lower quality
-            yield v, HTTPStream(self.session, url, params=params)
-
-        self.author = data["nickname"]
-        self.category = data["game"]
-        self.title = data["title"]
+            yield v, HLSStream(self.session, url, params=params)
 
 
 __plugin__ = NimoTV
