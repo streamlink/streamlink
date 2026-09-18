1bd90e9a2f0e39c71140a4611fdd0ebdc10a4177
bastimeyer <mail@bastimeyer.de>
2026-09-18 21:36:38 +0200

    tests.cli: move test_setup_session_options()
    CLI invocation tests should be in the tests/cli/main package

--- tests/cli/main/test_setup_session_options.py
+++ tests/cli/main/test_setup_session_options.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+from unittest.mock import Mock
 
 import pytest
 
@@ -10,18 +11,29 @@ import streamlink_cli.main
 if TYPE_CHECKING:
     from pathlib import Path
 
+    from streamlink.session import Streamlink
 
-@pytest.mark.parametrize(
-    "argv",
-    [
-        pytest.param(
-            ["--http-cookies-file", "doesnotexist"],
-            id="http-cookies-file",
-        ),
-    ],
-    indirect=["argv"],
-)
+
+def test_setup_session_options(monkeypatch: pytest.MonkeyPatch, session: Streamlink, argv: list[str]):
+    argv.extend(["--quiet"])
+
+    mock_setup_session_options = Mock()
+    monkeypatch.setattr("streamlink_cli.main.setup_session_options", mock_setup_session_options)
+
+    with pytest.raises(SystemExit) as exc_info:
+        streamlink_cli.main.main()
+    assert exc_info.value.code == 0
+
+    assert mock_setup_session_options.call_count == 1
+    expected_session, expected_namespace = mock_setup_session_options.call_args_list[0].args
+    assert expected_session is session
+    assert expected_namespace is streamlink_cli.main.args
+    assert expected_namespace.quiet
+
+
 def test_failure(monkeypatch: pytest.MonkeyPatch, tmp_path: Path, capsys: pytest.CaptureFixture[str], argv: list[str]):
+    argv.extend(["--http-cookies-file", "doesnotexist"])
+
     monkeypatch.chdir(tmp_path)
     with pytest.raises(SystemExit) as exc_info:
         streamlink_cli.main.main()
